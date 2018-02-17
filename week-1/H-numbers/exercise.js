a1f281ba5adc690e0ce0fc23cb65c54ea08561d2
Rukmini Saraswatibhatla <neelunsiri@gmail.com>
2018-02-17 15:38:09 +0000

    changes done

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,11 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var age=27;
+console.log(age);
+var add=20+2;
+console.log(add);
+var diff=20-8;
+console.log(diff);
+var mentors=22;
+var students=24;
+var total=22+24;
+console.log(total);
