366c432894febadbb4f492ddf8020f5c9790afd1
Mozafar <kabaros@gmail.com>
2018-02-19 23:12:46 +0000

    practice exercises: find errors in code

---
+++ week-1-practise/1-easy/d-function-output-1.js
@@ -0,0 +1,12 @@
+function getNumber() {
+    return Math.random() * 10;
+}
+
+var result = getNumber();
+console.log(result);
+
+// Answer these questions:
+// - What does this program do?
+// - How many parameters does the function take?
+// - What is the function name?
+// - Where is the function called? with what parameters?
