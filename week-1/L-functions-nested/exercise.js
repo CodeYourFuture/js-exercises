9a9a16fc996defbeca4a5fa4cb116102e6879ad8
Abdulkadir <ethiogentelman@gmail.com>
2018-02-24 06:46:18 -0500

    nameAge

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,16 +3,30 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
 function getAgeInDays(age) {
-    return age * 365;
+  return age * 365;
 }
 
 function createCreeting(name, age) {
-    var ageInDays = getAgeInDays(age);
-    var message =
-        "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
-    return message;
+  var ageInDays = getAgeInDays(age);
+  var message =
+    "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
+  return message;
 }
-var result = createCreeting("Awel", 28);
 
-console.log(result);
+console.log(createCreeting("Awel", 28));
+
+function percentage(numberOfStudents, total) {
+  return 100 * numberOfStudents / total;
+}
+var precisePercentageOfStudents = percentage(15, 23);
+var precisepercentageOfMentors = percentage(8, 23);
+var roughPercentageOfStudents = Math.round(precisePercentageOfStudents);
+var roughpercentageOfMentors = Math.round(precisepercentageOfMentors);
+
+function message(percentage) {
+  return "Percentage students: " + percentage;
+}
+console.log(message(roughPercentageOfStudents));
+console.log(message(roughpercentageOfMentors));
