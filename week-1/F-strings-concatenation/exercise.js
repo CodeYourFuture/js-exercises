984f59b04f59f432f1dbb4243a62b0e29acadbca
tchapda daniella <dany2kul@yahoo.com>
2018-02-24 16:32:44 +0000

    dany-javascript

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var greetingStart="hi,i'm called ";
+var name="daniella";
+var greeting=greetingStart + name;
+console.log(greeting);
