ca1cef6a724f77b0295b7ac95712e0f376bd0215
Soha <s.azadi12319@gmail.com>
2018-02-24 11:25:15 +0000

    week 1 exercise

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,6 +1,6 @@
 // Start by creating a variable `greeting`
 var greeting = "hello";
 
-console.log(greeting+ "world");
-console.log(greeting+ " Glasgow");
+console.log(greeting + " world");
+console.log(greeting + " Glasgow");
 console.log(22);
--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function add(a, b) {
+  return 13 + 124;
+  var sum = a + b;
+}
 // Call the function and assign to a variable `sum`
 
 console.log(sum);
