c6a07baf0af2f79829a926739a1cbb38dbf1f0bd
Abdulkadir <ethiogentelman@gmail.com>
2018-02-24 06:29:43 -0500

    New changes

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,16 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function getAgeInDays(age) {
+    return age * 365;
+}
+
+function createCreeting(name, age) {
+    var ageInDays = getAgeInDays(age);
+    var message =
+        "My Name is " + name + " and I was born over " + ageInDays + " days ago!";
+    return message;
+}
+var result = createCreeting("Awel", 28);
+
+console.log(result);
