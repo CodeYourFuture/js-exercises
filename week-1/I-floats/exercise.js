b272289ce39327c7cf25b0ef57ff6723c25edccb
Abdulkadir <ethiogentelman@gmail.com>
2018-02-24 07:05:13 -0500

    all changes

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,14 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+function percentage(numberOfStudents, total) {
+  return 100 * numberOfStudents / total;
+}
+var precisePercentageOfStudents = percentage(15, 23);
+var precisepercentageOfMentors = percentage(8, 23);
+
+var roughPercentageOfStudents = Math.round(precisePercentageOfStudents);
+var roughpercentageOfMentors = Math.round(precisepercentageOfMentors);
+
+console.log(roughPercentageOfStudents);
+console.log(roughpercentageOfMentors);
--- week-1/K-functions-parameters/exercise2.js
+++ week-1/K-functions-parameters/exercise2.js
@@ -1,5 +1,9 @@
 // Declare your function first
-
+function divide(a, b) {
+  var divide = a / b;
+  return divide;
+}
 var result = divide(3, 4);
-
+console.log(result);
+var result = divide(4, 2);
 console.log(result);
