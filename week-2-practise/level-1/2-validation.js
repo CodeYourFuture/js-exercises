1e1ddd7b4e73b4df634ce1cb8225d6061afbfa6b
Daniel Grant <danieljohngrant@gmail.com>
2018-02-28 10:07:08 +0000

    Add validation exercises

---
+++ week-2-practise/level-1/2-validation.js
@@ -0,0 +1,14 @@
+var num = 10;
+var isBigEnough; // complete this statement
+
+if (isBigEnough) {
+  console.log("num is bigger than or equal to 10");
+} else {
+  console.log("num is not big enough");
+}
+
+/* 
+  EXPECTED RESULT
+  ---------------
+  I am not happy
+*/
