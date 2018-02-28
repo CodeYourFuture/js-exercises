6d5b0b934d9d6363a3c3b892ce4448cdad02ebd3
matl2018 <lumierematerne11@gmail.com>
2018-02-28 21:25:01 +0000

    exercises correction. Problem with level 1 ex-8 , leve 2 ex-4 and level 3

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? Returns a random number between 0 and i (excluded ) then multiply it to 10
+// - How many parameters does the function take? 1
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters? at five .
