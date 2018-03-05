926d3d955b5db777536b6a51778cab9e44c59aaa
majroy <majidroyan93@gmail.com>
2018-03-05 19:58:22 +0000

    done all exercises

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `message`
-
+var name="Daniel   ";
+var msg1="My name is ";
+var msg2=" and my name is ";
+var msg3=" characters long";
+var namelength=name.length;
+var message=msg1+name+msg2+namelength+msg3;
 console.log(message);
--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,11 @@
-var name = " Daniel  ";
-
+var name="Daniel   ";
+var msg1="My name is ";
+var msg2=" and my name is ";
+var msg3=" characters long";
+var namelength=name.length;
+var message=msg1+name+msg2+namelength+msg3;
+console.log(message);
+name=name.trim();
+var namelength=name.length;
+var message=msg1+name+msg2+namelength+msg3;
 console.log(message);
