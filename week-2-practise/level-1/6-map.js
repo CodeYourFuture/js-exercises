1763840bd60efd7b86880e5c9f52ee6215e69da0
Omar Suliman <gosuliman@gmail.com>
2018-03-05 23:55:39 +0000

    home work week-2-practice

--- week-2-practise/level-1/6-map.js
+++ week-2-practise/level-1/6-map.js
@@ -11,7 +11,7 @@ function tidyUpString(str) {
 }
 
 var mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-var mentorsTidy=replace.toLowerCase(); // ONLY EDIT THIS LINE
+var mentorsTidy=mentors.map(tidyUpString); // ONLY EDIT THIS LINE
 
 console.log(mentorsTidy);
 
