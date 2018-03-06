7f18df8579021fa85c2b492a9f944bf397ffa32c
matl2018 <lumierematerne11@gmail.com>
2018-03-06 15:18:16 +0000

    correction of  Week2 practise leve1 and level 2 (js1&2)

--- week-2-practise/level-2/2-validation.js
+++ week-2-practise/level-2/2-validation.js
@@ -4,7 +4,13 @@
 // - is less than or equal to 100
 // Tip: write other small functions for each requirement
 
-function validate(num) {}
+function validate(num) {
+  if (num >= 0 && num <= 100 && typeof num === "number") {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
