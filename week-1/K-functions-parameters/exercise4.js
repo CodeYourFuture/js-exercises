47b0ec87acb47b9914adb13769fd5edf313c3a78
harun <harunk3uk@gmail.com>
2018-03-08 21:01:40 +0000

    I finished K-functions-parameters. Its address is in week-1/K-functions-parameters

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function first(num1, num2) {
+  return num1 + num2;
+}
+var sum = first(13, 124);
 // Call the function and assign to a variable `sum`
 
 console.log(sum);
