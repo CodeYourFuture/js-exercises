99e82ade0507f181d51a383ac6e56c28bafa58c6
harun <harunk3uk@gmail.com>
2018-03-08 21:29:08 +0000

    I finished floats exercise. Its address is in week-1/I-floats

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var preciseAge = 30.612437;
+var roughAge = Math.round(preciseAge);
+console.log(roughAge);
+var studentpercent = Math.round(
+  numberOfStudents / (numberOfMentors + numberOfStudents) * 100
+);
+var mentorpercent = Math.round(
+  numberOfMentors / (numberOfMentors + numberOfStudents) * 100
+);
+console.log("Percentage mentors: " + mentorpercent + "%");
+console.log("Percentage students: " + studentpercent + "%");
