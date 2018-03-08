8eb5ee1c26cd6603018316be4e801a123cc69295
harun <harunk3uk@gmail.com>
2018-03-08 21:45:57 +0000

    I finished variables exercise. Its address is in week-1/D-variables

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `greeting`
-
+var greeting = "Hello world";
+var x1 = "my name is harun";
+var x2 = "we are friends for you";
 console.log(greeting);
+console.log(x1, x2);
