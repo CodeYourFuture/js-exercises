f1d4dba25e5cace5cbb8d429a6e80266b9c9a9ab
harun <harunk3uk@gmail.com>
2018-03-08 00:42:15 +0000

    I finished level-1 exercise. Its address is in week-1-practise/level-1

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,18 @@
-function s(w1, w2) {
-    return w1.concat(w2);
+function s(w1, w2, w3) {
+  var total = w1.concat(w2);
+  return total.concat(w3);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your", "future");
 console.log(result);
 
-// Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+// - it associates three word
 // - How many parameters does the function take?
+// - 3
 // - What is the function name?
+// - s
 // - Where is the function called? with what parameters?
+// - console.log(result); // result
 // - What does the function return?
+// - codeyourfuture
 // - How can this function be improved?
