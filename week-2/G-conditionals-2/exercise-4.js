738642fa7e4ca5129ec70b5558f17cce1dc7d72c
Abdulkadir <ethiogentelman@gmail.com>
2018-03-10 10:22:18 -0500

    week-2/G-conditionals-2

--- week-2/G-conditionals-2/exercise-4.js
+++ week-2/G-conditionals-2/exercise-4.js
@@ -9,7 +9,10 @@
 */
 
 function containsCode(sentence) {
-
+  if (sentence.includes("code")) {
+    return true;
+  }
+  return false;
 }
 
 /* 
@@ -19,11 +22,11 @@ var sentence1 = "code your future";
 var sentence2 = "draw your future";
 var sentence3 = "design your future";
 
-console.log("'" + sentence1 + "': " + containsCode(sentence1))
-console.log("'" + sentence2 + "': " + containsCode(sentence2))
-console.log("'" + sentence3 + "': " + containsCode(sentence3))
+console.log("'" + sentence1 + "': " + containsCode(sentence1));
+console.log("'" + sentence2 + "': " + containsCode(sentence2));
+console.log("'" + sentence3 + "': " + containsCode(sentence3));
 
-    /* 
+/* 
     EXPECTED RESULT
     ---------------
     'code your future': true
