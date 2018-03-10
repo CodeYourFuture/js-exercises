32b582f66e198e1e8e7211a8e83971d9eab492d9
Awet Fessha <awetkifle94@gmail.com>
2018-03-10 18:21:25 +0000

    Finished all week-2 and started week-3

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -3,9 +3,11 @@
   Any result over 60 is a pass. 
   Using .find() (and any other array methods necessary), find the lowest pass score
 */
-
+function findLowestPassScore(num) {
+  return num.sort((a, b) => a - b).find(num => num > 60); //using ES6
+}
 var testScores = [90, 50, 100, 25, 81, 66, 80];
-var lowestPassScore = findLowestPassScore(haystack);
+var lowestPassScore = findLowestPassScore(testScores);
 
 console.log(lowestPassScore);
 
