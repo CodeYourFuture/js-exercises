50bf7437879f72de9e12c2577b5217a94d8173d9
harun <harunk3uk@gmail.com>
2018-03-11 16:48:23 +0000

    I finished array-every exercise. Its address is week3/C-array-every

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -5,7 +5,9 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = group.every(name =>
+  students.some(sname => name === sname)
+); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
