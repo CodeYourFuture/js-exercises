1c9a58b927187ce3e7a37723a399f3985ab93a2b
Daniel Grant <danieljohngrant@gmail.com>
2018-03-12 01:02:48 +0000

    Add iterable array to fizz buzz exercise

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -1,13 +1,13 @@
 /* 
-  Using .forEach() print the numbers 0 to 99, with some exceptions:
+  Using .forEach() print the numbers 0 to 15, with some exceptions:
   - For multiples of 3 print “Fizz” instead of the number 
   - For the multiples of 5 print “Buzz”. 
   - For numbers which are multiples of both 3 and 5 print “FizzBuzz”
 
-  You have been given an array with 100 empty items.
+  An array with numbers 0-15 has been provided.
 */
 
-var arr = new Array(100);
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 /* EXPECTED OUTPUT */
 
@@ -27,5 +27,4 @@ var arr = new Array(100);
 13
 14
 'FizzBuzz'
-... etc ...
 */
