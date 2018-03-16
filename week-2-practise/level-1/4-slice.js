61ae13a5b7d9fe4f1f76f0d437e36e22bf81ddff
Rukmini Saraswatibhatla <neelunsiri@gmail.com>
2018-03-16 22:26:06 +0000

    Rukmini's week-2 practise level-1 exercises

--- week-2-practise/level-1/4-slice.js
+++ week-2-practise/level-1/4-slice.js
@@ -4,9 +4,8 @@
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
-var numbers = [1, 2, 3, 4, 5, , 6, 7, 8];
-var first5Numbers = first5(numbers);
-
+var numbers = [1, 2, 3, 4, 5, , 6, 7];
+var first5Numbers = numbers.slice(0, 5);
 console.log(first5Numbers);
 
 /* 
