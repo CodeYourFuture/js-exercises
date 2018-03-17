c38f75339241ed41ad0e331376d72b8ed5ee1ca4
Islam Dudaev <islamwolf23@gmail.com>
2018-03-17 13:12:12 +0000

    Complete week4 folder A exercise

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+var laptop = {
+    name: "Islam's MBP",
+    display: "retina",
+    inches: 13
+};
+
+console.log(laptop);
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,44 @@ Assign each of them to a separate variable
 
 */
 
+var house = {
+    address: "bla bla",
+    isBig: false,
+    rooms: 2
+};
+
+var person = {
+    firstName: "Islam",
+    lastName: "Dudaev",
+    age: 28,
+    nationality: "Chechen",
+    isGood: true
+};
+
+var dog = {
+    name: "Sharpy",
+    legs: 4,
+    color: "white",
+    isFunny: true
+};
+
+var country = {
+    name: "UK",
+    population: 15343243,
+    language: "English"
+};
+
+var student = {
+	name: "Islam",
+	age: "28",
+	interests: [
+        "JavaScript", 
+        "PHP", 
+        "Wordpress",
+        "Laravel",
+        "MySQL",
+        "Bootstrap"
+    ]
+};
+
+
