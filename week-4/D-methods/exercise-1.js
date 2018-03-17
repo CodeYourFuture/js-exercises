0e3df216afd9eda8d7e5179b93a1f3e5c15321f4
matl2018 <lumierematerne11@gmail.com>
2018-03-17 16:52:31 +0000

    In class exercises. Not finished yet ....work on progress

--- week-4/D-methods/exercise-1.js
+++ week-4/D-methods/exercise-1.js
@@ -3,16 +3,18 @@ A person named Alice is defined below.
 Add a method "greet" so this person can say hello.
 */
 
-
 var person = {
-    name: "Alice",
-    age: 25
+  name: "Alice",
+  age: 25,
+  greet: function() {
+    return "Hello everybody";
+  }
 };
-
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-
-console.log("Expected result: Hello everybody. Actual result: " + person.greet());
+console.log(
+  "Expected result: Hello everybody. Actual result: " + person.greet()
+);
