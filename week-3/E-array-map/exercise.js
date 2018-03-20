e73c2b7c7ce30a1320c2141c58e2c92f90b046e4
Abdulkadir <ethiogentelman@gmail.com>
2018-03-20 17:14:05 -0400

    Finish week3

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,4 +2,9 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
-
+var hundredTimesNumber = numbers.map(number => number * 100);
+var hundredTimesNumber1 = numbers.map(function(number) {
+  return number * 100;
+});
+console.log(hundredTimesNumber);
+console.log(hundredTimesNumber1);
