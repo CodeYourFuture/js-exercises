02e9276b4935aae3b9c2428750ce1f660a29e8a4
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-03-23 10:58:23 +0000

    week4-part1-and-part2

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,27 @@ Assign each of them to a separate variable
 
 */
 
+var car = {
+  brand: "rangeRoversport",
+  speed: "1,000mph",
+  engine: "jet",
+  weight: "7.5tonnes"
+};
+
+var AI = {
+  body: "hydraulic",
+  sense: "senseless",
+  use: "warefare"
+};
+
+var digitalCamera = {
+  brand: "canon",
+  type: "full frame DSLR",
+  megapixels: "26-30.9"
+};
+
+var iPad = {
+  brand: "Apple",
+  type: "iPad Pro",
+  screenSize: "12.9"
+};
