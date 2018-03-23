f0277b4e7d97650284c73a02771eefe2df413286
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-03-23 09:51:09 +0000

    objects-intro

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -1,7 +1,13 @@
-/*
-
-Describe your own laptop as a JavaScript object
+/*Describe your own laptop as a JavaScript object
 
 Try to think of as many properties as you can!
 
 */
+
+var laptop = {
+  brand: "Window8",
+  Screensize: 15,
+  isTouchscreen: false,
+  bateryCapacity: "4hrs",
+  RAM: "32GB"
+};
