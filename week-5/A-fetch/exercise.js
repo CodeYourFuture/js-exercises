c8ccf0fd21d60fa4cbf1b18a612d53c89fee44ff
Loic <loic.lehenaff@gmail.com>
2018-03-23 00:03:11 +0000

    Add reference to fetch doc

--- week-5/A-fetch/exercise.js
+++ week-5/A-fetch/exercise.js
@@ -4,6 +4,9 @@ in the HTML element with the id "greeting-text".
 
 API: https://codeyourfuture.herokuapp.com/api/greetings
 Response: A greeting in a random language
+
+To know more about fetch, refer to the doc:
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 */
 
 fetch('*** Write here the API address ***')
