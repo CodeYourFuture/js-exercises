77382c224150408e9a9fb9160465befb8b2d401f
Abdulkadir <ethiogentelman@gmail.com>
2018-03-25 10:00:57 -0400

    week-5 A&B

---
+++ week-5/A-dom-manipulation/js/main.js
@@ -0,0 +1,21 @@
+var myButton = document.querySelector("#bgrChangeBtn");
+myButton.addEventListener("click", function() {
+  var box = document.querySelector(".jumbotron");
+  box.style.backgroundColor = "blue";
+});
+
+var myButton = document.querySelector("#alertBtn");
+myButton.addEventListener("click", function() {
+  var box = document.querySelector(".jumbotron");
+  alert("Hello");
+});
+
+var myButton = document.querySelector("#addTextBtn");
+myButton.addEventListener("click", function() {
+  var box = document.querySelector("#addTextBtn");
+  var newText = document.createElement("p");
+  newText.innerText = "What do you wait now go and donate!!";
+  newText.style.fontColor = "brawn";
+  newText.id = "myPra";
+  box.appendChild(newText);
+});
