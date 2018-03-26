234a4663408a737a3553cc5c61df4dc549bf1a94
Abdulkadir <ethiogentelman@gmail.com>
2018-03-26 19:47:22 -0400

    week-4/D-methods/exercise-1.js

--- week-4/D-methods/exercise-1.js
+++ week-4/D-methods/exercise-1.js
@@ -3,24 +3,18 @@ A person named Alice is defined below.
 Add a method "greet" so this person can say hello.
 */
 
-
 var person = {
-<<<<<<< Updated upstream
-    name: "Alice",
-    age: 25
-=======
   name: "Alice",
   age: 25,
   greet: function() {
     return "Hello" + " everybody";
   }
->>>>>>> Stashed changes
 };
-
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-
-console.log("Expected result: Hello everybody. Actual result: " + person.greet());
+console.log(
+  "Expected result: Hello everybody. Actual result: " + person.greet()
+);
