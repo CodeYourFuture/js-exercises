bca74d2635b08849b835e51441cf2979c94884d6
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-03-30 13:46:08 +0100

    more-complex-objects

--- week-4/C-more-complex-objects/exercise-3.js
+++ week-4/C-more-complex-objects/exercise-3.js
@@ -5,50 +5,62 @@
 */
 
 var kinningParkHouse = {
-    address: "1 Kinning Park",
-    price: 180000,
-    currentOwner: {
-      firstName: "Margaret",
-      lastName: "Conway",
-      email: "margaret@fake-emails.com"
-    }
+  address: "1 Kinning Park",
+  price: 180000,
+  currentOwner: {
+    firstName: "Margaret",
+    lastName: "Conway",
+    email: "margaret@fake-emails.com"
+  }
 };
 
 var parkAvenueHouse = {
-    address: "50 Park Avenue",
-    price: 195000,
-    currentOwner: {
-      firstName: "Marie",
-      lastName: "McDonald",
-      email: "marie.m@real-emails.com"
-    }
+  address: "50 Park Avenue",
+  price: 195000,
+  currentOwner: {
+    firstName: "Marie",
+    lastName: "McDonald",
+    email: "marie.m@real-emails.com"
+  }
 };
 
 /*
   DO NOT EDIT ANYTHING ABOVE THIS LINE
+function getOwnerFullName(house) {
+   return house.currentOwnerFullName;
+}
+var currentHouseOwner = getOwnerFullName(parkAvenueHouse)
+console.log(currentHouseOwner);
 
-  WRITE YOUR CODE BELOW
+ 
+var currentHouseOwner = getOwnerFullName(kinningParkHouse)
+console.log(kinningParkHouse);
+
+
+WRITE YOUR CODE BELOW
 */
 
 // returns the full name (first name + last name) of the owner of the house
-function getOwnerFullName(house) {
-
-}
+function getOwnerFullName(house) {}
 
 // returns an array of the owners' email addresses of the two houses
-function getEmailAddresses(house1, house2) {
-
-}
+function getEmailAddresses(house1, house2) {}
 
 // returns the address for the cheapest house out of the two
-function getCheapestAddress(house1, house2) {
-
-}
-
+function getCheapestAddress(house1, house2) {}
 
 /*
   DO NOT EDIT ANYTHING BELOW THIS LINE
 */
-console.log("Expected result: Margaret Conway. Actual result: " + getOwnerFullName(kinningParkHouse));
-console.log("Expected result: margaret@fake-emails.com, marie.m@real-emails.com. Actual result: " + getEmailAddresses(kinningParkHouse, parkAvenueHouse));
-console.log("Expected result: 1 Kinning Park. Actual result: " + getCheapestAddress(parkAvenueHouse, kinningParkHouse));
+console.log(
+  "Expected result: Margaret Conway. Actual result: " +
+    getOwnerFullName(kinningParkHouse)
+);
+console.log(
+  "Expected result: margaret@fake-emails.com, marie.m@real-emails.com. Actual result: " +
+    getEmailAddresses(kinningParkHouse, parkAvenueHouse)
+);
+console.log(
+  "Expected result: 1 Kinning Park. Actual result: " +
+    getCheapestAddress(parkAvenueHouse, kinningParkHouse)
+);
