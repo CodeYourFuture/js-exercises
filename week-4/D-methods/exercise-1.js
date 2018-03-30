189e121c0a15cb36df65396319014360b5609463
Austine Ogochukwu Okolo <okolochuks4u@gmail.com>
2018-03-30 22:23:55 +0100

    D-metods

--- week-4/D-methods/exercise-1.js
+++ week-4/D-methods/exercise-1.js
@@ -3,16 +3,25 @@ A person named Alice is defined below.
 Add a method "greet" so this person can say hello.
 */
 
-
 var person = {
-    name: "Alice",
-    age: 25
+  name: "Alice",
+  age: 25
 };
 
+var person = {
+  name: "Alice",
+  age: 25,
+  sayHello: function() {
+    return "how are you";
+  }
+};
+person.sayHello();
+console.log(person.sayHello());
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-
-console.log("Expected result: Hello everybody. Actual result: " + person.greet());
+console.log(
+  "Expected result: Hello everybody. Actual result: " + person.greet()
+);
