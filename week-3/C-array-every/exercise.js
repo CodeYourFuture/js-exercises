cf2c7363951e89c69e1e5917b7df60936b056c89
Abdulkadir <ethiogentelman@gmail.com>
2018-03-30 16:58:06 -0400

    completed week-2 practice w3

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -5,7 +5,11 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+function checkingGroup(student) {
+  return students.includes(student);
+}
+var groupIsOnlyStudents = group.every(checkingGroup);
+// complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
