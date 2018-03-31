d2eee30faaab8da5bf1376a1fd146f48869ae680
matl2018 <lumierematerne11@gmail.com>
2018-03-31 09:43:51 +0100

    Week 4 complete . Week-4 practice yet to be finished

--- week-4/C-more-complex-objects/exercise-3.js
+++ week-4/C-more-complex-objects/exercise-3.js
@@ -32,13 +32,14 @@ var parkAvenueHouse = {
 
 // returns the full name (first name + last name) of the owner of the house
 function getOwnerFullName(house) {
-  return house.currentOwner.firstName + house.currentOwner.lastName;
+  return house.currentOwner.firstName + " " + house.currentOwner.lastName;
 }
 
 // returns an array of the owners' email addresses of the two houses
 function getEmailAddresses(house1, house2) {
   var result = [house1.currentOwner.email, house2.currentOwner.email];
-  return result.toString();
+  // return result.toString();
+  return result.join(" , ");
 }
 
 // returns the address for the cheapest house out of the two
