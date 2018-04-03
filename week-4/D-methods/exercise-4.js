7cb44a64fe63dc776c00ed4fb1065acac86dd3ff
Abdulkadir <ethiogentelman@gmail.com>
2018-04-03 02:15:10 -0400

    finished Week-4-ex

--- week-4/D-methods/exercise-4.js
+++ week-4/D-methods/exercise-4.js
@@ -5,15 +5,17 @@ Define a method "makeFriend" to add a new friend to her list.
 
 var person = {
   name: "Alice",
-  friends: ["John", "Nina"]
+  friends: ["John", "Nina"],
+  makeFriend: function(name) {
+    this.friends.push(name);
+  }
 };
-person.friends[2] = "Bob";
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-// person.makeFriend("Bob");
+person.makeFriend("Bob");
 
 console.log(
   "Expected result: 'John,Nina,Bob'. Actual result: " + person.friends
