0fa1b0cd02b05a54cd45a2895adf0793275f34c1
matl2018 <lumierematerne11@gmail.com>
2018-04-05 16:03:41 +0100

    finished homework week-4-ptactice

--- week-4-practise/level-1/exercise-1.js
+++ week-4-practise/level-1/exercise-1.js
@@ -6,7 +6,7 @@ and for each object, `console.log()` out the sentence:
 
 Here is the array:
 
-*/ 
+*/
 
 var writers = [
   {
@@ -36,9 +36,26 @@ var writers = [
     occupation: "writer",
     age: 64,
     alive: true
-  },
+  }
 ];
 
+function introduction(writer) {
+  return (
+    "Hi, my name is " +
+    writer.firstName +
+    " " +
+    writer.lastName +
+    " I am " +
+    writer.age +
+    " years old, and work as a " +
+    writer.occupation +
+    "."
+  );
+}
+
+var fullIntroduction = writers.map(introduction);
+
+console.log(fullIntroduction);
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
--- week-4-practise/level-2/exercise-1.js
+++ week-4-practise/level-2/exercise-1.js
@@ -9,17 +9,17 @@ We made a start on this for you here:
 */
 
 var bottle = {
-	volume: 0,
-	fill: function(){
-	 	// calling this function should make you bottles volume = 100; 
-	},
-	drink: function(){
-		// calling this function should decrease your bottles volume by 10;
-	},
-	empty: function(){
-		// this function should return true if your bottles volume = 0
-	}
-}
+  volume: 0,
+  fill: function(amount) {
+    return amount + this.volume;
+  },
+  drink: function(amount) {
+    this.volume - amount;
+  },
+  empty: function(amount) {
+    return this.volume === 0;
+  }
+};
 
 /*
 --TIP--
@@ -31,11 +31,11 @@ Once you have completed your object run the following and see if your answer
 matches the expected result at the bottom :)
 */
 
-bottle.fill();
-bottle.drink();
-bottle.drink();
-bottle.drink();
-if(!bottle.empty()){
-	console.log('bottles volume = ' + bottle.volume);
+bottle.fill(100);
+bottle.drink(10);
+bottle.drink(10);
+bottle.drink(10);
+if (!bottle.empty()) {
+  console.log("bottles volume = " + bottle.volume);
 }
 console.log("Above volume should be: 70");
