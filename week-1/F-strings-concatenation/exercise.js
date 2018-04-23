7d4a4a77170475d8da227f2e6f22e14524ccd665
eliasadam <elias-adam@hotmail.co.uk>
2018-04-23 22:59:50 +0100

    completed js-exercise week-1

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,11 @@
 // Start by creating a variable `message`
 
-console.log(message);
+
+
+var greetingStart = "Hello, my name is ";
+var name = "Elias";
+
+var greeting = greetingStart + name;
+
+console.log(greeting);
+
--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,10 @@
+
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+var numberOfStudents= 15;
+var numberOfMentors= 8;
+var TotalNumber= numberOfStudents + numberOfMentors;
+console.log('number of students: ' + numberOfStudents );
+console.log( 'number of mentors: ' + numberOfMentors);
+console.log('Total number of students and mentors:' + ( numberOfStudents + numberOfMentors));
+
