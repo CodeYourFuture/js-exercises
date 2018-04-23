bde39946f85f7ecbba5ee96a4c604878d9d68383
raj <rraju12@gmail.com>
2018-04-23 10:46:38 +0100

    Add solutions to week-1-practice for the second pull request

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+    /*it generates random numbers between 0 and 9; includes 0 but not 10*/ 
 // - How many parameters does the function take?
+    /*none*/ 
 // - What is the function name?
+    /*getNumber*/ 
 // - Where is the function called? with what parameters?
+    /*in line 7 and with no arguments*/ 
