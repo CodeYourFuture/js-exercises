cb3e81459ec4d78e1aeb5785ce6836a2d2cb9c7e
Behnaz <behnaz.pourafshari@gmail.com>
2018-04-24 12:00:25 +0100

    complete class exercise

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,3 +1,14 @@
 // Start by creating a variable `message`
-
+var message = "This is a message and its type is";
+var messageType = typeof message;
 console.log(message);
+console.log(messageType);
+
+var message = 23.3;
+var messageType = typeof message;
+
+console.log(messageType);
+//write the top codes in 2 commands......
+var message = "This is a message and its type is",
+  messageType = typeof message;
+console.log(message, messageType);
