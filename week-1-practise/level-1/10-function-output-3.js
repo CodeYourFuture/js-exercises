0a164fafa4d5508df12ae22e306962bc82e526f5
Sue <than.than2042@gmail.com>
2018-04-27 21:41:49 +0100

    fixed

--- week-1-practise/level-1/10-function-output-3.js
+++ week-1-practise/level-1/10-function-output-3.js
@@ -1,8 +1,4 @@
 function concatenate(firstWord, secondWord, thirdWord) {
-    return ('');
-    // Write the body of this function to concatenate three words together
-}
-function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
     return firstWord.concat(secondWord).concat(thirdWord);
    }
