311c2f6c17ea17d6828cba32df0f35954b395801
Sue <than.than2042@gmail.com>
2018-04-27 21:39:50 +0100

    fixed

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -6,17 +6,16 @@ var result = s('code', 'your');
 console.log(result);
 
 
-// Answer these questions:
+//Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+//using concatente two words in one word.
 // - How many parameters does the function take?
+//two
 // - What is the function name?
+//s
 // - Where is the function called? with what parameters?
+//function called line 5. code, your
 // - What does the function return?
+//codeyour
 // - How can this function be improved?
-
-// 
-// two
-// s
-// code, your
-// w1, w2
-// 
+//maybe different function and parameters names
