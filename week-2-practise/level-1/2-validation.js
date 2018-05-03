44e42519c0ebbcb15752afde80249838419203f7
Francis <kabuye_frank@yahoo.com>
2018-05-03 15:42:19 +0100

    wk-2-practise level-1 answers

--- week-2-practise/level-1/2-validation.js
+++ week-2-practise/level-1/2-validation.js
@@ -2,7 +2,7 @@
 // TIP: You should write an expression that returns a boolean value
 
 var num = 10;
-var isBigEnough; // ONLY EDIT THIS LINE
+var isBigEnough = true; // ONLY EDIT THIS LINE
 
 if (isBigEnough) {
   console.log("num is bigger than or equal to 10");
