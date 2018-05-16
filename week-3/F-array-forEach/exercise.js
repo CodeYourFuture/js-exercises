ddca46eac1c6fa6d2cdb5df401957d8442288fb7
Steve <mhdghanembalhawan@hotmail.com>
2018-05-16 21:02:36 +0100

    changed F-array

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -10,7 +10,7 @@
 var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 arr.forEach(function(item) {
-  // for (var i = 0; i < arr.length; i++) {
+  for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
       arr[i] = "FizzsBuzz";
     }
@@ -20,7 +20,7 @@ arr.forEach(function(item) {
     if (arr[i] % 5 == 0) {
       arr[i] = "Buzz";
     }
-  // }
+  }
 });
 console.log(arr);
 
