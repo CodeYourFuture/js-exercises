c9519c7a447c0c6a93828f765073d258e4bd8da4
Steve <mhdghanembalhawan@hotmail.com>
2018-05-16 20:53:49 +0100

    week3 second commit

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -1,7 +1,7 @@
-/* 
+/*
   Using .forEach() print the numbers 0 to 15, with some exceptions:
-  - For multiples of 3 print “Fizz” instead of the number 
-  - For the multiples of 5 print “Buzz”. 
+  - For multiples of 3 print “Fizz” instead of the number
+  - For the multiples of 5 print “Buzz”.
   - For numbers which are multiples of both 3 and 5 print “FizzBuzz”
 
   An array with numbers 0-15 has been provided.
@@ -10,7 +10,7 @@
 var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 arr.forEach(function(item) {
-  for (var i = 1; i < arr.length; i++) {
+  // for (var i = 0; i < arr.length; i++) {
     if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
       arr[i] = "FizzsBuzz";
     }
@@ -20,7 +20,7 @@ arr.forEach(function(item) {
     if (arr[i] % 5 == 0) {
       arr[i] = "Buzz";
     }
-  }
+  // }
 });
 console.log(arr);
 
