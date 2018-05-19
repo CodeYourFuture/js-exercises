5986f90b0d9fd22d33fd962ccbf65a94b8c9167a
Francisca Vargas <francisca@franciscavargas.me>
2018-05-20 00:01:30 +0100

    Exercises week 3

--- week-3/D-array-filter/exercise.js
+++ week-3/D-array-filter/exercise.js
@@ -8,7 +8,13 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(function isPair(value) {
+  if (Array.isArray(value)) {
+    return value.length == 2;
+  } else {
+    return false;
+  }
+}); // Complete this statement
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
