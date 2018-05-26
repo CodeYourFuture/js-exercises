85c1b5467cae418e6f0db3799e55d736fbc8cb60
Steve <mhdghanembalhawan@hotmail.com>
2018-05-26 06:31:46 +0100

    Steve Week-4 js

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  model: "Lenovo",
+  screen: "15 inch",
+  color: "black",
+  operatingSystem: "ubuntu",
+  goodPrefomance: true
+};
+console.log(laptop.goodPrefomance)
--- week-4/B-objects-get-set/exercise-1.js
+++ week-4/B-objects-get-set/exercise-1.js
@@ -3,16 +3,15 @@
 */
 
 var kitten = {
-    ageMonths: 3,
-    isFemale: true,
-    furColour: "brown"
+  ageMonths: 3,
+  isFemale: true,
+  furColour: "brown"
 };
 
 // YOUR CODE GOES BELOW HERE
 
-
-
-
-
-
+// console.log(kitten.furColour);
+Object.keys(kitten).map(item => {
+  console.log(kitten[item]);
+});
 // YOUR CODE GOES ABOVE HERE
