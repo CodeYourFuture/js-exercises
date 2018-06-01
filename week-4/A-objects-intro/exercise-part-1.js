55c0202d5ae0de094a45a5c4e9253ae9cfcd50ab
ZaimAbz <zaimabodi@gmail.com>
2018-06-01 20:33:56 +0100

    week-4 A,B and C

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,4 +6,35 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var car = {
+    model: 'BMW',
+    power: '3300cc',
+    color: 'blue',
+    year: 2018,
+    isManuale: false,        
+}
 
+var studetn = {
+    firstName:'Abz',
+    lastName:'Zaim',
+    age: 32,
+    cualification: 'MBA',
+    studentId: 007,
+    isAdult: true,
+}
+var mentor ={
+    firstName: 'Gerhman',
+    posetion: 'Team Leader',
+    age: 32,
+    mentorId: 001,
+}
+var meal = {
+    name: 'Pizza',
+    type: 'Italian',
+    calories: 1500,
+}
+var country = {
+    name: 'Syria',
+    continint: 'Asia',
+    population: 11000000,
+}
