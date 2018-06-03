2008da36b76789796b314088ace3695144c0b7e1
Seraphine Young <seraphine_ene@yahoo.com>
2018-06-03 12:18:54 +0100

    Work in Progress

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  brand: "Apple",
+  screensize: 13.3,
+  isTouchScreen: true,
+  colour: silver,
+  batteryCycleCount: 30
+};
