1809a097630b446638fdb51afe274eba6167a71b
Alasdair Smith <ali@alasdairsmith.co.uk>
2018-06-11 20:24:54 +0100

    Extract callbacks exercises to separate file and address feedback

--- week-5/C-promises/exercise.js
+++ week-5/C-promises/exercise.js
@@ -1,5 +1,5 @@
 /*
-  Callbacks and Promises
+  Promises
   ---------------------------------
   Open index.html in your browser
 */
@@ -8,107 +8,71 @@
   EXERCISE 1
   =======
 
-  Write a function that inserts your name into the #exercise1 element when
-  the button is clicked
+  Insert the result of promise1 into the #exercise1 element. Hint: .then()
 */
-document.querySelector('#button1')
-  .addEventListener('click', exercise1)
-
 function exercise1() {
-  // Write your implementation here
+  var promise1 = resolvedPromise()
 }
 
 /*
   EXERCISE 2
   =======
 
-  Write a callback function that inserts the result into the #exercise2
-  element
+  Insert the result of promise2 into the #exercise2 element. Hint: .catch()
 */
-functionThatCallsBack(function() {
-  // Write your implementation here
-})
+function exercise2() {
+  var promise2 = rejectedPromise()
+}
 
 /*
   EXERCISE 3
   =======
 
-  Write a function that calls the callback with "Hello from the callback"
+  Insert the result of promise3 into the #exercise3 element. Notice how it
+  takes longer than the others to finish
 */
-
-function exercise3(callback) {
-  // Write your implementation here
+function exercise3() {
+  var promise3 = delayedPromise()
 }
 
 /*
   EXERCISE 4
   =======
 
-  Insert the result of promise1 into the #exercise4 element. Hint: .then()
-*/
-function exercise4() {
-  var promise1 = resolvedPromise()
-}
-
-/*
-  EXERCISE 5
-  =======
-
-  Insert the result of promise2 into the #exercise5 element. Hint: .catch()
-*/
-function exercise5() {
-  var promise2 = rejectedPromise()
-}
-
-/*
-  EXERCISE 6
-  =======
-
-  Insert the result of promise3 into the #exercise6 element. Notice how it
-  takes longer than the others to finish
-*/
-function exercise6() {
-  var promise3 = delayedPromise()
-}
-
-/*
-  EXERCISE 7
-  =======
-
   Take the result of promise4 and concatenate your name onto the end of it.
   Then return your concatenated string as a promise. Finally insert the result
-  of your promise and insert it into the #exercise7 element
+  of your promise and insert it into the #exercise4 element
 */
-function exercise7() {
+function exercise4() {
   var promise4 = concatPromise()
 }
 
 /*
-  EXERCISE 8 (Stretch Goal)
+  EXERCISE 5 (Stretch Goal)
   =======
   Start this exercise if you have completed all of the other exercises and
   you understand them
 
   Create a new Promise that resolves with the string "Hello Promises!".
-  Take the result of your promise and insert the it into the #exercise8
+  Take the result of your promise and insert the it into the #exercise5
   element
 */
 
-function exercise8() {
+function exercise5() {
   // Write your implementation here
 }
 
 /*
-  EXERCISE 9 (Stretch Goal)
+  EXERCISE 6 (Stretch Goal)
   =======
   Start this exercise if you have completed all of the other exercises and
   you understand them
 
   Create a new Promise that rejects with the string "Something went wrong!".
-  Take the result of your promise and insert the it into the #exercise9
+  Take the result of your promise and insert the it into the #exercise6
   element
 */
-function exercise9() {
+function exercise6() {
   // Write your implementation here
 }
 
@@ -121,10 +85,6 @@ function exercise9() {
 //
 // -------------------------------------
 //
-function functionThatCallsBack(callback) {
-  callback('Hello from the function caller')
-}
-
 function resolvedPromise() {
   return Promise.resolve('A Promising Promise')
 }
@@ -145,12 +105,9 @@ function concatPromise() {
   return Promise.resolve('A Promise from: ')
 }
 
-exercise3((text) => {
-  document.querySelector('#exercise3').innerText = text
-})
+exercise1()
+exercise2()
+exercise3()
 exercise4()
 exercise5()
 exercise6()
-exercise7()
-exercise8()
-exercise9()
