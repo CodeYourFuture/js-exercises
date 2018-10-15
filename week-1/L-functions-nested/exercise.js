543d65a876f620d0f1421ed8054f193295b22c37
Mahshid Azami <m.azami2008@gmail.com>
2018-10-15 12:52:41 +0100

    functions nested

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,17 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+function changetoupercase(name) {
+  return name.toUpperCase();
+}
+
+function shoutyGreeting(name) {
+  return "HELLO!" + changetoupercase(name);
+}
+
+console.log(shoutyGreeting(mentor1));
+console.log(shoutyGreeting(mentor2));
+console.log(shoutyGreeting(mentor3));
+console.log(shoutyGreeting(mentor4));
+console.log(shoutyGreeting(mentor5));
