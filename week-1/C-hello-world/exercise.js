5216857b1c61f82de41b4747a35ff0f3d2d91d8c
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 10:23:33 +0100

    console.log exercise

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,1 +1,4 @@
-console.log("Hello world");
+console.log("Hello world. This is my first JS experience");
+console.log("this is first part", "this is second part");
+//console.log(without quotation mark) syntax error
+console.log(324);
