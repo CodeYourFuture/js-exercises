f6d49fcfd2521c0d592fca6207089b6faafc6c2f
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 11:52:30 +0100

    function parameters exercises

--- week-1/K-functions-parameters/exercise3.js
+++ week-1/K-functions-parameters/exercise3.js
@@ -1,5 +1,7 @@
 // Write your function here
-
-var greeting = createGreeting("Daniel");
+function createGreeting(name) {
+  return "Hello, my name is " + name;
+}
+var greeting = createGreeting("Behzad");
 
 console.log(greeting);
--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function adding(a, b) {
+  return a + b;
+}
 // Call the function and assign to a variable `sum`
-
+const sum = adding(13, 124);
 console.log(sum);
