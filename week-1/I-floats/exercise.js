6a41588aaaf0b2af6fbf9e86681a44f845a223c9
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 11:28:41 +0100

    floats exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,7 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+const numberOfStudents = 15;
+const numberOfMentors = 8;
+const totalNumbers = numberOfMentors + numberOfStudents;
+const studentPer = (numberOfStudents * 100) / totalNumbers;
+const mentorsPer = (numberOfMentors * 100) / totalNumbers;
+console.log(`Percentage students: ${Math.round(studentPer)}%`);
+console.log(`Percentage mentors: ${Math.round(mentorsPer)}%`);
