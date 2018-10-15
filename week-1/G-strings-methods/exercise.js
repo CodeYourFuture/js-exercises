8a64356b8195e68d34f780fab19c98b148a8ab4b
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 11:04:10 +0100

    string methods exercises

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
-
+const name = "Behzad";
+const nameLength = name.length;
+const message = `My name is ${name} and my name is ${nameLength} characters long.`;
 console.log(message);
--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-var name = " Daniel  ";
-
+const name = "  Behzad  ";
+const trimmedName = name.trim();
+const nameLength = trimmedName.length;
+const message = `My name is ${trimmedName} and my name is ${nameLength} characters long.`;
 console.log(message);
