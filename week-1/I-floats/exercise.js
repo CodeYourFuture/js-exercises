d63de29217a92dcb38586bdd71647593d95bdb61
Mahshid Azami <m.azami2008@gmail.com>
2018-10-15 12:02:48 +0100

    round numbers

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+var percentageStudent = Math.round((numberOfStudents * 100) / total);
+var percentageMentors = Math.round((numberOfMentors * 100) / total);
+console.log("Percent of students: " + percentageStudent + " %");
+console.log("Percent of mentors: " + percentageMentors + " %");
