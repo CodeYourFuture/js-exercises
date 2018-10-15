3c63f87a82fce9bdee9bd7e2d50eb82fd4049e86
Mahshid Azami <m.azami2008@gmail.com>
2018-10-15 10:41:13 +0100

    Hello World

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,1 +1,6 @@
 console.log("Hello world");
+console.log("Hello World. I just started learning JavaScript!");
+console.log("Hello World. I just started learning JavaScript!");
+console.log("Hello World. I just started learning JavaScript!");
+console.log("Hello World. I just started learning JavaScript!");
+console.log(456);
--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `greeting`
-
+var greeting = "Hello world";
+console.log(greeting);
+console.log(greeting);
 console.log(greeting);
