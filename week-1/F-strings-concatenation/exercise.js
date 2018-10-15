aa8a8d119d87019de803e40afee880e01f87a819
Mahshid Azami <m.azami2008@gmail.com>
2018-10-15 10:50:49 +0100

    add two strings

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
+var greetingStart = "Hello, my name is ";
+var name = " Mahshid";
 
-console.log(message);
+var greeting = greetingStart + name;
+
+console.log(greeting);
