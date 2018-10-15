19113453d0121f3fed6e313d5e867967f5f6be25
Mahshid Azami <m.azami2008@gmail.com>
2018-10-15 12:14:16 +0100

    functions

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,8 @@
 function halve(number) {
-  // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
-
+var result1 = halve(20);
+console.log(result1);
 console.log(result);
