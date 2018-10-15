d31e49fa4bddaf3e1140af0a9654e7dcd9ad026a
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 12:57:40 +0100

    nested exercise

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,14 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function upperMentor(name) {
+  return name.toUpperCase();
+}
+function greeting(name) {
+  return "HELLO " + upperMentor(name);
+}
+console.log(greeting(mentor1));
+console.log(greeting(mentor2));
+console.log(greeting(mentor3));
+console.log(greeting(mentor4));
+console.log(greeting(mentor5));
