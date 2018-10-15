03e5295f00262ee5d0009751fd470c88793f7645
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-10-15 11:11:04 +0100

    numbers exercise

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,7 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+const numberOfStudents = 5;
+const numberOfMentors = 3;
+const totalAttentdance = numberOfMentors + numberOfStudents;
+console.log("Number of students:", numberOfStudents);
+console.log("Number of mentors:", numberOfMentors);
+console.log("Total numnber of students and mentors:", totalAttentdance);
