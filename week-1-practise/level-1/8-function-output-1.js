336febfa1844248ae4a61187ef01f3e3269b9965
Ahmad <ahmadshagel@gmail.com>
2018-11-10 22:04:22 +0000

    random math answered

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+//generate random numbers everytime it's called, from 0 to one (not included)
 // - How many parameters does the function take?
+//none needed as random() will generate the number alone
 // - What is the function name?
+//getNumber
 // - Where is the function called? with what parameters?
+//line 6 with console log witout any parameters
