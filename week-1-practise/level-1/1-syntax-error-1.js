d56266f7c2972494be9f1a35bbefa78d59fa4f0e
Ahmad <ahmadshagel@gmail.com>
2018-11-10 21:40:41 +0000

    first error solved

--- week-1-practise/level-1/1-syntax-error-1.js
+++ week-1-practise/level-1/1-syntax-error-1.js
@@ -1,6 +1,6 @@
 // There is a syntax error in this code. Fix it.
 
-function addNumbers(a b c) {
+function addNumbers(a ,b ,c) {
     return a + b + c;
 }
 
@@ -10,5 +10,8 @@ console.log(result); // Expected result 8 - Run the program and make sure it wor
 
 // Answer these questions:
 // 1. How many parameters does the function take?
+//3
 // 2. What is the function name?
+//addNumbers
 // 3. Where is the function called? with what parameters?
+//console.log (results)
