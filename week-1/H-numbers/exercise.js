c20c88e573ec1d2d2ac575b57d49f89b2c156d37
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-11 15:27:25 +0000

    commit number

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,10 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudent = 15;
+var numberOfMentors = 8;
+
+console.log("Number of students : " + numberOfStudent);
+console.log("Number of Mentors : " + numberOfMentors);
+console.log(
+  "Total of number of student and mentors : " +
+    (numberOfStudent + numberOfMentors)
+);
