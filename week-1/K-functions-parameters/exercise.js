3b342ed6cb509b91f9f60e0c3cff29079137a5f0
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-11 18:31:50 +0000

    commit multiply function

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,9 +1,7 @@
-// Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
+function multiply(a, b) {
+  return a * b;
 }
 
-// Assign the result of calling the function the variable `result`
 var result = multiply(3, 4);
 
 console.log(result);
