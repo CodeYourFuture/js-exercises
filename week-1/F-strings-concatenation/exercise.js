9d19ba22b67ad0c28233eafe9dc1f594a4bad357
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-11 13:56:28 +0000

    commit string conca

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,18 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var myName = "My name is";
+var name = "Abdoulrazack";
+var age = 23;
+var location = "135 Ainsworth Road";
+var postCode = "\nBL8 2RT";
+console.log(
+  "here is my information : " +
+    myName +
+    " " +
+    name +
+    ", " +
+    age +
+    " " +
+    location +
+    " " +
+    postCode
+);
