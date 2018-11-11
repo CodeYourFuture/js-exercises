2d014b1076ce31d625924381b0b9d76355f3d91d
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-11 15:46:19 +0000

    floats commit

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var rate = numberOfStudents / (numberOfStudents + numberOfMentors);
+var rateM = numberOfMentors / (numberOfStudents + numberOfMentors);
+
+console.log("Percentage of Students : " + Math.round(rate * 100) + "%");
+console.log("Percentage of Mentors : " + Math.round(rateM * 100) + "%");
