f42fcfc34ffab3ef407f7ba37492c83940858efc
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-11 14:20:52 +0000

    strings methods

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+var name = "Abdoulrazack";
+var message = name.length;
+var upper = name.toUpperCase();
 
-console.log(message);
+console.log("My ame is " + upper + ", " + "My name's charactere is " + message);
