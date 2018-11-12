2abc6472dfbc2369619d83706137070d0368725e
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-12 08:53:04 +0000

    answers for questions concat method

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,20 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+// This program will join together two or more strings.
 // - How many parameters does the function take?
+// The funtion take 2 parameters.
 // - What is the function name?
+// the function name is s.
 // - Where is the function called? with what parameters?
+// The function is called at line 5.
 // - What does the function return?
+// the function return "codeyour".
 // - How can this function be improved?
+// this function can be improved strings with space inside the parenthese of concat.
