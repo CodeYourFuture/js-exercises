98529e0c122dadb3cef12653c8c33a9199d7042f
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-12 08:37:53 +0000

    answers of questions

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,5 +1,5 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+// this program generate a random number whenever it is called between 0 to 1.
 // - How many parameters does the function take?
+// There is no parameters;
 // - What is the function name?
+// The function's name is getNumber.
 // - Where is the function called? with what parameters?
+// the function is called at line 5. with getNumber.
