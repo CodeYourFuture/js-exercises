27e8f534e67d727872d4639b6e87aeb00062c455
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-12 08:11:25 +0000

    adding dot next to word

--- week-1-practise/level-1/4-logic-error-1.js
+++ week-1-practise/level-1/4-logic-error-1.js
@@ -1,7 +1,7 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return wordtrim();
+  return word.trim();
 }
 
 var result = trimWord("  CodeYourFuture ");
