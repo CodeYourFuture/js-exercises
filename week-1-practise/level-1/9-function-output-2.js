3afbf33e11d968eadf62065db9bbf7b394ad3f3f
Maaly <maalykaraki@yahoo.com>
2018-11-12 23:06:21 +0000

    9-function-output-2 solved:string concat function

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,22 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// it join two strings togather
+// - How many parameters does the function take?It took two
+// - What is the function name? s
+// - Where is the function called? with what parameters? In line no.5 with two parameters 'code' and 'your'
+// - What does the function return? codeyour
+// - How can this function be improved? we could change the name of the function and paramerers so it shows
+// it will be easy to understand the function
+/*function string(str1, str2) {
+  return str1.concat(str2);
+}
+var result = string("code", "your");
+console.log(result);*/
+//or to write it  this way : return str1.concat('').concat(str2); if we want a space between the two words
