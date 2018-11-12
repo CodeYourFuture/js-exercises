6b176c8d7e608a3674961639e36589bb189af6f0
samm <sammura.mg@googlemail.com>
2018-11-12 00:36:30 +0000

    concat two strings

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. concated two strings
+// - How many parameters does the function take? 2 parameter
+// - What is the function name? s
+// - Where is the function called? with what parameters? assign to result with two parameters code and your
 // - What does the function return?
 // - How can this function be improved?
