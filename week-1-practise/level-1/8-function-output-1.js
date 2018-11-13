a09e3640b93fe78a11de40ded49bf21c6bf720d1
goingfar <orde.design@gmail.com>
2018-11-13 20:35:10 +0000

    commit 8-function-output-1.js

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+	return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
+// - What does this program do? return a random number from between 0-9(inclusive)
+// - How many parameters does the function take? (any)
+// - What is the function name? (doesn't take parameters)
 // - Where is the function called? with what parameters?
