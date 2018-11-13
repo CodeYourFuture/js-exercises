101b80be84678670873d7d4acb3aa9612a030120
goingfar <orde.design@gmail.com>
2018-11-13 20:14:33 +0000

    commit J exercise.js

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,15 @@
 function halve(number) {
-  // complete the function here
+	return number / 2;
 }
 
 var result = halve(12);
+
+console.log(result);
+
+function halve(number) {
+	return number / 2;
+}
+
+var result = halve(6);
 
 console.log(result);
