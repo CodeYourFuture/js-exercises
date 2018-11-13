637d51404bd23e04dac92b2686627f0a57cdfe80
goingfar <orde.design@gmail.com>
2018-11-13 20:13:13 +0000

    commit I exercise.js

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,10 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var sum = numberOfMentors + numberOfStudents;
+
+var perOfStudents = numberOfStudents / sum * 100;
+var raund = Math.round(perOfStudents);
+var perOfMentors = numberOfMentors / sum * 100;
+var raund2 = Math.round(perOfMentors);
+console.log('Percentage students: ' + raund + '%');
+console.log('Percentage mentors: ' + raund2 + '%');
