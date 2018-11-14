104fc9aace569f721a4460ae143ad02f757ed093
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-14 23:14:36 +0000

    answers for questions to 8-function-output

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,10 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
-// this program generates a random number whenever it is called between 0 to 1.
+// Math.random() return a number in the range 0 and 1.
 // - How many parameters does the function take?
 // There is no parameters;
 // - What is the function name?
 // The function's name is getNumber.
 // - Where is the function called? with what parameters?
-// the function is called at line 5. with getNumber.
+// the function is called at line 5. with getNumber. There is no parameters  so there is no agreements.
