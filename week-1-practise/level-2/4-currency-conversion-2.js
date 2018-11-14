ddc098cded018040582cb1d4b620a22e6027c948
samm <sammura.mg@googlemail.com>
2018-11-14 18:18:48 +0000

    price with fees

--- week-1-practise/level-2/4-currency-conversion-2.js
+++ week-1-practise/level-2/4-currency-conversion-2.js
@@ -6,9 +6,15 @@
   They have also decided that they should add a 1% fee to all foreign transactions
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
-
-function convertToUSD(price) {}
-function convertToBRL(price) {}
+function fee(price){
+  return price * 0.01;
+}
+function convertToUSD(price) {
+  return "$" + ((price + fee(price)) * 1.4).toFixed(2);
+}
+function convertToBRL(price) {
+  return 	"	R$" + ((price + fee(price)) * 4.9).toFixed(2);
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
