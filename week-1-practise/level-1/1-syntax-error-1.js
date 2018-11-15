d5f8863e37a2a154963170e393d358a355845a56
Eisen9 <izen.dwork111@gmail.com>
2018-11-15 16:00:43 +0000

    adapted

--- week-1-practise/level-1/1-syntax-error-1.js
+++ week-1-practise/level-1/1-syntax-error-1.js
@@ -1,14 +1,15 @@
 // There is a syntax error in this code. Fix it.
 
 function addNumbers(a, b, c) {
-  return a, b, c;
+  return a + b + c;
 }
 
-var result = addNumbers(1 + 3 + 4);
+var result = addNumbers(1, 3, 4);
 console.log(result); // Expected result 8 - Run the program and make sure it works
 
 // Answer these questions:
-// 1. How many parameters does the function take? it can accept up to 255 arguments
+// 1. How many parameters does the function take? 3
 // 2. What is the function name? addNumbers
-// 3. Where is the function called? with what parameters? the function is called on line 7, it
-// is called with 1, 3 , 4
+// 3. Where is the function called? with what parameters? the function is called on line 7, it is assigned to the variable result. It  is called with 0, 1, 2 parameters
+
+//OK
