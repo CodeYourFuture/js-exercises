806355e94f1b67e9a5ef64b7d70c4df2aa73cfb0
Eisen9 <izen.dwork111@gmail.com>
2018-11-15 16:08:31 +0000

    Updated

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,6 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? It generats random numbers that are above 1 and below 10
-// - How many parameters does the function take? as many as you like
+// - How many parameters does the function take? No parameters in this exercise.
 // - What is the function name? getNumber
-// - Where is the function called? with what parameters? the function is assigned to the variable result and it has an empty parameter.
+// - Where is the function called? with what parameters? the function is assigned to the variable result (thus it is called at line 5) and it has an empty parameter.
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -9,6 +9,6 @@ console.log(result);
 // - What does this program do? Run it and see, or Google some of the methods used to understand them. It conctenates the two words 'code' and 'your' together (puts them together)
 // - How many parameters does the function take? as many as possible
 // - What is the function name? s
-// - Where is the function called? with what parameters? It is assigned to the variable result and it has two strings as parameters "code", "your"
+// - Where is the function called? with what parameters? It is assigned to the variable result (called at line 5) and it has two strings as parameters "code", "your"
 // - What does the function return?  concatenated words 'code your'
 // - How can this function be improved? By adding empty strings instead of writing the word. E.g. ''
