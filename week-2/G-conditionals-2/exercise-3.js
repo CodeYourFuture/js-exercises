30d4528981c6f404ef22b6a3706add5e0c93754c
efahmed <engineerfa.24816@gmail.com>
2018-11-17 22:24:48 +0000

    exercise -3 answer

--- week-2/G-conditionals-2/exercise-3.js
+++ week-2/G-conditionals-2/exercise-3.js
@@ -9,7 +9,15 @@
 */
 
 function calculateGrade(mark) {
-
+  if (mark >= 80) {
+    return "A";
+  }
+  if (mark >= 60 && mark < 80) {
+    return "B";
+  }
+  if (mark >= 50 && mark < 60) {
+    return "C";
+  } else return "F";
 }
 
 /* 
@@ -20,11 +28,11 @@ var grade2 = 90;
 var grade3 = 70;
 var grade4 = 55;
 
-console.log("'" + grade1 + "': " + calculateGrade(grade1))
-console.log("'" + grade2 + "': " + calculateGrade(grade2))
-console.log("'" + grade3 + "': " + calculateGrade(grade3))
-
-  /* 
+console.log("'" + grade1 + "': " + calculateGrade(grade1));
+console.log("'" + grade2 + "': " + calculateGrade(grade2));
+console.log("'" + grade3 + "': " + calculateGrade(grade3));
+console.log("'" + grade4 + "': " + calculateGrade(grade4));
+/* 
   EXPECTED RESULT
   ---------------
   '49': F
