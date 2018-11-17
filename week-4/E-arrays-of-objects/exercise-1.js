f9140e323551a51ef754da419d707e87ac2f7c2a
Behzadkk <40770751+Behzadkk@users.noreply.github.com>
2018-11-17 12:43:29 +0000

    week 4 remained

--- week-4/E-arrays-of-objects/exercise-1.js
+++ week-4/E-arrays-of-objects/exercise-1.js
@@ -24,28 +24,28 @@ DO NOT EDIT ANYTHING ABOVE THIS LINE
 WRITE YOUR CODE BELOW
 */
 
-var persons = [person1, person2, person3]; // Complete here
-
-var personNames = persons.map(person => person.name); // Complete here
-
-var personsYoungerThan28YearsOld = persons.filter(person => person.age < 28); // Complete here
-
-/*
+const persons = [person1, person2, person3]; // Complete here
+const getName = person => person.name;
+const personNames = persons.map(getName);
+const predicrateFunction = person => person.age < 28;
+const personsYoungerThan28YearsOld = persons.filter(predicrateFunction).map(getName) // Complete here // Complete here
+    /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
-console.log(
-  "Question 1: array defined with 3 persons -> ",
-  persons[0] === person1 && persons[1] === person2 && persons[2] === person3
-    ? "Passed :)"
-    : "Not yet :("
+    console.log(
+      "Question 1: array defined with 3 persons -> ",
+      persons[0] === person1 && persons[1] === person2 && persons[2] === person3
+        ? "Passed :)"
+        : "Not yet :("
+    ))
 );
 
 console.log(
   "Question 2: array containing the person names -> ",
   personNames[0] === "Alice" &&
-  personNames[1] === "Bob" &&
-  personNames[2] === "John"
+    personNames[1] === "Bob" &&
+    personNames[2] === "John"
     ? "Passed :)"
     : "Not yet :("
 );
@@ -53,7 +53,7 @@ console.log(
 console.log(
   "Question 3: array containing the persons younger than 28 years old -> ",
   personsYoungerThan28YearsOld[0] === person1 &&
-  personsYoungerThan28YearsOld[1] === person3
+    personsYoungerThan28YearsOld[1] === person3
     ? "Passed :)"
     : "Not yet :("
 );
