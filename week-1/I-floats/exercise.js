034eb5a5385dbd925c234796314134c52f8c4966
Boubacar <Boubaweb22@gmail.com>
2018-11-18 14:00:40 +0000

    My floats solution

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,4 +1,8 @@
 var students = 15;
+var Menttors = 8;
+var percentageMentors = 35;
 var PercentageStudents = 65;
 var netAge = "percentageStudents: " + PercentageStudents +"%";
+var round = "percentageMentors: " + percentageMentors +"%";
 console.log(netAge);
+console.log(round);
