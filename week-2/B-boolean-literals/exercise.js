3c0e39b8fc339288a36dd3438b93775e5734abaf
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-19 15:51:19 +0000

    commit boolean exercise.js

--- week-2/B-boolean-literals/exercise.js
+++ week-2/B-boolean-literals/exercise.js
@@ -5,14 +5,16 @@
   Add the required variables with the correct boolean values assigned.
 */
 
-var codeYourFutureIsGreat = true;
-
+var coderFutureIsGreat = true;
+var abdoulIsCool = false;
+var caluclationCorrect = true;
+var moreThan10Students = false;
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 
-console.log("Is Code Your Future great?", codeYourFutureIsGreat);
-console.log("Is Mozafar cool?", mozafarIsCool);
+console.log("Is Code Your Future great?", coderFutureIsGreat);
+console.log("Is abdoul cool?", abdoulIsCool);
 console.log("Does 1 + 1 = 2?", caluclationCorrect);
 console.log("Are there more than 10 students?", moreThan10Students);
 
