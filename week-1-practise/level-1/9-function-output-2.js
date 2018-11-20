446d52e6f5f03ecad3167377e2a619011a845495
folukeOpenuni <folukeagbede@googlemail.com>
2018-11-20 14:10:29 +0000

    answered the question in comment

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,8 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - How many parameters does the function take? take 2 parameters
+// - What is the function name? s
+// - Where is the function called? with what parameters? function was called on line 5 with 'code' and 'word' argument 
+// - What does the function return? the function return the two argurment concatenated  
+// - How can this function be improved? function name and parameter given meaningful names. the is no need for the concat function, a + sign could do the same job
