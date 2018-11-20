b7ace48d0584ceeadb2d731c5bdebfeff505a970
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-20 08:07:32 +0000

    commit exercise conditionals

--- week-2/E-conditionals/exercise.js
+++ week-2/E-conditionals/exercise.js
@@ -4,18 +4,28 @@
   Add an if statement to only show the name if the role is student
 */
 
-
 var name = "Daniel";
 var role = "mentor";
 
-var name2 = "Harun";
+var name2 = "Abdoulrazack";
 var role2 = "student";
 
+if (role === "student" && role2 === "student") {
+  console.log("My name is " + name);
+} else {
+  console.log("My name is " + name2);
+}
 
-console.log("Hi, My name is " + name);
-console.log("Hi, My name is " + name2);
-
-
+/*
+if (role == "student" && role2 == "student") {
+  console.log("Hi, My name is " + name2);
+  console.log("Hi, My name is " + name);
+} else if (role !== "student" && role2 == "student") {
+  console.log("Hi, My name is " + name2);
+} else if (role == "student" && role2 !== "student") {
+  console.log("Hi, My name is " + name);
+}
+*/
 
 /* 
 EXPECTED RESULT
