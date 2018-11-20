158f7a17b5a0a404640e9aaac3b1261aedba334d
folukeOpenuni <folukeagbede@googlemail.com>
2018-11-20 15:04:17 +0000

    added function to format price

--- week-1-practise/level-2/2-format-currency.js
+++ week-1-practise/level-2/2-format-currency.js
@@ -6,7 +6,11 @@
   Write a function that transforms numbers into the format £0.00
 */
 
-function formatCurrency() {}
+
+function formatCurrency(price) {
+      return "£" + price.toFixed(2);
+  }
+
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
