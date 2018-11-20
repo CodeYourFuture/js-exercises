6b0ad9c9d9c71a2d762f9dfe1940e11b4790949a
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-20 09:02:46 +0000

    commit exercise.js array property

--- week-2/I-array-properties/exercise.js
+++ week-2/I-array-properties/exercise.js
@@ -6,7 +6,11 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
+  return arr.length; // complete this statement
+}
+
+function isEmpty(arr) {
+  return arr.length; // complete this statement
 }
 
 /* 
@@ -15,7 +19,7 @@ function isEmpty(arr) {
 var numbers = [1, 2, 3];
 var names = [];
 
-console.log(isEmpty(arr));
+console.log(isEmpty(numbers));
 console.log(isEmpty(names));
 
 /* 
