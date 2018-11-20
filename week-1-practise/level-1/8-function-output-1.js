219694818afc75fd0f3cc0a6ecdfea55da6e4ecf
folukeOpenuni <folukeagbede@googlemail.com>
2018-11-20 14:12:14 +0000

    answered question about Math random

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? generate a random number between 0.0 and 1 and multiply the number by 10 
+// - How many parameters does the function take? the function takes no parameters
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters? theh function is called on line 5
