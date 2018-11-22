5d93ebd90d3063ff195fb29b8a491d4e2070bab0
MariyaPo <mariya.podosinova@gmail.com>
2018-11-22 08:36:34 -0800

    boolean if-else

--- week-2-practise/level-3/1-boolean.js
+++ week-2-practise/level-3/1-boolean.js
@@ -3,9 +3,11 @@
 // What was wrong with the original code?
 // Leave comments above your changes to explain
 
-var isHappy = "false";
+var isHappy = false;
+//if is logical true or false - so don't need to check inside (),
+//{} in case when it is true - first condition, false - second
 
-if (isHappy == true) {
+if (isHappy) {
   console.log("I am happy");
 } else {
   console.log("I am not happy");
