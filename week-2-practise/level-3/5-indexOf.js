8de23227bd99c80cb602f2dfa1c18ba3e5d68d5e
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-22 20:28:16 +0000

    commit 5-indexOf level-3

--- week-2-practise/level-3/5-indexOf.js
+++ week-2-practise/level-3/5-indexOf.js
@@ -6,7 +6,10 @@
 // - return a new array with the replacement value inserted
 
 function replace(arr, valueToReplace, newValue) {
-  return; // complete this statement
+  var index = arr.indexOf(valueToReplace);
+  var firstConcat = arr.slice(0, index);
+  var secondConcat = arr.slice(index + 1);
+  return firstConcat.concat(newValue).concat(secondConcat); // complete this statement
 }
 
 /* 
@@ -15,8 +18,8 @@ function replace(arr, valueToReplace, newValue) {
 var numbers = [1, 3, 3];
 var names = ["Irina", "Ashleigh", "Mozafar"];
 
-var newNumbers = replace(arr, 3, 2);
-var newNames = replace(arr, "Ashleigh", "Rares");
+var newNumbers = replace(numbers, 3, 2);
+var newNames = replace(names, "Ashleigh", "Rares");
 
 console.log(numbers);
 console.log(newNumbers);
