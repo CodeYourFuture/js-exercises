b1518633298922e443ac4e12b7b8266f5ffe923a
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-22 17:27:38 +0000

    commit 1-boolean level-3

--- week-2-practise/level-3/1-boolean.js
+++ week-2-practise/level-3/1-boolean.js
@@ -1,11 +1,17 @@
 // The code is valid but is misleading and could be improved
 // Refactor the code to make it better
 // What was wrong with the original code?
-// Leave comments above your changes to explain
+// The issue was the double quote.
+// What was wrong was the double quote.
+// Boolean doesn't take quote.
+// Leave comments above your changes to explain.
+// We need to print "I am not happy" and it is false so we put false.
+// Why i change it because Boolean does not take quote.
+// Because it will considere as a string.
 
-var isHappy = "false";
+var isHappy = false;
 
-if (isHappy == true) {
+if (isHappy) {
   console.log("I am happy");
 } else {
   console.log("I am not happy");
