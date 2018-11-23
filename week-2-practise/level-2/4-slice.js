c56829e5b25e2c06c8b08893d1b53e90946772c5
efahmed <engineerfa.24816@gmail.com>
2018-11-23 22:27:46 +0000

    the answer is added

--- week-2-practise/level-2/4-slice.js
+++ week-2-practise/level-2/4-slice.js
@@ -5,7 +5,8 @@
 // - remove the item at the specified index
 
 function remove(arr, index) {
-  return; // complete this statement
+  return arr.slice(arr, index);
+  // complete this statement
 }
 
 /* 
