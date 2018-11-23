e1e03a4adc8890a53c0eb271e343697ac1460b4c
Maaly <maalykaraki@yahoo.com>
2018-11-23 20:13:20 +0000

    week 2 I-array properties

--- week-2/I-array-properties/exercise.js
+++ week-2/I-array-properties/exercise.js
@@ -2,11 +2,14 @@
   Array properites
   ----------------
   Complete the function to test if an array is empty (has no values in it)
-
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
+  if (arr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -15,7 +18,7 @@ function isEmpty(arr) {
 var numbers = [1, 2, 3];
 var names = [];
 
-console.log(isEmpty(arr));
+console.log(isEmpty(numbers));
 console.log(isEmpty(names));
 
 /* 
