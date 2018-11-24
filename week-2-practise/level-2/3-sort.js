8ff1170eab71bb5c9c0652b338f3d1090d6798dc
folukeOpenuni <folukeagbede@googlemail.com>
2018-11-24 10:34:42 +0000

    sorting multi arrays in ascending order

--- week-2-practise/level-2/3-sort.js
+++ week-2-practise/level-2/3-sort.js
@@ -3,7 +3,8 @@
 
 var letters = ["a", "n", "c", "e", "z", "f"];
 var letters2 = ["w", "b", "v", "g", "l", "o"];
-var sortedLetters; // ONLY EDIT THIS LINE
+var letter3 =  letters.concat(letters2);
+var sortedLetters = letter3.sort(); // ONLY EDIT THIS LINE
 
 console.log(sortedLetters);
 
