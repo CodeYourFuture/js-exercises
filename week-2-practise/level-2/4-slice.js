40209a91b892c393265f1fdeb5a851bd267f4ebc
folukeOpenuni <folukeagbede@googlemail.com>
2018-11-24 11:18:45 +0000

    func removes an element from an array

--- week-2-practise/level-2/4-slice.js
+++ week-2-practise/level-2/4-slice.js
@@ -5,7 +5,10 @@
 // - remove the item at the specified index
 
 function remove(arr, index) {
-  return; // complete this statement
+  //return arr.splice(arr, index); // complete this statement
+  // return arr.slice(0, index).concat(arr);
+  return arr.slice(0, index).concat(arr.slice(index+1));
+
 }
 
 /* 
