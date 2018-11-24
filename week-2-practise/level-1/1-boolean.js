a8c942abfbd30b081889a3ceefefbca2f614196f
Maaly <maalykaraki@yahoo.com>
2018-11-24 19:44:05 +0000

    week 2 level 1 3-sort solution

--- week-2-practise/level-1/1-boolean.js
+++ week-2-practise/level-1/1-boolean.js
@@ -1,14 +1,13 @@
 // The code is valid but the program does not produce the expected result
 // Fix it.
 
-var isHappy = false;
+var isHappy = true;
 
 if (isHappy) {
   console.log("I am happy");
 } else {
   console.log("I am not happy");
 }
-
 /* 
   EXPECTED RESULT
   ---------------
