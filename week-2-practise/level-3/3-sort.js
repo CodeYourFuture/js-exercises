fb429f7de175eac1015ab9f8081800db7775b76b
Maaly <maalykaraki@yahoo.com>
2018-11-24 22:41:39 +0000

    week2 level 3 3-sort  solution

--- week-2-practise/level-3/3-sort.js
+++ week-2-practise/level-3/3-sort.js
@@ -4,7 +4,12 @@
 
 var nums = [10, 1, 5, 29, 100];
 var nums2 = [11, 6, 3, 29, 12];
-var sortedNums; // complete this statement
+var sortedNums = nums.concat(nums2).sort(function(a, b) {
+  return a - b;
+}); // complete this statement
+//if we don't use the compare function the numbers will convert to strings and sorts
+// according to that ,which will lead to mistakes like  100 less than 29 and the result
+// will be like this [1,10,100,11,12,29,29,3,5,6]
 
 console.log(sortedNums);
 
