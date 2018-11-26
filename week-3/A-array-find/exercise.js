a4b43a01b116aa9e708aa19d618673e72803aa91
efahmed <engineerfa.24816@gmail.com>
2018-11-26 01:03:03 +0000

    it is done

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -3,7 +3,15 @@
   Any result over 60 is a pass. 
   Using .find() (and any other array methods necessary), find the lowest pass score
 */
-
+function overScore(score) {
+  return score >= 60;
+}
+function findLowestPassScore(testScores) {
+  testScores.sort(function(a, b) {
+    return a - b;
+  });
+  return testScores.find(overScore);
+}
 var testScores = [90, 50, 100, 25, 81, 66, 80];
 var lowestPassScore = findLowestPassScore(testScores);
 
