0bae071da6fa774ff4727750a55d8d7889092402
efahmed <engineerfa.24816@gmail.com>
2018-11-29 23:38:28 +0000

    the answer is added

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -8,6 +8,17 @@
 */
 
 var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+arr.forEach(function(num) {
+  if (num % 3 == 0 && num % 5 == 0 && num !== 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0 && num !== 0) {
+    console.log("Fizz");
+  } else if (num % 5 == 0 && num !== 0) {
+    console.log("Buzz");
+  } else {
+    console.log(num);
+  }
+});
 
 /* EXPECTED OUTPUT */
 
