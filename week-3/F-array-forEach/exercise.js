38e776075daa5007a9f9c197e24a5de9ab1cfc2a
MariyaPo <mariya.podosinova@gmail.com>
2018-11-29 15:51:54 -0800

    .forEach(if,elseif,elseif,else)

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -8,7 +8,17 @@
 */
 
 var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
+arr.forEach(function(element) {
+  if (element % 5 == 0 && element % 3 == 0 && element !== 0) {
+    console.log("FizzBuzz");
+  } else if (element % 5 == 0 && element !== 0) {
+    console.log("Buzz");
+  } else if (element % 3 == 0 && element !== 0) {
+    console.log("Fizz");
+  } else {
+    console.log(element);
+  }
+});
 /* EXPECTED OUTPUT */
 
 /*
