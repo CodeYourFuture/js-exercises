3f3203f75ee0a8e577f3d0b5f97162cfcfe592b2
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-30 11:39:31 +0000

    commit exercise find method

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -4,6 +4,17 @@
   Using .find() (and any other array methods necessary), find the lowest pass score
 */
 
+function overScore(score) {
+  return score >= 60;
+}
+
+function findLowestPassScore(testScores) {
+  testScores.sort(function(a, b) {
+    return a - b;
+  });
+  return testScores.find(overScore);
+}
+
 var testScores = [90, 50, 100, 25, 81, 66, 80];
 var lowestPassScore = findLowestPassScore(testScores);
 
