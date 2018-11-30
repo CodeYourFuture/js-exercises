0b0024d736897788b1db21bc2953d55c4d676785
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-11-30 11:50:34 +0000

    commit map method

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -3,3 +3,37 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multiply(num) {
+  return num * 100;
+}
+
+var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+var multiplication = numbers.map(multiply);
+
+console.log(multiplication);
+
+/*
+var multiplication = numbers.map(function multiply(num) {
+  return num * 100;
+}); 
+console.log(multiplication);
+*/
+
+/*
+var multiplication = numbers.map(function(num) {
+  return num * 100;
+});
+console.log(multiplication);
+*/
+
+/*
+var multiplication = numbers.map(num => {
+  return num * 100;
+});
+console.log(multiplication);
+*/
+
+/*
+var multiplication = numbers.map(num => num * 100);
+console.log(multiplication);
+*/
