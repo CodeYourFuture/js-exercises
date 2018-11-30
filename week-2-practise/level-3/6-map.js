83412c54c029238f7915cb28dbb83ff954ddc521
MariyaPo <mariya.podosinova@gmail.com>
2018-11-30 13:22:18 -0800

    str.charAt(0).toUpperCase() + str.slice(1)

--- week-2-practise/level-3/6-map.js
+++ week-2-practise/level-3/6-map.js
@@ -11,11 +11,11 @@ function tidyUpString(str) {
 }
 
 function captialise(str) {
-  // complete this function
+  return str.charAt(0).toUpperCase() + str.slice(1); // complete this function
 }
 
 var mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
-var mentorsTidyAndCapitalised;
+var mentorsTidyAndCapitalised = mentors.map(tidyUpString).map(captialise);
 
 console.log(mentorsTidyAndCapitalised);
 
