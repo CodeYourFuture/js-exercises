7b697627de68afcbb7d54683a96762b50e98285f
MariyaPo <mariya.podosinova@gmail.com>
2018-12-01 07:17:41 -0800

    Object.values

--- week-4/B-objects-get-set/exercise-1.js
+++ week-4/B-objects-get-set/exercise-1.js
@@ -3,16 +3,13 @@
 */
 
 var kitten = {
-    ageMonths: 3,
-    isFemale: true,
-    furColour: "brown"
+  ageMonths: 3,
+  isFemale: true,
+  furColour: "brown"
 };
 
 // YOUR CODE GOES BELOW HERE
 
-
-
-
-
+console.log(Object.values(kitten));
 
 // YOUR CODE GOES ABOVE HERE
