949bbe102eab5a5c04df924b25dc0aa43b9d185f
Abdoulrazack <abdoulrazackkahin@gmail.com>
2018-12-01 14:18:00 +0000

    commit object Intro

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,14 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+var myLaptop = {
+  Brand: "Hp",
+  Edition: "Windows 10 Home",
+  SystemType: 64,
+  DeviceName: "Desktop-N72B9DL",
+  InstalledRam: "8 GB",
+  Processor: "Intel-core i5"
+};
+
+console.log(myLaptop);
