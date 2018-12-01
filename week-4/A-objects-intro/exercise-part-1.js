f8ca9a0d67294e8c5d801af69f5846ff7e6c7233
MariyaPo <mariya.podosinova@gmail.com>
2018-12-01 06:52:37 -0800

    5 random objects + dateToday

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,33 @@ Assign each of them to a separate variable
 
 */
 
+var Liverpool = {
+  weather: 15,
+  population: 1500000,
+  location: "UK"
+};
+
+var week = {
+  weekdays: ["Monday", "Tuesday", "Wednesday"],
+  weekends: ["Saturday", "Sunday"]
+};
+
+var tickets = {
+  zone1: 299,
+  zone2: 199,
+  discountEarlyBirds: 20
+};
+
+var meeting = {
+  people: ["Mariya", "Elena"],
+  date: "01.12.2018",
+  where: "Manchester"
+};
+
+var date = {
+  day: 01,
+  month: 12,
+  year: 2018
+};
+var dateToday = Object.values(date);
+console.log(dateToday);
