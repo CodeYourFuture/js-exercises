9e833ec72483262dd9b795eca5864903869387a0
MariyaPo <mariya.podosinova@gmail.com>
2018-12-01 06:32:50 -0800

    laptopMy

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptopMy = {
+  color: "Red",
+  brand: "HP",
+  year: 2015,
+  belongstome: true
+};
+
+console.log(laptopMy.color);
