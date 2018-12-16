5b2a04737dab5dfc060e01aecd8be9ce22ebef26
Ivina Pontes <ivinapontes@outlook.com>
2018-12-16 17:17:04 +0000

    Functions exercises done

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,8 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
-var result = halve(12);
+var result = halve(69958);
 
 console.log(result);
--- week-1/J-functions/exercise2.js
+++ week-1/J-functions/exercise2.js
@@ -1,7 +1,8 @@
 function triple(number) {
   // complete function here
+  return number * 3;
 }
 
-var result = triple(12);
+var result = triple(552);
 
 console.log(result);
