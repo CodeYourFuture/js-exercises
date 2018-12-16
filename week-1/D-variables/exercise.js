27da2751c8638511d295c4dcfd1710bc6fe04661
Joan Yassi <sahiealexandre@hotmail.fr>
2018-12-16 16:39:09 +0000

    Joan answers to the part d

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `greeting`
+var greeting = "Just a normal text, not hello world";
+var myName ="LexyKing";
 
+console.log(myName);
+console.log(greeting);
+console.log(greeting);
 console.log(greeting);
