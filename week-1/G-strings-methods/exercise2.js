1a0f7e099d18a64524e57d29ed44b5f2857231d3
Kadir <kegilmez@gmail.com>
2018-12-18 21:42:27 +0000

    G-strings-methods2

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,7 @@
-var name = " Daniel  ";
+var nameLong = " Daniel  ";
+var name = nameLong.trim();
+var nameLength = name.length;
+var message =
+  "My name is " + name + " and my name is " + name.length + " characters long";
 
 console.log(message);
