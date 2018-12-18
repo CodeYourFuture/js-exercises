3cd943d7477a19c427f17f3fec4e4e23a73bdd64
clement <clemohiomoje@gmail.com>
2018-12-18 22:24:55 +0000

    Exercise H - numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+console.log(total);
