ad0044d2c3add1035afa6fe8e85a32e7320d0eab
clement <clemohiomoje@gmail.com>
2018-12-18 22:18:15 +0000

    D variables

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,5 +1,4 @@
 // Start by creating a variable `greeting`
-
-console.log(greeting);
-console.log(greeting);
-console.log(greeting);
+console.log("greeting");
+console.log("greeting");
+console.log("greeting");
