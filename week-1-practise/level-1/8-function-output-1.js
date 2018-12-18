21375ae6bfe9bfd91631b99d3217962a7c63f57b
Bart Crowley <bmcrowley@hotmail.com>
2018-12-18 21:26:39 +0000

    level 1 exercise 8

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? takes a random number and multiplies it by 10
+// - How many parameters does the function take? 1
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters? Called in var result with random paramters.
