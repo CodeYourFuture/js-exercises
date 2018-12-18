c902ad788e7a317a3de9c7ca42c48ee622ee1e73
Kadir <kegilmez@gmail.com>
2018-12-18 22:53:14 +0000

    J-functions-halve

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -4,6 +4,12 @@ function halve(number) {
   // complete the function here
 }
 
-var result = halve(12);
+var result = halve(44);
+var result1 = halve(122);
+var result2 = halve(25);
+var result3 = halve(13762495743967456);
 
 console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3);
