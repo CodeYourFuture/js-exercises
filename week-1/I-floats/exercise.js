4dff17a44b223685790c8257ed497bbcfb594ea2
Bart Crowley <bmcrowley@hotmail.com>
2018-12-18 19:16:18 +0000

    Floats Exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,7 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalPeople = numberOfStudents + numberOfMentors;
+var percentageOfStudents = (numberOfStudents / totalPeople) * 100;
+var percentageOfMentors = (numberOfMentors / totalPeople) * 100;
+console.log("Percentage students: " + Math.round(percentageOfStudents) + "%");
+console.log("Percentage mentors: " + Math.round(percentageOfMentors) + "%");
