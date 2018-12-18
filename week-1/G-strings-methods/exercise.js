e8fb8094c23683c11b0fbe49e5eda779f1917455
Kadir <kegilmez@gmail.com>
2018-12-18 21:37:28 +0000

    G-strings-methods-1

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
-
-console.log(message);
+var name = "Kadir";
+var nameLength = name.length;
+console.log(
+  "My name is " + name + " and my name is " + name.length + " characters long"
+);
