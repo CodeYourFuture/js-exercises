1348caacc6d94e41bef0b12ebef56da1276f40ef
clement <clemohiomoje@gmail.com>
2018-12-19 18:10:19 +0000

    nested function exercise

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,13 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var students = 30;
+var mentors = 6;
+
+var total = students + mentors;
+
+  var percentMentors = Math.floor((mentors *100)/total);
+
+  var percentstudents = Math.floor((students *100)/total);
+function message (){
+    var gM = "percentage of mentors is " + percentMentors+ "%" + " and percentage of students is " + percentstudents + "%";
+    return gM;
+}
+console.log(message());
