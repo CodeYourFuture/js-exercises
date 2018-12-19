9f777bdb1fa635fc60a572081da33c62154f92cd
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-19 12:57:45 +0000

    D-variable exercise

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,9 +1,8 @@
-// Start by creating a variable `greeting`
-
-var var1 = "good Morning";
-console.log(var1);
-console.log(var1);
-console.log(var1);
+// Start by creating a variable `greeting
+var greeting = "good Morning";
+console.log(greeting);
+console.log(greeting);
+console.log(greeting);
 
 
 
