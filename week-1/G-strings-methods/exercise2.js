f1f2e3a71092dbf137776d47b8a61c3844fa2739
ZaimAbz <zaimabodi@gmail.com>
2018-12-19 21:07:05 +0000

    string-methods

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,4 +1,6 @@
 var name = " Daniel  ";
+var trimedName = name.trim();
+var nameLength = trimedName.length;
 
-console.log(name);
-console.log(name.trim());
+console.log("My name is " + trimedName + " and my name is " + nameLength + " characters long");
+
