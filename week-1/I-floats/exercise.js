b2ae640c79fba559bd133a3fc768382ab7993531
clement <clemohiomoje@gmail.com>
2018-12-19 17:06:26 +0000

    float exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -2,5 +2,5 @@ var numberOfStudents = 15;
 var numberOfMentors = 8;
 var percentstudent = Math.floor(numberOfStudents);
 var percentmentor = Math.floor(numberOfMentors);
-console.log(percentstudent);
-console.log(percentmentor);
+console.log(percentstudent + "%");
+console.log(percentmentor + "%");
