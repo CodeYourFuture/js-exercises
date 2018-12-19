efbfed1d6584c29f20fc0ba26b5f4d3867fc2663
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 17:44:10 +0000

    K- exercise4

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -1,5 +1,9 @@
 // Declare your function first
+function add(a, b) {
+  var sum = a + b;
+  return sum;
+}
 
 // Call the function and assign to a variable `sum`
-
+var sum = add(13, 124);
 console.log(sum);
