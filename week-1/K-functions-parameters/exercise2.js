deaff7b96eb6e256869abf6b449fd10f18e12eeb
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 17:21:55 +0000

    K-exercise

--- week-1/K-functions-parameters/exercise2.js
+++ week-1/K-functions-parameters/exercise2.js
@@ -1,5 +1,10 @@
 // Declare your function first
+function divide(a, b) {
+  return a / b;
+  // Calculate the result of the function and return it
+}
 
+// Assign the result of calling the function the variable `result`
 var result = divide(3, 4);
 
 console.log(result);
