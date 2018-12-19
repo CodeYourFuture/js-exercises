611f525bdcbb09d7e857c0047b32a93cee9d25e3
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 12:42:43 +0000

    C-exercice

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,1 +1,2 @@
 console.log("Hello world");
+console.log("hello world, we are working in the constructor lab");
--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,8 @@
 // Start by creating a variable `greeting`
+var greeting = "aslema";
 
 console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+
