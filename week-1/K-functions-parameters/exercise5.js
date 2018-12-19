392bb880df07693c6613ce38bf998e41c7a765d0
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 18:06:24 +0000

    K-exercise5

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,5 +1,7 @@
 // Declare your function here
-
+function createLongGreeting(name, age) {
+  return "hello My name is " + name + "and I am " + age + " years old";
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
