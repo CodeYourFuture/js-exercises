eb3382895e705ea2e2d60e2e9befaf098b49b179
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-19 15:52:45 +0000

    H numbers Exercise

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numbervofstudent = 10;
+var numberOfMentors = 15;
+var total = numbervofstudent + numberOfMentors;
+console.log(total);
