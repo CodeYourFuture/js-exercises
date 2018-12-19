24b5807dfa8fb057602aac7d82411220f70e9985
clement <clemohiomoje@gmail.com>
2018-12-19 23:31:47 +0000

    function-output 2

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,8 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - How many parameters does the function take? two
+// - What is the function name? s
+// - Where is the function called? with what parameters? the function is called on line number 5 with the following parameters('code', 'your')
+// - What does the function return? the function return s
+// - How can this function be improved? the function can be improved in my view by have a better function name and proper spacing.
