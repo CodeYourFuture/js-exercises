93960d67275bc8c707f605ac83ffb7ab63c94b9a
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-19 16:27:11 +0000

    I-floats Exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,6 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var perstudent = Math.floor(numberOfStudents);
+var permentors = Math.floor(numberOfMentors);
+console.log(perstudent + "%");
+console.log(permentors + "%");
