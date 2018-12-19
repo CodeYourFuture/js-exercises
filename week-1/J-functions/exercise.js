ff369160fedb3492975a2975c6c2244a695a0066
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 12:52:16 +0000

    J-exercise

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,9 @@
-function halve(number) {
+function halve(number) { 
+  return number / 2;
   // complete the function here
 }
 
 var result = halve(12);
+var result = halve (50)
 
 console.log(result);
