fcbb58c26482d7e0bd4f1028e3e0fcdf5544bd4e
najlasoltana <soltananajlaaa.gmail.com>
2018-12-19 16:25:34 +0000

    H-exercice

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 40;
+var numberOfMentors = 20;
+var total = numberOfMentors + numberOfStudents;
+console.log(total);
