2a080d1515dd21001aa792bdaad4f147b985c0b3
clement <clemohiomoje@gmail.com>
2018-12-19 15:53:41 +0000

    Functions- parameters exercise 1

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,9 +1,11 @@
 // Complete the function so that it takes input parameters
 function multiply(num1, num2) {
+  console.log(num1);
+  console.log(num2);
+  return num1 * num2;
   // Calculate the result of the function and return it
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
-
+var result = multiply(2, 6);
 console.log(result);
