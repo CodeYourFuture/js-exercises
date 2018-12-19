a2b7e07a6bef29dd43dc2a0dd2918207a5e07af3
clement <clemohiomoje@gmail.com>
2018-12-19 22:19:56 +0000

    Float exercise correction

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,6 +1,7 @@
-var numberOfStudents = 15.456789;
+var numberOfStudents = 15;
 var numberOfMentors = 8;
-var percentstudent = Math.floor(numberOfStudents);
-var percentmentor = Math.floor(numberOfMentors);
+var total = numberOfStudents + numberOfMentors;
+var percentstudent = Math.round (100 * numberOfStudents/total);
+var percentmentor = Math.round (100 * numberOfMentors/total);
 console.log(percentstudent);
 console.log(percentmentor);
