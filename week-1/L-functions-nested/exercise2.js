9361c126df22afc4c2bbeebedaae012d21f18e34
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-20 13:02:12 +0000

    L-function Exercise2

---
+++ week-1/L-functions-nested/exercise2.js
@@ -0,0 +1,16 @@
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+function mentors(name) {
+  return name.toUpperCase(name);
+}
+function greeting(name) {
+  return "HELLO  " + mentors(name);
+}
+console.log(greeting(mentor1));
+console.log(greeting(mentor2));
+console.log(greeting(mentor3));
+console.log(greeting(mentor4));
+console.log(greeting(mentor5));
