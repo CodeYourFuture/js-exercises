c3764b825a378c104aea31caccb1d7a80546269d
Ivina <ivinapontes@outlook.com>
2018-12-20 18:35:43 +0000

    Update 8-function-output-1.js

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -8,6 +8,6 @@ console.log(result);
 // Answer these questions:
 // - What does this program do? It gets a random number and multiplies by 10.
 // - How many parameters does the function take? 0
-// - What is the function name? Get Number
+// - What is the function name? getNumber
 // - Where is the function called? with what parameters? 
 //   Right after defining the function, When defining the variable result, there are no parameters.
