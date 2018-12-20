4f7457d87b6c9397f2930c028dc8272e88a11537
zareen <evocativevict@gmail.com>
2018-12-20 22:11:01 +0000

    G-strings

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
 
+var myName = "zareen khan";
+var namel = myName.length;
+var message =
+  "my name is " + myName + " my name is " + namel + " characters long";
 console.log(message);
