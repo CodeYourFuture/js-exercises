32bd6b1d44cc5ee1d54f3ee63020d8ab92587d81
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-20 13:05:46 +0000

    L-function exercise1

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,17 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+var student = 104;
+var mentor = 56;
+var total = student + mentor;
+var percentMentor = Math.round((mentor * 100) / total);
+var percentStudent = Math.round((student * 100) / total);
+function message() {
+  var var1 =
+    "percentage of mentors is " +
+    percentMentor +
+    "%" +
+    " and percentage of student is  " +
+    percentStudent +
+    "%";
+
+  return var1;
+}
+console.log(message());
