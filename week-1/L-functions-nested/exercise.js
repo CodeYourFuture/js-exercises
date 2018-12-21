c165b72e0fce8cf889224a1e089ed127f1ac7f8d
Alisina92 <asb92.jobs@gmail.com>
2018-12-21 22:50:32 +0000

    Part L Solution

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,5 +1,16 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+function percentage(number) {
+  return Math.round(100 * number);
+}
+function massage(numberOfStudents, numberOfMentors) {
+  var total = numberOfStudents + numberOfMentors;
+  var percentageStudents = percentage(numberOfStudents / total);
+  var percentageMentors = percentage(numberOfMentors / total);
+
+  var massage1 = "Percentage Students = " + percentageStudents + "%";
+  var massage2 = "Percentage Mentors =" + percentageMentors + "%";
+
+  console.log(massage1);
+  console.log(massage2);
+}
+
+massage(26, 10);
---
+++ week-1/L-functions-nested/exercise2.js
@@ -0,0 +1,18 @@
+function capitalize(name) {
+  return name.toUpperCase();
+}
+function greeting(name) {
+  return "HELLO " + capitalize(name);
+}
+
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+
+console.log(greeting(mentor1));
+console.log(greeting(mentor2));
+console.log(greeting(mentor3));
+console.log(greeting(mentor4));
+console.log(greeting(mentor5));
