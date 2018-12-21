0f7ead572e56732d53943e13f0f92301857d2b82
Alisina92 <asb92.jobs@gmail.com>
2018-12-21 22:13:58 +0000

    Part H Solutions

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,12 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 26;
+var numberOfMentors = 10;
+
+var total = numberOfStudents + numberOfMentors;
+var Students = "The number of Studenrs " + numberOfStudents;
+var Mentors = "The Number of Mentors " + numberOfMentors;
+var Sum = "The Total of Students& Mentors are " + total;
+
+console.log(Students);
+console.log(Mentors);
+console.log(Sum);
