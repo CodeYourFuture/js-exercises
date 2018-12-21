34e31534d9b250a1a3e7e86fa792d600c41252e6
Alisina92 <asb92.jobs@gmail.com>
2018-12-21 22:29:42 +0000

    Part I Solutions

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,12 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var total = numberOfStudents + numberOfMentors;
+
+var percentageStudents = Math.round((100 * numberOfStudents) / total);
+var percentageMentors = Math.round((100 * numberOfMentors) / total);
+
+var massage1 = "Percentage Students = " + percentageStudents + "%";
+var massage2 = "Percentage Mentors =" + percentageMentors + "%";
+
+console.log(massage1);
+console.log(massage2);
