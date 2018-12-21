2640882a9a4cb101b0691ee6d63ec610d176e0f4
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-21 22:33:13 +0000

    level-1 functionoutput-2

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,15 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
+/*- What does this program do? Run it and see, or Google some of 
+ the methods used ot understand them. - it adds two string */
+// - How many parameters does the function take?2
+// - What is the function name?s
+// - Where is the function called?f with what parameters?called nside the paranthesis ,w1,w2
+// - What does the function return?it returns adding two strings
 // - How can this function be improved?
