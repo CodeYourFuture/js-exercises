f472b9bb45691023f2a0595245cc4ed0a9cae2cb
Deepa Akhil <deepaakhiltvm@gmail.com>
2018-12-21 14:05:17 +0000

    level-1 function output-1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,13 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+/*- What does this program do?it accept a random number 
+and multiplied by 10 (move   one integer left)and returns a floating point number */
+// - How many parameters does the function take? 1
+// - What is the function name?getNumber
+// - Where is the function called? with what parameters?inside function,a random number as parameter.
