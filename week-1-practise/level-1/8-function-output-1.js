c8228cae10b95903dd817dbb42c99d7f9fad9618
Mussie Tecle <filago14@gmail.com>
2018-12-22 14:49:43 +0000

    level 1 practice answers

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,13 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? Generate random numbers from 0 to less than 10.
+// - How many parameters does the function take? 0 parameters
+// - What is the function name? getNumber
+// - Where is the function called? line  5
+// - with what parameters? with 0 parametres
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,15 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. Concatenate two strings
+// - How many parameters does the function take? 2 parameters
+// - What is the function name? s
+// - Where is the function called?  line 5
+// - with what parameters? 2 parameters 'code' and 'your'
+// - What does the function return? The function return is codeyour
 // - How can this function be improved?
