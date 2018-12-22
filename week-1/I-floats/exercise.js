ef5173e6c8265a528151b3d45296e3e2a8070552
ahmet <ahmet.kio@hotmail.com>
2018-12-22 23:52:48 +0000

    I exercises

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percentagestudent = Math.round(numberOfStudents * 100 / (numberOfStudents + numberOfMentors));
+var percentagementors = Math.round(
+  (numberOfMentors * 100) / (numberOfStudents + numberOfMentors)
+);
+console.log("Percentage Students: " + percentagestudent+ " %");
+console.log("Percentage Mentors: " + percentagementors + " %");
