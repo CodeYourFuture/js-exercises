608b6cec190f838f4b790f8b72a3440e28dbda09
Mussie Tecle <filago14@gmail.com>
2018-12-22 00:24:51 +0000

    H-numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,13 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 26;
+var numberOfMentors = 14;
+
+var allTogether = numberOfStudents + numberOfMentors;
+
+var Students = "The Number of Students " + numberOfStudents;
+var Mentors = "The Number of Mentors " + numberOfMentors;
+var Sum = "The Total of Students and Mentors are " + allTogether;
+
+console.log(Students);
+console.log(Mentors);
+console.log(Sum);
