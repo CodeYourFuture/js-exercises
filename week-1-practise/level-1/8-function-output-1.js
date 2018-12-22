c1f727f2c2d3ce4e16fffffb493fd0fe9846a6bf
Mohbeen29 <mohbeen29@gmail.com>
2018-12-22 21:06:46 +0000

    completed week1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? round up results.
-// - How many parameters does the function take?1
+// - What does this program do? roduces random number.
+// - How many parameters does the function take? empty
 // - What is the function name? getNumber
-// - Where is the function called? with what parameters? in the result.
+// - Where is the function called? with what parameters? in the result. getNumber() -function no parameter.
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - How many parameters does the function take? 2
+// - What is the function name? s
+// - Where is the function called? with what parameters? line 5
+// - What does the function return? code your
+// - How can this function be improved? add + on line 2 (return)
