8a9ac671859ce0c3097e068468a16db666dceaae
Alisina92 <asb92.jobs@gmail.com>
2018-12-22 15:04:52 +0000

    Level-1 practise exercise answers

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? This program generate random Numbers starts from 0 < 10
+// - How many parameters does the function take? 0 parameters.
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters?  line 5 with zero parameters>
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. This program concatenate two strings using function concat.
+// - How many parameters does the function take? 2 parameters
+// - What is the function name? s
+// - Where is the function called? with what parameters? line5, 2 parameters 'code' and 'your'
+// - What does the function return? This function returns two string concatenation. codeyour
+// - How can this function be improved? Write descriptive function name like function string instead of s.
