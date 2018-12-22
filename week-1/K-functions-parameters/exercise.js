a8da932a01df2cf81db911739afb71b8699e28f0
zareen <evocativevict@gmail.com>
2018-12-22 20:12:48 +0000

    K-functions-parameters

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,9 +1,10 @@
 // Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
-}
+
+    function multiply(3, 4) {
+        // Calculate the result of the function and return it
+        return 3 * 4;
+    }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
-
+let function = result;
 console.log(result);
