1524b2f89c7957ff42de29a6d6befe03c3c7e620
Tayo Ayoola <tayoa@hotmail.com>
2018-12-22 09:44:34 +0000

    Tayo-week1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,5 +1,5 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
@@ -7,6 +7,6 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? generates a random number and multiply it by 10
-// - How many parameters does the function take?1
+// - How many parameters does the function take?0
 // - What is the function name? getNumber
 // - Where is the function called? with what parameters?var result=getNumber()
