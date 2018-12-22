09f7fc93d6ed52a20c8cc0bf719c2d821740bcd5
Sola Sobowale <sholasobowale@yahoo.co.uk>
2018-12-22 18:30:21 +0000

    Nested Functions

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -1,8 +1,28 @@
-function createPercentage(sts, mts) {
-var sumT = sts + mts
-var percSts = sts/sumT * 100;
-var percMts = mts/sumT * 100;
-    return percSts, percMts;
+var mentor1 = "Daniel";
+var mentor2 = "Irina";
+var mentor3 = "Mimi";
+var mentor4 = "Rob";
+var mentor5 = "Yohannes";
+
+
+// the percentage function//
+function createPercentages(sts,mts) {
+var percSts = (mts/(mts+sts))* 100;
+var percMts = (sts/(mts+sts))* 100;
+displayPercentages(percSts,percMts);
  }
- function greeting ()= createLongCreeting('Daniel', 30);
- console.log(greeting);
+
+function displayPercentages(percSts,percMts){
+console.log("Percentage of Students " + Math.round(percSts) + "%");
+console.log("Percentage of mentors " + Math.round(percMts) + "%");
+}
+
+function displayGreetings(name){ 
+    console.log("Hello " + name.toUpperCase() + "!");
+}
+createPercentages(8,15);
+displayGreetings(mentor1);
+displayGreetings(mentor2);
+displayGreetings(mentor3);
+displayGreetings(mentor4);
+displayGreetings(mentor5);
