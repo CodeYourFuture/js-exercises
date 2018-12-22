6be171e8d0ec597828d80e757bf34fae4d8f8842
ahmet <ahmet.kio@hotmail.com>
2018-12-22 23:22:36 +0000

    I added F exercise

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,18 +1,16 @@
 // Start by creating a variable `message`
 
-
 var message = 10;
 var messageType = typeof message;
 console.log(message);
 console.log(messageType);
 
-var message = "hello" ;
+var message = "hello: ";
 var messageType = typeof message;
 console.log(message);
 console.log(messageType);
 
-var message = true
+var message = true;
 var messageType = typeof message;
 console.log(message);
 console.log(messageType);
-
