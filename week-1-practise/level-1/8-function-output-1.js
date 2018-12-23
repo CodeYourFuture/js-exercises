8462820a08d44a01310ea1efc8598d96136e2172
zareen <evocativevict@gmail.com>
2018-12-23 19:36:42 +0000

    level-1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? picks a random number between 0 and 1 then multiplied by 10.
+// - How many parameters does the function take?none
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters?funtion called at line 5 with no specified parameters
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. concatenation of two strings
+// - How many parameters does the function take? two
+// - What is the function name? s
+// - Where is the function called? with what parameters? line 5 with parameters 'code'and 'your'
+// - What does the function return?codeyour
+// - How can this function be improved? more than 2 strings can be added
