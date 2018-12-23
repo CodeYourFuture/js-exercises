b8e318e22e843cb9c52388326155955ebf9edf68
Ismail <a.akbas.ismail@gmail.com>
2018-12-23 13:08:36 +0000

    K1_name and age

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,5 +1,7 @@
 // Declare your function here
-
-const greeting = createLongGreeting("Daniel", 30);
+function createLongGreeting (name, age) {
+return "Hello, my name is " + name + " and I'm " + age + " years old.";
+}
+const greeting = createLongGreeting("Ismail", 30);
 
 console.log(greeting);
