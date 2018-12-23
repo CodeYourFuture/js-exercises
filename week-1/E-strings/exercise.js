8d1332d30b50b509465c6d6428c53ee35c33f3a6
Kadir <kegilmez@gmail.com>
2018-12-23 15:50:57 +0000

    update array name

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variable `message`
-var array = [1, 2, 3, 4];
-var message = typeof array;
+var arrayExample = [1, 2, 3, 4];
+var message = typeof arrayExample;
 
 console.log(message);
