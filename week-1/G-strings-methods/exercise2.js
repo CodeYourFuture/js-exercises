84b6cd12084f060c06c190219758ff7a9b7face7
Ismail <a.akbas.ismail@gmail.com>
2018-12-23 12:14:46 +0000

    G2-String

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-var name = " Daniel  ";
-
+var name = "  Ismail  ";
+var nameshort = name.trim ();
+var nameLength = nameshort.length;
+var message = "My name is " + nameshort + " and my name is " + nameLength + " characters long"
 console.log(message);
