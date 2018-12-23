4f62269efb7706c66df2fc105758f5dd11937d2d
Ismail <a.akbas.ismail@gmail.com>
2018-12-23 12:23:35 +0000

    H-String

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,12 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+var totalNumberOfPeople = numberOfStudents + numberOfMentors;
+
+var message1 = "Number of students: " + numberOfStudents;
+var message2 = "Number of mentors: " + numberOfMentors;
+var message3 = "Total number of students and mentors: " + totalNumberOfPeople;
+
+console.log (message1);
+console.log (message2);
+console.log (message3);
