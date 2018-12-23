bb661a28676690967fe3b24b97b890bda2a57591
ermiii <45388574+ermiii@users.noreply.github.com>
2018-12-23 21:54:38 +0000

    Update exercise2.js

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,4 +1,6 @@
-var name = " Ermiyas!  ";
-var message= name + "Hello mates";
+var name = " Ermiyas,  ";
+var message= name.trim() + " is my name, a 32 years old Ethiopian national and passionate about computer programming.";
 
 console.log(message);
+
+
