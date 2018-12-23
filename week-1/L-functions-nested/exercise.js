6ec566c62371d466cdc9c71f0bce98b1b7cc8f2a
ahmet <ahmet.kio@hotmail.com>
2018-12-23 01:05:51 +0000

    L functions

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,18 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+
+function createGreeting(name) {
+    return name.toUpperCase();
+}
+var result = "HELLO " + createGreeting(mentor1);
+console.log(result);
+var result = "HELLO " + createGreeting(mentor2);
+console.log(result);
+var result = "HELLO " + createGreeting(mentor3);
+console.log(result);
+var result = "HELLO " + createGreeting(mentor4);
+console.log(result);
+var result = "HELLO " + createGreeting(mentor5);
+console.log(result);
