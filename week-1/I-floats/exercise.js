a9885b9d96920e0144e328d1c725aca70bc6c568
Ismail <a.akbas.ismail@gmail.com>
2018-12-23 12:35:05 +0000

    I_String

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,12 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var totalNumberOfPeople = numberOfStudents + numberOfMentors;
+var percentageOfStudents = Math.round(numberOfStudents * 100 / totalNumberOfPeople);
+var percentageOfMentors= Math.round(numberOfMentors * 100 / totalNumberOfPeople);
+
+var message1 = "Percentage students: " + percentageOfStudents + "%";
+var message2 = "Percentage mentors: " + percentageOfMentors + "%";
+
+console.log (message1);
+console.log (message2);
+
