e236a29fffd122b5169af3f9195974213e07d715
Ismail <a.akbas.ismail@gmail.com>
2018-12-23 12:42:19 +0000

    J1_String

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
   // complete the function here
+return number / 2;
 }
 
 var result = halve(12);
+var result2 = halve(24);
+var result3 = halve(120);
 
 console.log(result);
+console.log(result2);
+console.log(result3);
