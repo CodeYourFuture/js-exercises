cf5a6e8b52c4cf0fd7883eae048d87cec5993955
ermiii <45388574+ermiii@users.noreply.github.com>
2018-12-23 21:57:26 +0000

    Update exercise.js

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,6 +1,6 @@
 // Complete the function so that it takes input parameters
-function multiply(num1,num2) {
-  return num1*num2;}
+function multiply(num1,num2){
+ return num1*num2;}
  
  var result = 3*4;
  
