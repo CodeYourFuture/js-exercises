9507db0f78161a582bc53dcbf5d037b2b9ef78f1
Kadir <kegilmez@gmail.com>
2018-12-23 16:27:27 +0000

    8-function name corrected

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -4,9 +4,10 @@ function getNumber() {
 
 var result = getNumber();
 console.log(result);
+console.log(getNumber.name);
 
 // Answer these questions:
 // - What does this program do? first pick up a number between 0 and 1 randomly and after multiply it with 10.
 // - How many parameters does the function take? this function doesn`t need any parameters.
-// - What is the function name? function name is getNumber().
+// - What is the function name? function name is getNumber.
 // - Where is the function called? with what parameters? in line 5, this function is called without any parameters.
