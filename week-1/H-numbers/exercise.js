d09812199bc88e47a6d64c8562e384d22a755642
clement <clemohiomoje@gmail.com>
2018-12-24 10:23:13 +0000

    numbers minor correction

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
 var numberOfStudents = 15;
 var numberOfMentors = 8;
-var grandTotal = numberOfStudents + numberOfMentors;
+var studentsAndMentors = numberOfStudents + numberOfMentors;
 console.log(groundTotal);
