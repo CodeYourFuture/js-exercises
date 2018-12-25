b6201135c2abd2f0fbf4f21475314c0a519a6fe0
ahmet <ahmet.kio@hotmail.com>
2018-12-25 07:25:09 +0000

    Week 1-E-REPUSHING

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -4,12 +4,12 @@ var message = 10;
 var messageType = typeof message;
 console.log(message);
 console.log(messageType);
-
+console.log("");
 var message = "hello: ";
 var messageType = typeof message;
 console.log(message);
 console.log(messageType);
-
+console.log("");
 var message = true;
 var messageType = typeof message;
 console.log(message);
