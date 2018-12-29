2bcc6e5c7f742ee55df4a52f8c595b7a67813ffa
Bart Crowley <bmcrowley@hotmail.com>
2018-12-29 08:54:12 +0000

    updated strings concat exercise

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,5 +1,5 @@
 // Start by creating a variable `message`
-var message = "Hello my name is";
+var message = "Hello my name is ";
 var name = "Bart";
 var myName = message + name;
 console.log(myName);
