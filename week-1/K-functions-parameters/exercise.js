91aead18cceac62fa34c5f54eb0bc1fb190a6603
Bart Crowley <bmcrowley@hotmail.com>
2018-12-29 09:13:44 +0000

    updated paramters exercise

--- week-1/K-functions-parameters/exercise.js
+++ week-1/K-functions-parameters/exercise.js
@@ -1,9 +1,9 @@
 // Complete the function so that it takes input parameters
 function multiply(a, b) {
-  return 3 * 4;
+  return a * b;
 }
 
 // Assign the result of calling the function the variable `result`
-var result = multiply(3, 4);
+var result = multiply(12, 3);
 
 console.log(result);
