ff49f5b7a2014d0d84de930ed2742ff447274fa5
ahmet <ahmet.kio@hotmail.com>
2018-12-30 06:37:20 +0000

    Week 1-Level 1-REPUSHING

--- week-1-practise/level-1/10-function-output-3.js
+++ week-1-practise/level-1/10-function-output-3.js
@@ -1,6 +1,6 @@
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
-    return firstWord+secondWord+thirdWord;
+    return firstWord + secondWord + thirdWord;
 
 }
 
