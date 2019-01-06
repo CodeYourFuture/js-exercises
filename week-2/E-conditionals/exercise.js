54f21b85eef161118b7ee085c6d229dc59cbbe94
lexyking <sahiealexandre@hotmail.fr>
2019-01-06 15:50:13 +0000

    Joan Exercise E week2

--- week-2/E-conditionals/exercise.js
+++ week-2/E-conditionals/exercise.js
@@ -11,9 +11,10 @@ var role = "mentor";
 var name2 = "Harun";
 var role2 = "student";
 
-
-console.log("Hi, My name is " + name);
-console.log("Hi, My name is " + name2);
+if (role2 = "student") {
+  console.log("Hi, My name is " + name2);
+}
+//console.log("Hi, My name is " + name);
 
 
 
