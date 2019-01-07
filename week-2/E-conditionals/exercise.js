514e0af3a779d80ef0db68db7d523eaa622350f8
Ivina Pontes <ivinapontes@outlook.com>
2019-01-07 20:40:59 +0000

    exercises c to e done

--- week-2/E-conditionals/exercise.js
+++ week-2/E-conditionals/exercise.js
@@ -4,18 +4,24 @@
   Add an if statement to only show the name if the role is student
 */
 
-
 var name = "Daniel";
 var role = "mentor";
 
 var name2 = "Harun";
 var role2 = "student";
 
+if (role === "student") {
+  console.log("Hi, My name is " + name);
+} else {
+  console.log(name + " it's a mentor");
+}
 
-console.log("Hi, My name is " + name);
-console.log("Hi, My name is " + name2);
 
-
+if (role2 === "student") {
+  console.log("Hi, My name is " + name2);
+} else  {
+  console.log(name + " it's a mentor");
+}
 
 /* 
 EXPECTED RESULT
