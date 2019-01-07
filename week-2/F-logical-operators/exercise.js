76a2b18022abedcf109f43deaec3457ea940b4d4
clement <clemohiomoje@gmail.com>
2019-01-07 22:46:22 +0000

    Logical-operators exercise1

--- week-2/F-logical-operators/exercise.js
+++ week-2/F-logical-operators/exercise.js
@@ -13,13 +13,35 @@ var cssLevel = 4;
 // (hint: use the comparison operator from before)
 var htmlLevelAbove5;
 var cssLevelAbove5;
+if(htmlLevel > 5){
+  htmlLevelAbove5 = true;
+}
+else{
+htmlLevelAbove5 = false;
+}
+if(cssLevel >5){
+  cssLevelAbove5 = true;
+}
+else cssLevelAbove5 =false;
 
 // Finish the next two statement
 // Use the previous variables and logical operators
 // Do not "hardcode" the answers
 var cssAndHtmlAbove5;
 var cssOrHtmlAbove5;
+if (htmlLevelAbove5 && cssLevelAbove5){
+  cssAndHtmlAbove5 = true;
+}
+else{
+  cssAndHtmlAbove5 = false;
+}
 
+if (htmlLevelAbove5 || cssLevelAbove5) {
+  cssOrHtmlAbove5 = true;
+}
+else {
+  cssOrHtmlAbove5 = false;
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
