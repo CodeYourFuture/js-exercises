2cdbbd95695bb4fd2ab30c2d44f86e925f9c9b1d
Ivina Pontes <ivinapontes@outlook.com>
2019-01-07 20:14:38 +0000

    testing branch

--- week-2/B-boolean-literals/exercise.js
+++ week-2/B-boolean-literals/exercise.js
@@ -7,13 +7,19 @@
 
 var codeYourFutureIsGreat = true;
 
+var mozafarIsCool = false;
+
+var calculationCorrect = true;
+
+var moreThan10Students = false;
+
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 
 console.log("Is Code Your Future great?", codeYourFutureIsGreat);
 console.log("Is Mozafar cool?", mozafarIsCool);
-console.log("Does 1 + 1 = 2?", caluclationCorrect);
+console.log("Does 1 + 1 = 2?", calculationCorrect);
 console.log("Are there more than 10 students?", moreThan10Students);
 
 /* 
