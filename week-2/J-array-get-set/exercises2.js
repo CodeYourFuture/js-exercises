3793ace0678fb8cbf7351076ef0dba8d8d407454
clement <clemohiomoje@gmail.com>
2019-01-08 14:30:40 +0000

    Array-get-set exercise2

--- week-2/J-array-get-set/exercises2.js
+++ week-2/J-array-get-set/exercises2.js
@@ -7,7 +7,9 @@
 */
 
 var numbers = [2, 2, 3]; // Don't change this array literal declaration
-
+numbers.push(4);
+numbers.shift();
+numbers.unshift(1);
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
