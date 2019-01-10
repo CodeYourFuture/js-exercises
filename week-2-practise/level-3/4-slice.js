dd41c8389ab00740a9a07aebe0b4d63ae465332b
clement <clemohiomoje@gmail.com>
2019-01-10 12:13:52 +0000

    Slice level-3 practice

--- week-2-practise/level-3/4-slice.js
+++ week-2-practise/level-3/4-slice.js
@@ -5,17 +5,17 @@
 // - insert the replacement value at the provided index
 
 function replace(arr, index, value) {
-  return; // complete this statement
+  //arr.slice(index,value);
+  return  arr.slice(0, index).concat(value, arr.slice(index +1)); // complete this statement
 }
-
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 var numbers = [1, 3, 3];
 var names = ["Irina", "Ashleigh", "Mozafar"];
 
-var newNumbers = replace(arr, 1, 2);
-var newNames = replace(arr, 2, "Rares");
+var newNumbers = replace(numbers, 1, 2);
+var newNames = replace(names, 2, "Rares");
 
 console.log(numbers);
 console.log(newNumbers);
