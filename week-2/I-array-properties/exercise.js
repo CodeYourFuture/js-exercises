4e77c07acdcc945bc7774af39b9e5ebbc8b2a58d
zareen <evocativevict@gmail.com>
2019-01-12 23:04:28 +0000

    array properties correction

--- week-2/I-array-properties/exercise.js
+++ week-2/I-array-properties/exercise.js
@@ -6,11 +6,7 @@
 */
 
 function isEmpty(arr) {
-  if (arr.length == 0) {
-    return true; // complete this statement
-  } else {
-    return false;
-  }
+  return arr.length == 0;
 }
 /* 
   DO NOT EDIT BELOW THIS LINE
