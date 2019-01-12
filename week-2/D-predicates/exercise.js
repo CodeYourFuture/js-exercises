695cc6585f64908606485c9e62632f356112bef6
ahmed <ahmed.almadina@hotmail.com>
2019-01-12 13:16:09 +0000

    made code shorter

--- week-2/D-predicates/exercise.js
+++ week-2/D-predicates/exercise.js
@@ -7,20 +7,12 @@
 
 // Finish the predicate function to test if the passed number is negative (less than zero)
 function isNegative(number) {
-  if (number < 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return number < 0;
 }
 
 // Finish the predicate function to test if the passed number is between 0 and 10
 function isBetweenZeroAnd10(number) {
-  if (number <= 10 && number >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return number <= 10 && number >= 0;
 }
 
 /* 
--- week-2/F-logical-operators/exercise2.js
+++ week-2/F-logical-operators/exercise2.js
@@ -5,33 +5,21 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative(num) {
-  if (num < 0) {
-    return true;
-  } else {
-    return false;
-  }
+function isNegative(number) {
+  return number < 0;
 }
+
 function isBetween5and10(number) {
-  if (number > 5 && number <= 10) {
-    return true;
-  }
-}
-var name = "Daniel";
-
-function isLongName(name) {
-  if (name.lenght > 5);
-  {
-    return true;
-  }
-}
-var theName = "Daniel";
-function startsWithD(theName) {
-  if (theName.includes("D")) {
-    return true;
-  }
+  return number >= 5 && number <= 10;
 }
 
+function isLongName(string) {
+  return string.length > 5;
+}
+
+function startsWithD(string) {
+  return string[0] === "D";
+}
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
@@ -39,7 +27,7 @@ function startsWithD(theName) {
 console.log("Is -10 is a negative number?" + isNegative(-10));
 console.log("Is 5 a negative number?" + isNegative(5));
 console.log("Is 10 in the range 5-10?" + isBetween5and10(10));
-console.log("Is Daniel a short name?" + isLongName("Daniel"));
+console.log("Is Daniel a along name?" + isLongName("Daniel"));
 console.log("Does Daniel start with 'D'?" + startsWithD("Daniel"));
 
 /* 
