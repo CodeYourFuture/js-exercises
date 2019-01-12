7b6140a705e5c2af38a9e64df16b7bdf7d302e4f
Deepa Akhil <deepaakhiltvm@gmail.com>
2019-01-12 21:37:12 +0000

    level2 map

--- week-2-practise/level-2/6-map.js
+++ week-2-practise/level-2/6-map.js
@@ -2,8 +2,12 @@
 // 1. Write a function that formats the numbers into a string with the percentage symbol appended e.g. "10%"
 // 2. Declare a new array, `percentagesFormatted`, containing
 //    - each item in `percentages` formatted by your function
+function percent(number) {
+  return number + "%";
+}
 
 var percentages = [1, 23, 92, 18];
+var percentagesFormatted = percentages.map(percent);
 
 console.log(percentagesFormatted);
 
