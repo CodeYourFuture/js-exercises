032e4f1669a14804289898fa288634718f075116
Martyna Maron <mm@ctrl-group.com>
2019-01-12 23:08:39 +0000

    Reword the week-3/A-array-find exercise instructions

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -1,7 +1,6 @@
 /* 
   You are given an array of names.
-  We'd like to find names that start with an A and are longer than 7 letters.
-  Using .find(), find the first name which starts with A and is longer than 7 letter.
+  Using .find(), we'd like to find the first name which starts with A and is longer than 7 letters.
 */
 
 // write your code here
