c0399a50912293a70842917141226478ef3616f9
Mussie Tecle <filago14@gmail.com>
2019-01-13 08:09:44 +0000

    week-2-practise leve-2

--- week-2-practise/level-2/4-slice.js
+++ week-2-practise/level-2/4-slice.js
@@ -5,7 +5,7 @@
 // - remove the item at the specified index
 
 function remove(arr, index) {
-  return; // complete this statement
+  return arr.slice(0, index).concat(arr.slice(index + 1)); // complete this statement
 }
 
 /* 
