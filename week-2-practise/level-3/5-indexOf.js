414c6c372aaf5b01e05556ca56255c35a95f8c72
ahmet <ahmet.kio@hotmail.com>
2019-01-13 09:42:58 +0000

    Level-2 Exercises Review-1

--- week-2-practise/level-3/5-indexOf.js
+++ week-2-practise/level-3/5-indexOf.js
@@ -5,9 +5,19 @@
 // - NOT change the original array
 // - return a new array with the replacement value inserted
 
+
+
 function replace(arr, valueToReplace, newValue) {
-  return; // complete this statement
-}
+
+
+var index = arr.indexOf(valueToReplace);
+
+  firstPart = arr.slice(0, index); 
+ // console.log(firstPart);
+  secondPart = arr.slice(index +1, arr.length);
+  // console.log(secondPart);
+  return firstPart.concat(newValue).concat(secondPart);
+}  ; // complete this statement
 
 /* 
   DO NOT EDIT BELOW THIS LINE
@@ -15,8 +25,8 @@ function replace(arr, valueToReplace, newValue) {
 var numbers = [1, 3, 3];
 var names = ["Irina", "Ashleigh", "Mozafar"];
 
-var newNumbers = replace(arr, 3, 2);
-var newNames = replace(arr, "Ashleigh", "Rares");
+var newNumbers = replace(numbers, 3, 2);
+var newNames = replace(names, "Ashleigh", "Rares");
 
 console.log(numbers);
 console.log(newNumbers);
