98522b1d0853962d8f1791ebd66f84fcff8d345a
Ivina Pontes <ivinapontes@outlook.com>
2019-01-14 09:31:52 +0000

    First commit

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -1,13 +1,18 @@
-/* 
-  You are given an array of test scores.
-  Any result over 60 is a pass. 
-  Using .find() (and any other array methods necessary), find the lowest pass score
+/*
+  You are given an array of names.
+  Using .find(), we'd like to find the first name which starts with A and is longer than 7 letters.
 */
 
-var testScores = [90, 50, 100, 25, 81, 66, 80];
-var lowestPassScore = findLowestPassScore(testScores);
+// write your code here
 
-console.log(lowestPassScore);
+var names = ["Rakeshss", "Alexandra", "Annam", "Mikey", "Karim", "Ahmed"];
+var longNameThatStartsWithA = names.find(findLongNameThatStartsWithA);
+
+function findLongNameThatStartsWithA(names) {
+  return names.length >= 7 && names[0] === "A";
+}
+
+console.log(longNameThatStartsWithA);
 
 /* EXPECTED OUTPUT */
-// 66
+// "Alexandra"
