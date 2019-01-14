a3a75518f6647471105ba1356e016af2a744ffd5
Ivina Pontes <ivinapontes@outlook.com>
2019-01-14 09:11:54 +0000

    Finishing week 3

--- week-2-practise/level-3/6-map.js
+++ week-2-practise/level-3/6-map.js
@@ -1,7 +1,7 @@
-// 1. Write a function (`captialise`) that capitlises the first letter of a provided string
+// 1. Write a function (`capitalise`) that capitlises the first letter of a provided string
 // 2. Declare a new array (`mentorsTidy`) containing:
 //    - every item from `mentors` run through the `tidyUpString` function
-//    - every resulting item run through the `captialise` function
+//    - every resulting item run through the `capitalise` function
 
 function tidyUpString(str) {
   return str
@@ -10,12 +10,15 @@ function tidyUpString(str) {
     .replace("/", "");
 }
 
-function captialise(str) {
-  // complete this function
+function capitalise(str) {
+  str = str.charAt(0).toUpperCase() + str.substr(1);
+  return str;
 }
 
 var mentors = ["/Daniel ", "irina ", " Gordon", "ashleigh "];
 var mentorsTidyAndCapitalised;
+var mentorsTidy = mentors.map(tidyUpString);
+var mentorsTidyAndCapitalised = mentorsTidy.map(capitalise);
 
 console.log(mentorsTidyAndCapitalised);
 
