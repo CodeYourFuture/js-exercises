afa9ab01da9ba7596d69043bd50bdd6754fe4db4
mtbdata711 <bengethomasmatthew@gmail.com>
2019-01-14 21:53:31 +0000

    Edit commitfind exercise

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -1,4 +1,21 @@
 /* 
+  You are given an array of names.
+  Using .find(), we'd like to find the first name which starts with A and is longer than 7 letters.
+*/
+
+// write your code here
+
+var names = ["Rakesh", "Alexandra", "Annam", "Mikey", "Karim", "Ahmed"];
+var longNameThatStartsWithA = names.find(function(name) {
+  return name[0].includes("A") && name.length > 7;
+});
+
+console.log(longNameThatStartsWithA);
+
+/* EXPECTED OUTPUT */
+// "Alexandra"
+
+/* 
   You are given an array of test scores.
   Any result over 60 is a pass. 
   Using .find() (and any other array methods necessary), find the lowest pass score
