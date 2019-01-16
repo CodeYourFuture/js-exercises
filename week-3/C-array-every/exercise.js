c8ca80baf63a100838127090dedf0490e39299a4
Bart Crowley <bmcrowley@hotmail.com>
2019-01-16 21:12:11 +0000

    Week 3 exercises complete

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -5,7 +5,9 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = students.every(student => {
+  return group.includes(student) !== true;
+});
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
--- week-3/D-array-filter/exercise.js
+++ week-3/D-array-filter/exercise.js
@@ -8,7 +8,9 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(pair => {
+  return pair !== null && pair.length === 2;
+});
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
