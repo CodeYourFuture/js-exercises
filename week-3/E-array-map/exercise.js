af87e6954005b9b1e08f9d8062d246907ef4cd28
Tayo Ayoola <tayoa@hotmail.com>
2019-01-16 20:17:10 +0000

    E-array-map/exercise

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,4 +2,6 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+var numbersByHundred = numbers.map(number => number * 100);
 
+console.log(numbersByHundred);
