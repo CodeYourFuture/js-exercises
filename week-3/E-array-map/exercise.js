20c806b2389e71c08fac29ad229448cf1d5b3767
Kadir <kegilmez@gmail.com>
2019-01-16 19:32:27 +0000

    Kadir-week3

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,4 +2,24 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+/*
+var numbersMultipliedBy100 = numbers.map(multiplyBy100);
+function multiplyBy100(number) {
+  return number * 100;
+}
 
+var numbersMultipliedBy100 = numbers.map(function multiplyBy100(number) {
+  return number * 100;
+});
+
+var numbersMultipliedBy100 = numbers.map(function(number) {
+  return number * 100;
+});
+
+var numbersMultipliedBy100 = numbers.map(number => {
+  return number * 100;
+});*/
+
+var numbersMultipliedBy100 = numbers.map(number => number * 100);
+
+console.log(numbersMultipliedBy100);
