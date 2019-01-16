986040ec823a0ee942155bcf48f2cd299f0b27a7
Sola Sobowale <sholasobowale@yahoo.co.uk>
2019-01-16 00:08:49 +0000

    more-week3-work

--- week-3/B-array-some/exercise.js
+++ week-3/B-array-some/exercise.js
@@ -6,7 +6,7 @@
   - Do not edit any of the existing code
 */
 
-var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
+var pairsByIndex = [[0, 3], [1, 2], [2, 1],[3, 0]];
 
 // If there is a null value in the array exit the program with the error code
 // https://nodejs.org/api/process.html#process_process_exit_code
