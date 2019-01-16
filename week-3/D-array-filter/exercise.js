1dc5ca1687bb567b02902cd2281ed417e4aad617
ahmed <ahmed.almadina@hotmail.com>
2019-01-16 08:22:50 +0000

    week3-exercises

--- week-3/D-array-filter/exercise.js
+++ week-3/D-array-filter/exercise.js
@@ -8,8 +8,15 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var filterarr = [];
 
+pairsByIndexRaw.filter(function(item) {
+  if (item !== null && item !== false && item !== "whoops" && item.length > 1) {
+    filterarr.push(item);
+  }
+});
+
+var pairsByIndex = filterarr; // Complete this statement
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,4 +2,37 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+function multipledBy100(numbers) {
+  return numbers * 100;
+}
+//solution 1
 
+var multipliedNumbers = numbers.map(multipledBy100);
+
+console.log(multipliedNumbers);
+
+//solution 2
+
+var multipliedNumbers = numbers.map(function multipledBy100(numbers) {
+  return numbers * 100;
+});
+console.log(multipliedNumbers);
+
+//solution 3
+
+var multipliedNumbers = numbers.map(function(numbers) {
+  return numbers * 100;
+});
+console.log(multipliedNumbers);
+
+//solution 4
+
+var multipliedNumbers = numbers.map(numbers => {
+  return numbers * 100;
+});
+console.log(multipliedNumbers);
+
+//solution 5
+
+var multipliedNumbers = numbers.map(numbers => numbers * 100);
+console.log(multipliedNumbers);
