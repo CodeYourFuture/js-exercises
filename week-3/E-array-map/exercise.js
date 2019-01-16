79bf67f816e26cfb1f65c3c33afc48179397b895
clement <clemohiomoje@gmail.com>
2019-01-16 13:51:41 +0000

    Array-map exercise

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -3,3 +3,14 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+// var number100 =numbers.map(double);
+
+// function double(numbers) {
+//     return numbers * 100;
+// }
+// console.log(number100);
+
+var number100 = numbers.map(function (num) {
+    return num * 100;
+});
+console.log(number100);
