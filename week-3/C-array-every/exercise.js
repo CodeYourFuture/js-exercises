8500f3e1513713ef4098405046127e8c58f0c2b6
clement <clemohiomoje@gmail.com>
2019-01-16 13:50:44 +0000

    Array-every exercise

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -5,7 +5,11 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+function studentsGroup(element) {
+  return students.includes(element);
+}
+
+var groupIsOnlyStudents = group.every(studentsGroup); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
