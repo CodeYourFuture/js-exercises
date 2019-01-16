de2494c2d9fccdf3219ecfb8bc08b3884e88f619
Tayo Ayoola <tayoa@hotmail.com>
2019-01-16 20:15:25 +0000

    D-array-filter/exercise

--- week-3/D-array-filter/exercise.js
+++ week-3/D-array-filter/exercise.js
@@ -8,7 +8,17 @@
 
 var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
+var pairsByIndex = pairsByIndexRaw.filter(function(indexes) {
+  // Complete this statement
+  if (
+    indexes !== null &&
+    indexes.length == 2 &&
+    indexes !== "Boolean" &&
+    indexes !== "string"
+  ) {
+    return indexes;
+  }
+});
 
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
