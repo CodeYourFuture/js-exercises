7171e3548486ba748d44b07d0af2a7e7afc9a78d
clement <clemohiomoje@gmail.com>
2019-01-16 13:52:24 +0000

    Array-forEach exercise

--- week-3/F-array-forEach/exercise.js
+++ week-3/F-array-forEach/exercise.js
@@ -9,6 +9,23 @@
 
 var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+arr.forEach(function (nums) {
+  if (nums === 0) {
+    console.log(nums);
+  }
+  else if (nums % 3 === 0 && nums % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  else if (nums % 5 === 0) {
+    console.log("Buzz");
+  }
+  else if (nums % 3 === 0) {
+    console.log("Fizz");
+  }
+  else {
+    console.log(nums);
+  }
+});
 /* EXPECTED OUTPUT */
 
 /*
