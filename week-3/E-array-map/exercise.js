f3847c5820e335af76d26f2277a5a09b37b775f8
clement <clemohiomoje@gmail.com>
2019-01-16 10:37:43 +0000

    Array-map exercise

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,4 +2,14 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+// var number100 =numbers.map(double);
 
+// function double(numbers) {
+//     return numbers * 100;
+// }
+// console.log(number100);
+
+var number100 =numbers.map(function(num){
+    return num *100;
+});
+console.log(number100);
