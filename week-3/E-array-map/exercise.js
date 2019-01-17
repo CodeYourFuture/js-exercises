aba3b80e05eb279283b1b5368f0b2cb488f9cbd6
Sola Sobowale <sholasobowale@yahoo.co.uk>
2019-01-17 01:43:55 +0000

    Wk3-completed

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -3,3 +3,30 @@
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function timesHundred(number) {
+  return number * 100;
+}
+
+var numbersTimesHundred1 = numbers.map(timesHundred);
+
+var numbersTimesHundred2 = numbers.map(function timesHundred2(number) {
+  return number * 100;
+});
+
+var numbersTimesHundred3 = numbers.map(function(number) {
+  return number * 100;
+});
+
+var numbersTimesHundred4 = numbers.map(number => {
+  return number * 100;
+});
+
+var numbersTimesHundred5 = numbers.map(number => number * 100);
+
+console.log(
+  numbersTimesHundred1,
+  numbersTimesHundred2,
+  numbersTimesHundred3,
+  numbersTimesHundred4,
+  numbersTimesHundred5
+);
