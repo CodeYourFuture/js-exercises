99c4ce010780582d2ba62ee946756c4b93c140ca
Ivina Pontes <ivinapontes@outlook.com>
2019-01-17 19:55:54 +0000

    fixed exercise

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,9 +2,22 @@
 // Write multiple solutions using different syntax (as shown in the README)
 
 var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+/*Case 1*/
 
 var multiplyNumbers = numbers.map(number => {
   return number * 100;
 });
 
 console.log(multiplyNumbers);
+
+/*Case 2*/
+
+var multiplyNumbers = numbers.map(function(number) {
+  return number * 100;
+});
+
+/*Case 3 */
+
+var multiplyNumbers = numbers.map(function multiplyNumbers(number) {
+  return number * 100;
+});
