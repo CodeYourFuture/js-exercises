3e4b2f40e95b3b5e6c66581ed1473e4059794ff0
Sola Sobowale <sholasobowale@yahoo.co.uk>
2019-01-17 00:38:07 +0000

    Wednesdays-work

--- week-3/C-array-every/exercise.js
+++ week-3/C-array-every/exercise.js
@@ -5,7 +5,7 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+var groupIsOnlyStudents = group.every(pupil => students.includes(pupil)); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
