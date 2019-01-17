750aac9031fb56c1806924b63e1e538187eeb901
ahmet <ahmet.kio@hotmail.com>
2019-01-17 01:20:45 +0000

    Ahmet-JS-Week3-Exercises Review-1

--- week-3/B-array-some/exercise.js
+++ week-3/B-array-some/exercise.js
@@ -15,10 +15,21 @@ var pairsByIndex = [[0, 3], [1, 2], [2, 1], null, [3, 0]];
 var students = ["Islam", "Lesley", "Harun", "Rukmini"];
 var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
+var isvalueNull = pairsByIndex.some(function(pair) {
+  return pair === null;
+});
+
+if (isvalueNull) {
+  process.exit(1);
+}
+
+
 var pairs = pairsByIndex.map(function(indexes) {
-  var student = students[indexes[0]];
-  var mentor = mentors[indexes[1]];
+    var student = students[indexes[0]];
+    var mentor = mentors[indexes[1]];
   return [student, mentor];
 });
+
+
 
 console.log(pairs);
