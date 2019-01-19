36abb5056e9c9794f33e37aff6d2a66e23749889
Sola Sobowale <sholasobowale@yahoo.co.uk>
2019-01-19 14:29:34 +0000

    End-of-wk3

--- week-3-practise/exercise-3.js
+++ week-3-practise/exercise-3.js
@@ -2,12 +2,18 @@
 they are written together. Create a new function in order to get a 
 new array with the joined words of each items in the array*/
 
-/*var newWords = [
+var newWords = [
   { firstword: "Ata", lastword: "turk" },
   { firstword: "Galata", lastword: "saray" },
   { firstword: "Eski", lastword: "sehir" }
-]; */
+];
+var fullWord = [];
+newWords.forEach(word => {
+  word.firstword.concat(word.lastword);
 
+  fullWord.push(word.firstword.concat(word.lastword));
+});
+console.log(fullWord.join());
 /* EXPECTED RESULT
  -----------
 Ataturk, Galatasaray, Eskisehir
