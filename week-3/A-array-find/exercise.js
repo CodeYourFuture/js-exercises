0e53ad17ff67aef808f9576a6261fff20d769e19
ermiii <45388574+ermiii@users.noreply.github.com>
2019-01-20 22:29:39 +0000

    Update exercise.js

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -5,9 +5,9 @@
 
 // write your code here
 function firstLongNameStartWithA(name) {
-  return name[0] === "A" && name.length > 7;
+  return name[0] === "A" || "a" && name.length > 7;
 }
-var names = ["Rakesh", "Alexandra", "Annam", "Mikey", "Karim", "Ahmed"];
+var names = ["Rakesh", "alexandra", "Annam", "Mikey", "Karim", "Ahmed"];
 var longNameThatStartsWithA = names.find(firstLongNameStartWithA);
 
 console.log(longNameThatStartsWithA);
