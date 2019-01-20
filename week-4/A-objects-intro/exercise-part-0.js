d879202629402a5eb050f0b5d5a860a3a07c49c5
zareen <evocativevict@gmail.com>
2019-01-20 21:10:18 +0000

    A-object

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+  type: "dell",
+  hardDisk: "128gb",
+  processor: "cori5",
+  battery: "dell",
+  windows: "7"
+};
+console.log(laptop);
