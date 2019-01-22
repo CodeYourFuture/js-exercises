25c44900999946cf9b52158cd3532fe89e47a305
clement <clemohiomoje@gmail.com>
2019-01-22 17:26:02 +0000

    Object-intro exercise-part-0

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+const myLaptop = { Model: "Mac-Air",
+processor: "2.5GHz intel core i5", 
+memory: "8GB 1666 MHz DDR3",
+disk: "Macintosh-HD",
+graphics:"HD-40003-MB",
+serialNumber: "C02GKSEEDJWT"}
+// Note !!! console.log, just for console display
+console.log(myLaptop);
+return myLaptop;
