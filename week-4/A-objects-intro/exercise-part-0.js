856ca9e64613d019be8d1f436b7faa8076f9d67c
Bart Crowley <bmcrowley@hotmail.com>
2019-01-22 20:53:58 +0000

    Week 4 A-F complete

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+const laptop = {
+  brand: "Dell",
+  screen: "15.4",
+  colour: "Black",
+  processor: "I5",
+  ram: "8gb"
+};
+console.log(laptop);
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,41 @@ Assign each of them to a separate variable
 
 */
 
+const car = {
+  brand: "Honda",
+  model: "Civic",
+  colour: "Silver",
+  engine: 1.8
+};
+
+const tv = {
+  brand: "Samsung",
+  screensize: 40,
+  resolution: "UHD"
+};
+
+const smartphone = {
+  brand: "Huawei",
+  model: "Mate 10 Pro",
+  processor: "Kirin",
+  screensize: 6
+};
+
+const sweatshirt = {
+  brand: "Next",
+  material: "Cotton",
+  size: "Large"
+};
+
+const drink = {
+  brand: "Coca Cola",
+  flavour: "Vanilla",
+  amountInMl: 330,
+  isSugarFree: true
+};
+
+console.log(car);
+console.log(tv);
+console.log(smartphone);
+console.log(sweatshirt);
+console.log(drink);
