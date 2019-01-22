f226a7716331d32b9db609afec34045532f8af69
ahmed <ahmed.almadina@hotmail.com>
2019-01-22 21:55:09 +0000

    week-4-practise

--- week-4-practise/level-1/exercise-1.js
+++ week-4-practise/level-1/exercise-1.js
@@ -38,7 +38,22 @@ var writers = [
     alive: true
   },
 ];
+for (i = 0; i < writers.length; i++) {
+  console.log(
+    `Hi, my name is ${writers[i].firstName} ${writers[i].lastName}.I am ${
+      writers[i].age
+    } years old, and work as a ${writers[i].occupation}.`
+  );
+}
 
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+for (let i = 0; i < writers.length; i++)
+  if (writers[i].alive === true) {
+    console.log(
+      `Hi, my name is ${writers[i].firstName} ${writers[i].lastName}. I am ${
+        writers[i].age
+      } years old, and work as a ${writers[i].occupation}.I am alive.`
+    );
+  }
