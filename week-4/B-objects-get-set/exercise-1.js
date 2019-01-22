5b4112a9c3788f4a6d45d1091c4dff341204d913
clement <clemohiomoje@gmail.com>
2019-01-22 17:28:47 +0000

    Objects-get-set exercise1

--- week-4/B-objects-get-set/exercise-1.js
+++ week-4/B-objects-get-set/exercise-1.js
@@ -5,11 +5,17 @@
 var kitten = {
     ageMonths: 3,
     isFemale: true,
-    furColour: "brown"
+    furColour: "brown",
 };
 
 // YOUR CODE GOES BELOW HERE
 
+// Not quite sure if i should print the property name or just the value so i did both
+console.log(kitten);
+
+console.log(kitten.ageMonths);
+console.log(kitten.isFemale);
+console.log(kitten.furColour);
 
 
 
