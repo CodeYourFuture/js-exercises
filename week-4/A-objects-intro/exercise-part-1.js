675102840906ebf05e72d663b759f05a2764bb2d
zareen <evocativevict@gmail.com>
2019-01-23 23:46:05 +0000

    week-4

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,4 +6,28 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
-
+var refrigerator = {
+  model: "LG",
+  colour: "white",
+  voltage: 220
+};
+var cellphone = {
+  model: "iphone",
+  colour: "black",
+  screenSize: 7
+};
+var car = {
+  model: "polo",
+  colour: "blue",
+  mileage: 30000
+};
+var microwave = {
+  model: "braun",
+  colour: "white",
+  voltage: 220
+};
+var shoes = {
+  company: "clarks",
+  size: 11,
+  colour: "grey"
+};
--- week-4/D-methods/exercise-4.js
+++ week-4/D-methods/exercise-4.js
@@ -3,12 +3,11 @@ Alice has a list of good friends.
 Define a method "makeFriend" to add a new friend to her list.
 */
 
-
 var person = {
-    name: "Alice",
-    friends: ["John", "Nina"]
+  name: "Alice",
+  friends: ["John", "Nina"],
+  makeFriend: name => person.friends.push(name)
 };
-
 
 /*
 DO NOT EDIT ANYTHING BELOW THIS LINE
@@ -16,4 +15,6 @@ DO NOT EDIT ANYTHING BELOW THIS LINE
 
 person.makeFriend("Bob");
 
-console.log("Expected result: 'John,Nina,Bob'. Actual result: " + person.friends);
+console.log(
+  "Expected result: 'John,Nina,Bob'. Actual result: " + person.friends
+);
