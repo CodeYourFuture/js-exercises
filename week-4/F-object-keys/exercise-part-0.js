612e8cd23366be92a432122fee1ae5d29022e0b3
zareen <evocativevict@gmail.com>
2019-01-23 23:34:55 +0000

    zareen-js-week-4-exercise

--- week-4/F-object-keys/exercise-part-0.js
+++ week-4/F-object-keys/exercise-part-0.js
@@ -5,28 +5,27 @@ Return the keys of the following object
 */
 
 var capitalCities = {
-  scotland: 'Edinburgh',
-  kenya: 'Nairobi',
-  australia: 'Canberra',
-  canada: 'Ottawa'
+  scotland: "Edinburgh",
+  kenya: "Nairobi",
+  australia: "Canberra",
+  canada: "Ottawa"
 };
 
 var highScores = {
-  55: 'Alistair',
-  100: 'David',
-  89: 'Hannah',
-  34: ['Sergi', 'Frank',]
+  55: "Alistair",
+  100: "David",
+  89: "Hannah",
+  34: ["Sergi", "Frank"]
 };
 
 // ONLY EDIT BELOW HERE
 
-var capitalCitiesKeys = ;
-var highScoresKeys;
-
+var capitalCitiesKeys = Object.keys(capitalCities);
+var highScoresKeys = Object.keys(highScores);
 // ONLY EDIT ABOVE HERE
 
 console.log(capitalCitiesKeys);
 // prints [ 'scotland', 'kenya', 'australia', 'canada' ]
 
-console.log(highScoresKeys)
+console.log(highScoresKeys);
 // prints ['34, '55', '89', '100']
