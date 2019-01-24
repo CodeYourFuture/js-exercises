73bd50886b59a295361d2e4183536b7e3e6d26c7
zareen <evocativevict@gmail.com>
2019-01-24 22:22:17 +0000

    A-objects-intro

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,7 +5,7 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
-var laptop = {
+var Laptop = {
   brand: "Dell",
   screenSize: 13,
   isTouchscreen: false,
