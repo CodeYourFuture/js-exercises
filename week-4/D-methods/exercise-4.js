c07a922e354a7d33db1f97224b628766341cda38
Deepa Akhil <deepaakhiltvm@gmail.com>
2019-01-26 01:01:01 +0000

    D-methodsExercise-4

--- week-4/D-methods/exercise-4.js
+++ week-4/D-methods/exercise-4.js
@@ -3,17 +3,20 @@ Alice has a list of good friends.
 Define a method "makeFriend" to add a new friend to her list.
 */
 
-
 var person = {
-    name: "Alice",
-    friends: ["John", "Nina"]
+  name: "Alice",
+  friends: ["John", "Nina"],
+  makeFriend: function(name) {
+    this.friends.push(name);
+  }
 };
 
-
-/*
+/*:
 DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
 person.makeFriend("Bob");
 
-console.log("Expected result: 'John,Nina,Bob'. Actual result: " + person.friends);
+console.log(
+  "Expected result: 'John,Nina,Bob'. Actual result: " + person.friends
+);
