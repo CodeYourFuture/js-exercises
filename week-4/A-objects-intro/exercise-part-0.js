cb6c6e48164a9486fc454143878cdf39835de1ee
Deepa Akhil <deepaakhiltvm@gmail.com>
2019-01-26 00:49:36 +0000

    A-Objects-Exercise0

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,12 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+const laptop = {
+  brand: "Hp",
+  screenSize: 13.3,
+  Graphics: "intel HD graphics",
+  Ram: "8GB",
+  cost: "£599",
+  colour: "silver"
+};
+console.log(laptop);
