428f77830071071755caaa14d9c195da7f5820f2
Sola Sobowale <sholasobowale@yahoo.co.uk>
2019-01-26 17:05:33 +0000

    Sola-F-object-keys

--- week-4/F-object-keys/exercise-part-1.js
+++ week-4/F-object-keys/exercise-part-1.js
@@ -15,9 +15,9 @@ var mentorsAges = {
 
 // ONLY EDIT BELOW THIS LINE
 
-var mentorsNames = ;
+var mentorsNames = Object.keys(mentorsAges);
 
-var mentorsNamedCapitalised = ;
+var mentorsNamedCapitalised = mentorsNames.map(a => a.toUpperCase());
 
 // ONLY EDIT ABOVE THIS LINE
 
