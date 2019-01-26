a91d2ba4da8428216ef5d99a12dbaa168532ba42
Deepa Akhil <deepaakhiltvm@gmail.com>
2019-01-26 00:50:33 +0000

    A-Objects-Exercise1

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,32 @@ Assign each of them to a separate variable
 
 */
 
+var book = {
+  name: 1984,
+  author: "george Orwell",
+  numberOfPage: 1234,
+  Language: "English"
+};
+var car = {
+  name: "bmwX5",
+  EngineSize: 2.0,
+  cost: "Rs.5500000"
+};
+var pen = {
+  name: "fountain",
+  color: "black",
+  ink: "blue"
+};
+var fruit = {
+  name: "banana",
+  color: "yellow",
+  condition: "ripe",
+  cost: "£1"
+};
+
+var cake = {
+  name: "strawberry cake",
+  cost: "£10",
+  isBirthdayCake: true
+};
+console.log(book, car, pen, fruit, cake);
