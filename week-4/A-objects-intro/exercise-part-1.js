c6162a628f981b46e73a2b49afd45f3ca2b54dc2
ahmet <ahmet.kio@hotmail.com>
2019-01-27 16:07:08 +0000

    Ahmet-JS-Week5

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,32 +6,3 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
-
-var car = {
-    brand: "Mercedes",
-    model: "CLK",
-    seat: 5
-};
-
-var shoppinglist = {
-  fruits: ["apple","orange"],
-  vegetables: ["leek","onion"]
-};
-
-var shoe = {
-  brand: "adidas",
-  soldBy: "Tkmax",
-  size: 25
-};
-
-var table = {
-    brand: "noname",
-    soldBy: "Well Furniture",
-    sizeMetre: 2
-};
-
-var mug = {
-  colour: "white",
-  madeBy: "Glass",
-  size: "big"
-};
