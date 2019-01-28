8c39e3774a8ab7fbac9fb09b45f38bc3d01876b7
Ivina Pontes <ivinapontes@outlook.com>
2019-01-28 10:41:03 +0000

    practise exercise done

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,14 +1,27 @@
-var myButton = document.querySelector("#blueBtn");
+//Defining buttons
+var blueBtn = document.querySelector("#blueBtn");
+var orangeBtn = document.querySelector("#orangeBtn");
+var greenBtn = document.querySelector("#greenBtn");
 var jumbotronBackground = document.querySelector(".jumbotron");
 var donateBtn = document.querySelector("#donateBtn");
 var volunteerBtn = document.querySelector("#volunteerBtn");
 //Check append child
 
-myButton.addEventListener("click", changeColor);
-
-function changeColor() {
+blueBtn.addEventListener("click", function() {
   jumbotronBackground.style.backgroundColor = "#588fbd";
   donateBtn.style.backgroundColor = "#ffa500";
   volunteerBtn.style.backgroundColor = "black";
   volunteerBtn.style.color = "white";
-}
+});
+
+orangeBtn.addEventListener("click", function() {
+  jumbotronBackground.style.backgroundColor = "#f0ad4e";
+  donateBtn.style.backgroundColor = "#5751fd";
+  volunteerBtn.style.backgroundColor = "#31b0d5";
+  volunteerBtn.style.color = "white";
+});
+greenBtn.addEventListener("click", function() {
+  jumbotronBackground.style.backgroundColor = "#87ca8a";
+  donateBtn.style.backgroundColor = "#5751fd";
+  volunteerBtn.style.backgroundColor = " black ";
+});
