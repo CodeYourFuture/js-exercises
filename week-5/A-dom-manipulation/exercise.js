d71b2425306799997ec3e4205a7902c435bfc0a4
Kadir <kegilmez@gmail.com>
2019-01-29 21:29:01 +0000

    week5A-updated

--- week-5/A-dom-manipulation/exercise.js
+++ week-5/A-dom-manipulation/exercise.js
@@ -4,8 +4,9 @@ Task 1
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-var changeButton = document.querySelector("#bgrChangeBtn");
-changeButton.addEventListener("click", changeBackgroundColor);
+document
+  .querySelector("#bgrChangeBtn")
+  .addEventListener("click", changeBackgroundColor);
 
 function changeBackgroundColor() {
   document.body.style.backgroundColor = "green";
@@ -17,8 +18,7 @@ Task 2
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
-var alertButton = document.querySelector("#alertBtn");
-alertButton.addEventListener("click", alertText);
+document.querySelector("#alertBtn").addEventListener("click", alertText);
 
 function alertText() {
   alert("Thanks for visiting Bikes for Refugees!");
@@ -30,12 +30,12 @@ Task 3
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-var textButton = document.querySelector("#addTextBtn");
 var paragraph = document.createElement("p");
 paragraph.innerText = "Read more below.";
 paragraph.style.fontSize = "1.5em";
 var addTextArea = document.querySelector(".jumbotron");
-textButton.addEventListener("click", addText);
+document.querySelector("#addTextBtn").addEventListener("click", addText);
+
 function addText() {
   addTextArea.appendChild(paragraph);
 }
@@ -46,8 +46,9 @@ Task 4
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
-var largeButton = document.querySelector("#largerLinksBtn");
-largeButton.addEventListener("click", increaseFontSize);
+document
+  .querySelector("#largerLinksBtn")
+  .addEventListener("click", increaseFontSize);
 var buttons = document.querySelectorAll("a");
 function increaseFontSize() {
   for (var button of buttons) {
