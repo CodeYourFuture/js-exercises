b9a23bd5888c3766c239b501f100395f4b790d39
Kadir <kegilmez@gmail.com>
2019-01-30 16:44:11 +0000

    week5-Practise-modified

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,36 +1,94 @@
 //  Part-1
 
 var jumbotronDiv = document.querySelector(".jumbotron");
-var buttons = document.querySelector(".buttons");
-var donateBikeButton = buttons.querySelector(".btn-primary");
-var volunteerButton = buttons.querySelector(".btn-secondary");
+var donateBikeBtn = document.querySelector(".buttons").firstElementChild;
+var volunteerBtn = document.querySelector(".buttons").lastElementChild;
 var colorButtons = document.querySelectorAll(".colorButton");
 
-//clicking blue button events
-colorButtons[0].addEventListener("click", blueBtnEvent);
-function blueBtnEvent() {
-  jumbotronDiv.style.backgroundColor = `#588fbd`;
-  donateBikeButton.style.backgroundColor = `#ffa500`;
-  volunteerButton.style.backgroundColor = "black";
-  volunteerButton.style.color = "white";
+//function handling the events when click buttons
+function colorButtonEvent(
+  jumboBackGrColor,
+  donateBikeBtnBackGrColor,
+  volunteerBtnBackGrColor,
+  volunteerBtnColor
+) {
+  jumbotronDiv.style.backgroundColor = jumboBackGrColor;
+  donateBikeBtn.style.backgroundColor = donateBikeBtnBackGrColor;
+  volunteerBtn.style.backgroundColor = volunteerBtnBackGrColor;
+  volunteerBtn.style.color = volunteerBtnColor;
 }
+
+//object creator `factory function` defining the changes for each color event
+function ButtonCreator(
+  button,
+  jumboBackGrColor,
+  donateBikeBtnBackGrColor,
+  volunteerBtnBackGrColor,
+  volunteerBtnColor
+) {
+  this.button = button;
+  this.jumboBackGrColor = jumboBackGrColor;
+  this.donateBikeBtnBackGrColor = donateBikeBtnBackGrColor;
+  this.volunteerBtnBackGrColor = volunteerBtnBackGrColor;
+  this.volunteerBtnColor = volunteerBtnColor;
+  this.colorEventStarter = function() {
+    return colorButtonEvent(
+      jumboBackGrColor,
+      donateBikeBtnBackGrColor,
+      volunteerBtnBackGrColor,
+      volunteerBtnColor
+    );
+  };
+}
+//instantiation of factory function
+var blueBtn = new ButtonCreator(
+  colorButtons[0],
+  `#588fbd`,
+  `#ffa500`,
+  "black",
+  "white"
+);
+var orangeBtn = new ButtonCreator(
+  colorButtons[1],
+  `#f0ad4e`,
+  `#5751fd`,
+  "#31b0d5",
+  "white"
+);
+var greenBtn = new ButtonCreator(
+  colorButtons[2],
+  `#87ca8a`,
+  `black`,
+  "#8c9c08",
+  "white"
+);
+
+colorEvents = [blueBtn, orangeBtn, greenBtn];
+
+//button click events
+function bond007() {
+  colorEvents.forEach(
+    colorEvt => (colorEvt.button.onclick = colorEvt.colorEventStarter)
+  );
+}
+bond007();
+
+/*
+//clicking blue button events
+colorButtons[0].onclick = function colorEventStarter() {
+  return colorButtonEvent(`#588fbd`, `#ffa500`, "black", "white");
+};
 
 //clicking orange button events
-colorButtons[1].addEventListener("click", orangeBtnEvent);
-function orangeBtnEvent() {
-  jumbotronDiv.style.backgroundColor = `#f0ad4e`;
-  donateBikeButton.style.backgroundColor = `#5751fd`;
-  volunteerButton.style.backgroundColor = "#31b0d5";
-  volunteerButton.style.color = "white";
-}
+colorButtons[1].onclick = function orangeEventStarter() {
+  return colorButtonEvent(`#f0ad4e`, `#5751fd`, "#31b0d5", "white");
+};
 
 //clicking green button events
-colorButtons[2].addEventListener("click", greenBtnEvent);
-function greenBtnEvent() {
-  jumbotronDiv.style.backgroundColor = `#87ca8a`;
-  donateBikeButton.style.backgroundColor = `black`;
-  volunteerButton.style.backgroundColor = "#8c9c08";
-}
+colorButtons[2].onclick = function greenEventStarter() {
+  return colorButtonEvent(`#87ca8a`, `black`, "#8c9c08", "white");
+};
+*/
 //-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-//
 //Part-2
 
@@ -47,14 +105,20 @@ function checkForm(event) {
   isMailAddressValid = checkMailAddressValidity(inputs[0].value);
 
   if (!isMailAddressValid) {
-    invalidInputCounter++;
     inputs[0].style.backgroundColor = "red";
+    invalidInputCounter++;
+    inputs[0].onmouseout = function() {
+      alert("All areas must be filled out!");
+    };
   }
 
   for (var i = 1; i < inputs.length; i++) {
     if (inputs[i].value.length === 0) {
       inputs[i].style.backgroundColor = "red";
       invalidInputCounter++;
+      inputs[i].onmouseout = function() {
+        alert("All areas must be filled out!");
+      };
     }
   }
 
