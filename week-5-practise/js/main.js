f4b3491141ea860a4376f95d795dc80806535079
Mohbeen29 <mohbeen29@gmail.com>
2019-01-31 20:02:38 +0000

    completed week-5

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -1,4 +1,6 @@
-/*When clicking **blue** it should change:
+/*### part 1
+
+whhen clicking **blue** it should change:
         - **Jumbotron** background color to `#588fbd`
         - **Donate a bike** button background color to `#ffa500`
         - **Volunteer** button background color to `black` and text color to `white`*/
@@ -50,3 +52,52 @@ function greenBackground() {
   volunteer.style.backgroundColor = "#8c9c08`";
   volunteer.style.color = "white";
 }
+/*
+### Part 2
+
+Just below the buttons, there's a form called **Register with us**.
+
+Continue working in `./js/main.js` to add the following functionality:
+
+When the submit button is pressed, it should check that all the form fields are valid:
+- The ** Your name **, ** Email ** and ** Describe yourself ** fields need to be non - empty(Hint: their`value` length has to be greater than zero)
+    - For the ** Email ** field also check if it contains the`@` character
+
+For all the fields that invalid, it should make their background color`red`.
+IF all the fields are valid, when you click ** Submit ** it should:
+- Display an alert to thank you for filling out the form
+    - Blank out(make empty) all the text fields
+
+        ** Important hint:** In your function that handles clicks on the`Submit` button you will need to call`event.preventDefault()` to stop the browser from refreshing the page.To read more on how to do this: https://developer.mozilla.org/en/docs/Web/API/Event/preventDefault 
+*/
+var submitBtn = document.querySelector("#submitBtn");
+var controlForm = document.querySelectorAll(".form-control");
+var email = document.querySelector("#exampleInputEmail1");
+var name2 = document.querySelector("#name-input");
+var describe = document.querySelector("#exampleTextarea");
+submitBtn.addEventListener("click", confirmFilled);
+function confirmFilled() {
+  event.preventDefault();
+  if (email.value == "" || !email.value.includes("@")) {
+    email.style.backgroundColor = "red";
+  }
+  if (name2.value == "") {
+    name2.style.backgroundColor = "red";
+  }
+  if (describe.value == "") {
+    describe.style.backgroundColor = "red";
+  }
+  if (
+    email.value.length > 0 &&
+    name2.value.length > 0 &&
+    describe.value.length > 0 &&
+    email.value.includes("@")
+  ) {
+    alert("Thank you for filling out this form");
+    //form.reset();
+    name2.value = "";
+    email.value = "";
+    describe.value = "";
+  }
+}
+//confirmFilled();
