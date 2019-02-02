7d87229775eaf3a4517bceeb54a465dcd93efa6d
Tayo Ayoola <tayoa@hotmail.com>
2019-02-02 12:30:12 +0000

    A-objects-intro/exercise-part-1

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,4 +6,36 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var person = {
+  age: 26,
+  hairColour: "Brown"
+};
+var car = {
+  make: "Fiat Punto",
+  age: 11,
+  isMot: true
+};
+var propertyRent = {
+  location: "Bromley",
+  type: "Flat",
+  bedroomNumber: 2,
+  isStairs: false,
+  isGarden: true,
+  monthlyPrice: 1250
+};
+var codingBootcamp = {
+  Price: 3000,
+  location: "London",
+  legthOfStudy: "3 months",
+  languages: "HTML, CSS, JavaScript, Ruby",
+  isScholarship: true,
+  maxNumberOfStudent: 20
+};
 
+var film = {
+  name: "Glass",
+  filmCertificate: 15,
+  genre: "Thriller",
+  castMembers: "Bruce Willis, James McAvoy, Samuel L.Jackson",
+  isReleased: true
+};
