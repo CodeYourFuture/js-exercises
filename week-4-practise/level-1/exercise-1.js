2aa5e34fe7d65afbcd31f85dc7731cb836fda9c0
Tayo Ayoola <tayoa@hotmail.com>
2019-02-02 19:17:48 +0000

    ../week-4-practise/level-1/exercise-1

--- week-4-practise/level-1/exercise-1.js
+++ week-4-practise/level-1/exercise-1.js
@@ -6,7 +6,7 @@ and for each object, `console.log()` out the sentence:
 
 Here is the array:
 
-*/ 
+*/
 
 var writers = [
   {
@@ -36,9 +36,16 @@ var writers = [
     occupation: "writer",
     age: 64,
     alive: true
-  },
+  }
 ];
 
+for (i = 0; i < writers.length; i++) {
+  console.log(
+    `Hi, my name is ${writers[i].firstName} ${writers[i].lastName}.I am ${
+      writers[i].age
+    } years old, and work as a ${writers[i].occupation}.`
+  );
+}
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
