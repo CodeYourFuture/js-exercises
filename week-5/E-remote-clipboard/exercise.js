c136a2101c935e75183ac2dc2d8c0ae87d69ae43
ermiii <vectonv@gmail.com>
2019-02-08 14:39:46 +0000

    Week-5 exercise Fetch & remoteclipoard

--- week-5/E-remote-clipboard/exercise.js
+++ week-5/E-remote-clipboard/exercise.js
@@ -20,24 +20,35 @@ Also, for GET request, you can use the url directly in your browser address bar
 // Task 1: create a new clipboard
 // Complete the code below
 
-var clipboardTitle = "CHANGE ME";
-var clipboardText = "CHANGE ME";
-var requestBody = { title: clipboardTitle, text: clipboardText };
+var clipboardTitle = "myTrialClipboard";
+var clipboardText = "come on! JS";
+var requestBody = {
+    title: clipboardTitle,
+    text: clipboardText
+};
 
 var postRequestParameters = {
     body: JSON.stringify(requestBody),
-    method: 'POST',
+    method: "POST",
     headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json"
     }
 };
 
-fetch(/* Write the API address here */, postRequestParameters);
-
+fetch(
+    "https://codeyourfuture.herokuapp.com/api/clipboard",
+    postRequestParameters
+);
 
 // Task 2: Load an existing clipboard
 // Add your code below
 
-fetch(/* ... */).then(function(response) {
-    return response.text();
-}).then(/* ... */);
+fetch(
+        "https://codeyourfuture.herokuapp.com/api/clipboard?title=myTrialClipboard"
+    )
+    .then(response => {
+        return response.text();
+    })
+    .then(value => {
+        console.log(value);
+    });
