dca5ac68b71866eeea42a89c896fb039fcb756b7
ahmet <ahmet.kio@hotmail.com>
2019-02-10 12:44:17 +0000

    Ahmet-JS-Wee 5-E

--- week-5/E-remote-clipboard/exercise.js
+++ week-5/E-remote-clipboard/exercise.js
@@ -20,8 +20,8 @@ Also, for GET request, you can use the url directly in your browser address bar
 // Task 1: create a new clipboard
 // Complete the code below
 
-var clipboardTitle = "CHANGE ME";
-var clipboardText = "CHANGE ME";
+var clipboardTitle = "MyName";
+var clipboardText = "Ahmet";
 var requestBody = { title: clipboardTitle, text: clipboardText };
 
 var postRequestParameters = {
@@ -32,12 +32,16 @@ var postRequestParameters = {
     }
 };
 
-fetch(/* Write the API address here */, postRequestParameters);
+fetch("https://codeyourfuture.herokuapp.com/api/clipboard", postRequestParameters);
 
 
 // Task 2: Load an existing clipboard
 // Add your code below
 
-fetch(/* ... */).then(function(response) {
+
+fetch("https://codeyourfuture.herokuapp.com/api/clipboard?title=" + clipboardTitle).then(response => {
     return response.text();
-}).then(/* ... */);
+  })
+  .then(text => {
+    console.log(text);
+});
