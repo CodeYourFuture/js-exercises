af5b04b0c7e257ff8de7a606ce1abd5e10ce0d9b
ZaimAbz <zaimabodi@gmail.com>
2019-02-10 16:18:33 +0000

    Abz week-5 B

--- week-5/B-callbacks/exercise.js
+++ week-5/B-callbacks/exercise.js
@@ -14,10 +14,11 @@
   EXPECTED RESULT: The #exercise1 element has textContent = "YOUR NAME" when
   the button is clicked
 */
-document.querySelector('#button1').addEventListener('click', exercise1)
+document.querySelector("#button1").addEventListener("click", exercise1);
 
 function exercise1() {
   // Write your implementation here
+  document.getElementById("exercise1").textContent = "Abz";
 }
 
 /*
@@ -34,10 +35,11 @@ function exercise1() {
   EXPECTED RESULT: The #exercise2 element has textContent = "Hello from the
   function caller"
 */
-functionThatCallsBack(exercise2)
+functionThatCallsBack(exercise2);
 
 function exercise2(result) {
   // Write your implementation here
+  document.getElementById("exercise2").textContent = result;
 }
 
 /*
@@ -58,22 +60,22 @@ function exercise2(result) {
 
 function exercise3(callback) {
   // Write your implementation here
-
   // Write your explanation here
+  callback("Hello from the callback");
 }
 
-// 
+//
 // -------------------------------------
-// 
+//
 // DON'T EDIT THE FUNCTIONS BELOW!
 // THEY ARE PROVIDED FOR YOU
 //
 // -------------------------------------
 //
 function functionThatCallsBack(callback) {
-  callback('Hello from the function caller')
+  callback("Hello from the function caller");
 }
 
-exercise3((text) => {
-  document.querySelector('#exercise3').innerText = text
-})
+exercise3(text => {
+  document.querySelector("#exercise3").innerText = text;
+});
