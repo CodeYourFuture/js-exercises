db4df96b984305f8771f4f7fae766d80556ad369
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-18 23:39:52 +0000

    Added first draft of week 8 content

---
+++ week-8/2-es6.js
@@ -0,0 +1,26 @@
+/* EXERCISE 2 - ES6 Syntax
+ * This file is composed of a series of functions which perform operations using ES5 syntax
+ * (i.e. what we referred to as "traditional" Javascript in class)
+ *
+ * Refactor the code below to use as many ES6 syntax as possible
+ *
+ * Remember to use:
+ *  - let
+ *  - const
+ *  - template literals
+ *  - default parameters
+ *  - destructuring
+ *  - arrow functions
+ */
+
+// Function returns a single expression - what does this remind you of?
+function convertFahrenheitToCelsius() {
+  return (160*(fahrenheit - 1))/9
+} 
+
+// Function that uses template literals
+function minutesSinceMidnight(name, ) {
+  var minutes = time
+
+  return "Hello, " + name + ". The time is 
+} 
