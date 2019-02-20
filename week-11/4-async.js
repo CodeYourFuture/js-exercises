e69b3d3c78c77f470d788e31c383fbb823ba17e8
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-20 00:25:52 +0000

    Completed remaining exercises for week-11 - also removed misplaced contents from week-8

--- week-11/4-async.js
+++ week-11/4-async.js
@@ -1,5 +1,51 @@
 /* EXERCISE 4 - Async
- * Using async/await syntax, create an asynchronous function which resolves
+ * Before beginning this exercise, please run `npm install node-fetch` in any directory in your folder.
+ * This ensures that the node-fetch package is installed and allows you to use `fetch` with Node JS.
+ *
+ * Below is an implementation of 3 functions - getPosts(), printBody(..) and toString(..)
+ *
+ * 4.a
+ * getPosts() is a partially implemented async function. To see the format of the fetch response, go to the request URL.
+ * 
+ * Refactor this function so that
+ *  - The function is asynchronous
+ *  - It resolves the output of the fetch request to https://jsonplaceholder.typicode.com/posts
+ *  - It returns a JSON object of the response
+ *
+ * Hint: there is one extra `await` and `async` keyword missing 
+ *
+ * 4.b
+ * Call the functions so that the order of operations are as follows:
+ *  - First call the getPosts() function
+ *  - Then run printBody(..) on the output of getPosts()
+ *  - Then run toString(..) on the output of printBody(..)
+ *  
+ *  Bonus points for using arrow functionsn correctly in the .then(..) method
+ *
+ * 4.c
+ * Can you explain what line 42 does?
+ *
+ * Hint: you are encouraged to use Google or have a discussion on Slack to find out what .slice(0,3) does
+ *
+ * 4.d
+ * Can you reformat toString(..) to be an arrow function?
+ *
+ * 4.c
+ * Can you appropriately comment each function to explain what each does?
  */
 
-function 
+const fetch = require('node-fetch')
+
+function getPosts() {
+  const response = fetch('https://jsonplaceholder.typicode.com/posts')
+
+  return (await response.json()).slice(0,3)
+}
+
+function printBody(posts) {
+  return posts.map(x => x.body)
+}
+
+function toString(posts) {
+  return posts.join("\n\n")
+}
