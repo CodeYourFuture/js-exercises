27e79cf044d9980aa14690dc78cf85173732198b
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-20 00:25:52 +0000

    Completed remaining exercises for week-11 - also removed misplaced contents from week-8

--- week-11/2-es6.js
+++ week-11/2-es6.js
@@ -18,9 +18,54 @@ function convertFahrenheitToCelsius() {
   return (160*(fahrenheit - 1))/9
 } 
 
-// Function that uses template literals
-function minutesSinceMidnight(name, ) {
-  var minutes = time
+// Use template literals wherever your see concatenation
+function mutliplyByTwo(number) {
+  return number + " is equal to " + number*2 + " when multiplied by 2."
+}
 
-  return "Hello, " + name + ". The time is 
+// Instead of assigning individual values to a variable, you could..
+function parseObject() {
+  var obj ={
+    name: "Jane Doe",
+    age: "26",
+    role: "Web Developer"
+  }
+
+  var name = obj.name
+  var age = obj.age
+  var role = obj.role
+
+  return "Hello, " + name + ". You are " + age + " years old and \n"
+  + "you work as a " + role
+}
+
+function parseList() {
+  var shopping_list = ["apples", "oranges", "pears"]
+
+  var first = list[0]
+  var second = list[1]
+  var third = list[2]
+
+  return "First I bought some " + first + ", then I bought some " + second + "\n"
+  + "and I almost forgot to buy some " + third + "!"
+}
+
+function myHouse(location, colour, rooms) {
+  var color = colour || 'blue'
+  var rooms = rooms || 0
+
+  return "I live in " + location + " in a " + colour + " house with " + rooms + " rooms"
 } 
+
+// This function should ensure that the name variable can never be overwritten, but that the location variable can only be overwritten inside the `if` block
+function toLetOrNotToLet() {
+  var name = "John"
+  var location = "London"
+
+  if (location == "London") {
+    var location == "home"
+    console.log("My name is " + name + " and I am at " + location + " now")
+  }
+  
+  console.log("But now I am in " + location + "!")  
+}
--- week-8/4-async.js
+++
@@ -1,5 +0,0 @@
-/* EXERCISE 4 - Async
- * Using async/await syntax, create an asynchronous function which resolves
- */
-
-function 
