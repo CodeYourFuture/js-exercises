3e7131c3d83077ba29704638b6daf42b6daf3222
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-22 23:06:02 +0000

    Added further instructions to run ES6 import and export syntax in NodeJS

--- week-8/8-export.js
+++ week-8/8-export.js
@@ -1,4 +1,7 @@
 // Modify this script so that you may access these functions in 8-import.js
+//
+// For this to work with NodeJS, please modify the extension of both the export and import files to .mjs and
+// then run `node --experimental-modules <import-file>.mjs` instead of the normal `node <import-file>.js`
 
 function getArrivalTime(source, dest) {
   console.log(`Your flight from ${source} to ${dest} arrives at 10:00 GMT`)
