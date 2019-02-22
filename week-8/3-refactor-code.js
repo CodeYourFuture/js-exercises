1a175682a375dc876b3fd08902207b2036818776
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-22 18:53:36 +0000

    Added in-class exercises for week-8 and renamed homework folder (again)

---
+++ week-8/3-refactor-code.js
@@ -0,0 +1,17 @@
+function getCircleArea(radius) {
+  var pi = Math.PI;
+  var rSquared = Math.pow(radius, 2);
+
+  return pi * rSquared;
+}
+
+function getCircleAreas(radiusArr) {
+  var areasArr = [];
+
+  for (var i = 0; i < radiusArr.length; i++) {
+    var circleArea = getCircleArea(radiusArr[i]);
+    areasArr.push(circleArea);
+  }
+
+  return areasArr;
+}
