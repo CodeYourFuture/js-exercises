26cc94a307b2e5120d33b5f1178b27d33c575472
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-22 20:27:30 +0000

    Added exercises for week 8 class

---
+++ week-8/9-export.js
@@ -0,0 +1,4 @@
+// See 9-import.js for exercise
+export default function () 
+  console.log("I'm a little teapot!")  
+}
---
+++ week-8/9-import.js
@@ -0,0 +1,5 @@
+// Write an import statement so that you use the function in 9-export.js when you call a()
+
+/* Write import statement here */
+
+a()
