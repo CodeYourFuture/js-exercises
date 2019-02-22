a98981fac4dd53edfe100cc2e99c88e229aa282d
Sanyia Saidova <sanyia.saidova@capgemini.com>
2019-02-22 22:39:06 +0000

    Renamed let-const example file and added async exercise

---
+++ week-8/2-async.js
@@ -0,0 +1,18 @@
+// Turn getFlights() into an async function and use the .then(..) method to implement the following functions in order
+// - getFlightDetails()
+// - selectFlight(..)
+// - bookFlight(..)
+
+function getFlightDetails() {
+  setTimeout(() => {}, 300)
+
+  return 'LDN -> JPN'
+}
+
+function selectFlight(flight) {
+  return `Flight ${flight} selected`)
+}
+
+function bookFlight(flight) {
+  return `Flight ${flight} booked`
+}
