084e8e0cc94059cc3eb584c758c933b532916207
Nabraha <nebaytedros@yahoo.com>
2019-02-23 02:02:18 +0000

    E-strings

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
 
+//Program that logs a message and it's type
+
+var message = 'This is a string';
+var messageType = typeof message;
+
 console.log(message);
+console.log(messageType);
