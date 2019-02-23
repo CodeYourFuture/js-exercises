da9f3d4d0efd1dfd4ed510cd3d94901b4308b0c7
Nabraha <nebaytedros@yahoo.com>
2019-02-23 02:06:14 +0000

    F-String concat

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
+//program that logs a message with a greeting and your name
+var greetings = 'Hello, my name is ';
+var myName = 'Jordan';
 
-console.log(message);
+console.log(greetings + myName);
