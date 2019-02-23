3b3519774be41cf41b99d89cd31e41a408a7a4e1
Nabraha <nebaytedros@yahoo.com>
2019-02-23 01:54:13 +0000

    C-hello-world

--- week-1/C-hello-world/exercise.js
+++ week-1/C-hello-world/exercise.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+// Run the program using node
+console.log('firstName');
+
+// Hello world + something different
+console.log('Hello World ' + 'Something different');
+
+//get rid of the quote marks
+//console.log(what happens); syntax missing erro is produced
+
+//console.log but this time only numbers
+console.log(12);
