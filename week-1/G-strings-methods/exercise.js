e36fee2a045059e4da8f0dd8ce4275842448fe12
Nabraha <nebaytedros@yahoo.com>
2019-02-23 02:19:20 +0000

    G-string methods

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,5 @@
 // Start by creating a variable `message`
+//Log a message that includes the length of your name
+var myName = 'Jordan';
 
-console.log(message);
+console.log(' My name is ' + myName + ' and my name is ' + myName.length + ' characters long.');
--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-var name = " Daniel  ";
+//Log the same message using the variable, `name` provided and Use the `.trim` method to remove the extra whitespace
 
-console.log(message);
+var name = " Jordan  ";
+var myNameAfterTrim = name.trim();
+
+console.log('My name is ' + myNameAfterTrim + ' and my name is ' + myNameAfterTrim.length + ' characters long.');
