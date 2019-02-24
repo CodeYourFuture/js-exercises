a2954702ed06fbd09be4607038d5e49df9b23cf3
Samantha Chigbo <samchigbo@gmail.com>
2019-02-24 13:11:07 +0000

    week5 practice not yet complete

--- week-5-practise/js/main.js
+++ week-5-practise/js/main.js
@@ -27,10 +27,31 @@ greenBtn.addEventListener("click", function() {
 
 var submitBtn = document.querySelector("");
 submitBtn.addEventListener("click", function() {
-    var yourName = document.querySelector(".form-group #example-text-input");
-    var emailInput = document.querySelector(".form-group #exampleInputEmail1d");
-    var describeYourself = document.querySelector(".form-group #exampleTextarea");
-    if(yourName.value != "" && emailInput.value != "" && describeYourself.value != "" && emailInput.value.includes("@")) {
-        alert("Thank you for filling out the form!");
-    } else if( )
-})
+  var yourName = document.querySelector(".form-group #example-text-input");
+  var emailInput = document.querySelector(".form-group #exampleInputEmail1d");
+  var describeYourself = document.querySelector(".form-group #exampleTextarea");
+
+  var formThings = { yourName, emailInput, describeYourself };
+  if (
+    yourName.value != "" &&
+    emailInput.value != "" &&
+    describeYourself.value != "" &&
+    emailInput.value.includes("@")
+  ) {
+    //form is valid - alert and clear forms
+    for (var i = 0; i < formThings.length; i++) {
+      formThings[i].value = "";
+      event.preventDefault();
+    }
+    alert("Thank you for filling out the form!");
+  } else {
+    //go through invalid forms and mark them red
+    for (var i = 0; i < formThings.length; i++) {
+      if ((formThings[i].value = "")) {
+        return (formThings[i].style.backgroundColor = "red");
+      }
+    }
+  }
+});
+
+// have not yet determined where to put    event.preventDefault();
