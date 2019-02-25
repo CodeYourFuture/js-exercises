a2e17c80ee0f777216b8a10c4795fb832b9eaa00
Nabraha <nebaytedros@yahoo.com>
2019-02-25 20:30:26 +0000

    I-floats

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var PercentageOfStudents, PercentageOfMentors;
+
+var totalNumbersOfStudentsAndMentors = numberOfStudents + numberOfMentors;
+
+PercentageOfMentors = numberOfMentors / totalNumbersOfStudentsAndMentors * 100;
+PercentageOfStudents = numberOfStudents / totalNumbersOfStudentsAndMentors * 100;
+
+console.log("Percentage of Student: " + Math.round(PercentageOfStudents) + '%');
+console.log("Percentage of mentors " + Math.round(PercentageOfMentors) + '%');
