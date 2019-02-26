6dea42abdc602c854004254c0951cfabebdbf8ef
aldeez988 <aldeez911088@gmail.com>
2019-02-26 13:36:11 +0000

    Elamin week-8-practise

--- week-8-practise/3-classes.js
+++ week-8-practise/3-classes.js
@@ -1,16 +1,16 @@
 /* EXERCISE 3 - Classes
- * 3.a 
+ * 3.a
  * An incomplete House class is defined below. The House class should accept the following parameters:
  *  - windows
  *  - colour (must default to white - you can use ES6 default parameters syntax)
  *  - number of rooms
- *  - location 
+ *  - location
  *
- * 3.b 
+ * 3.b
  * A method has already been created which computes the energy cost based on the number of windows
  *
  * The House class should also have a method which computes the price of the house based on the location and number of rooms
- *  - The price should have a base value of 50,000GBP, with an addition of 10,000GBP for each room it has (if it's a 
+ *  - The price should have a base value of 50,000GBP, with an addition of 10,000GBP for each room it has (if it's a
  *  studio flat, the room number should be 0)
  *  - If the house is based in London, the value of the house should increase by 80%
  *
@@ -23,20 +23,98 @@
  * Using your newly created class
  *  - Instantiate (hint: new House())a brown house with 3 windows and 2 bedrooms that is based in Woking
  *  - Instantiate another blue house with 5 windwos and 2 bedrooms that is based in London
- *  - Return and compare the price of the two houses
+ *  - Return and compare the price of the two housese value of the house should increase by 80%
  *  - Return and compare the energy bill of the two houses
  *  - Return how many square meters are in 950 square feet
  */
 
 class House {
-  constructor(windows, colour) {
-    this.windows = windows
+  constructor(windows, colour = "white", numberOfRooms, location) {
+    this.windows = windows;
+    this.colour = colour;
+    this.numberOfRooms = numberOfRooms;
+    this.location = location;
+  }
+  monthlyEnergyBill(windows) {
+    let base = 40; // base energy bill without windows
+    let multiplier = 1.2; // energy bill expected to go up 20% for each additional window
+    return base + windows * multiplier;
+  }
+  housePrice() {
+    let baseValue = 50000;
+    let roomsAddedPrice = 10000 * this.numberOfRooms;
+    let totalValue = baseValue + roomsAddedPrice;
+    return this.location === "London" ? (totalValue *= 1.8) : totalValue;
   }
 
-  monthlyEnergyBill(windows) {
-    let base = 40 // base energy bill without windows
-    let multiplier = 1.2 // energy bill expected to go up 20% for each additional window
-    
-    return base + windows*multiplier
+  static feetIntoMetersSquar(feet) {
+    const oneMetInFootSq = 0.092903;
+    return feet * oneMetInFootSq;
   }
 }
+
+//comparePrice function receives the houses in an array  as a parameter and compare the price for the two houses
+function comparePrice(houses) {
+  if (houses[0].housePrice() > houses[1].housePrice()) {
+    console.log(
+      `The ${houses[0].colour.toUpperCase()} house is £${houses[0].housePrice() -
+        houses[1].housePrice()} more expensive than ${houses[1].colour.toUpperCase()} House`
+    );
+  } else if (houses[0].housePrice() < houses[1].housePrice()) {
+    console.log(
+      `The ${houses[1].colour.toUpperCase()} house is  £${houses[1].housePrice() -
+        houses[0].housePrice()} more expensive than ${houses[0].colour.toUpperCase()} House`
+    );
+  } else {
+    console.log("Both houses are equally priced");
+  }
+}
+
+//compareEnergy function receives the houses in array as a parameter and compare the energy for the two houses
+function compareEnergy(houses) {
+  if (
+    houses[0].monthlyEnergyBill(houses[0].windows) >
+    houses[1].monthlyEnergyBill(houses[1].windows)
+  ) {
+    console.log(
+      `The ${houses[0].colour.toUpperCase()} house energy bill is  ${houses[0].monthlyEnergyBill(
+        houses[0].windows
+      ) -
+        houses[1].monthlyEnergyBill(
+          houses[1].windows
+        )} more expensive than ${houses[1].colour.toUpperCase()} House`
+    );
+  } else if (
+    houses[0].monthlyEnergyBill(houses[0].windows) <
+    houses[1].monthlyEnergyBill(houses[1].windows)
+  ) {
+    console.log(
+      `The ${houses[1].colour.toUpperCase()} house energy bill is  ${houses[1].monthlyEnergyBill(
+        houses[1].windows
+      ) -
+        houses[0].monthlyEnergyBill(
+          houses[0].windows
+        )} more expensive than ${houses[0].colour.toUpperCase()} House`
+    );
+  } else {
+    console.log("Both house's energy bills are equally priced");
+  }
+}
+
+//Declaring two House instances
+var brownHouse = new House(3, "brown", 2, "Woking");
+var blueHouse = new House(5, "blue", 2, "London");
+comparePrice([brownHouse, blueHouse]);
+compareEnergy([brownHouse, blueHouse]);
+
+console.log(
+  `There is ${House.feetIntoMetersSquar(950)} meter squar in 950 foot squar`
+);
+
+/*When we declare a method as static it's not called on the instances(example:brownHouse, blueHouse )
+instead they are called on the class it self in other words we don't need to create an instance of 
+that class to access  the method we can access it directly use class name and the dot notation 
+for example: House.feetIntoMetersSquar(950), if we tried to access a static method using instance
+it will give us an error for example: brownHouse.feetIntoMetersSquar(950)  will generate this error 
+"TypeError: brownHouse.feetIntoMetersSquar is not a function"
+*/
--- week-8-practise/4-async.js
+++ week-8-practise/4-async.js
@@ -6,20 +6,20 @@
  *
  * 4.a
  * getPosts() is a partially implemented async function. To see the format of the fetch response, go to the request URL.
- * 
+ *
  * Refactor this function so that
  *  - The function is asynchronous
  *  - It resolves the output of the fetch request to https://jsonplaceholder.typicode.com/posts
  *  - It returns a JSON object of the response
  *
- * Hint: there is one extra `await` and `async` keyword missing 
+ * Hint: there is one extra `await` and `async` keyword missing
  *
  * 4.b
  * Call the functions so that the order of operations are as follows:
  *  - First call the getPosts() function
  *  - Then run printBody(..) on the output of getPosts()
  *  - Then run toString(..) on the output of printBody(..)
- *  
+ *
  *  Bonus points for using arrow functionsn correctly in the .then(..) method
  *
  * 4.c
@@ -35,18 +35,27 @@
  * Can you appropriately comment each function to explain what each does?
  */
 
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-function getPosts() {
-  const response = fetch('https://jsonplaceholder.typicode.com/posts')
+//getPosts function use fetch to get the data from the server and return promise that has a resolved value of the first three user objects as an array
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-  return (await response.json()).slice(0,3)
+  //This slice method here slice the first three objects in the  response.json() and return them as an array for the resolved promise that is retured by getPost function
+  return (await response.json()).slice(0, 3);
 }
 
+//printBody function receive and array of objects each object  that has a userId, id, title and body, using the map it returns and new array of the body of the three object
 function printBody(posts) {
-  return posts.map(x => x.body)
+  return posts.map(x => x.body);
 }
 
-function toString(posts) {
-  return posts.join("\n\n")
-}
+//The toString method join the array returned from printBody and make double new line between the joined array elements
+var toString = posts => posts.join("\n\n");
+
+getPosts()
+  .then(printBody)
+  .then(toString)
+  .then(posts => {
+    console.log(posts);
+  });
