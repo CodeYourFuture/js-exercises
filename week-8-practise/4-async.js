54a00fdb1d8f12d9598ca11b64ee79aa51902753
clement <clemohiomoje@gmail.com>
2019-02-27 13:35:07 +0000

    async exercise

--- week-8-practise/4-async.js
+++ week-8-practise/4-async.js
@@ -35,18 +35,24 @@
  * Can you appropriately comment each function to explain what each does?
  */
 
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
-function getPosts() {
-  const response = fetch('https://jsonplaceholder.typicode.com/posts')
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+ return (await response.json()).slice(0, 3);
 
-  return (await response.json()).slice(0,3)
+
 }
 
 function printBody(posts) {
-  return posts.map(x => x.body)
+  return posts.map(textValue => textValue.body)
 }
 
-function toString(posts) {
-  return posts.join("\n\n")
-}
+const toString = posts => posts.join("\n\n");
+
+getPosts()
+  .then(printBody)
+  .then(toString)
+  .then(posts => {
+    console.log(posts);
+  });
