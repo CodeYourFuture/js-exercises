a09d42c3f088b903a3268eeed339e0608c009b58
Nabraha <nebaytedros@yahoo.com>
2019-02-27 20:55:24 +0000

    C-comparison-operator

--- week-2/C-comparison-operators/exercise.js
+++ week-2/C-comparison-operators/exercise.js
@@ -7,14 +7,15 @@
 
 var studentCount = 16;
 var mentorCount = 9;
-var moreStudentsThanMentors; // finish this statement
+var moreStudentsThanMentors = studentCount > mentorCount; // finish this statement
 
 var roomMaxCapacity = 25;
-var enoughSpaceInRoom; // finish this statement
+var enoughSpaceInRoom = studentCount + mentorCount === roomMaxCapacity; // finish this statement
 
 var personA = "Daniel";
 var personB = "Irina";
-var sameName; // finish this statement
+var sameName = personA === personB; // finish this statement
+//console.log(sameName.valueOf());
 
 /* 
   DO NOT EDIT BELOW THIS LINE
