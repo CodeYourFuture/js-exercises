8d79c3c5e8f9afaa1f89db65994718bac90157a4
Tayo Ayoola <tayoa@hotmail.com>
2019-02-27 10:23:06 +0000

    Tayo-week3practice-map-exercise

--- week-2-practise/level-2/6-map.js
+++ week-2-practise/level-2/6-map.js
@@ -2,9 +2,13 @@
 // 1. Write a function that formats the numbers into a string with the percentage symbol appended e.g. "10%"
 // 2. Declare a new array, `percentagesFormatted`, containing
 //    - each item in `percentages` formatted by your function
-
+function format(num) {
+  return num + "%";
+}
 var percentages = [1, 23, 92, 18];
-
+var percentagesFormatted = percentages.map(function(num) {
+  return num + "%";
+});
 console.log(percentagesFormatted);
 
 /* 
--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+var laptop = {
+  brand: "Apple",
+  name: "MacBook Pro",
+  ageInMonths: 3,
+  isTouchscreen: true
+};
