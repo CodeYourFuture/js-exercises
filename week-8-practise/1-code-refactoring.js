7fb92b5eec5fc1680265ae562db2549973441473
clement <clemohiomoje@gmail.com>
2019-02-27 13:33:57 +0000

    code refactoring exercise

--- week-8-practise/1-code-refactoring.js
+++ week-8-practise/1-code-refactoring.js
@@ -8,28 +8,28 @@
  * There may be both syntax and logical errors present.
  */
 
-var reversed;
-let capitilised;
 
-// Function that reverses and capitlises text
-exports function reverseAndCaptilise(text) 
-  var capitlised = text.toUpper()
-  reversed = capitilised.split('').reverse().join('') 
-
-  return reversed
-  }
-
-// Function that returns text with every other letter capitlised
-export function everyOtherLetterCapitilised(text) {
-  for (i==0; i++; i<text.length) {
-      if (i/2==0) {
-       text[i] = text[i].toUpperCase() 
-    }    
-  } 
-}
-
-export  function lowerCase(text) {
-  let text = text.toLowerCase()
-
+// this function handle the split the reverse ad the join method
+export function reverseAndCaptilise(text) {
   return text
+    .split("")
+    .reverse()
+    .join("")
+    .toUpperCase();
 }
+
+//this function will transform all text to upperCase,
+export function everyOtherLetterCapitilised(text) {
+  return text.toUpperCase();
+}
+
+//this function will tranform all the text to lowerCase;
+export function lowerCase(text) {
+  return text.toLowerCase();
+}
+
+// please note the function call  below is just  to test that the function are working accordingly,
+// to test this function call  remove the  word export from all the functions. and this line of code will run.
+console.log(reverseAndCaptilise("clement"));
+console.log(everyOtherLetterCapitilised("mildred"));
+console.log(lowerCase("MARY"));
