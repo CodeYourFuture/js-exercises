8028dbdc214807ef5f2489f62bf195a00e6c46b8
ahmet <ahmet.kio@hotmail.com>
2019-02-28 20:31:56 +0000

    Ahmet-JS-Week3-Practise-Exercises

---
+++ week-3-practise/level-3/1-practise-all.js
@@ -0,0 +1,48 @@
+/* Password Validation
+   This program should check if each passwords in the array ("password[]") 
+   contains valid Password (see below for password criterias) and return 
+   new array ("PasswordValidationResult[]") with True/False booleans.
+
+LEVEL 1: 
+
+Passwords must 
+- Have at least 5 characters, 
+- Have English uppercase letters (A-Z),
+- Have English lowercase letters (a-z),
+- Have numbers (0-9).
+
+Expected Result:
+PasswordValidationResult= [false, false, frue, true, true]
+
+LEVEL 2:
+
+Passwords must 
+- Have at least 5 characters.
+- Have English uppercase letters (A-Z)
+- Have English lowercase letters (a-z)
+- Have numbers (0-9)
+- Have non-alphanumeric symbols ("!", "#", "$", "%", ".")
+
+Expected Result:
+PasswordValidationResult= [false, false, false, true, true]
+
+LEVEL 3:
+
+Passwords must 
+- Have at least 5 characters.
+- Have English uppercase letters (A-Z)
+- Have English lowercase letters (a-z)
+- Have numbers (0-9)
+- Have non-alphanumeric symbols ("!", "#", "$", "%", ".")
+
+Passwords must not be any previous password in previousPassword Array. 
+PreviousPassword = ["fhD8!yrjj", "ttkTu.wer3", "dvyyeyY!5", "qwbfj76%", "tytT3729."];
+
+Expected Result:
+PasswordValidationResult=  [false, false, false, false, true]
+
+*/
+
+var password = ["Se%5", "TktE.TJTU", "384HsHF", "dvyyeyY!5", "tryT3729."];
+
+console.log(PasswordValidationResult)
