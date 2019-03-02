697e8f56484ce28f3255d8f739f0f67252dc7793
clement <clemohiomoje@gmail.com>
2019-03-02 16:28:03 +0000

    ES6 exercise

--- week-8-practise/2-es6.js
+++ week-8-practise/2-es6.js
@@ -14,78 +14,77 @@
  */
 
 // Function returns a single expression - what does this remind you of?
-function convertFahrenheitToCelsius(fahrenheit) {
-  return (fahrenheit - 32)*5/9
-} 
+convertFahrenheitToCelsius = fahrenheit => ((fahrenheit - 32) * 5) / 9;
 
 // Use template literals wherever your see concatenation
-function mutliplyByTwo(number) {
-  return number + " is equal to " + number*2 + " when multiplied by 2."
+function multiplyByTwo(number) {
+  console.log(`${number}  is equal to   ${number * 2}  when multiplied by 2.`);
+  // return (`${number}  is equal to   ${number * 2}  when multiplied by 2.`)
 }
 
 // Instead of assigning individual values to a variable, you could..
 function parseObject() {
-  var obj ={
+  var obj = {
     name: "Jane",
     age: "26",
     role: "Web Developer"
-  }
+  };
+  const { name, age, role } = obj;
 
-  var name = obj.name
-  var age = obj.age
-  var role = obj.role
-
-  return "Hello, " + name + ". You are " + age + " years old and \n"
-  + "you work as a " + role
+  console.log(
+    `Hello, ${name}. You are ${age} years old and you work as a ${role}`
+  );
+  //return (`Hello, ${name}. You are ${age} years old and
+  //you work as a ${role}`);
 }
 
 function parseList() {
-  var shopping_list = ["apples", "oranges", "pears"]
-
-  var first = list[0]
-  var second = list[1]
-  var third = list[2]
-
-  return "First I bought some " + first + ", then I bought some " + second + "\n"
-  + "and I almost forgot to buy some " + third + "!"
+  var shopping_list = ["apples", "oranges", "pears"];
+  const [first, second, third] = shopping_list;
+  console.log(
+    `First I bought some ${first} , then I bought some ${second}\nand I almost forgot to buy some ${third} "!`
+  );
+  //return (`First I bought some ${first} , then I bought some ${second} \n
+  //and I almost forgot to buy some ${third}  "!`)
 }
 
-function myHouse(location, colour, rooms) {
-  var color = colour || 'blue'
-  var rooms = rooms || 2
-
-  return "I live in " + location + " in a " + colour + " house with " + rooms + " rooms"
-} 
+function myHouse(location, colour = "blue", rooms = 2) {
+  console.log(
+    `I live in   ${location} in a ${colour}  house with ${rooms} rooms`
+  );
+  //return (`I live in   ${location} in a ${colour}  house with ${rooms} rooms`)
+}
 
 // This function should ensure that the name variable can never be overwritten, but that the location variable can only be overwritten inside the `if` block
-function toLetOrNotToLet() {
-  var name = "John"
-  var location = "London"
 
+function toLetOrNotToLet() {
+  const name = "John";
+  const location = "London";
   if (location == "London") {
-    var location == "home"
-    console.log("My name is " + name + " and I am at " + location + " now.")
+    let location = "home";
+    console.log("My name is " + name + " and I am at " + location + " now.");
+    //return ("My name is " + name + " and I am at " + location + " now.");
   }
-  
-  console.log("But I am also in " + location + "!")  
+  console.log("But I am also in " + location + "!");
+  //return ("But I am also in " + location + "!");
 }
 
 /* DO NOT EDIT BELOW THIS LINE */
 
 // Expected output: 5
-convertFahrenheitToCelsius(41)
+convertFahrenheitToCelsius(41);
 
 // Expected output: '15 is equal to 30 when multiplied by 2'
-multiplyByTwo(15)
+multiplyByTwo(15);
 
 // Expected output: 'Hello, Jane. You are 26 years old and you work as a Web Developer'
-parseObject()
+parseObject();
 
 // Expected output: 'First I bought some apples, then I bought some oranges and I almost forgot to buy some pears!'
-parseList()
+parseList();
 
 // Expected output: 'I live in Kent in a white house with 2 rooms'
-myHouse('Kent', 'white')
+myHouse("Kent", "white");
 
 // Expected output: "My name is John and I am at home now. But I am also in London!"
-toLetOrNotToLet()
+toLetOrNotToLet();
