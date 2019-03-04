83a1814d6578154f2243158aaaf90632142db89e
Phil Ezemonye <philezemonye@gmail.com>
2019-03-04 04:02:10 +0000

    exercise.js

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,11 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var message = "Total number of students and mentors: ";
+var numberOfStudents = 15;
+var students = "Number of students: ";
+var numberOfMentors = 8;
+var mentors = "Number of mentors: ";
+var number = 15 + 8;
+
+console.log(students + numberOfStudents);
+console.log(mentors + numberOfMentors);
+console.log(message + number);
