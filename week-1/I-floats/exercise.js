e0ecb127eee55c1fa05556db5b7edc1ad6459f22
Phil Ezemonye <philezemonye@gmail.com>
2019-03-08 13:40:58 +0000

    exercise.js

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var precisePercentageStudents =
+  (numberOfStudents / (numberOfStudents + numberOfMentors)) * 100;
+var percentageStudents = Math.round(precisePercentageStudents);
+var precisePercentageMentors =
+  (numberOfMentors / (numberOfStudents + numberOfMentors)) * 100;
+var percentageMentors = Math.round(precisePercentageMentors);
+console.log(
+  "Percentage students: " + Math.round(precisePercentageStudents) + "%"
+);
+console.log("Percentage mentors: " + percentageMentors + "%");
