9f49e08ea3f292ca04fa7e04461826ac91ce924a
Samantha Chigbo <samchigbo@gmail.com>
2019-03-12 16:31:44 +0000

    week8 practise 123

--- week-8-practise/1-code-refactoring.js
+++ week-8-practise/1-code-refactoring.js
@@ -1,9 +1,9 @@
 /* EXERCISE 1 - Code Refactoring
- * This script functions as a library. 
+ * This script functions as a library.
  * Its purpose is to provide a set of functions that users can use to manipulate and decorate text.
  *
  * Using the coding practices you have learnt (you can use the syllabus as reference),
- * edit the code so that it establishes good coding standards. 
+ * edit the code so that it establishes good coding standards.
  *
  * There may be both syntax and logical errors present.
  */
@@ -12,24 +12,24 @@ var reversed;
 let capitilised;
 
 // Function that reverses and capitlises text
-exports function reverseAndCaptilise(text) 
-  var capitlised = text.toUpper()
-  reversed = capitilised.split('').reverse().join('') 
-
-  return reversed
-  }
+module.exports = function reverseAndCaptilise(text) {
+  var capitalised = text.toUpper();
+  reversed = capitalised
+    .split("")
+    .reverse()
+    .join("");
+  return reversed;
+};
 
 // Function that returns text with every other letter capitlised
-export function everyOtherLetterCapitilised(text) {
-  for (i==0; i++; i<text.length) {
-      if (i/2==0) {
-       text[i] = text[i].toUpperCase() 
-    }    
-  } 
+export function capitaliseSomeLetters(text) {
+  for (i = 0; i < text.lengthj; i++) {
+    if (i % 2 == 0) {
+      text[i] = text[i].toUpperCase();
+    }
+  }
 }
 
-export  function lowerCase(text) {
-  let text = text.toLowerCase()
-
-  return text
+export function lowerCase(text) {
+  return text.toLowerCase();
 }
--- week-8-practise/3-classes.js
+++ week-8-practise/3-classes.js
@@ -1,16 +1,16 @@
 /* EXERCISE 3 - Classes
- * 3.a 
+ * 3.a
  * An incomplete House class is defined below. The House class should accept the following parameters:
  *  - windows
  *  - colour (must default to white - you can use ES6 default parameters syntax)
  *  - number of rooms
- *  - location 
+ *  - location
  *
- * 3.b 
+ * 3.b
  * A method has already been created which computes the energy cost based on the number of windows
  *
  * The House class should also have a method which computes the price of the house based on the location and number of rooms
- *  - The price should have a base value of 50,000GBP, with an addition of 10,000GBP for each room it has (if it's a 
+ *  - The price should have a base value of 50,000GBP, with an addition of 10,000GBP for each room it has (if it's a
  *  studio flat, the room number should be 0)
  *  - If the house is based in London, the value of the house should increase by 80%
  *
@@ -29,14 +29,45 @@
  */
 
 class House {
-  constructor(windows, colour) {
-    this.windows = windows
+  constructor(windows, colour = "white", numbRooms, location) {
+    this.windows = windows;
+    this.colour = colour;
+    this.numbRooms = numbRooms;
+    this.location = location;
   }
 
   monthlyEnergyBill(windows) {
-    let base = 40 // base energy bill without windows
-    let multiplier = 1.2 // energy bill expected to go up 20% for each additional window
-    
-    return base + windows*multiplier
+    let base = 40; // base energy bill without windows
+    let multiplier = 1.2; // energy bill expected to go up 20% for each additional window
+
+    return base + windows * multiplier;
+  }
+
+  cost(rooms, location) {
+    let basePrice = 50000;
+
+    if (rooms > 0) {
+      basePrice += rooms * 10000;
+    }
+
+    if (location == "London") {
+      basePrice += basePrice * 0.8;
+    }
+  }
+
+  static convertToSqMtrs(squareFeet) {
+    return squareFeet * 0.092903;
   }
 }
+
+var brownHouse = new House(3, "brown", 2, "Woking");
+var blueHouse = new House(5, "blue", 2, "London");
+
+if (brownHouse.cost > blueHouse.cost) {
+  console.log(`The brown house is more expensive`);
+} else if (blueHouse.cost > brownHouse) {
+  console.log("The blue house is more expensive");
+} else {
+  console.log("They are the same price");
+}
+console.log(House.convertToSqMtrs(950) + " m2");
