138f45944429b44e980f2e6dc15f1695f13280b8
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-03-14 12:29:47 +0000

    3-currency-conversion.js

--- week-1-practise/level-2/3-currency-conversion.js
+++ week-1-practise/level-2/3-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+  return price * 1.4;
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
@@ -14,5 +16,5 @@ var product = "The life and times of Mozafar Haider";
 var price = 12.5;
 var priceInUSD = convertToUSD(price);
 
-console.log("Product: " + product);
-console.log("Price in USD: " + priceInUSD);
+console.log("Product:  " + product);
+console.log("Price in USD: $ " + priceInUSD);
