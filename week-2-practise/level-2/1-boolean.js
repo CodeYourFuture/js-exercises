1593ec0c8fd9b2e8723bb967110bfe7877c4c398
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-03-14 15:13:22 +0000

    1-boolean.js

--- week-2-practise/level-2/1-boolean.js
+++ week-2-practise/level-2/1-boolean.js
@@ -1,8 +1,8 @@
 // The code is valid but the program does not produce the expected result
-// Why doesn't it work?
+// Why doesn't it work? the boolean condition was not correctly done, mistake is the Quotation marks.
 // Fix it.
 
-var isHappy = "false";
+var isHappy = false;
 
 if (isHappy) {
   console.log("I am happy");
