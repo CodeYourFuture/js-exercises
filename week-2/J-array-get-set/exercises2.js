a77cf03c489ee74bc70d92f9b39a36406ca96514
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-03-15 18:53:10 +0000

    J-exe2.js

--- week-2/J-array-get-set/exercises2.js
+++ week-2/J-array-get-set/exercises2.js
@@ -8,6 +8,8 @@
 
 var numbers = [2, 2, 3]; // Don't change this array literal declaration
 
+numbers[0] = 1;
+numbers = numbers.concat(4);
 /* 
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
