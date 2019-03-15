8d6310d8030e5dd9b5d435c6d8e011fd823b51b9
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-03-15 17:18:39 +0000

    B-boolean-literals

--- week-2/B-boolean-literals/exercise.js
+++ week-2/B-boolean-literals/exercise.js
@@ -10,6 +10,7 @@ var codeYourFutureIsGreat = true;
 var mozafarIsCool = false;
 var calculationCorrect = true;
 var moreThan10Students = false;
+
 /* 
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
