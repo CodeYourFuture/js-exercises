d93a71620e60e695c23463969d99e0e93964d230
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-03-15 19:08:33 +0000

    Week-2-level-1

--- week-2-practise/level-1/4-slice.js
+++ week-2-practise/level-1/4-slice.js
@@ -1,6 +1,10 @@
 // Write a function `first5` that:
 // - returns the first 5 items from a provided array
 
+function first5(numbers) {
+  return numbers.slice(0, 5);
+}
+
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
