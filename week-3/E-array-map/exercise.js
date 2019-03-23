7fb974a303cb0ef1812787a3cae30d2be3bd8f75
Mariama <mariamasillah81@gmail.com>
2019-03-23 21:21:16 +0000

    completed exercise E-array-map

--- week-3/E-array-map/exercise.js
+++ week-3/E-array-map/exercise.js
@@ -2,9 +2,27 @@
   Using the .map() method, return a new array with `numbers` multiplied by 100
   Write multiple solutions using different syntax (as shown in the README)
 */
-
 // Complete this function -->
-function multiplyBy100(numbers) {}
+function multiplyBy100(numbers) {
+  return numbers.map(number => number * 100);
+}
+function multiplyBy100(numbers) {
+  return numbers.map(function times100(number) {
+    return number * 100;
+  });
+}
+function multiplyBy100(numbers) {
+  return numbers.map(function(number) {
+    return number * 100;
+  });
+}
+function multiplyBy100(numbers) {
+  return numbers.map(number => {
+    return number * 100;
+  });
+}
+var numbers = [10, 20, 30, 40, 50, 60, 70];
+console.log(multiplyBy100(numbers));
 
 // DEBUG
 // Debug your function by
