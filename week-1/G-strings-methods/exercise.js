7ea15bb989bde587144e7640e0f4332974cd845e
MarcoC <mar1774co@gmail.com>
2019-05-23 18:10:03 +0200

    week-1 G string methods exercise

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `message`
 
+var name = "Marco";
+var message =
+  "My name is " + name + " and my name is " + name.length + " characters long";
+
 console.log(message);
