1635469abf80afa6dd1406d98baa6b9cf07cfde4
MarcoC <mar1774co@gmail.com>
2019-05-23 18:14:54 +0200

    week-1 J function exercise

--- week-1/J-functions/exercise.js
+++ week-1/J-functions/exercise.js
@@ -1,7 +1,12 @@
 function halve(number) {
   // complete the function here
+  return number / 2;
 }
 
 var result = halve(12);
+
+console.log(result);
+
+var result = halve(20);
 
 console.log(result);
