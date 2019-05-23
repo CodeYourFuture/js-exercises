ae835ae72cfd1912ab1f198d6baf4336d8a015c3
CYF17 <alishadman955@gmail.com>
2019-05-23 19:19:16 +0200

    Completed exercise I

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,12 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var lowerPercentage =
+  (numberOfMentors / (numberOfStudents + numberOfMentors)) * 100;
+
+var preciseLowerPercentage = Math.round(lowerPercentage);
+
+var higherPercentage = 100 - preciseLowerPercentage;
+
+console.log("Percentage students: " + higherPercentage);
+console.log("Percentage mentors: " + preciseLowerPercentage);
