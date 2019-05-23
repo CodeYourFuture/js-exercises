8c5f593aec56ee182345f70733d9a70a143f6ffa
MarcoC <mar1774co@gmail.com>
2019-05-23 18:13:36 +0200

    week-1 I float exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,8 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+var percStudents =
+  (100 / (numberOfMentors + numberOfStudents)) * numberOfStudents;
+var percMentors =
+  (100 / (numberOfMentors + numberOfStudents)) * numberOfMentors;
+console.log("Percentage students: " + Math.round(percStudents) + "%");
+console.log("Percentage mentors: " + Math.round(percMentors) + "%");
