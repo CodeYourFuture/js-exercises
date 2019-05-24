321f4294be9d26f6ddadd82f790ef47ebad182aa
AliShadman95 <alishadman955@gmail.com>
2019-05-24 12:23:04 +0200

    Completed level 1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -1,12 +1,12 @@
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What does this program do? It returnes a random number
+// - How many parameters does the function take? zero
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters? is called to assing it to a variable,with no parameters
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -1,14 +1,14 @@
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
-var result = s('code', 'your');
+var result = s("code", "your");
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? concatenes two strings
+// - How many parameters does the function take? two
+// - What is the function name? s
+// - Where is the function called? with what parameters? is called to assing it to a variable , with "code" and "your" parameters
+// - What does the function return? returnes a string
+// - How can this function be improved? using an array as a parameter, this way we can do a loop and concatenate more than 2 strings!
