3f8e7e16afe35a5f18cebc277e8cc5c4e32170cb
MarcoC <mar1774co@gmail.com>
2019-05-24 08:51:19 +0200

    week-1 practise level-2 format currency

--- week-1-practise/level-2/2-format-currency.js
+++ week-1-practise/level-2/2-format-currency.js
@@ -6,7 +6,9 @@
   Write a function that transforms numbers into the format £0.00
 */
 
-function formatCurrency() {}
+function formatCurrency(a) {
+  return "£" + a.toFixed(2);
+}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
