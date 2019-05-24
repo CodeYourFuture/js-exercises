cc488496c4957dca6c28d7bfe55dcc3a8887e31d
Andrea Avella <50200771+AndreaAvella@users.noreply.github.com>
2019-05-24 23:12:53 +0200

    Update 1-syntax-error-1.js

--- week-1-practise/level-1/1-syntax-error-1.js
+++ week-1-practise/level-1/1-syntax-error-1.js
@@ -9,6 +9,6 @@ console.log(result); // Expected result 8 - Run the program and make sure it wor
 
 
 // Answer these questions:
-// 1. How many parameters does the function take?
-// 2. What is the function name?
-// 3. Where is the function called? with what parameters?
+// 1. How many parameters does the function take? 3
+// 2. What is the function name? addNumbers 
+// 3. Where is the function called? with what parameters? (1, 3, 4);
