db8689a7e2141d810461e07b0c36f0e0a630d83d
Javier S. Lopez <jslopez@gmail.com>
2019-06-18 16:37:04 -0400

    Fix a typo and a couple of errors

--- week-2-practise/level-2/6-map.js
+++ week-2-practise/level-2/6-map.js
@@ -1,4 +1,4 @@
-// You have been given an array of percetages
+// You have been given an array of percentages
 // 1. Write a function that formats the numbers into a string with the percentage symbol appended e.g. "10%"
 // 2. Declare a new array, `percentagesFormatted`, containing
 //    - each item in `percentages` formatted by your function
