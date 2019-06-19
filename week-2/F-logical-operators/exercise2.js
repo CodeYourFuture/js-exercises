7789c0ce05458c4952a9d3a5b6c93844d8d0aaba
Javier S. Lopez <jslopez@gmail.com>
2019-06-19 17:03:51 -0400

    Add missing semicolon

--- week-2/F-logical-operators/exercise2.js
+++ week-2/F-logical-operators/exercise2.js
@@ -13,7 +13,7 @@ function isNegative() {}
 
 console.log("Is -10 is a negative number?", isNegative(-10));
 console.log("Is 5 a negative number?", isNegative(5));
-console.log("Is 10 in the range 5-10?", isBetween5and10(10))
+console.log("Is 10 in the range 5-10?", isBetween5and10(10));
 console.log("Is Daniel a short name?", isShortName("Daniel"));
 console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
 
