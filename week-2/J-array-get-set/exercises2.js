d83226bceda36468e1fcd89883714bb4ffc6ef02
Javier S. Lopez <jslopez@gmail.com>
2019-06-21 12:30:25 -0400

    Fix test case

--- week-2/J-array-get-set/exercises2.js
+++ week-2/J-array-get-set/exercises2.js
@@ -6,7 +6,7 @@
   - change the first value in the array to the number 1
 */
 
-var numbers = [2, 2, 3]; // Don't change this array literal declaration
+var numbers = [1, 2, 3]; // Don't change this array literal declaration
 
 /* 
   DO NOT EDIT BELOW THIS LINE
