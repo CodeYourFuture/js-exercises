36b0e58dc27edb36bc6e9e5fe167d35467a695ab
AliShadman95 <alishadman955@gmail.com>
2019-07-11 18:05:46 +0200

    Roma Class1 - Week 4

--- week-4/B-objects-get-set/exercise-1.js
+++ week-4/B-objects-get-set/exercise-1.js
@@ -3,16 +3,15 @@
 */
 
 var kitten = {
-    ageMonths: 3,
-    isFemale: true,
-    furColour: "brown"
+  ageMonths: 3,
+  isFemale: true,
+  furColour: "brown"
 };
 
 // YOUR CODE GOES BELOW HERE
 
-
-
-
-
+for (var property in kitten) {
+  console.log(kitten[property]);
+}
 
 // YOUR CODE GOES ABOVE HERE
