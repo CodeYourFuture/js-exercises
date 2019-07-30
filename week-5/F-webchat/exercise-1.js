4f8a237eb4b492ee2226f259b674e9c1608921d8
Perasperad <you@example.com>
2019-07-30 15:11:17 +0200

    Webchat exercise n.3

--- week-5/F-webchat/exercise-1.js
+++ week-5/F-webchat/exercise-1.js
@@ -37,13 +37,16 @@ When you open index.html in your browser, it should display the existing message
 
 // Write your code here
 
-fetch("https://codeyourfuture.herokuapp.com/api/messages", {
-  method: "GET",
-  headers: { "Content-Type": "application/json" }
-})
-  .then(res => {
-    return res.json();
+function name() {
+  fetch("https://codeyourfuture.herokuapp.com/api/messages", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
   })
-  .then(text => {
-    document.getElementById("message-list").innerHTML = JSON.stringify(text);
-  });
+    .then(res => {
+      return res.json();
+    })
+    .then(text => {
+      document.getElementById("message-list").innerHTML = JSON.stringify(text);
+    });
+}
+setInterval(name, 2000);
--- week-5/F-webchat/exercise-3.js
+++ week-5/F-webchat/exercise-3.js
@@ -29,10 +29,13 @@ Task 3
 Use the above example to get familiar with setInterval function. 
 For example, print your name every 2 seconds.
 */
+function name() {
+  console.log("Teresa");
+}
 
+setInterval(name, 2000);
 
 // Write your code here
-
 
 /*
 ========
@@ -42,4 +45,3 @@ Task 4
 Use the setInterval function to reload automatically the messages of your webchat every 2 seconds.
 The code responsible to show the messages in the page is in exercise-1.js, so you will need to write your code there :-)
 */
-
