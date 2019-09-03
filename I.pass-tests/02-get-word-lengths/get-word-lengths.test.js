a56459b82e66e7931a88ecb44781da987d5bab81
Flaminiat <47352051+Flaminiat@users.noreply.github.com>
2019-09-03 15:02:44 +0200

    Delete get-word-lengths.test.js
    ò

--- I.pass-tests/02-get-word-lengths/get-word-lengths.test.js
+++
@@ -1,9 +0,0 @@
-var wordLengths = require("./get-word-lengths");
-
-test("Get word lengths", function() {
-  var words = ["sun", "potato", "roundabout", "pizza"];
-  var expected = [3, 6, 10, 5];
-
-  var output = wordLengths(words);
-  expect(output).toEqual(expected);
-});
