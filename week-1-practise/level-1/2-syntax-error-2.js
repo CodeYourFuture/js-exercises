49640bdf0eedc1115b746850b409e73383d29908
Yared <yayeyaye6@gmail.com>
2020-02-12 04:23:08 +0100

    syntaxfix 2

--- week-1-practise/level-1/2-syntax-error-2.js
+++ week-1-practise/level-1/2-syntax-error-2.js
@@ -4,5 +4,5 @@ function calculate(a, b, c)
 return a + b + c;
 
 
-var result = addNumbers(1, 3, 4);
+var result = calculate(1, 3, 4);
 console.log(result); // Expected result 8 - Run the program and make sure it works
