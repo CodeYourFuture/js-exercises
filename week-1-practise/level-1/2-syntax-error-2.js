155573efbfc58590103f94d7f16918372f01f24a
Yared <yayeyaye6@gmail.com>
2020-02-15 07:25:36 +0100

    syntaxFix2

--- week-1-practise/level-1/2-syntax-error-2.js
+++ week-1-practise/level-1/2-syntax-error-2.js
@@ -1,10 +1,8 @@
 // There is a syntax error in this code. Fix it.
 
-function calculate(a, b, c){
+function addNumbers(a, b, c){
     return a + b + c;
-}
-
-
-
-var result = calculate(1, 3, 4);
-console.log(result); // Expected result 8 - Run the program and make sure it works
+  }
+  
+  var result = addNumbers(1, 3, 4);
+    console.log(result); // Expected result 8 - Run the program and make sure it works
