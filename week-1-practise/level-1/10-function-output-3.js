7bb3725969a24715908325a1a7298a4d26255d7a
Yared <yayeyaye6@gmail.com>
2020-02-15 07:43:46 +0100

    syntaxFix11

--- week-1-practise/level-1/10-function-output-3.js
+++ week-1-practise/level-1/10-function-output-3.js
@@ -1,4 +1,5 @@
 function concatenate(firstWord, secondWord, thirdWord) {
+    return firstWord + secondWord + thirdWord;
     // Write the body of this function to concatenate three words together
 }
 
