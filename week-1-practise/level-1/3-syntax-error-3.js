5125bbe3492f6ae70dfe151cb29d6e34c33098f5
Yared <yayeyaye6@gmail.com>
2020-02-15 07:33:19 +0100

    syntaxFix5

--- week-1-practise/level-1/3-syntax-error-3.js
+++ week-1-practise/level-1/3-syntax-error-3.js
@@ -3,5 +3,6 @@
 function(a, b, c) {
     return a + b + c;
 }
-var result = addNumbers(1, 3, 4);
+
+let result = addNumbers(1, 3, 4);
     console.log(result);  // Expected result 8 - Run the program and make sure it works
