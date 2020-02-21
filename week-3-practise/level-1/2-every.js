9fec0628bb4e6bc298a2e62782d047299a7d8d74
mesutBE <gelukkig.zijn.be@gmail.com>
2020-02-21 19:49:27 +0100

    w3-pr lv-1

--- week-3-practise/level-1/2-every.js
+++ week-3-practise/level-1/2-every.js
@@ -11,8 +11,8 @@
 var bush1BerryColours = ["pink", "pink", "pink", "neon", "pink", "transparent"];
 var bush2BerryColours = ["pink", "pink", "pink", "pink"];
 
-var bush1SafeToEat; //Complete this statement
-var bush2SafeToEat; //Complete this statement
+var bush1SafeToEat = bush1BerryColours.every(x => x==='pink'); //Complete this statement
+var bush2SafeToEat = bush2BerryColours.every(x => x === 'pink'); //Complete this statement
 
 if (bush1SafeToEat) {
   console.log("Bush 1 is safe to eat from");
