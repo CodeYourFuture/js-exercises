207b7b3cab38dd15ab425509002b46bce8fed847
sansaid <sanyia.saidova@gmail.com>
2020-02-22 21:36:41 +0000

    Merged currency conversion exercises and refactored piping exercise

--- week-1/Homework/extra/1-currency-conversion.js
+++ week-1/Homework/extra/1-currency-conversion.js
@@ -11,19 +11,25 @@ function convertToUSD() {}
   CURRENCY FORMATTING
   ===================
   The business is now breaking into the Brazilian market
-  Write a new function for converting to the Brazilian real
+  Write a new function for converting to the Brazilian real (exchange rate is 5.7 BRL to £)
   They have also decided that they should add a 1% fee to all foreign transactions
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
 function convertToBRL() {}
 
-/* DO NOT EDIT BELOW THIS LINE */
-/* --------------------------- */
+/* ======= TESTS - DO NOT MODIFY ===== */
 
-const product = "The life and times of Mozafar Haider";
-const price = 12.5;
-const priceInUSD = convertToUSD(price);
+function test(test_name, expr) {
+  let status;
+  if (expr) {
+      status = "PASSED"
+  } else {
+      status = "FAILED"
+  }
 
-console.log("Product: " + product);
-console.log("Price in USD: " + priceInUSD);
+  console.log(`${test_name}: ${status}`)
+}
+
+test('convertToUSD function works', convertToUSD(32) === 44.8)
+test('convertToBRL function works', convertToBRL(30) === 172.71)
