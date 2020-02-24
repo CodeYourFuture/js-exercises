375662698dc651518e5ee7dfb279cdd01fc00926
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-02-24 21:54:45 +0000

    udated

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,17 +1,23 @@
 // Add comments to explain what this function does. You're meant to use Google!
+
+//Gets random numberand multiplies it by 10.
 function getNumber() {
     return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+//cotatinates two variables wi and w2. if a and b replace w1 and w2 we get ab as result.
 function s(w1, w2) {
     return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+    return firstWord.concat(" ", secondWord," ",thirdWord);
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
 }
+console.log (concatenate("code"," your","future"));
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
