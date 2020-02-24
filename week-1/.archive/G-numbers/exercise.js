5a7ea133d594bcc29bb6b96cc677aae0920ef16b
ola <olasvoice@gmail.com>
2020-02-24 16:23:02 +0000

    numbers
    done

--- week-1/.archive/G-numbers/exercise.js
+++ week-1/.archive/G-numbers/exercise.js
@@ -1,1 +1,6 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+const numberOfStudents = 15
+const numberOfMentors = 8
+const totalNumber = numberOfStudents + numberOfMentors
+console.log(totalNumber)
