ef26a157303de456b1ca8c9701dcea82b446b0cc
tayfuntayfun <40833117+tayfuntayfun@users.noreply.github.com>
2020-02-24 23:19:38 +0000

    Update 3-function-output.js
    Update 3-function-output.js/tayfun

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,14 +1,20 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//This function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1) with approximately uniform distribution over that range — which you can then scale to your desired range.
 function getNumber() {
     return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+// This function concatenates the second string after the first string as seen below.
 function s(w1, w2) {
     return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+
+    return firstWord.concat(` ${secondWord} ${thirdWord}`);
+
+    //return firstWord.concat(" " + secondWord + " " + thirdWord);
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
 }
