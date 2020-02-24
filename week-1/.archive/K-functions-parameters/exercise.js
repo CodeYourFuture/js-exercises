81bb0f51bae3af543d49a5b99bad465a4b646dfd
ola <olasvoice@gmail.com>
2020-02-24 21:22:45 +0000

    functions-parameters
    done

--- week-1/.archive/K-functions-parameters/exercise.js
+++ week-1/.archive/K-functions-parameters/exercise.js
@@ -1,6 +1,6 @@
 // Complete the function so that it takes input parameters
-function multiply() {
-  // Calculate the result of the function and return it
+function multiply(num1, num2) {
+  return num1 * num2
 }
 
 // Assign the result of calling the function the variable `result`
--- week-1/.archive/K-functions-parameters/exercise4.js
+++ week-1/.archive/K-functions-parameters/exercise4.js
@@ -1,5 +1,9 @@
 // Declare your function first
 
 // Call the function and assign to a variable `sum`
+function addTwoNumbers(num1, num2) {
+    return num1 + num2
+}
 
+const sum = addTwoNumbers(13, 124)
 console.log(sum);
