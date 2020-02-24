5246cbc9c932ac3b1cf7ed1b726df03b49a9e2fe
ola <olasvoice@gmail.com>
2020-02-24 16:31:32 +0000

    floats
    done

--- week-1/.archive/I-floats/exercise.js
+++ week-1/.archive/I-floats/exercise.js
@@ -1,2 +1,9 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+const total = numberOfStudents + numberOfMentors
+
+const studentsInProcent = Math.round(numberOfStudents * 100 / 23)
+const mentorsInProcent = Math.round(numberOfMentors * 100 / 23)
+
+console.log(studentsInProcent)
+console.log(mentorsInProcent)
