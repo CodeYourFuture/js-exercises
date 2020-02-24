93b80836238ecbac9368f29ded896665f582bd53
ola <olasvoice@gmail.com>
2020-02-24 15:13:22 +0000

    hello world
    done

--- week-1/.archive/B-hello-world/exercise.js
+++ week-1/.archive/B-hello-world/exercise.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+console.log("Hello world. I just started learning JavaScript!");
+
+console.log("How are you doing today?");
+
+console.log("I love to lear JavaScript!");
+
+console.log(5);
