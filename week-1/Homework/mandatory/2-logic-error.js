39f86faacdbacf07ebf02ed91de335cf70662746
Roxana <roxana.c2007@gmail.com>
2020-02-25 21:12:02 +0000

    exercise 2

--- week-1/Homework/mandatory/2-logic-error.js
+++ week-1/Homework/mandatory/2-logic-error.js
@@ -1,16 +1,15 @@
 // The syntax for this function is valid but it has an error, find it and fix it.
 
 function trimWord(word) {
-  return wordtrim();
+  return word.trim();
 }
 
 function getWordLength(word) {
-  return "word".length()
+  return word.length;
 }
 
 function multiply(a, b, c) {
-  a * b * c;
-  return;
+  return a * b * c;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
@@ -27,5 +26,5 @@ function test(test_name, expr) {
 }
 
 test("fixed trimWord function", trimWord("  CodeYourFuture ") === "CodeYourFuture")
-test("fixed wordLength function", wordLength("A wild sentence appeared!") === 25)
+test("fixed wordLength function", getWordLength("A wild sentence appeared!") === 25)
 test("fixed multiply function", multiply(2,3,6) === 36)
