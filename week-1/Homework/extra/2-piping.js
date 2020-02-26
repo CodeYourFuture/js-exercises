b165b1b93ad8af1972e7b9882784472a2ac16d88
ola <olasvoice@gmail.com>
2020-02-26 09:01:51 +0000

    done
    done

--- week-1/Homework/extra/2-piping.js
+++ week-1/Homework/extra/2-piping.js
@@ -1,41 +1,26 @@
-/*
-  PIPING FUNCTIONS
-  ================
-  1. Write 3 functions:
-  - one that adds 2 numbers together
-  - one that multiplies 2 numbers together
-  - one that formats a number so it's returned as a string with a £ sign before it (e.g. 20 -> £20)
-
-  2. Using the variable startingValue as input, perform the following operations using your functions all
-  on one line (assign the result to the variable badCode):
-  - add 10 to startingValue
-  - multiply the result by 2
-  - format it
-  
-  3. Write a more readable version of what you wrote in step 2 under the BETTER PRACTICE comment. Assign
-  the final result to the variable goodCode
-*/
-
 function add(num1, num2) {
   return num1 + num2
+
+}
+console.log(add(1, 3))
+console.log(add(2.4, 5.3))
+
+function multiply() {
+
 }
 
-function multiply(num1, num2) {
-  return num1 * num2
-}
+function format() {
 
-function format(num) {
-  return "£" + num
 }
 
 // const startingValue = 2
 
-// Why can this code be seen as bad practice? Comment your answer.
+// // Why can this code be seen as bad practice? Comment your answer.
 // let badCode =
 
-/* BETTER PRACTICE */
+//   /* BETTER PRACTICE */
 
-// let goodCode =
+//   let goodCode =
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -47,13 +32,7 @@ function test(test_name, expr) {
     status = "FAILED"
   }
 
-  console.log(`
-  $ {
-    test_name
-  }: $ {
-    status
-  }
-  `)
+  console.log(`${test_name}: ${status}`)
 }
 
 test('add function - case 1 works', add(1, 3) === 4)
