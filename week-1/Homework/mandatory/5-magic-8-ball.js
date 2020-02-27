0d11a4e9fc868c91163db01c71407f22d0fc97ab
Sonjide <41310379+Sonjide@users.noreply.github.com>
2020-02-27 00:08:30 +0000

    created an array and parameters

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -50,13 +50,48 @@ function shakeBall() {}
 // The answer should come from shaking the ball
 let answer;
 
+let veryPositive [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - definitely",
+  "You may rely on it"
+]
+
+let positive [
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes"
+]
+
+let negative [
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again"
+]
+
+let veryNegative [
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful"
+]
+  
+
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer(veryPositive, positive, negative, veryNegative) {
 
+}
+  /* parameters are checking answers against the array*/ 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
 let logged;
@@ -94,3 +129,5 @@ function testAll() {
 }
 
 testAll();
+
+/*https://developers.google.com/web/tools/chrome-devtools/console/*/ 
