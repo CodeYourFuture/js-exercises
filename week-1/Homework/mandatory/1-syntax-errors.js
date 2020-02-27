de4bfc6af0e0e9b5519f9726759f3e8a180810bd
Fatma Oymak <sugunum@gmail.com>
2020-02-27 18:56:39 +0000

    my mentor still helping

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -5,7 +5,7 @@ function addNumbers(a ,b ,c) {
 }
 
 function introduceMe(name, age)
-return "Hello, my name is " + name "and I am " age + "years old";
+return `Hello, my name is   ${name} and I am  ${age}  years old`;
 
 function getRemainder(a, b) {
     remainder = a %% b;
