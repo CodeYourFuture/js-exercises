0432fc13df21c37eef8dd27551daa3ad9518656a
ellie <ellie.tms1371@gmail.com>
2020-02-27 10:40:41 +0000

    Ellie-Js : week-1

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -1,61 +1,36 @@
-/**
-
-Let's peer into the future using a Magic 8 Ball!
-https://en.wikipedia.org/wiki/Magic_8-Ball 
-
-There are a few steps to being able view the future though:
-* Ask a question
-* Shake the ball
-* Get an answer
-* Decide if it's positive or negative
-
-The question can be anything, but the answers are fixed,
-and have different levels of positivity or negativity.
-
-Below are the possible answers:
-
-## Very positive
-It is certain.
-It is decidedly so.
-Without a doubt.
-Yes - definitely.
-You may rely on it.
-
-## Positive
-As I see it, yes.
-Most likely.
-Outlook good.
-Yes.
-Signs point to yes.
-
-## Negative
-Reply hazy, try again.
-Ask again later.
-Better not tell you now.
-Cannot predict now.
-Concentrate and ask again.
-
-## Very negative
-Don't count on it.
-My reply is no.
-My sources say no.
-Outlook not so good.
-Very doubtful.
-*/
-
-// This should log "The ball has shaken!"
-// and return the answer.
-function shakeBall() {}
-
-// The answer should come from shaking the ball
+allAnswers = [
+"It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
+function shakeBall() {
+  console.log("The ball has shaken!");
+  randomNum = Math.floor(Math.random() * 20);
+  answer = allAnswers[randomNum];
+  return answer;
+}
 let answer;
-
-// When checking the answer, we should tell someone if the answer is
-// - very positive
-// - positive
-// - negative
-// - very negative
-function checkAnswer() {}
+let randomNum;
+fourAnswers = ["very positive", "positive", "negative", "very negative"];
+function checkAnswer(answer) {
+  return fourAnswers[Math.floor(randomNum/5)];
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
