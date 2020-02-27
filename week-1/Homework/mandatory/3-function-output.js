1b43ae8d58c8b815c67fe512f0fae486a08ce233
tayfuntayfun <40833117+tayfuntayfun@users.noreply.github.com>
2020-02-27 22:50:12 +0000

    Update to 3-function-output

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -12,9 +12,9 @@ function s(w1, w2) {
 
 function concatenate(firstWord, secondWord, thirdWord) {
 
-    return firstWord.concat(` ${secondWord} ${thirdWord}`);
-
-    //return firstWord.concat(" " + secondWord + " " + thirdWord);
+    return firstWord.concat(" " + secondWord + " " + thirdWord);
+    
+    //return firstWord.concat(` ${secondWord} ${thirdWord}`);
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
 }
