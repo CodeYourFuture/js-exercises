4ea1e9dade96fd06aa5d27968dffcfbf5fb8b9f2
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-02-28 13:30:07 +0000

    Update 5-magic-8-ball.js

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -53,7 +53,26 @@ console.log("The ball has shaken");
 
 // The answer should come from shaking the ball
 let answer = [
-  "It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."
+  "It is certain.", 
+  "It is decidedly so.", 
+  "Without a doubt.", 
+  "Yes - definitely.", 
+  "You may rely on it.", 
+  "As I see it, yes.", 
+  "Most likely.", 
+  "Outlook good.", 
+  "Yes.", 
+  "Signs point to yes.", 
+  "Reply hazy, try again.", 
+  "Ask again later.", 
+  "Better not tell you now.", 
+  "Cannot predict now.", 
+  "Concentrate and ask again.", 
+  "Don't count on it.", 
+  "My reply is no.", 
+  "My sources say no.", 
+  "Outlook not so good.", 
+  "Very doubtful."
 ];
 
 // When checking the answer, we should tell someone if the answer is
