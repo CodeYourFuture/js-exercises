e7096a9ec994244804be1ed37098db733ddadf21
CY <cyeburu@yahoo.com>
2020-02-28 20:49:04 +0000

    wk1 first commit

--- week-1/Homework/mandatory/4-tax.js
+++ week-1/Homework/mandatory/4-tax.js
@@ -5,7 +5,10 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(sTax, product) {
+  return sTax * product;
+}
+calculateSalesTax(0.2, 1000);
 
 /*
   CURRENCY FORMATTING
@@ -24,18 +27,27 @@ function formatCurrency() {}
 function test(test_name, expr) {
   let status;
   if (expr) {
-      status = "PASSED"
+    status = "PASSED";
   } else {
-      status = "FAILED"
+    status = "FAILED";
   }
 
-  console.log(`${test_name}: ${status}`)
+  console.log(`${test_name}: ${status}`);
 }
 
-test("calculateSalesTax function - case 1 works", calculateSalesTax(15) === 18)
-test("calculateSalesTax function - case 2 works", calculateSalesTax(17.5) === 21)
-test("calculateSalesTax function - case 3 works", calculateSalesTax(34) === 40.8)
+test("calculateSalesTax function - case 1 works", calculateSalesTax(15) === 18);
+test(
+  "calculateSalesTax function - case 2 works",
+  calculateSalesTax(17.5) === 21
+);
+test(
+  "calculateSalesTax function - case 3 works",
+  calculateSalesTax(34) === 40.8
+);
 
-test("formatCurrency function - case 1 works", formatCurrency(15) === "£18.00")
-test("formatCurrency function - case 2 works", formatCurrency(17.5) === "£21.00")
-test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.80")
+test("formatCurrency function - case 1 works", formatCurrency(15) === "£18.00");
+test(
+  "formatCurrency function - case 2 works",
+  formatCurrency(17.5) === "£21.00"
+);
+test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.80");
