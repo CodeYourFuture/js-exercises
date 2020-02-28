27232c674b6662d66713fc51b2ea9a1b4211573f
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-02-28 09:40:52 +0000

    excercise 1 finished

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -4,14 +4,16 @@ function addNumbers(a, b, c) {
     return a + b + c;
 }
 
-function introduceMe(name, age)
-return "Hello, my name is " + name + "and I am " + age + "years old";
+function introduceMe(name, age) {
+    return "Hello, my name is " + name + " and I am " + age + " years old";
+}
+
 
 function getRemainder(a, b) {
     remainder = a % b;
 
     // Use string interpolation here
-    return "The remainder is %{remainder}"
+    return `The remainder is ${remainder}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
