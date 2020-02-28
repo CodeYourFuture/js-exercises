f3ef139f0f05ae3c739faf3671f39010cc27bd01
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-02-28 10:39:49 +0000

    Third excersice finished

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,14 +1,24 @@
 // Add comments to explain what this function does. You're meant to use Google!
+
+//Math.random() generates a number between 0 and 1 and this fucntion returns a number between 0 and 10
+
 function getNumber() {
     return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+
+//The concat() function concatenates the string arguments to the calling string and returns a new string. Changes to the original string or the returned string don't affect the other.If the arguments are not of the type string, they are converted to string values before concatenating.
+
+// it takes two entity and concatinate them.w1 has to be string but w2 can be numbers aswell
+
 function s(w1, w2) {
     return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
+
+   return firstWord.concat(" ",secondWord, " ",thirdWord);
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
 }
