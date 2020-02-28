05c2c3d51737fe9bf43b335885fe399c5f53e753
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-02-28 16:07:02 +0000

    switch needs to be corrected

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -45,18 +45,68 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
 
-// The answer should come from shaking the ball
-let answer;
+const myFuture = [
+  [
+    "It is certain",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it"
+  ],
+  [
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good",
+    "Yes.",
+    "Signs point to yes."
+  ],
+  [
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again."
+  ],
+  [
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ]
+];
+let a = Math.floor(Math.random() * Math.floor(4));
+let b = Math.floor(Math.random() * Math.floor(5));
+function shakeBall() {
+  console.log("The ball has shaken!");
 
+  // The answer should come from shaking the ball
+
+  let answer = myFuture[a][b];
+  return answer;
+  console.log(answer);
+}
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
 function checkAnswer() {}
-
+switch (a) {
+  case 1:
+    text = "very positive";
+    break;
+  case 2:
+    text = "positive";
+    break;
+  case 3:
+    text = "negative";
+    break;
+  case 4:
+    text = "very negative";
+    break;
+}
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
 let logged;
