df1e927b01c5afe78cabb2b91ab01584f2a868ca
Abbas Mokhtari <49838027+abbasmokhtari@users.noreply.github.com>
2020-02-28 17:28:20 +0000

    1st part pass

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -46,46 +46,48 @@ Very doubtful.
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
-  alert("ask a question");
-
-console.log("The ball has shaken");
+  let randomNumber = Math.floor(Math.random() * 20);
+  console.log("The ball has shaken!");
+  return randomNumber;
 }
 
 // The answer should come from shaking the ball
-let answer = [
-  "It is certain.", 
-  "It is decidedly so.", 
-  "Without a doubt.", 
-  "Yes - definitely.", 
-  "You may rely on it.", 
-  "As I see it, yes.", 
-  "Most likely.", 
-  "Outlook good.", 
-  "Yes.", 
-  "Signs point to yes.", 
-  "Reply hazy, try again.", 
-  "Ask again later.", 
-  "Better not tell you now.", 
-  "Cannot predict now.", 
-  "Concentrate and ask again.", 
-  "Don't count on it.", 
-  "My reply is no.", 
-  "My sources say no.", 
-  "Outlook not so good.", 
-  "Very doubtful."
+let answer = function generateAnswer (){
+  [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
 ];
+}
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer() { }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
 let logged;
-console.log = function() {
+console.log = function () {
   log(...arguments);
   logged = arguments[0];
 };
