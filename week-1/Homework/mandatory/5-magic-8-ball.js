1ae5917ac2c112c13bd3f3d762980c3aa2ea5ad9
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2020-02-29 22:22:49 +0000

    new vertion switch corrected

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -45,10 +45,96 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
+
+//Created a switch 
+
+
+function shakeBall() {
+  console.log("The ball has shaken!");
+let randomNumber = Math.floor(Math.random() * 20);
+let eightBall = " ";
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'without a doubt';
+    break;
+  case 3:
+    eightBall = 'yes-definantly' ;
+    break;
+  case 4:
+    eightBall = 'You may rely on it';
+    break;
+
+  case 5:
+    eightBall = 'As I see it, yes.';
+    break;
+  case 6:
+    eightBall = 'Most likely.';
+    break;
+  case 7:
+    eightBall = 'Outlook good.';
+    break;
+
+  case 8:
+     eightBall = 'Yes.';
+    break;
+
+  case 9:
+     eightBall = 'Signs point to yes.';
+   break;
+  
+  case 10:
+     eightBall = 'Reply hazy, try again.';
+   break;
+   
+  case 11:
+    eightBall = 'Ask again later.';
+   break; 
+  
+   case 12:
+    eightBall = 'Better not tell you now.';
+   break; 
+  
+  case 13:
+    eightBall = 'Cannot predict now.';
+   break; 
+  
+   case 14:
+    eightBall = 'Concentrate and ask again.';
+   break; 
+  
+   case 15:
+    eightBall = 'Don\'t count on it.';
+   break; 
+
+  case 16:
+   eightBall = 'My reply is no.';
+  break; 
+  
+  case 17:
+    eightBall = 'My sources say no.';
+   break; 
+
+   case 18:
+    eightBall = 'Outlook not so good.';
+   break;
+
+   case 19:
+    eightBall = 'Very doubtful.';
+   break;
+}
+
+return eightBall;
+}
+
 
 // The answer should come from shaking the ball
-let answer;
+
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
