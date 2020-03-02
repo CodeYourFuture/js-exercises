dcab76f875c9ac76bd776f11d30c6f6c641ef6a6
ola <olasvoice@gmail.com>
2020-03-02 22:23:17 +0000

    .
    .

--- week-2/D-predicates/exercise.js
+++ week-2/D-predicates/exercise.js
@@ -7,7 +7,7 @@
 
 // Finish the predicate function to test if the passed number is negative (less than zero)
 function isNegative(number) {
-
+return 
 }
 
 // Finish the predicate function to test if the passed number is between 0 and 10
