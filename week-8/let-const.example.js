1f501264ceb899e4d10de8d26659ed6413e6fbef
Babak <babakjahangiri@gmail.com>
2020-03-02 13:42:41 +0000

    delete unuseful folders

--- week-8/let-const.example.js
+++
@@ -1,9 +0,0 @@
-function compareNumbers(m, n) {
-  if (m < n) {
-    let message = m + " is smaller than " + n;
-  } else {
-    let message = m + " is bigger than or equal to " + n;
-  }
-
-  return message;
-}
