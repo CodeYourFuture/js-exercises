bbe2eaa2776e3ed9f5df87fa2ea33c2eb01dba84
[Alisina92] <asb92.jobs@gmail.com>
2020-03-03 15:58:03 +0000

    JS1-week 2-Alisina

--- week-2/Homework/mandatory/1-fix-functions.js
+++ week-2/Homework/mandatory/1-fix-functions.js
@@ -1,21 +1,21 @@
 // The below functions are syntactically correct but not outputting the right results.
 // Look at the tests and see how you can fix them.
 
-function mood() {
-  let isHappy = true;
+function mood(isHappy) {
+  
 
-  if (isHappy) {
+  if (isHappy===true) {
     return "I am happy";
   } else {
     return "I am not happy";
   }
 }
 
-function greaterThan10() {
+function greaterThan10(isBigEnough) {
   let num = 10;
-  let isBigEnough;
+  
 
-  if (isBigEnough) {
+  if (isBigEnough>=num) {
     return "num is greater than or equal to 10";
   } else {
     return "num is not big enough";
@@ -24,15 +24,15 @@ function greaterThan10() {
 
 function sortArray() {
   let letters = ["a", "n", "c", "e", "z", "f"];
-  let sortedLetters;
-
+  let sortedLetters = letters.sort();
+   console.log(sortedLetters);
   return sortedLetters;
 }
 
 function first5() {
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  let sliced;
-
+  let sliced = numbers.slice() ;
+  console.log(sliced);
   return sliced;
 }
 
