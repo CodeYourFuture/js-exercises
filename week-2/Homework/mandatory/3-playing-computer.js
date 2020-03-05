2659a9a29ee4e612243c15fc11408ea101903df6
Alex <alex.andrul10@gmail.com>
2020-03-05 16:05:14 +0000

    sx3

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -32,9 +32,9 @@ const f2 = function(a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-// console.log(b);
+console.log(x); //2
+console.log(a); //6
+console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
