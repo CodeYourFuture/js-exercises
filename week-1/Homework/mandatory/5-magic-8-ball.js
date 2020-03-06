e8c68ae250bac284ad668eef0cc45f199e690764
BANI GIT <bani.rezaie@gmail.com>
2020-03-06 21:35:12 +0000

    improvement 1

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -45,17 +45,91 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
-
-// The answer should come from shaking the ball
+const number8 = Math.ceil(Math.random() * 20);
+function shakeBall() {
+  console.log(`The ball has shaken!`);
+  if (number8 === 1) {
+    answer = "It is certain.";
+  } else if (number8 === 2) {
+    answer = "It is decidedly so.";
+  } else if (number8 === 3) {
+    answer = "Without a doubt.";
+  } else if (number8 === 4) {
+    answer = "Yes - definitely.";
+  } else if (number8 === 5) {
+    answer = "You may rely on it.";
+  } else if (number8 === 6) {
+    answer = "As I see it, yes.";
+  } else if (number8 === 7) {
+    answer = "Most likely.";
+  } else if (number8 === 8) {
+    answer = "Outlook good.";
+  } else if (number8 === 9) {
+    answer = "Yes.";
+  } else if (number8 === 10) {
+    answer = "Signs point to yes.";
+  } else if (number8 === 11) {
+    answer = "Reply hazy, try again.";
+  } else if (number8 === 12) {
+    answer = "Ask again later.";
+  } else if (number8 === 13) {
+    answer = "Better not tell you now.";
+  } else if (number8 === 14) {
+    answer = "Cannot predict now.";
+  } else if (number8 === 15) {
+    answer = "Concentrate and ask again.";
+  } else if (number8 === 16) {
+    answer = "Don't count on it.";
+  } else if (number8 === 17) {
+    answer = "My reply is no.";
+  } else if (number8 === 18) {
+    answer = "My sources say no.";
+  } else if (number8 === 19) {
+    answer = "Outlook not so good";
+  } else if (number8 === 20) {
+    answer = "Very doubtful";
+  }
+  return answer;
+}
 let answer;
 
-// When checking the answer, we should tell someone if the answer is
-// - very positive
-// - positive
-// - negative
-// - very negative
-function checkAnswer() {}
+function checkAnswer() {
+  if (
+    number8 === 1 ||
+    number8 === 2 ||
+    number8 === 3 ||
+    number8 === 4 ||
+    number8 === 5
+  ) {
+    return "very positive";
+  } else if (
+    number8 === 6 ||
+    number8 === 7 ||
+    number8 === 8 ||
+    number8 === 9 ||
+    number8 === 10
+  ) {
+    return "positive";
+  } else if (
+    number8 === 11 ||
+    number8 === 12 ||
+    number8 === 13 ||
+    number8 === 14 ||
+    number8 === 15
+  ) {
+    return "negative";
+  } else if(
+    number8 === 16 ||
+    number8 === 17 ||
+    number8 === 18 ||
+    number8 === 19 ||
+    number8 === 20
+  ) {
+    return "very negative";
+  }
+}
+console.log(shakeBall());
+console.log(checkAnswer());
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
