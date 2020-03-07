68059da29d4d6c839239da72e10e17297321da8c
ola <olasvoice@gmail.com>
2020-03-07 20:44:43 +0000

    L-array
    done

--- week-2/L-array-methods-2/exercise2.js
+++ week-2/L-array-methods-2/exercise2.js
@@ -7,7 +7,10 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  return splitedString = str[0].toUpperCase() + str.slice(1)
+
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
--- week-2/L-array-methods-2/exercise3.js
+++ week-2/L-array-methods-2/exercise3.js
@@ -7,7 +7,13 @@
 var ukNations = ["Scotland", "Wales", "England", "Northern Ireland"];
 
 function isInUK(country) {
-  return; // complete this statement
+  if (country.includes("France")) {
+    return false
+  } else if (country.includes("Republic of Ireland")) {
+    return false
+  } else {
+    return true
+  }
 }
 
 /* 
