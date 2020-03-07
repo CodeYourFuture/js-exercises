e4b1897e8fa30b04a8e1909e607e3a89aab0fae0
tayfuntayfun <40833117+tayfuntayfun@users.noreply.github.com>
2020-03-07 21:37:19 +0000

    Extra Homework

--- week-2/Homework/extra/4-radio-stations.js
+++ week-2/Homework/extra/4-radio-stations.js
@@ -3,7 +3,6 @@
  * Let's use some code to help us build a program that helps us scan
  * the radio waves for some good music.
  */
-
 /**
  * First, let's create a function that creates a list of all the frequencies.
  * Call this function `getAllFrequencies`.
@@ -12,18 +11,15 @@
  * - Create an array  starting at 87 and ending in 108
  * - Should return this array to use in other functions
  */
-
 // `getAllFrequencies` goes here
-
-function getAllFrequencies(){
- 
+function getAllFrequencies() {
   let frequencies = [];
-  for (i = 87 ; i < 109 ; i ++){
+  for (let i = 87; i < 109; i++) {
     frequencies.push(i);
   }
   return frequencies;
 }
-//console.log(getAllFrequencies());
+console.log(getAllFrequencies());
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
  * Call this function `getStations`.
@@ -35,17 +31,32 @@ function getAllFrequencies(){
  * - Return only the frequencies that are radio stations.
  */
 // `getStations` goes here
-function getStations (stationHere){
-  let  isRadioFrequency = getAllFrequencies
-    
-    if(getAllFrequencies){
-
-
+function getStations() {
+  let avaStats = getAllFrequencies();
+  let stations = [];
+  for (let i = 0; i < avaStats.length; i++) {
+    if (isRadioStation(avaStats[i])) {
+      stations.push(avaStats[i]);
+      
     }
+    // stations.sort();
 
-
+       for (let b = 0; b < stations.length ; b++) {
+    for(let j = 0 ; j < stations.length-1-b; j++){ // this nested loop let the function to check the condition for all the elements of the array 
+    if (stations[j] > stations[j + 1]) {
+  //     // swap
+      var temp = stations[j];
+      stations[j] = stations[j+1];
+      stations[j + 1] = temp;
+    }
+   }
+  }
+  }
+  console.log (stations);
+  return stations;
+  
 }
-
+// console.log(getStations());
 /**
  * Lastly, let's make a function for people to use.
  * When this runs, it should use the above two functions to log to the `console`
@@ -54,11 +65,24 @@ function getStations (stationHere){
  * - "No station found at ${frequency}, moving on"
  */
 // `searchRadioWaves` goes here
-
+function searchRadioWaves() {
+  let lastArr = getStations();
+  console.log(lastArr, "***checking selected stations***")
+    if (lastArr.length > 0) {
+       for (let a = 0; a < lastArr.length; a++) {
+      // if ( lastArr[a]> 86 && lastArr[a]<109){
+          console.log(`  🎶  station found at ${lastArr[a]}, enjoy!`);
+        }
+    }
+    else {
+      console.log(`No station found at ${lastArr[0]}, moving on`);
+    }      
+}
+searchRadioWaves();
+// searchRadioWaves();
 /* ======= TESTS - DO NOT MODIFY ======= */
-
 function isRadioStation(frequency) {
-  // This is a way of storing the random frequency array inside a 
+  // This is a way of storing the random frequency array inside a
   // function as storing it outside is bad practice. Don't worry if you
   // don't understand some bits! You're more than welcome to ask questions
   // at any time. :)
@@ -67,17 +91,14 @@ function isRadioStation(frequency) {
     const availableStations = new Array(stationCount)
       .fill(undefined)
       .map((_, i) => {
-        return Math.floor(Math.random() * (10800 - 8700 + 1) + 8700) / 100;
+        return Math.floor(Math.floor(Math.random() * (10800 - 8700 + 1) + 8700) / 100);
       });
-
     this.stations = availableStations;
+    console.log(availableStations);
   }
-
   return this.stations.includes(frequency);
 }
-
 const assert = require("assert");
-
 function test(testName, fn) {
   try {
     fn();
@@ -86,12 +107,10 @@ function test(testName, fn) {
     console.log(`${testName}: FAIL`);
   }
 }
-
 test("getAllFrequencies", () => {
   const freqs = getAllFrequencies();
   assert((freqs.length = 22 && freqs[0] === 87 && freqs[21] === 108));
 });
-
 test("getStations", () => {
   const stations = getStations();
   assert(
