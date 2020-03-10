9dbdd1da9f1d15e1b4520b9b6b944ab764b65d19
Babak <babakjahangiri@gmail.com>
2020-03-10 17:56:26 +0000

    excer1 done

--- week-3/Homework/mandatory/1-oxygen-levels.js
+++ week-3/Homework/mandatory/1-oxygen-levels.js
@@ -9,32 +9,33 @@ To be safe, they need to land on the first unamed planet that has Oxygen levels 
 Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
 */
 
-function safeLevels() {
-
+function safeLevels(oxlevels) {
+  //19.5 - 23
+  return oxlevels.sort().find(el => parseFloat(el) > 19.5);
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"]
-const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"]
+const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"];
+const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"];
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels1) === "19.9%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels1) === "19.9%"
 );
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels2) === "20.2%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels2) === "20.2%"
 );
