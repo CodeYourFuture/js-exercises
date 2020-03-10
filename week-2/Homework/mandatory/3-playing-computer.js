1923651bdf47ba7ec75ca37b1ea076bcd72b3deb
ellie <ellie.tms1371@gmail.com>
2020-03-10 01:03:15 +0000

    Ellie_Week_2

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -8,11 +8,11 @@
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
   2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
-  7. What is the value of the "a" outer variable when "f1" is called for the first time?
+  3. What is printed to the console?//answer: b is not defined
+  4. How many times is "f1" called? //answer : 3 times
+5. How many times is "f2" called? // answer: 2 times
+  6. What value does the "a" parameter take in the first "f1" call?//answer: 8
+  7. What is the value of the "a" outer variable when "f1" is called for the first time?//answer: 6
 */
 
 let x = 2;
@@ -28,7 +28,6 @@ const f2 = function(a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
