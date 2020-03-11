86ba38e59328daa1c0898ffb7a547a122efa57c5
Sonjide <41310379+Sonjide@users.noreply.github.com>
2020-03-11 00:20:57 +0000

    update of playing computer

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -31,6 +31,7 @@ Task 2 - for loop exercise -
  // I don't understand the question???  Do you mean variable "let a = 6"???
 
 //Final table result of playing computer task 
+
 //                 Table of values
 //-------------------------------------------------------
 //   i   |   a   |   d   |   e   |   f1   |   f2   |
@@ -48,7 +49,8 @@ Task 2 - for loop exercise -
 let x = 2;
 let a = 6;
 const f1 = function(a, b) {   
-  // a = i, b = a from the "else" statement dealing [odd numbers]. // 1st time f1 is called the argument is (a = 1 , b = 8)  //  2nd time f1 is called the argument is (a = 3, b = 10)                   
+  // a = i, b = a from the "else" statement dealing [odd numbers]. 
+  // 1st time f1 is called the argument is (a = 1 , b = 8)  //  2nd time f1 is called the argument is (a = 3, b = 10)                   
   return a + b;  
   // 1st f1 result "(a = 1) + (b = 8) = 9"  // 2nd f1 result "(a = 3) + (b = 10) = 13" 
 };
@@ -56,17 +58,19 @@ const f2 = function(a, b) {
   //  a = i, b = x from the "if" statement dealing with [even numbers].   
   // 1st time f2 is called (a = 0 , b = 2)  // 2nd time f2 is called (a = 2, b = 2)  //3rd time f2 is called (a = 4, b = 2)
   return a + b + x;   
-  // 1st f2 result (a = 0) + (b = 2) + (x = 2) = [ 4 ]  // 2nd f2 result (a = 2) + (b = 2) + (x = 2) = [ 6 ]   // 3rd f2 result (a = 4) + (b = 2) + (x = 2) = [ 8 ]    
+  // 1st f2 result (a = 0) + (b = 2) + (x = 2) = [ 4 ]  // 2nd f2 result (a = 2) + (b = 2) + (x = 2) = [ 6 ]   
+  // 3rd f2 result (a = 4) + (b = 2) + (x = 2) = [ 8 ]    
 }
 console.log(x); 
 console.log(a); 
-//console.log(b); == Task 1 - answer - error - no variable assigned
+//console.log(b); == Task 1 - answer - error - no variable / no value assigned
 
 //Task 2 - 
 for (let i = 0; i < 5; ++i) {   
 // for loop result of [let i ] = 0,1,2,3,4 is less then 5 and then the loop ends. 
   a = a + 1;   
-//  a is a new variable = old variable + 1 = answer new variable // new variable a = 7, 8, 9, 10, 11           
+//  a is a new variable = old variable + 1 = answer new variable 
+// new variable a = 7, 8, 9, 10, 11           
   if (i % 2 === 0) {       
 // if [ i ] is an even number resulting from the for loop, then divide by number % 2 with zero remainder, then enter the "if" statement   
     const d = f2(i, x);    
