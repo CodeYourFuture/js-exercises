6d153aea637b698f7c76dd5959dbb726ca3079fc
CY <cyeburu@yahoo.com>
2020-03-11 20:42:41 +0000

    Sorting algorithm updated

--- week-2/Homework/mandatory/4-sorting-algorithm.js
+++ week-2/Homework/mandatory/4-sorting-algorithm.js
@@ -13,45 +13,72 @@ sort method 😎)
 You don't have to worry about making this algorithm work fast! The idea is to get you to
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
+function bubbleSort(array) {
+  var done = false;
+  while (!done) {
+    done = true;
+    for (var i = 1; i < array.length; i += 1) {
+      if (array[i - 1] > array[i]) {
+        done = false;
+        var tmp = array[i - 1];
+        array[i - 1] = array[i];
+        array[i] = tmp;
+      }
+    }
+  }
+
+  return array;
+}
 
 function sortAges(arr) {
+  const arrNumbers = []
+  for (let index = 0; index < arr.length; index++) {
+    if (typeof arr[index] === "number") {
+      arrNumbers.push(arr[index])
+
+    }
+
+  }
+  console.log(bubbleSort(arrNumbers))
+  return bubbleSort(arrNumbers)
+
 
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-const agesCase1 = ['🎹', 100, '💩', 55, '🥵', '🙈', 45, '🍕', 'Sanyia', 66, 'James', 23, '🎖','Ismeal']
+const agesCase1 = ['🎹', 100, '💩', 55, '🥵', '🙈', 45, '🍕', 'Sanyia', 66, 'James', 23, '🎖', 'Ismeal']
 const agesCase2 = ['28', 100, 60, 55, '75', '🍕', 'Elamin']
 
 function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-  
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-  
-    return true;
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
 }
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
-  
+
 test(
-    "sortAges function works - case 1",
-    arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
+  "sortAges function works - case 1",
+  arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
 );
-  
+
 test(
-    "sortAges function works - case 2",
-    arraysEqual(sortAges(agesCase2), [55, 60, 100])
+  "sortAges function works - case 2",
+  arraysEqual(sortAges(agesCase2), [55, 60, 100])
 );
