f367fe8c305dfad834d3b7cc13190f9e76268af4
Nader Akhgari <nader90akhgari@gmail.com>
2020-03-12 21:09:12 +0000

    I have done 5-journey-planner exercise

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -7,16 +7,38 @@
   NOTE: only the names should be returned, not the means of transport.
 */
 
-function journeyPlanner() {
 
+function journeyPlanner(transportMode){
+  const londonLocations = [
+  ["Angel", "tube", "bus"],
+  ["London Bridge", "tube", "river boat"],
+  ["Tower bridge", "tube", "bus"],
+  ["Greenwich", "bus", "river boat"]
+]
+  let bus =[];
+  let tube =[];
+  let riverBoat =[];
+  if(transportMode == "bus"){
+    bus = londonLocations.filter(Element => Element[1] == "bus" || Element[2] == "bus" );
+    return bus.map(Element => Element[0]);
+  } 
+  else if(transportMode == "tube"){
+    tube = londonLocations.filter(Element => Element[1] == "tube" || Element[2] == "tube" );
+    return tube.map(Element => Element[0]);
+  } 
+  else if(transportMode == "river boat"){
+    riverBoat = londonLocations.filter(Element => Element[1] == "river boat" || Element[2] == "river boat" );
+    return riverBoat.map(Element => Element[0]);
+  }
 }
+  console.log(journeyPlanner("river boat"));
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const londonLocations = [
   ["Angel", "tube", "bus"],
   ["London Bridge", "tube", "river boat"],
-  ["Tower bridge", "tube", "bus"],
+  ["Tower Bridge", "tube", "bus"],
   ["Greenwich", "bus", "river boat"]
 ]
 
@@ -53,13 +75,13 @@ test("journeyPlanner function works - case 1".
 test("journeyPlanner function works - case 2".
   arraysEqual(
     journeyPlanner(londonLocations, "bus"),
-    ["Angle", "Tower Bridge", "Greenwich"]
+    ["Angel", "Tower Bridge", "Greenwich"]
   )
 )
 
 test("journeyPlanner function works - case 3".
   arraysEqual(
     journeyPlanner(londonLocations, "tube"),
-    ["Angle", "London Bridge", "Tower Bridge"]
+    ["Angel", "London Bridge", "Tower Bridge"]
   )
 )
--- week-3/Homework/mandatory/7-password-validator.js
+++ week-3/Homework/mandatory/7-password-validator.js
@@ -29,7 +29,7 @@ function validatePasswords(passwords) {
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const passwords1 = ["Se%5", "TktE.TJTU", "384HsHF", "dvyyeyY!5", "tryT3729."]
-const passwords2 = ["StUFf27", "PlEnty", "Jai.33" "shajsaUA**&&", "PlEnty"]
+const passwords2 = ["StUFf27", "PlEnty", "Jai.33", "shajsaUA**&&", "PlEnty"]
 
 function arraysEqual(a, b) {
     if (a === b) return true;
