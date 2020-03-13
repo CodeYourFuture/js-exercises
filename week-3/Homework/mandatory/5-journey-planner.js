362f60b5569cdbd5a9ef768d8a65b66f36b1ce9e
Nader Akhgari <nader90akhgari@gmail.com>
2020-03-13 00:12:14 +0000

    I edited exercise 5

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -8,30 +8,17 @@
 */
 
 
-function journeyPlanner(transportMode){
-  const londonLocations = [
-  ["Angel", "tube", "bus"],
-  ["London Bridge", "tube", "river boat"],
-  ["Tower bridge", "tube", "bus"],
-  ["Greenwich", "bus", "river boat"]
-]
+function journeyPlanner(londonLocations, transportMode){
   let bus =[];
-  let tube =[];
-  let riverBoat =[];
-  if(transportMode == "bus"){
-    bus = londonLocations.filter(Element => Element[1] == "bus" || Element[2] == "bus" );
+    bus = londonLocations.filter(Element => Element.includes(transportMode) );
     return bus.map(Element => Element[0]);
-  } 
-  else if(transportMode == "tube"){
-    tube = londonLocations.filter(Element => Element[1] == "tube" || Element[2] == "tube" );
-    return tube.map(Element => Element[0]);
-  } 
-  else if(transportMode == "river boat"){
-    riverBoat = londonLocations.filter(Element => Element[1] == "river boat" || Element[2] == "river boat" );
-    return riverBoat.map(Element => Element[0]);
-  }
 }
-  console.log(journeyPlanner("river boat"));
+  console.log(journeyPlanner([
+    ["Angel", "tube", "bus"],
+    ["London Bridge", "tube", "river boat"],
+    ["Tower Bridge", "tube", "bus", "taxi"],
+    ["Greenwich", "bus", "river boat"]
+  ], "taxi"));
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -65,21 +52,21 @@ function test(test_name, expr) {
     console.log(`${test_name}: ${status}`);
 }
 
-test("journeyPlanner function works - case 1".
+test("journeyPlanner function works - case 1",
   arraysEqual(
     journeyPlanner(londonLocations, "river boat"),
     ["London Bridge", "Greenwich"]
   )
 )
 
-test("journeyPlanner function works - case 2".
+test("journeyPlanner function works - case 2",
   arraysEqual(
     journeyPlanner(londonLocations, "bus"),
     ["Angel", "Tower Bridge", "Greenwich"]
   )
 )
 
-test("journeyPlanner function works - case 3".
+test("journeyPlanner function works - case 3",
   arraysEqual(
     journeyPlanner(londonLocations, "tube"),
     ["Angel", "London Bridge", "Tower Bridge"]
