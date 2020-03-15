2123509563c2692d580745bb3f47616d5fe72e12
Sonjide <41310379+Sonjide@users.noreply.github.com>
2020-03-15 00:29:41 +0000

    upload corrects files

--- week-1/Homework/mandatory/4-tax.js
+++ week-1/Homework/mandatory/4-tax.js
@@ -22,7 +22,7 @@ function calculateSalesTax(productPrice) {
 
 function formatCurrency(productPrice) {
   let totalPrice = calculateSalesTax(productPrice);
-  return ("£" + totalPrice);
+  return ("£" + totalPrice.toFixed(2));
 }
 
 
@@ -43,6 +43,6 @@ test("calculateSalesTax function - case 1 works", calculateSalesTax(15) === 18)
 test("calculateSalesTax function - case 2 works", calculateSalesTax(17.5) === 21)
 test("calculateSalesTax function - case 3 works", calculateSalesTax(34) === 40.8)
 
-test("formatCurrency function - case 1 works", formatCurrency(15) === "£18")
-test("formatCurrency function - case 2 works", formatCurrency(17.5) === "£21")
-test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.8")
+test("formatCurrency function - case 1 works", formatCurrency(15) === "£18.00")
+test("formatCurrency function - case 2 works", formatCurrency(17.5) === "£21.00")
+test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.80")
