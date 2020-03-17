60c8853e1b3c68b62efb447af4ca7e7e0b2932cf
Rayhan Miah <rayhanart94@yahoo.co.uk>
2020-03-17 14:20:07 +0000

    Update 5-magic-8-ball.js

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -46,18 +46,98 @@ Very doubtful.
 // This should log "The ball has shaken!"
 // and return the answer.
 function shakeBall() {
-  
+  console.log("The ball has shaken!");
+  let answers = answer[Math.floor(Math.random() * answer.length)];
 }
 
+let check = shakeBall();
+
 // The answer should come from shaking the ball
-let answer;
+//store in one array of all possibilities
+let answer = 
+[
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes - definitely.",
+  "You may rely on it.",
+  
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+
+  "Very negative.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+
+];
+
+let veryPositive = 
+[ 
+"It is certain.",
+"It is decidedly so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+];
+let positive = [
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+];
+let negative = [
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+];
+let veryNegative = [
+  "Very negative.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
+
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer() {
+   if(veryPositive.includes(check))
+   {
+     return "very positive.";
+   }
+  else if(positive.includes(check))
+   {
+     return "positive.";
+   }
+   else if(veryNegative.includes(check))
+   {
+     return "very negative.";
+   }
+   else (negative.includes(check))
+   {
+     return "negative.";
+   }
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
