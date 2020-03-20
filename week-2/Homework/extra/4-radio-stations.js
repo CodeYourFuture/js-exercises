8b15d68affff609204825013c976383d83deb41e
ola <olasvoice@gmail.com>
2020-03-20 22:48:52 +0000

    done
    done

--- week-2/Homework/extra/4-radio-stations.js
+++ week-2/Homework/extra/4-radio-stations.js
@@ -1,6 +1,4 @@
-
-
-for (let i = 0; i < 15; i++){
+for (let i = 0; i < 15; i++) {
   if (i % 2 === 0) {
     return `${i} is even`
   } else {
@@ -8,9 +6,10 @@ for (let i = 0; i < 15; i++){
   }
 }
 
+kd velv lefnvl
 
 
-
+test
 
 
 
