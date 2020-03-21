4e3f3af9e3b6b0e0b2bf0e70ea233a43296da9c6
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-03-21 14:21:52 +0000

    change

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -13,12 +13,14 @@ function journeyPlanner() {
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
+/* ======= TESTS - DO NOT MODIFY ===== */
+
 const londonLocations = [
   ["Angel", "tube", "bus"],
   ["London Bridge", "tube", "river boat"],
-  ["Tower bridge", "tube", "bus"],
+  ["Tower Bridge", "tube", "bus"],
   ["Greenwich", "bus", "river boat"]
-]
+];
 
 function arraysEqual(a, b) {
   if (a === b) return true;
@@ -33,33 +35,38 @@ function arraysEqual(a, b) {
 }
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
-test("journeyPlanner function works - case 1".
-  arraysEqual(
-    journeyPlanner(londonLocations, "river boat"),
-    ["London Bridge", "Greenwich"]
-  )
-)
+test(
+  "journeyPlanner function works - case 1",
+  arraysEqual(journeyPlanner(londonLocations, "river boat"), [
+    "London Bridge",
+    "Greenwich"
+  ])
+);
 
-test("journeyPlanner function works - case 2".
-  arraysEqual(
-    journeyPlanner(londonLocations, "bus"),
-    ["Angle", "Tower Bridge", "Greenwich"]
-  )
-)
+test(
+  "journeyPlanner function works - case 2",
+  arraysEqual(journeyPlanner(londonLocations, "bus"), [
+    "Angel",
+    "Tower Bridge",
+    "Greenwich"
+  ])
+);
 
-test("journeyPlanner function works - case 3".
-  arraysEqual(
-    journeyPlanner(londonLocations, "tube"),
-    ["Angle", "London Bridge", "Tower Bridge"]
-  )
-)
+test(
+  "journeyPlanner function works - case 3",
+  arraysEqual(journeyPlanner(londonLocations, "tube"), [
+    "Angel",
+    "London Bridge",
+    "Tower Bridge"
+  ])
+);
