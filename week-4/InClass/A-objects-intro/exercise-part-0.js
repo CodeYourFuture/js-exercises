e4627a6d21ecdff39de4438a660817b3498177af
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-03-22 23:09:22 +0000

    class exercise

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -4,4 +4,12 @@ Describe your own laptop as a JavaScript object
 
 Try to think of as many properties as you can!
 
+
 */
+
+let laptop={
+  brand:"Mac",
+  screensize:15,
+  isTouchable:false,
+  color:"grey",
+}
