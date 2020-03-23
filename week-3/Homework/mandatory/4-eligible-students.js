ca91c99c077eaabb5d2b7ca55e8f711608eaed8d
Mo Nashawati <59769628+mnashawati@users.noreply.github.com>
2020-03-23 18:00:27 +0000

    Update 4-eligible-students.js

--- week-3/Homework/mandatory/4-eligible-students.js
+++ week-3/Homework/mandatory/4-eligible-students.js
@@ -4,7 +4,7 @@
   Create a function which:
   - Accepts an array which contains all the students' names and their attendance counts
     (see tests to confirm how this data will be structured)
-  - Returns an array containing only the names of the students who have attended AT LEAST 8 classes
+  - Returns an array containing on the names of the students who have attended AT LEAST 8 classes
 */
 
 function eligibleStudents(arr) {
