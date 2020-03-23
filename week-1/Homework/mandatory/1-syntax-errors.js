2ef49bd119971c261f05cd38922c550b1d10ee17
sugun fatma <sugunum@gmail.com>
2020-03-23 15:27:57 +0000

    sonjide showed me  how to save files manually
    all the exercises compeleted please review

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -1,11 +1,13 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a ,b ,c) {
+function addNumbers(a b c) {
     return a + b + c;
 }
 
+function
+
 function introduceMe(name, age)
-return `Hello, my name is   ${name} and I am  ${age}  years old`;
+return "Hello, my name is " + name "and I am " age + "years old";
 
 function getRemainder(a, b) {
     remainder = a %% b;
@@ -14,12 +16,6 @@ function getRemainder(a, b) {
     return "The remainder is %{remainder}"
 }
 
-
-function getRemainder (a,b) {
-    remainder = a % b;
-
-    return `The remainder is ${remainder}`
-}
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -2,25 +2,15 @@
 function getNumber() {
     return Math.random() * 10;
 }
-// Math.random() returns a random number between 0(inclusive) and 1(exclusive)
-// Math.random() always returns a number lower than 1
 
 // Add comments to explain what this function does. You're meant to use Google!
 function s(w1, w2) {
     return w1.concat(w2);
 }
 
-// concat() joins two or more strings and returns a new joined string
-//(if w1,w2 are arrays it will merge them)
-
-
-
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
-}
-function concatenate(firstWord, secondWord, thirdWord) {
-    return 
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
