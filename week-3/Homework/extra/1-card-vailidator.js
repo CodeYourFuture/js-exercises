4217bfad89a3c467f3ce4eb4bb23d67dd0184e51
Babak <babakjahangiri@gmail.com>
2020-03-23 17:51:27 +0000

    update

--- week-3/Homework/extra/1-card-vailidator.js
+++ week-3/Homework/extra/1-card-vailidator.js
@@ -1,13 +1,29 @@
-const checkCard = function(cardNo) {
+function validateCard(cardNo) {
   if (cardNo.length !== 16 && isNaN(cardNo)) return false;
 
   // The final digit must be even
   let lastDigit = cardNo[cardNo.length - 1];
   if (lastDigit % 2 !== 0) return false;
-};
 
-function cardValidator(cNumbers) {
-  return console.log(cNumbers.map(cn => checkCard(cNumbers)));
+  // You must have at least two different digits represented (all of the digits cannot be the same)
+  let obj = {};
+  for (let i = 0; i < cardNumber.length; i++) {
+    obj[cardNumber[i]] = true;
+  }
+  if (Object.keys(obj).length < 2) {
+    return false;
+  }
 }
 
-console.log(checkCard());
+const cardNumbers1 = [
+  "6786766656778888",
+  "1234567890123456",
+  "9999999999999999",
+  "01928383922929a9",
+  "1010101010101001",
+  "2273829397627821"
+];
+
+console.log(cNumbers.map(cn => checkCard(cNumbers)));
+
+console.log(checkCard(787657577897));
