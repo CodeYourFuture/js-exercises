2062821860a2ae8f9d360845f6f5311f0b066782
ola <olasvoice@gmail.com>
2020-03-23 17:32:00 +0000

    com
    com

--- week-2/Homework/extra/4-radio-stations.js
+++ week-2/Homework/extra/4-radio-stations.js
@@ -7,14 +7,16 @@
 /**
  * First, let's create a function that creates a list of all the frequencies.
  * Call this function `getAllFrequencies`.
- *
+
  * This function should:
  * - Create an array  starting at 87 and ending in 108
  * - Should return this array to use in other functions
  */
-
-// `getAllFrequencies` goes here
-
+//sort
+function getAllFrequencies(frequencies) {
+  return frequencies = [87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108]
+}
+console.log(getAllFrequencies())
 /**
  * Next, let's write a function that gives us only the frequencies that are radio stations.
  * Call this function `getStations`.
@@ -22,33 +24,41 @@
  * This function should:
  * - Get the available frequencies from `getAllFrequencies`
  * - There is a helper function called isRadioFrequency that takes an integer as an argument and returns a boolean.
+ * - Sort the stations by low - high e.g. 1,2,3,4,5
  * - Return only the frequencies that are radio stations.
  */
-// `getStations` goes here
+function getStations() {
 
+}
+
+/**
+ * Lastly, let's make a function for people to use.
+ * When this runs, it should use the above two functions to log to the `console`
+ * Call this function `searchRadioWaves`.
+ * - "🎶 station found at ${frequency}, enjoy!"
+ * - "No station found at ${frequency}, moving on"
+ */
+// `searchRadioWaves` goes here
 
 /* ======= TESTS - DO NOT MODIFY ======= */
 
-function getAvailableStations() {
-  // Using `stations` as a property as defining it as a global variable wouldn't
-  // always make it initialized before the function is called
-  if (!getAvailableStations.stations) {
+function isRadioStation(frequency) {
+  // This is a way of storing the random frequency array inside a 
+  // function as storing it outside is bad practice. Don't worry if you
+  // don't understand some bits! You're more than welcome to ask questions
+  // at any time. :)
+  if (!this.stations) {
     const stationCount = 4;
-    getAvailableStations.stations = new Array(stationCount)
+    const availableStations = new Array(stationCount)
       .fill(undefined)
-      .map(function() {
-        return Math.floor(Math.random() * (108 - 87 + 1) + 87);
-      })
-      .sort(function(frequencyA, frequencyB) {
-        return frequencyA - frequencyB;
+      .map((_, i) => {
+        return Math.floor(Math.random() * (10800 - 8700 + 1) + 8700) / 100;
       });
+
+    this.stations = availableStations;
   }
 
-  return getAvailableStations.stations;
-}
-
-function isRadioStation(frequency) {
-  return getAvailableStations().includes(frequency);
+  return this.stations.includes(frequency);
 }
 
 const assert = require("assert");
@@ -56,43 +66,20 @@ const assert = require("assert");
 function test(testName, fn) {
   try {
     fn();
-    console.log(`\n✅ ${testName}: PASS`);
-  } catch (error) {
-    console.log(
-      `\n❌ ${testName}: FAIL (see details below)\n\n${error.message}`
-    );
+    console.log(`${testName}: PASS`);
+  } catch (e) {
+    console.log(`${testName}: FAIL`);
   }
 }
 
-test("getAllFrequencies() returns all frequencies between 87 and 108", function() {
-  const frequencies = getAllFrequencies();
-  assert.deepStrictEqual(frequencies, [
-    87,
-    88,
-    89,
-    90,
-    91,
-    92,
-    93,
-    94,
-    95,
-    96,
-    97,
-    98,
-    99,
-    100,
-    101,
-    102,
-    103,
-    104,
-    105,
-    106,
-    107,
-    108
-  ]);
+test("getAllFrequencies", () => {
+  const freqs = getAllFrequencies();
+  assert((freqs.length = 22 && freqs[0] === 87 && freqs[21] === 108));
 });
 
-test("getStations() returns all the available stations", () => {
+test("getStations", () => {
   const stations = getStations();
-  assert.deepStrictEqual(stations, getAvailableStations());
+  assert(
+    JSON.stringify(stations) === JSON.stringify(isRadioStation.stations.sort())
+  );
 });
