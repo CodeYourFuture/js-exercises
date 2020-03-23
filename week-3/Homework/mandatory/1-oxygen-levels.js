d255779a988d731921852ac76d07f1d4d4c6ff57
ola <olasvoice@gmail.com>
2020-03-23 17:00:58 +0000

    push
    push

--- week-3/Homework/mandatory/1-oxygen-levels.js
+++ week-3/Homework/mandatory/1-oxygen-levels.js
@@ -1,42 +1,42 @@
-/*
-Many years into the future, a team of Space Voyagers find their ship is low on Oxygen and need to dock
-somewhere safe while they call home for help.
-  
-Their computer detects a list of nearby planets that have Oxygen in their atmosphere.
+// /*
+// Many years into the future, a team of Space Voyagers find their ship is low on Oxygen and need to dock
+// somewhere safe while they call home for help.
 
-To be safe, they need to land on the first unamed planet that has Oxygen levels between 19.5% and 23.5%.
+// Their computer detects a list of nearby planets that have Oxygen in their atmosphere.
 
-Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
-*/
+// To be safe, they need to land on the first unamed planet that has Oxygen levels between 19.5% and 23.5%.
 
-function safeLevels(arr) {
-  return arr.find(element => element > "19.5%" && element < "23.5%")
-}
+// Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
+// */
 
-// safeLevels(oxygenLevels1)
-// safeLevels(oxygenLevels2)
-/* ======= TESTS - DO NOT MODIFY ===== */
+// function safeLevels(arr) {
+//   return arr.find(element => element > "19.5%" && element < "23.5%")
+// }
 
-const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"]
-const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"]
+// // safeLevels(oxygenLevels1)
+// // safeLevels(oxygenLevels2)
+// /* ======= TESTS - DO NOT MODIFY ===== */
 
-function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+// const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"]
+// const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"]
 
-  console.log(`${test_name}: ${status}`);
-}
+// function test(test_name, expr) {
+//   let status;
+//   if (expr) {
+//     status = "PASSED";
+//   } else {
+//     status = "FAILED";
+//   }
 
-test(
-  "safeLevels function works - case 2",
-  safeLevels(oxygenLevels1) === "19.9%"
-);
+//   console.log(`${test_name}: ${status}`);
+// }
 
-test(
-  "safeLevels function works - case 2",
-  safeLevels(oxygenLevels2) === "20.2%"
-);
+// test(
+//   "safeLevels function works - case 2",
+//   safeLevels(oxygenLevels1) === "19.9%"
+// );
+
+// test(
+//   "safeLevels function works - case 2",
+//   safeLevels(oxygenLevels2) === "20.2%"
+// );
