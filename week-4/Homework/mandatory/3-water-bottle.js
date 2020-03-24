bf9b83aa3020c3d5f2ca3f3678321483ff7076be
Babak <babakjahangiri@gmail.com>
2020-03-24 22:10:57 +0000

    update

--- week-4/Homework/mandatory/3-water-bottle.js
+++ week-4/Homework/mandatory/3-water-bottle.js
@@ -12,11 +12,11 @@ let bottle = {
   volume: 0,
   fill: function() {
     // calling this function should make you bottles volume = 100;
-   return this.volume = 100;
+    return (this.volume = 100);
   },
   drink: function() {
     // calling this function should decrease your bottles volume by 10;
-    return this.volume =- 10;
+    return (this.volume -= 10);
   },
   empty: function() {
     // this function should return true if your bottles volume = 0
