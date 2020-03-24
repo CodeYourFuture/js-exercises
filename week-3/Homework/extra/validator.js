2d914a5e9b23566d396508bbc8ed69eed5c1ccd2
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-03-24 19:44:16 +0000

    new

--- week-3/Homework/extra/validator.js
+++ week-3/Homework/extra/validator.js
@@ -1,5 +1,19 @@
+var regNum = new RegExp('[0-9]');
 validateCardNum(num){
     return {
-        num.find()
+        num.length===16 &&
+        regNum.test(num)! &&
+        
+
     }
-}
+// if (
+//     pass.length >= 5 &&
+//     regexUpper.test(pass) &&
+//     regexLower.test(pass) &&
+//     regexNum.test(pass) &&
+//     regexSymbol.test(pass) &&
+//     !existingPass
+//   ) {
+//     return true;
+//   } else {
+//     return false;
