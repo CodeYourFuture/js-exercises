2210fd0e159c540e1e1801c7bb7d05f1f1803b53
hamidreza <hrfarzadeh@gmail.com>
2020-03-24 14:46:02 +0000

    Pls check my code

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -7,9 +7,82 @@
   NOTE: only the names should be returned, not the means of transport.
 */
 
-function journeyPlanner() {
-
+function isPlannerBus(element) {
+  return element == 'bus';
 }
+function isPlannerTube(element) {
+  return element == 'tube';
+}
+function isplannerBoat(element) {
+  return element == 'river boat';
+}
+
+function journeyPlanner(arr, transpo) {
+  if (transpo == 'bus') {
+    let newArr = [];
+    let row1 = arr[0].filter(isPlannerBus);
+    let row2 = arr[1].filter(isPlannerBus);
+    let row3 = arr[2].filter(isPlannerBus);
+    let row4 = arr[3].filter(isPlannerBus);
+    if (row1 == 'bus') {
+      newArr.push(arr[0][0]);
+    }
+    if (row2 == 'bus') {
+      newArr.push(arr[1][0]);
+    }
+    if (row3 == 'bus') {
+      newArr.push(arr[2][0]);
+    }
+    if (row4 == 'bus') {
+      newArr.push(arr[3][0]);
+    }
+    console.log(newArr);
+    return newArr;
+  }
+  if (transpo == 'tube') {
+    let newArrTube = [];
+    let rowTube1 = arr[0].filter(isPlannerTube);
+    let rowTube2 = arr[1].filter(isPlannerTube);
+    let rowTube3 = arr[2].filter(isPlannerTube);
+    let rowTube4 = arr[3].filter(isPlannerTube);
+    if (rowTube1 == 'tube') {
+      newArrTube.push(arr[0][0]);
+    }
+    if (rowTube2 == 'tube') {
+      newArrTube.push(arr[1][0]);
+    }
+    if (rowTube3 == 'tube') {
+      newArrTube.push(arr[2][0]);
+    }
+    if (rowTube4 == 'tube') {
+      newArrTube.push(arr[3][0]);
+    }
+    console.log(newArrTube);
+    return newArrTube;
+  }
+  if (transpo == 'river boat') {
+    let newArrFinal = [];
+    let rowFinal1 = arr[0].filter(isplannerBoat);
+    let rowFinal2 = arr[1].filter(isplannerBoat);
+    let rowFinal3 = arr[2].filter(isplannerBoat);
+    let rowFinal4 = arr[3].filter(isplannerBoat);
+    if (rowFinal1 == 'river boat') {
+      newArrFinal.push(arr[0][0]);
+    }
+    if (rowFinal2 == 'river boat') {
+      newArrFinal.push(arr[1][0]);
+    }
+    if (rowFinal3 == 'river boat') {
+      newArrFinal.push(arr[2][0]);
+    }
+    if (rowFinal4 == 'river boat') {
+      newArrFinal.push(arr[3][0]);
+    }
+    console.log(newArrFinal);
+    return newArrFinal;
+  }
+}
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
