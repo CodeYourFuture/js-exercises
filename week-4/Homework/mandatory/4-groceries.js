73e89698c756ce109ac22d28f6c7f2aa1929f000
yusuf963 <58535314+yusuf963@users.noreply.github.com>
2020-03-24 19:36:59 +0000

    done

--- week-4/Homework/mandatory/4-groceries.js
+++ week-4/Homework/mandatory/4-groceries.js
@@ -6,7 +6,13 @@
 let groceriesToBuy = [];
 
 let groceryList = {
-  item1: "",
-  item2: "",
-  item3: ""
+  item1: "Potatoes",
+  item2: "Orange Juice",
+  item3: "Rice"
 };
+
+for(let key in groceryList){
+ groceriesToBuy.push(groceryList[key])
+}
+console.log(groceriesToBuy)
+
