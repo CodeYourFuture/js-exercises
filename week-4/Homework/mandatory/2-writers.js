480c5233cf916fb09863e2eae22bfd41ac3c3c2d
Mo Nashawati <mo.nashawati@gmail.com>
2020-03-25 15:03:59 +0000

    Update 2-writers.js

--- week-4/Homework/mandatory/2-writers.js
+++ week-4/Homework/mandatory/2-writers.js
@@ -42,3 +42,11 @@ let writers = [
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+
+writers.forEach(writer => {
+  if (writer.alive) {
+    console.log(
+      `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`
+    );
+  }
+});
