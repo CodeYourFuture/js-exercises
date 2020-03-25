826e28bf94b39a2507daf83d78efdf79b84f24c6
Alexey Brambalov <44169757+AlexeyBrambalov@users.noreply.github.com>
2020-03-25 21:46:48 +0000

    homework

--- week-4/Homework/mandatory/2-writers.js
+++ week-4/Homework/mandatory/2-writers.js
@@ -39,6 +39,10 @@ let writers = [
   }
 ];
 
+writers.filter(writer => writer.alive).forEach(writer => console.log(
+ `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`));
+
+
 /*
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
