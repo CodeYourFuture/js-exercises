b1625d49e3f07d8e896ebc62de7b5d840c242845
antonio miranda <antsmiranda@gmail.com>
2020-03-27 23:23:05 +0100

    Revert changes to match Migracode-Barcelona Syllabus
    Fixed an issue introduced in
    https://github.com/CodeYourFuture/js-exercises/pull/399#commitcomment-37567367
    where the content of the exercices got out of sync with the current content in
    the Syllabus.

---
+++ week-1-practise/level-2/1-tax.js
@@ -0,0 +1,20 @@
+/*
+  SALES TAX
+  =========
+  A business requires a program that calculates how much sales tax to charge
+  Sales tax is 20% of the price of the product
+*/
+
+function calculateSalesTax() {}
+
+/* DO NOT EDIT BELOW THIS LINE */
+/* --------------------------- */
+
+var product = "The life and times of Mozafar Haider";
+var price = 12.5;
+var salesTax = calculateSalesTax(price);
+
+console.log("Product: " + product);
+console.log("Price: £" + price);
+console.log("Sales tax: £" + salesTax);
+console.log("Total: £" + (price + salesTax));
---
+++ week-4-practise/level-3/CODEWARS.md
@@ -0,0 +1,9 @@
+Complete the following CodeWars exercises. Go to the webpages below and follow the instructions there.
+
+Click "ATTEMPT" to test your solution.
+
+Exercises:
+- https://www.codewars.com/kata/crash-override/train/javascript
+- https://www.codewars.com/kata/strive-matching-number-1/train/javascript
+- http://www.codewars.com/kata/strive-matching-number-2/train/javascript
+- http://www.codewars.com/kata/strive-matching-number-3/train/javascript
