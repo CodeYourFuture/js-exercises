481708296e4335434b08dbaeb51592ce423e602e
Babak <babakjahangiri@gmail.com>
2020-03-28 21:22:54 +0000

    update part-0

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,13 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+let = laptop = {
+   brand : "Asus",
+   screenSize : 17,
+   isTouchscreen : false,
+   memory: "16 GB",
+   hdd: "1 Tb",
+   os: ["windows" , "linux"]
+
+}
