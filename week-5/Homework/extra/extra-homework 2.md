3e987064617d246861bceb3433386cc8fd6a1edf
Melanie <melanie_titley@hotmil.com>
2020-03-29 22:25:48 +0100

    Updates

---
+++ week-5/Homework/extra/extra-homework 2.md
@@ -0,0 +1,15 @@
+# Complete this online course
+
+You should complete the following sections from this tutorial:
+
+- DOM animation
+- Using JS libraries in your webpage
+- Further learning
+
+https://www.khanacademy.org/computing/computer-programming/html-css-js
+
+
+# Readings
+
+- [Eloquent JavaScript - JavaScript and the Browser](https://eloquentjavascript.net/13_browser.html)
+
---
+++ week-6/InClass/E-webchat/exercise-1 2.js
@@ -0,0 +1,37 @@
+/*
+Let's build a webchat to communicate with each other!
+
+The requirements of a basic webchat are:
+- The user can write a message in an input and send it after clicking on the submit button.
+- After clicking on the submit button, the input should be empty.
+- The user can read the existing messages in the message list.
+- The message list is refreshed automatically every few seconds so 
+the user can keep reading the incoming messages without refreshing the page.
+
+
+========
+Task 1
+========
+
+Your task is to build the webchat frontend. 
+Some code is already written in index.html, that you can open in your browser.
+For the purpose of this exercise, you are provided a server which you can interact 
+with to get and save messages. Some messages already exist on the server. 
+
+Your first task is to display those messages in the page, 
+in the HTML element with the id "message-list". Use the following API to get the messages:
+
+HTTP Verb: GET
+API: https://codeyourfuture.herokuapp.com/api/messages
+
+
+===============
+Expected result
+===============
+
+When you open index.html in your browser, it should display the existing messages on the page.
+
+*/
+
+
+// Write your code here
