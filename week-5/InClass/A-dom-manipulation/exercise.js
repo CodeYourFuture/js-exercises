da5b82d4e9635a7c3eb33f3dbd5cb7c68d19a591
ola <olasvoice@gmail.com>
2020-04-01 19:07:36 +0100

    done
    done

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -16,7 +16,6 @@ Write JavaScript below that logs:
 
 */
 
-
 /*
 Task 2
 ======
@@ -24,6 +23,12 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+let alertButton = document.querySelector("#alertBtn");
+console.log(alertButton);
+
+alertButton.addEventListener("click", function() {
+  alert("Thanks for visiting Bikes for Refugees!");
+});
 
 /*
 Task 3
@@ -32,15 +37,12 @@ Task 3
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
 
-
 /*
 Task 4
 ======
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
-
-
 
 /*
 Task 5
