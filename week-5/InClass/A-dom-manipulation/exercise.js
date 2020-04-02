9f1bcbb1f2abf66fa1f7bc9b2ad4ace0bbc7e3a7
Hanaif <hanaif1@gmail.com>
2020-04-02 21:45:55 +0100

    week1js1
    Ths my test

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,15 +15,21 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
+console.log(document.querySelectorAll('p'))
 
+console.log(document.querySelectorAll('prmery'))
+    /*
+    Task 2
+    ======
 
-/*
-Task 2
-======
+    When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
+    */
+    //let myButton = document.querySelector("#alertBtn");
+    //myButton.addEventListener("click", alertSomething);
 
-When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
-*/
-
+function alertSomething() {
+    alert("Thank You So Much! for visiting the Bikes for Refugees");
+}
 
 /*
 Task 3
@@ -31,15 +37,38 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
+//let myElement = document.querySelector("body");
+//myElement.style.backgroundColor = "lightGray";
 
+let changeClr = document.querySelector("#bgrChangeBtn");
+changeClr.addEventListener("click", changeBgr);
 
+function changeBgr() {
+    document.body.style.backgroundColor = "lightGray";
+}
 /*
 Task 4
 ======
 
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
+//let paragraph = document.createElement("p"); // here we're just creating it, element is not visible yet
+//myElement.appendChild(paragraph); // now the element is added to our view, but it's empty
+//let jumbotron = document.querySelector(".jumbotron");
+//jumbotron.appendChild(paragraph);
+//paragraph.innerText = "Read more below.";
 
+
+let myButton = document.querySelector("#addTextBtn");
+myButton.addEventListener("click", ReadMoreBelow);
+let paragraph = document.createElement("p"); // here we're just creating it, element is not visible yet
+let myElement = document.querySelector(".buttons")
+paragraph.innerText = "Read more below.";
+
+function ReadMoreBelow() {
+    //let paragraph = document.createElement("p"); // here we're just creating it, element is not visible yet
+    myElement.appendChild(paragraph); // now the element is added to our view, but it's empty
+}
 
 
 /*
