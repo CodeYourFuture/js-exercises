40bd47a7ebf8337cccbb6958bcbdca3967493467
hamidreza <hrfarzadeh@gmail.com>
2020-04-03 21:02:11 +0100

    Update main.js

---
+++ week-5/Homework/mandatory/3-project/js/main.js
@@ -0,0 +1,12 @@
+let btnBlue = document.querySelector("#blueBtn");
+//let btnOrange = 
+function btnBackColor() {
+    let jumbortonn = document.querySelector(".jumbotron");
+    //let donateColor = document.querySelector(".btn btn-primary");
+    //donateColor.style.backgroundColor = "red"
+    jumbortonn.style.backgroundColor = "#588fbd"
+    
+}
+btnBlue.addEventListener("click",btnBackColor);
+
+
