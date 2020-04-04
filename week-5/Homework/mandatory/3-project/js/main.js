c04f7d9cbb086df5f17588e28fb26a99b9929a19
hamidreza <hrfarzadeh@gmail.com>
2020-04-04 15:32:17 +0100

    Update main.js

--- week-5/Homework/mandatory/3-project/js/main.js
+++ week-5/Homework/mandatory/3-project/js/main.js
@@ -1,11 +1,12 @@
 let btnBlue = document.querySelector("#blueBtn");
-//let btnOrange = 
+let btnDiv = document.querySelectorAll(".btn-lrg");
+let getChild1 = btnDiv[0]
+let getChild2 = btnDiv[1];
 function btnBackColor() {
     let jumbortonn = document.querySelector(".jumbotron");
-    //let donateColor = document.querySelector(".btn btn-primary");
-    //donateColor.style.backgroundColor = "red"
     jumbortonn.style.backgroundColor = "#588fbd"
-    
+    getChild1.style.backgroundColor = "#ffa500"
+    getChild2.style.backgroundColor = "black"
 }
 btnBlue.addEventListener("click",btnBackColor);
 
