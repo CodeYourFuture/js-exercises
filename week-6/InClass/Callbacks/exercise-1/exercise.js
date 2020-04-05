c466a8ee7bf93b33c1a86fbb7416a00a09f710e8
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-04-05 14:53:03 +0100

    backgroundChange

--- week-6/InClass/Callbacks/exercise-1/exercise.js
+++ week-6/InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,16 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+
+
+let colorPage = function(){
+    let colors = ["white", "black", "green","pink"];
+  
+    let background = document.getElementsByTagName("body");
+    console.log(background);
+background[0].style.backgroundColor = colors[Math.floor(Math.random() * 3)];
+}
+
+
+
+setInterval(colorPage, 500)
