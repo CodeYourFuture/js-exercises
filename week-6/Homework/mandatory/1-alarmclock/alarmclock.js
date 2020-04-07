a0c25d0a94670d206dcd30b8c720c193b8c78417
Mo Nashawati <mo.nashawati@gmail.com>
2020-04-08 00:08:20 +0100

    Update alarmclock.js

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,10 +1,25 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeLeft = Number(document.getElementById("alarmSet").value);
+  // TODO: prevent multiple clicks of set alarm from creating multiple timers
+  const intervalID = window.setInterval(myCallback, 1000);
+  function myCallback() {
+    if (timeLeft > 0) {
+      timeLeft -= 1;
+      document.getElementById(
+        "timeRemaining"
+      ).innerText = `Time Remaining: 00:${timeLeft}`;
+      if (timeLeft === 0) {
+        playAlarm();
+        clearInterval(intervalID);
+      }
+    }
+  }
+}
 
 // DO NOT EDIT BELOW HERE
+var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
   console.log(document.getElementById("set"));
 
   document.getElementById("set").addEventListener("click", () => {
@@ -16,11 +31,11 @@ function setup() {
   });
 }
 
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
 
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
 
