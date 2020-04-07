78c6d0f88b819b8f4e8d7648ed067ac7e644372b
TamBahri <59769742+TamBahri@users.noreply.github.com>
2020-04-07 14:55:54 +0100

    JS exercuses week5

--- week-5/Homework/mandatory/2-exercises/exercises.js
+++ week-5/Homework/mandatory/2-exercises/exercises.js
@@ -1,5 +1,5 @@
 /**
- *
+ *  objects = key + value , name: "Chris", job: "Teacher"
  * For each of the names in the array passed into this function
  *
  * - Add a <h1> tag to the website containing the name of the person
@@ -14,17 +14,34 @@
  * </div>
  */
 function exerciseOne(arrayOfPeople) {
-  let content = document.querySelector("#content");
+  arrayOfPeople.forEach((person) => {
+    let content = document.querySelector("#content");
+    let Names = document.createElement("h1");
+    let Jobs = document.createElement("h2");
+    Names.innerHTML = person.name;
+    Jobs.innerHTML = person.job;
+    content.appendChild(Names);
+    content.appendChild(Jobs);
+  });
 }
 
 /**
  *
- * Create a list of shopping items. You should use an unordered list.
+ * Create a list of shopping items.[Array of items[] You should use an unordered list.
  *
  * All of your HTML should go inside the Div tag with the id "content".
  *
  */
 function exerciseTwo(shopping) {
+  let content = document.querySelector("#content");
+  let shoppingList = document.createElement("ul");
+  shopping.forEach((arrayListItem) => {
+    let unorderList = document.createElement("li");
+    shoppingList.appendChild(unorderList);
+    unorderList.innerHTML = arrayListItem;
+  });
+  content.appendChild(shoppingList);
+
   //Write your code in here
 }
 
@@ -58,8 +75,26 @@ function exerciseTwo(shopping) {
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
 function exerciseThree(books) {
-  //Write your code in here
+  books.forEach((bookBody) => {
+    let content = document.querySelector("#content");
+    let bookInfo = document.createElement("p");
+    let unorderBookList = document.createElement("ul");
+    let infList = document.createElement("li");
+    content.appendChild(unorderBookList);
+    unorderBookList.appendChild(infList);
+    infList.appendChild(bookInfo);
+    bookInfo.innerHTML = `${bookBody.title}, ${bookBody.author}`;
+
+    if (bookBody.alreadyRead == true) {
+      infList.style.backgroundColor = "green";
+    } else {
+      infList.style.backgroundColor = "red";
+    }
+  });
 }
+// (bookBody.alreadyRea)? infList.style.backgroundColor = "green": infList.style.backgroundColor = "red";
+
+//Write your code in here
 
 //
 //
@@ -74,7 +109,7 @@ function exerciseThree(books) {
 let people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
-  { name: "Boris", job: "Prime Minister" }
+  { name: "Boris", job: "Prime Minister" },
 ];
 
 exerciseOne(people);
@@ -87,18 +122,18 @@ const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
-  }
+    alreadyRead: true,
+  },
 ];
 
 exerciseThree(books);
