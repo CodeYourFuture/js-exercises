1aa916a9e3dcdabe3ec83dc2bebb429155e787b8
Mo Nashawati <mo.nashawati@gmail.com>
2020-04-08 00:54:00 +0100

    Fix #13

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,13 +1,12 @@
 function setAlarm() {
   let timeLeft = Number(document.getElementById("alarmSet").value);
-  // TODO: prevent multiple clicks of set alarm from creating multiple timers
-  const intervalID = window.setInterval(myCallback, 1000);
-  function myCallback() {
+  const intervalID = window.setInterval(intervalCallback, 1000);
+  function intervalCallback() {
     if (timeLeft > 0) {
       timeLeft -= 1;
       document.getElementById(
         "timeRemaining"
-      ).innerText = `Time Remaining: 00:${timeLeft}`;
+      ).innerText = `Time Remaining: ${formatTime(timeLeft)}`;
       if (timeLeft === 0) {
         playAlarm();
         clearInterval(intervalID);
@@ -15,6 +14,13 @@ function setAlarm() {
     }
   }
 }
+
+function formatTime(numOfSecs) {
+  let minsToDisplay = Math.floor(numOfSecs / 60);
+  let secsToDisplay = numOfSecs % 60;
+  return `${minsToDisplay}:${secsToDisplay}`;
+}
+
 // Testing github issues
 
 // DO NOT EDIT BELOW HERE
