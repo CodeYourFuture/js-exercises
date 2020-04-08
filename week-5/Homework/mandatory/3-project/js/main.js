ddd5c4cab33a0add656f6dbacfe023a120e2142d
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-08 20:59:01 +0100

    nearly finish

--- week-5/Homework/mandatory/3-project/js/main.js
+++ week-5/Homework/mandatory/3-project/js/main.js
@@ -1,58 +1,51 @@
 // All button 
-const clickerButtonB = document.getElementById("blueBtn");
+let clickerButtonB = document.getElementById("blueBtn");
 const clickerButtonO = document.getElementById("orangeBtn");
 const clickerButtonG = document.getElementById("greenBtn");
+let divForm = document.querySelector('.jumbotron');
+let bakeB = document.querySelector('.buttons .btn-primary');
+let valeB = document.querySelector('.buttons .btn-secondary');
 
 // for Blue button
-clickerButtonB.addEventListener('click', function(){
-     const divForm = document.getElementsByClassName('jumbotron');
-     const bakeB = document.getElementsByClassName('btn btn-primary btn-lrg');
-     const valeB = document.getElementsByClassName('btn btn-secondary btn-lrg');
-
+var clickerButton = function() {
      document.body.style.backgroundColor ='#588fbd';
      divForm.style.backgroundColor = '#588fbd';
 
      bakeB.style.backgroundColor = '#ffa500';
      valeB.style.backgroundColor = 'black';
-     valeB.style.textColor = 'white';
-});
+     valeB.style.color = 'white';
+};
+clickerButtonB.addEventListener('click', clickerButton);
 
 // for Orang button
 clickerButtonO.addEventListener('click', function(){
-    const divForm = document.getElementsByClassName('jumbotron');
-    const bakeB = document.getElementsByClassName('btn btn-primary btn-lrg');
-    const valeB = document.getElementsByClassName('btn btn-secondary btn-lrg');
-
     document.body.style.backgroundColor =`#f0ad4e`;
     divForm.style.backgroundColor = `#f0ad4e`;
 
     bakeB.style.backgroundColor = `#5751fd`;
     valeB.style.backgroundColor = `#31b0d5`;
-    valeB.style.textColor = `white`;
+    valeB.style.color = `white`;
 });
 
 //for green button 
 clickerButtonG.addEventListener(`click`, function(){
-    const divForm = document.getElementsByClassName(`jumbotron`);
-    const bakeB = document.getElementsByClassName(`btn btn-primary btn-lrg`);
-    const valeB = document.getElementsByClassName(`btn btn-secondary btn-lrg`);
-
     document.body.style.backgroundColor =`#87ca8a`;
     divForm.style.backgroundColor =`#87ca8a`;
 
     bakeB.style.backgroundColor = `black`;
     valeB.style.backgroundColor =`#8c9c08`;
-    valeB.style.textColor = `white`;
+    valeB.style.color = `white`;
 });
 
 // part 2:
 
-const SubButton = document.getElementsByClassName('lib-btn btn-primary');
-SubButton.addEventListener('click',function() {
-    const Dive = document.getElementById('content > div.alert.alert-info > strong ');
+const SubButton = document.querySelector('.form-group .btn-primary');
+SubButton.addEventListener('click', function(){
+    const Dive = document.getElementById('content');
     const Email = document.getElementById('exampleInputEmail1').value;
     const Name = document.getElementById('example-text-input').value;
     const Describe = document.getElementById('exampleTextarea').value;
+
     if (Email.length === 0 || Email.value === '@'){
         Email.style.backgroundColor = 'red';
     }if ( Name.length === 0 ){
@@ -61,10 +54,8 @@ SubButton.addEventListener('click',function() {
         Describe.style.backgroundColor = 'red';
     }else {
         Dive.innerHTML = 'thank you for filling out the form';
-        Email.value = 0;
+        Email.innerHTML = " ";
         Name.value = 0;
         Describe.value = 0;
-    }
-}
-
-);
+    };
+});
