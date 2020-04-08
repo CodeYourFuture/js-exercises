ed89a75253b4e5e7ebd5a258502a25f99fa6ad12
[Alisina92] <asb92.jobs@gmail.com>
2020-04-08 15:29:54 +0100

    Count down project done but is buggy
    my program works however the the timer only activates on the second click so would you please guide me?

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,56 @@
-function setAlarm() {}
+
+
+
+function setAlarm() {
+  let getAlarmBtn = document.getElementById('set');
+  let getSetTimeTo = document.getElementById('alarmSet');
+  let getTimeRemaing = document.getElementById('timeRemaining');
+  let getStopBtn = document.getElementById('stop');
+  let audioFile = document.createElement('audio');
+  audioFile.src = './alarmsound.mp3';
+  let userInput = getSetTimeTo.value;
+  
+  getAlarmBtn.addEventListener('click',()=>{
+    setInterval(finalCountDown,1000);
+    function finalCountDown(){
+      if (userInput>10 && userInput<60) {
+        getTimeRemaing.textContent = `Time Remaining: 00:${--userInput}`;
+      }else if(userInput>0 && userInput <=10){
+        getTimeRemaing.textContent =`Time Remaining: 00:0${--userInput}`;
+      }else if(userInput===0){
+        getTimeRemaing.textContent = `Time Remaining: 00:0${userInput}`;
+        audioFile.play();
+      } 
+      
+    }
+  });
+  getStopBtn.addEventListener('click', () => {
+    audioFile.pause();
+    audioFile.src='';
+
+  });
+}
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
+var audio = new Audio("alarmsound.mp3");
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
-  console.log(document.getElementById("set"));
-
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm(audio);
+    pauseAlarm();
   });
 }
-
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
-
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
-
 window.onload = setup;
