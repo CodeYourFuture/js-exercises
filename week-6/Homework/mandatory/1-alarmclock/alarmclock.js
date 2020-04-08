dbac7bbf54f7e599d4ba6ae995e14c8650b7772f
Mo Nashawati <mo.nashawati@gmail.com>
2020-04-08 01:30:45 +0100

    Misc fixes
    Adding displayTimeRemaining function
    Fixing bug of timeLeft == 1
    close #14 by adding 0 padding

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -2,26 +2,33 @@ function setAlarm() {
   let timeLeft = Number(document.getElementById("alarmSet").value);
   const intervalID = window.setInterval(intervalCallback, 1000);
   function intervalCallback() {
+    displayTimeRemaining(timeLeft);
     if (timeLeft > 0) {
       timeLeft -= 1;
-      document.getElementById(
-        "timeRemaining"
-      ).innerText = `Time Remaining: ${formatTime(timeLeft)}`;
-      if (timeLeft === 0) {
-        playAlarm();
-        clearInterval(intervalID);
-      }
+    } else {
+      playAlarm();
+      clearInterval(intervalID);
     }
   }
+}
+
+function displayTimeRemaining(timeLeft) {
+  document.getElementById(
+    "timeRemaining"
+  ).innerText = `Time Remaining: ${formatTime(timeLeft)}`;
 }
 
 function formatTime(numOfSecs) {
   let minsToDisplay = Math.floor(numOfSecs / 60);
   let secsToDisplay = numOfSecs % 60;
+  if (minsToDisplay < 10) {
+    minsToDisplay = "0" + minsToDisplay;
+  }
+  if (secsToDisplay < 10) {
+    secsToDisplay = "0" + secsToDisplay;
+  }
   return `${minsToDisplay}:${secsToDisplay}`;
 }
-
-// Testing github issues
 
 // DO NOT EDIT BELOW HERE
 var audio = new Audio("alarmsound.mp3");
