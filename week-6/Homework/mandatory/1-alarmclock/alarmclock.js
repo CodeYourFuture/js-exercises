3c7f4b5dc8813671e4cd7635d27665c9461fc06a
tayfuntayfun <40833117+tayfuntayfun@users.noreply.github.com>
2020-04-08 15:16:51 +0100

    Project-1 done, working on HTML/ Project-2

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,46 @@
-function setAlarm() {}
+
+function setAlarm() {
+  let audEl = document.querySelector("#myAudio");
+
+  let inputField = document.querySelector("#alarmSet");
+  let title = document.querySelector("#timeRemaining");
+  let counter = inputField.value;
+  return setInterval(function() {
+    title.innerHTML = "Time Remaining: 00:" + counter;
+    if (counter > 0){
+      counter--;
+      if (counter == 0){
+      clearInterval(counter);
+      playAlarm(audEl);
+      }     
+    }
+  }, 1000); 
+};
+
+
+
+// When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field. For example, if the `input` field says `10` then the title should say `Time Remaining: 00:10`.
+
+// Every one second the title should count down by one.
+
+// When the `Time Remaining` reaches `00:00` the alarm should play a sound. You can make the sound happen by using `playAlarm()`.
+
+// You can stop the alarm sound by pressing the `Stop Alarm` button.
 
 // DO NOT EDIT BELOW HERE
-
+var audio = new Audio("alarmsound.mp3");
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
-  console.log(document.getElementById("set"));
-
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm(audio);
+    pauseAlarm();
   });
 }
-
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
-
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
-
 window.onload = setup;
