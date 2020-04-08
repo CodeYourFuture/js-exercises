4a0df8a912f208b35d3f4d75a78bac6e534c0aa2
byohannes <42332389+byohannes@users.noreply.github.com>
2020-04-08 22:53:54 +0100

    Committing alarm counter  and quote generator

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,54 @@
-function setAlarm() {}
+let counter = {};
+function setAlarm() {
+  // getting the html elements
+  counter.timer = document.querySelector('#alarmSet').value;
+  counter.message = document.querySelector('#timeRemaining');
+  // if a negative number is entered , changing it to positive
+  if (counter.timer < 0) {
+    counter.timer *= -1;
+  }
+  // start the alarm countdown
+
+  counter.ticker = setInterval(function() {
+    counter.timer--;
+    //stop the countdown of the alarm and play the alarm
+    if (counter.timer <= 0) {
+      playAlarm();
+      clearInterval(counter.ticker);
+      counter.timer = 0;
+    }
+    // Calculate remaining time
+    counter.secs = counter.timer;
+    counter.mins = Math.floor(counter.secs / 60);
+    counter.secs -= counter.mins * 60;
+    // updating the HTML and setting up the time format on the heading message to 00:00
+    if (counter.mins < 10 && counter.secs < 10) {
+      counter.message.textContent = `Time Remaining: 0${counter.mins}:0${counter.secs}`;
+    } else if (counter.mins >= 10 && counter.secs < 10) {
+      counter.message.textContent = `Time Remaining: ${mins}:0${secs}`;
+    } else if (counter.mins < 10 && counter.secs >= 10) {
+      counter.message.textContent = `Time Remaining: 0${counter.mins}:${counter.secs}`;
+    } else {
+      counter.message.textContent = `Time Remaining: ${counter.mins}:${counter.secs}`;
+    }
+  }, 1000);
+}
+//setInterval(setAlarm,1000);
 
 // DO NOT EDIT BELOW HERE
-
+var audio = new Audio('alarmsound.mp3');
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
-  console.log(document.getElementById("set"));
-
-  document.getElementById("set").addEventListener("click", () => {
+  document.getElementById('set').addEventListener('click', () => {
     setAlarm();
   });
-
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm(audio);
+  document.getElementById('stop').addEventListener('click', () => {
+    pauseAlarm();
   });
 }
-
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
-
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
-
 window.onload = setup;
