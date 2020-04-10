05f8af187f8728de2f4260baa10eeeab655a8e9a
Nader Akhgari <nader90akhgari@gmail.com>
2020-04-10 13:58:45 +0100

    I have done exercise 1 of inclass

--- week-6/InClass/Callbacks/exercise-1/exercise.js
+++ week-6/InClass/Callbacks/exercise-1/exercise.js
@@ -11,3 +11,14 @@ Update your code to make the colour change every 5 seconds to something differen
 Prefer to work on a codepen? https://codepen.io/makanti/pen/abOreLg
 ================
 */
+function randColor(){
+    var x= Math.floor(Math.random(0,255)*255);
+    var y= Math.floor(Math.random(0,255)*255);
+    var z= Math.floor(Math.random(0,255)*255);
+    return `rgb(${x}, ${y}, ${z})`;
+}
+
+function changeColor(){
+    document.body.style.backgroundColor = randColor();
+}
+setInterval(changeColor,5000);
