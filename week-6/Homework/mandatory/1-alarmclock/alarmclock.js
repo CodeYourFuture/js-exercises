a7b21d3b86554dc9d6261715e3f44c30d728dc40
Nader Akhgari <nader90akhgari@gmail.com>
2020-04-11 19:16:56 +0100

    I have done alarmclock exercise

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,32 @@
-function setAlarm() {}
+let timeRemaining;
+let inNumVal;
+let innerval;
+
+ function setAlarm() {
+  let inputNum = document.querySelector("#alarmSet");
+   inNumVal = inputNum.value;
+   timeRemaining = document.querySelector("#timeRemaining");
+   console.log(inNumVal);
+   innerval = setInterval(decrement, 1000);
+}
+
+function decrement(){
+  if(inNumVal > 0){
+    inNumVal -= 1
+  } else{
+    playAlarm()
+    clearInterval(innerval);
+  };
+timeRemaining.innerHTML = `Time Remaining: ${numToTime(inNumVal)}`;
+}
+function numToTime(i) {
+  var minutes = Math.floor(i / 60);
+  var seconds = i  - (minutes * 60);
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return minutes+':'+seconds;
+}
+
 
 // DO NOT EDIT BELOW HERE
 
@@ -7,6 +35,7 @@ var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
+    
   });
 
   document.getElementById("stop").addEventListener("click", () => {
