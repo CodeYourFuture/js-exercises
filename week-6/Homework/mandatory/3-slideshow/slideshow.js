4ada069f9d8609ad005b891686459c5b12de38c3
Alex <alex.andrul10@gmail.com>
2020-04-12 17:21:14 +0100

    slideshow_first_buttons

--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,29 @@
 // Write your code here
+
+let slideIndex = 1;
+slideShow(slideIndex);
+
+function plusSlide(n) {
+  slideShow((slideIndex += n));
+}
+
+function slideShow(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+function forward() {
+  setTimeout(() => {
+    slideIndex++;
+  }, 1000);
+}
+document.getElementById("autoForward").addEventListener("click", forward);
