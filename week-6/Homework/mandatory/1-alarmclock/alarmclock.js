8d94aa9b7b6e54704a84d063d55ebe51a23af160
ola <olasvoice@gmail.com>
2020-04-12 18:27:59 +0100

    timer
    timer

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,38 @@
-function setAlarm() {}
+let inputField = document.querySelector("#timeRemaining");
+let alarmSet = document.querySelector("#alarmSet");
+let timerId = null
+
+function setAlarm() {
+  let getTheValue = alarmSet.value
+  inputField.innerHTML = `Time Remaining: 00:${getTheValue}`;
+  const timeCountDown = setInterval(() => {
+    getTheValue--
+    let remainingTime = inputField.innerHTML = `Time Remaining: 00:${getTheValue}`
+    if (getTheValue === 0) {
+      playAlarm()
+      clearInterval(timeCountDown)
+      document.body.style.backgroundColor = "pink"
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
+var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
-  console.log(document.getElementById("set"));
-
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm(audio);
+    pauseAlarm();
   });
 }
 
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
 
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
-
 window.onload = setup;
