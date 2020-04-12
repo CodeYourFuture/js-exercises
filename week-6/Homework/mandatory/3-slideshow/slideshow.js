c1c17eec3a028c342d65617d4fe29d4ac93b70ec
[Alisina92] <asb92.jobs@gmail.com>
2020-04-12 03:25:52 +0100

    image Slider first part done
    Please help me to display images

--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,39 @@
 // Write your code here
+const images = [{
+ url:'./example-screenshots/cat1.jpg', 
+},{
+  url:'./example-screenshots/cat2.jpg'
+},{
+  url:'./example-screenshots/cat3.jpg'
+  }, {
+  url:'./example-screenshots/cat4.jpg'
+  }];
+
+let index = 0;
+
+function nextItem(){
+    index++;
+    index = index % images.length;
+     return images[index].url;
+}
+
+function previousItem(){
+     if(index === 0){
+         index = images.length;
+     }
+        index--;
+      return images[index].url;
+    }
+    
+document.getElementById('imageHolder').textContent = images[index].url; 
+
+
+let forwBtn = document.getElementById('forwBtn');
+   forwBtn.addEventListener('click',()=>{
+     document.getElementById('imageHolder').textContent = nextItem();
+   });
+    
+let backwBtn = document.getElementById('backwBtn');
+   backwBtn.addEventListener('click',()=>{
+       document.getElementById('imageHolder').textContent = previousItem();
+   });
