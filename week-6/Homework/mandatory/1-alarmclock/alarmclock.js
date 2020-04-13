37602317a085541853e31326eabd9a5ac807fcb8
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-04-14 00:16:51 +0100

    week-6/1,2,3 js complete
    need styling 2 and 3

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,27 +1,73 @@
-function setAlarm() {}
+// ## How the clock should work
+
+// When you click the `Set Alarm` button the counter at the top of the screen should change to the number you entered in the `input` field. For example, if the `input` field says `10` then the title should say `Time Remaining: 00:10`.
+
+// Every one second the title should count down by one.
+
+// When the `Time Remaining` reaches `00:00` the alarm should play a sound. You can make the sound happen by using `playAlarm()`.
+
+// You can stop the alarm sound by pressing the `Stop Alarm` button.
+
+let reset = document.querySelector('#reset')
+
+function setAlarm(){
+  let inputField = document.querySelector('#alarmSet')
+  let title = document.querySelector('#timeRemaining')
+  let time = document.querySelector("#time");
+  let audioEl = document.querySelector("#myAudio")
+  let body = document.querySelector('body')
+  let set = inputField.value*1000
+  let countdown = setInterval(timer, 1000)
+
+    function timer() {
+      let min = Math.floor((set % (1000 * 60 * 60)) / (1000 * 60));
+      let sec = Math.floor((set % (1000 * 60)) / 1000);
+    
+      time.innerHTML = `${min} : ${sec}`
+      set = set-1000
+      if (set < 10000) {
+        let red = setTimeout(function () {
+          time.style.color = 'white'
+          title.style.color = 'white'
+        }, 700)
+        let white= setTimeout(function white () {
+          time.style.color = 'red'
+          title.style.color = 'red'
+        }, 300)
+        if (set < 0) {
+          clearTimeout(red)
+          clearTimeout(white)
+          body.style.background = 'red'
+          title.style.color = 'white'
+          time.style.color = 'white'
+        }
+      }
+      if (set < 0) {
+        clearInterval(countdown)
+        playAlarm(audioEl);
+      }
+    }
+}
+
+reset.addEventListener('click', ()=>{
+  location.reload(true)
+})
+
 
 // DO NOT EDIT BELOW HERE
-
+var audio = new Audio("alarmsound.mp3");
 function setup() {
-  var audio = new Audio("alarmsound.mp3");
-
-  console.log(document.getElementById("set"));
-
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
   });
-
   document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm(audio);
+    pauseAlarm();
   });
 }
-
-function playAlarm(audio) {
+function playAlarm() {
   audio.play();
 }
-
-function pauseAlarm(audio) {
+function pauseAlarm() {
   audio.pause();
 }
-
-window.onload = setup;
+window.onload = setup
--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,51 @@
 // Write your code here
+let a = "https://alicerosehome.files.wordpress.com/2019/12/20191203_164312-1-copy.jpg?w=906"
+let b = "https://alicerosehome.files.wordpress.com/2019/12/img-20191219-wa0008-1.jpg?w=616"
+let c = "https://alicerosehome.files.wordpress.com/2020/02/anna-and-olaf.jpg?w=1024"
+let d = "https://alicerosehome.files.wordpress.com/2019/02/20190209_152248_signature-1.jpg?w=616"
+let e = "https://alicerosehome.files.wordpress.com/2018/12/HarryPotter_20180327.jpg?w=616"
+let f = "https://alicerosehome.files.wordpress.com/2018/12/IceDragon_201705.jpg?w=616"
+
+links = [a, b, c, d, e, f];
+
+const autoBack = document.querySelector('#auto-back')
+const frwBtn = document.querySelector('#forwardbtn')
+const stopBtn = document.querySelector('#stop')
+const backBtn = document.querySelector('#backbtn')
+const autoFrw = document.querySelector('#auto-forward')
+let images = document.querySelector('#images')
+
+images.src = links[0]
+let i = 0
+let j;
+
+function forwardImg() {
+  images.src = links[i];
+  i++;
+  j = i - 2;
+  i > links.length - 1 ? i = 0 : i;
+}
+
+function backwardImg() {
+  images.src = links[j];
+  j--;
+  i = j + 2;
+  j < 0 ? j = links.length - 1 : j;
+}
+
+frwBtn.addEventListener("click", forwardImg);
+backBtn.addEventListener("click", backwardImg);
+
+autoFrw.addEventListener("click", function() {
+  let setFrwInt = setInterval(forwardImg, 1000);
+  stopBtn.addEventListener("click", function() {
+    clearInterval(setFrwInt);
+  });
+});
+
+autoBack.addEventListener('click', function() {
+    let setBackInt = setInterval(backwardImg, 1000)
+    stopBtn.addEventListener('click', function() {
+        clearInterval(setBackInt)
+    })
+})
