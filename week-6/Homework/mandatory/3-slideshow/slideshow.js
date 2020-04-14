ea2288f18b7f6d4d42d16dc54c9a515b9e2eef16
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-14 19:10:08 +0100

    its hear

--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,42 @@
 // Write your code here
+const slideShow = document.querySelector('.slide');
+const imgAll = document.querySelectorAll('.slide img');
+
+// button 
+const nextBt = document.getElementById('next');
+const prevBt = document.getElementById('prev');
+
+// counter
+let counter = 1; 
+const size = imgAll[0].clientWidth;
+slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//button
+nextBt.addEventListener('click',()=>{
+    if(counter >= imgAll.length -1 )return;
+  slideShow.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  console.log(counter);
+  slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBt.addEventListener('click',()=>{
+    if(counter <= 0)return;
+    slideShow.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+slideShow.addEventListener('transitionend',() => {
+    if(imgAll[counter].id === 'last-one') {
+       slideShow.style.transition = "none";
+       counter= imgAll.length - 2;
+       slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(imgAll[counter].id === 'first-one') {
+        slideShow.style.transition = "none";
+        counter= imgAll.length - counter;
+        slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
+     }
+});
+ 
