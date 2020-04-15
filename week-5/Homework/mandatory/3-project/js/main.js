1165bf15b82dd0c5478873c2c0db27e121eea6ca
ellie <ellie.tms1371@gmail.com>
2020-04-15 16:58:40 +0100

    latest changes for bike website

--- week-5/Homework/mandatory/3-project/js/main.js
+++ week-5/Homework/mandatory/3-project/js/main.js
@@ -1,13 +1,13 @@
 let myBlueButton = document.querySelector("#blueBtn");
 let myJumbotron = document.querySelector(".jumbotron");
-let myButtons = document.querySelectorAll("div[class *=\"buttons\"]>a");
+let myButtons = document.querySelectorAll('div[class *="buttons"]>a');
 let myDonateBike = myButtons[0];
 let myVolunteer = myButtons[1];
 let myOrangeButton = document.querySelector("#orangeBtn");
 let myGreenButton = document.querySelector("#greenBtn");
-let mySubmitButton = document.querySelector("button[type*=\"submit\"]");
-let myRegisterInput = document.querySelectorAll("div[class*=\"form-group\"]");
-let MyArrayOfMyRegisterInput= Array.from(myRegisterInput);
+let mySubmitButton = document.querySelector('button[type*="submit"]');
+let myRegisterInput = document.querySelectorAll('div[class*="form-group"]');
+let MyArrayOfMyRegisterInput = Array.from(myRegisterInput);
 myBlueButton.addEventListener("click", changeWithBlueButton);
 function changeWithBlueButton() {
   myJumbotron.style.backgroundColor = `#588fbd`;
@@ -15,42 +15,92 @@ function changeWithBlueButton() {
   myVolunteer.style.backgroundColor = "black";
   myVolunteer.style.color = "white";
 }
-myOrangeButton.addEventListener("click",changeWithOrangeButton);
-function changeWithOrangeButton(){
-    myJumbotron.style.backgroundColor = `#f0ad4e`;
-    myDonateBike.style.backgroundColor = `#5751fd`;
-    myVolunteer.style.backgroundColor = "#31b0d5";
-    myVolunteer.style.color = "white";
+myOrangeButton.addEventListener("click", changeWithOrangeButton);
+function changeWithOrangeButton() {
+  myJumbotron.style.backgroundColor = `#f0ad4e`;
+  myDonateBike.style.backgroundColor = `#5751fd`;
+  myVolunteer.style.backgroundColor = "#31b0d5";
+  myVolunteer.style.color = "white";
 }
-myGreenButton.addEventListener("click",changeWithGreenButton);
-function changeWithGreenButton(){
-    myJumbotron.style.backgroundColor = `#87ca8a`;
-    myDonateBike.style.backgroundColor = `black`;
-    myVolunteer.style.backgroundColor = "#8c9c08";
+myGreenButton.addEventListener("click", changeWithGreenButton);
+function changeWithGreenButton() {
+  myJumbotron.style.backgroundColor = `#87ca8a`;
+  myDonateBike.style.backgroundColor = `black`;
+  myVolunteer.style.backgroundColor = "#8c9c08";
 }
-mySubmitButton.addEventListener("click",checkValid); 
-function checkValid(event){
-    if(!(MyArrayOfMyRegisterInput[0].querySelector("input").value.length > 0) ||
-      !(MyArrayOfMyRegisterInput[0].querySelector("input").value.split("").includes("@")))
-    {
-        MyArrayOfMyRegisterInput[0].querySelector("input").style.backgroundColor = "red";
-        MyArrayOfMyRegisterInput[0].querySelector("input").value = "please try again";
-        event.preventDefault();
-    }
-    if(!(MyArrayOfMyRegisterInput[1].querySelector("input").value.length > 0)){
-        MyArrayOfMyRegisterInput[1].querySelector("input").style.backgroundColor = "red";
-        event.preventDefault();
-    }
-    if(!(MyArrayOfMyRegisterInput[2].querySelector("#exampleTextarea").value.length > 0)){
-        MyArrayOfMyRegisterInput[2].querySelector("#exampleTextarea").style.backgroundColor = "red";
-        event.preventDefault();
-    }
-    else {
-        alert("thank you for filling out the form");
-        MyArrayOfMyRegisterInput[0].querySelector("input").value = "";
-        MyArrayOfMyRegisterInput[1].querySelector("input").value = "";
-        MyArrayOfMyRegisterInput[2].querySelector("#exampleTextarea").value = "";
-    }
-}
+mySubmitButton.addEventListener("click", checkValid);
+let myInput1 = MyArrayOfMyRegisterInput[0].querySelector("input");
+let myInput2 = MyArrayOfMyRegisterInput[1].querySelector("input");
+let myInput3 = MyArrayOfMyRegisterInput[2].querySelector("#exampleTextarea");
 
-       
+function checkValid(event) {
+  if (!(myInput1.value.length > 0) || !myInput1.value.split("").includes("@")) {
+    myInput1.style.backgroundColor = "red";
+    myInput1.placeholder = "please try again";
+    event.preventDefault();
+  }
+  if (!(myInput2.value.length > 0)) {
+    myInput2.style.backgroundColor = "red";
+    event.preventDefault();
+  }
+  if (!(myInput3.value.length > 0)) {
+    myInput3.style.backgroundColor = "red";
+    event.preventDefault();
+  }
+  if (
+    myInput3.value.length > 0 &&
+    !(myInput2.value.length > 0) &&
+    myInput1.value.length > 0 &&
+    myInput1.value.split("").includes("@")
+  ) {
+    myInput2.style.backgroundColor = "red";
+    event.preventDefault();
+  }
+  if (
+    myInput3.value.length > 0 &&
+    myInput2.value.length > 0 &&
+    (!(myInput1.value.length > 0) || !myInput1.value.split("").includes("@"))
+  ) {
+    myInput1.style.backgroundColor = "red";
+    myInput1.placeholder = "please try again";
+    event.preventDefault();
+  }
+  if (
+    myInput3.value.length > 0 &&
+    !(myInput2.value.length > 0) &&
+    !(myInput1.value.length > 0)
+  ) {
+    myInput1.style.backgroundColor = "red";
+    myInput2.style.backgroundColor = "red";
+    event.preventDefault();
+  }
+  if (
+    !(myInput3.value.length > 0) &&
+    !(myInput2.value.length > 0) &&
+    !(myInput1.value.length > 0)
+  ) {
+    myInput1.style.backgroundColor = "red";
+    myInput2.style.backgroundColor = "red";
+    myInput3.style.backgroundColor = "red";
+    event.preventDefault();
+  }
+  if (
+    myInput3.value.length > 0 &&
+    myInput2.value.length > 0 &&
+    myInput1.value.length > 0 &&
+    !myInput1.value.split("").includes("@")
+  ) {
+    myInput1.value = "";
+    myInput1.style.backgroundColor = "red";
+    myInput1.placeholder = "please try again";
+    event.preventDefault();
+  }
+  if (
+    myInput3.value.length > 0 &&
+    myInput2.value.length > 0 &&
+    myInput1.value.length > 0 &&
+    myInput1.value.split("").includes("@")
+  ) {
+    alert("thank you for filling out the form");
+  }
+}
