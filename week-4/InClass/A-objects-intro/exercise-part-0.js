3d7a0bfe8e4ff14113257479c5122f96c699a408
Nader Akhgari <nader90akhgari@gmail.com>
2020-04-16 14:45:07 +0100

    I've done InClass A-exercise-part0

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,9 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+let laptop = {
+    brand: "dell",
+    model: "XPS",
+    color:"silver",
+    capacityOfHDD: "1TB"
+};
