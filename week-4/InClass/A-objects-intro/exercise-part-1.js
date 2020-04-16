fbebd7225eaea41f0584c9fa4dd9bb55486addb0
Nader Akhgari <nader90akhgari@gmail.com>
2020-04-16 14:53:36 +0100

    I've done inclass-A-exercise-part1

--- week-4/InClass/A-objects-intro/exercise-part-1.js
+++ week-4/InClass/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,20 @@ Assign each of them to a separate variable
 
 */
 
+let mobile = {
+    brand: "iphone",
+    model: "11 pro",
+    color:"black"
+};
+
+let chair = {
+    brand: "Ikea",
+    material: "wood",
+    color:"brown"
+};
+
+let carpet = {
+    material: "plastic",
+    color:"cream",
+    size: "9 meter"
+}
