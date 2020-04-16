f881805965c93f4dbaab6ea94f9649f7dde4a3e3
Alexey Brambalov <44169757+AlexeyBrambalov@users.noreply.github.com>
2020-04-16 22:15:31 +0100

    homework update
    alarm clock improve

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -4,53 +4,49 @@ function setAlarm() {
   let stop = document.querySelector('#stop');
   let audio = document.createElement('audio');
   audio.src = './alarmsound.mp3';
-  document.body.style.backgroundColor = "white"
-
-  let isPaused = false;
-
-  
-  /*1. When the `Set Alarm` button is clicked, get the value of the input field*/
   let input = alarmSet.value;
+  let setAlarm = setInterval(countDown, 1000);
   
-  /*3. Work out how to update the `Time Remaining` title every second set interval funtion will update the counter every second*/
-  
-  
-let setAlarm = setInterval(countDown,1000);
-  
-  
+  let setColor = setInterval(colorChange, 100)
 
   function countDown(){
-    if (!isPaused){
+
     if (input >= 10 && input < 60) {
-    
-  /*2. When you have the input field value, set the title to the correct value*/
     timeRemaining.textContent = `Time Remaining: 00:${input}`;
     input--;
     } else if (input > 0 && input < 10) {
       timeRemaining.textContent =`Time Remaining: 00:0${input}`;
       input--;
     
-  /*4. When the remaining time left is 0, play the alarm sound*/
     } else if (input === 0) {
       timeRemaining.textContent = `Time Remaining: 00:00`;
       audio.play();
       clearInterval(setAlarm)
-      document.body.style.backgroundColor = "red"
+      // document.body.style.backgroundColor = "red"
+
     }
-  } 
-    
   }
+
+  function colorChange(){
+    if (input === 0) {
+      document.body.style.backgroundColor === "white" ? document.body.style.backgroundColor = "red" : document.body.style.backgroundColor = "white"
+    }
+  }
+
+
   stop.addEventListener('click', () => {
     audio.pause();
     alarmSet.value = ''
     audio.src='';
-    isPaused=!isPaused;
+
     clearInterval(setAlarm)
+    clearInterval(setColor)
+    document.body.style.backgroundColor = "white"
+
    
   });
  
 
- 
 }
 
 
