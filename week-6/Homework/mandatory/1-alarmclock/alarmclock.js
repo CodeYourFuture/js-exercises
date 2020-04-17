37737a899a24368b82ad6db00ff93706957e4e14
Mo Nashawati <mo.nashawati@gmail.com>
2020-04-17 21:26:03 +0100

    Adding pause button and fixing multiple set bug

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,46 +1,57 @@
-function setAlarm() {
-  let timeLeft = Number(document.getElementById("alarmSet").value);
-  const intervalId = window.setInterval(intervalCallback, 1000);
+let isPaused = false;
+let intervalId;
 
+function setAlarm() {
+  let timeLeft = document.getElementById("alarmSet").value;
+  displayTimeRemaining(timeLeft);
+  intervalId = window.setInterval(intervalCallback, 1000);
   function intervalCallback() {
-    displayTimeRemaining(timeLeft);
-    if (timeLeft > 0) {
+    if (!isPaused && timeLeft > 0) {
       timeLeft -= 1;
-    } else {
+    } else if (timeLeft === 0) {
       playAlarm();
       clearInterval(intervalId);
     }
+    displayTimeRemaining(timeLeft);
   }
 }
 
-// function stopTimer() {
-//   clearInterval(intervalId);
-// }
-
-function displayTimeRemaining(timeLeft) {
+function displayTimeRemaining(time) {
   document.getElementById(
     "timeRemaining"
-  ).innerText = `Time Remaining: ${formatTime(timeLeft)}`;
+  ).innerText = `Time Remaining: ${formatTime(time)}`;
 }
 
 function formatTime(numOfSecs) {
+  let hrsToDisplay = Math.floor(numOfSecs / 3600);
+  numOfSecs = numOfSecs % 3600;
   let minsToDisplay = Math.floor(numOfSecs / 60);
   let secsToDisplay = numOfSecs % 60;
-  return pad(minsToDisplay) + ":" + pad(secsToDisplay);
+  return (
+    pad(hrsToDisplay) + ":" + pad(minsToDisplay) + ":" + pad(secsToDisplay)
+  );
 }
 
 function pad(num) {
   return num < 10 ? "0" + num : num.toString();
 }
+console.log(isPaused);
 
 // DO NOT EDIT BELOW HERE
 var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
+    clearInterval(intervalId);
     setAlarm();
   });
   document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(intervalId);
     pauseAlarm();
+  });
+  document.getElementById("pause").addEventListener("click", () => {
+    pauseAlarm();
+    isPaused = !isPaused;
+    document.getElementById("pause").innerHTML = isPaused ? "Resume" : "Pause";
   });
 }
 function playAlarm() {
