9b8a28f7b9b1c6f444ebf73f946a81918b34cf20
Halitos <oskan96@gmail.com>
2020-04-18 22:07:08 +0100

    all syntax errors fixed

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -6,7 +6,7 @@ function addNumbers(a, b, c) {
 
 function introduceMe(name, age) {
   const greeting =
-    "Hello, my name is " + name + "and I am " + age + "years old";
+    "Hello, my name is " + name + " and I am " + age + " years old";
   return greeting;
 }
 
