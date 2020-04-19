c9b897ff2d7cee9e54e78f1863c4a1459aa7d08c
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-19 10:34:26 +0100

    finish

--- week-6/Homework/mandatory/1-alarmclock/alarmclock.js
+++ week-6/Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,6 +1,6 @@
 // set alarm button
 function setAlarm() {
-  let curren]Time = document.getElementById("alarmSet").value;
+  let currentTime = document.getElementById("alarmSet").value;
   let x = setInterval(function(){
     if (currentTime < 10 && currentTime >= 0){
       document.getElementById('timeRemaining').innerHTML= "00:0"+ currentTime ;
@@ -10,30 +10,31 @@ function setAlarm() {
     currentTime = currentTime - 1;
     if (currentTime < 0 ) {
       playAlarm();
-      colorB(z=0);
+      colorB(co);
       clearInterval(x);
     } 
 }, 1000);
-}
+} 
 
 // change color :
-
+let col = 0 ;
+let co;
 function colorB(co){ 
-  console.log(co);
-  let col = setInterval(function(){
+  if (co === 1 ){
+     setInterval(function(){
+    console.log("play"+ co);
     let color = ["orange", "purple", "yellow","red","brown"];
     for(let i = 0; i < 5; i++){
-      document.body.style.backgroundColor = color[Math.floor(Math.random() * 4)];  
-    }
-  }, 300);
-  document.body.style.backgroundColor = "white";
-  if (co === true ){
-    console.log("inside: " + co);
+       document.body.style.backgroundColor = color[Math.floor(Math.random() * 4)];  
+     }
+  }, 300); 
+  }else{ 
+    console.log("stop: "+ co);
     clearInterval(col);
-    
-  }
+    document.body.style.backgroundColor = "white";
+    co = 0;
+  } 
 }
-
 // The Timer in top left :
 var Timer = setInterval(myTimer, 1000);
 let Time = document.createElement("h1");
@@ -50,6 +51,7 @@ var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
+
   });
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
@@ -57,10 +59,10 @@ function setup() {
 }
 function playAlarm() {
   audio.play();
+  co = 1;
 }
 function pauseAlarm() {
   audio.pause();
-  let co = true;
-  colorB(co);
+  colorB(co = 0);
 }
 window.onload = setup;
