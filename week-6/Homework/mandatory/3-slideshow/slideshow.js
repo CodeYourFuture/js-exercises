82e1d9a5cf0244994472c979186566f3a1912b90
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-04-19 09:27:24 +0100

    i make pause and play for music but still need help to fix

--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -7,9 +7,15 @@ const nextBt = document.getElementById('next');
 const prevBt = document.getElementById('prev');
 const stopBt = document.getElementById('stop');
 const playBt = document.getElementById('play');
+const forwardBt = document.getElementById('forward');
+const backwardBt = document.getElementById('backward');
+const playMBt = document.getElementById('play-music');
+const pauseMBt = document.getElementById('pause_music');
 
 // counter
-let x = 3000;
+let music = 0;
+let audio = new Audio('music.mp3');
+let x = 5000;
 let counter = 1; 
 const size = imgAll[0].clientWidth;
 slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
@@ -40,17 +46,46 @@ playBt.addEventListener('click',()=>{
 stopBt.addEventListener('click',()=>{
     autoSlide(n = 1);
 });
+
+forwardBt.addEventListener('click',()=>{
+    clearInterval(myVar);
+    if (x > 1000 ){
+    autoSlide( x = x - 1000)
+    }else {
+    autoSlide( x = 1000);
+    }
+});
+
+backwardBt.addEventListener('click',()=>{
+    clearInterval(myVar);
+    autoSlide( x = x + 1000);
+});
+
+playMBt.addEventListener('click',()=>{
+    playMBt.style.visibility = "hidden";
+    pauseMBt.style.visibility = "show";
+    audio.play();
+    console.log("play");
+});
+pauseMBt.addEventListener('click',()=>{
+    pauseMBt.style.visibility="hidden";
+    playMBt.style.visibility = "show";
+    console.log("pause");
+    audio.pause(); 
+}); 
+
 // autoplay function :
 var myVar;
 function autoSlide(){
     myVar = setInterval (function(){
         if (n===0){  
-            console.log("start");
+            console.log("start"+ counter);
             if(counter >= imgAll.length  )return;
             slideShow.style.transition = "transform 0.4s ease-in-out";
             slideShow.style.transform = 'translateX(' + (-size * counter) + 'px)';
             counter ++;
             repeat();
+            console.log(x);
         }else{
             clearInterval(myVar);
             console.log("stop");
