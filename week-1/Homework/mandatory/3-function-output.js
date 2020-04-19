9e69c4848bb3075b340adf43d7e0e1fc8f6911f0
Laetitia Kinja <Laetitiawork@Laetitias-MacBook-Pro.local>
2020-04-19 19:57:26 +0200

    completed exercise 3

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,17 +1,28 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//The Math.random() function returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1).
+//The getNumber function returns the product of that random number and 10. 
 function getNumber() {
-    return Math.random() * 10;
+    
+        return Math.random() * 10;
 }
 
+console.log(getNumber());
+
 // Add comments to explain what this function does. You're meant to use Google!
+//This function joins two strings together (w1 and w2) using the .concat function. 
 function s(w1, w2) {
     return w1.concat(w2);
 }
 
+console.log(s("Good", " Morning"));
+
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
+    return firstWord.concat(" " + secondWord + " " + thirdWord);
 }
+
+console.log(concatenate('code', 'your', 'future'));
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
