0c9829648e04a88b0cf1ce02f66416d4c34a279f
azadaafo <azadaafo@gmail.com>
2020-04-19 15:52:22 +0100

    HW Mandatory Syntax error fixed

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -1,14 +1,14 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a b c) {
+function addNumbers(a, b, c) {
     return a + b + c;
 }
 
-function introduceMe(name, age)
-return "Hello, my name is " + name "and I am " age + "years old";
-
+function introduceMe(name, age) {
+return "Hello, my name is " + name + "and I am " + age + "years old";
+}
 function getRemainder(a, b) {
-    remainder = a %% b;
+    remainder = a % b;
 
     // Use string interpolation here
     return "The remainder is %{remainder}"
