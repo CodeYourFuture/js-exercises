70710311984255c773f01c85f49a6c1d024a494e
Iman Feisali <iman.feisali@gmail.com>
2020-04-21 00:23:37 +0100

    4 of 5 done

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -45,22 +45,60 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
+function shakeBall() {
+  return console.log("The ball has shaken!");
+}
 
 // The answer should come from shaking the ball
-let answer;
+let answer = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes",
+  "Signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer(answer) {
+  let i = Math.floor(Math.random() * 20) + 1;
+  if (i >= 0 && i <= 4) {
+    console.log("the answer is very positive");
+  }
+  if (i >= 5 && i <= 9) {
+    console.log("the answer is positive");
+  }
+  if (i >= 10 && i <= 14) {
+    console.log("the answer is negative");
+  }
+  if (i >= 15 && i <= 19) {
+    console.log("the answer is very negative");
+  }
+  return i;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
 let logged;
-console.log = function() {
+console.log = function () {
   log(...arguments);
   logged = arguments[0];
 };
