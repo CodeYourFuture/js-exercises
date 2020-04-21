b0756a5c9612543e13ccee671a8b2c2c7460005d
JeanPaul18 <61525313+JeanPaul18@users.noreply.github.com>
2020-04-21 20:23:56 +0100

    float exercise

--- week-1/.archive/I-floats/exercise.js
+++ week-1/.archive/I-floats/exercise.js
@@ -1,2 +1,7 @@
-var numberOfStudents = 15;
-var numberOfMentors = 8;
+var numberOfStudents = (15 / 23) * 100;
+var percent = Math.round(numberOfStudents);
+console.log("Percentage students:" + percent + "%");
+
+var numberOfMentors = (8 / 23) * 100;
+var percent = Math.round(numberOfMentors);
+console.log("Percentage mentors:" + percent + "%");
