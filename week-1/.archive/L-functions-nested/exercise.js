25d3e69397d5ef419aa640b32c39dd1376aedfc9
JeanPaul18 <61525313+JeanPaul18@users.noreply.github.com>
2020-04-22 13:27:38 +0100

    added a funtion-nested exercise

--- week-1/.archive/L-functions-nested/exercise.js
+++ week-1/.archive/L-functions-nested/exercise.js
@@ -1,5 +1,30 @@
-var mentor1 = "Daniel";
-var mentor2 = "Irina";
-var mentor3 = "Mimi";
-var mentor4 = "Rob";
-var mentor5 = "Yohannes";
+// exerice 1
+function mentorspercentage(mentor) {
+  return (mentor / 23) * 100;
+}
+
+function studentspercentage(student) {
+  return (student / 23) * 100;
+}
+
+function fullgreting() {
+  var mentorpercent = mentorspercentage(8);
+  var studentpercent = studentspercentage(15);
+  var message =
+    "My Name is " +
+    mentorpercent +
+    " and I was born over " +
+    studentpercent +
+    " days ago!";
+  return message;
+}
+const fullsentence = fullgreting();
+console.log(fullsentence);
+
+// exercise 2
+
+// var mentor1 = "Daniel";
+// var mentor2 = "Irina";
+// var mentor3 = "Mimi";
+// var mentor4 = "Rob";
+// var mentor5 = "Yohannes";
