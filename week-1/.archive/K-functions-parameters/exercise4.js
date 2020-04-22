08337444d286b1395aa30dcb8c5190fdf69ace09
JeanPaul18 <61525313+JeanPaul18@users.noreply.github.com>
2020-04-22 12:49:18 +0100

    added a function parameters  exercise 4

--- week-1/.archive/K-functions-parameters/exercise4.js
+++ week-1/.archive/K-functions-parameters/exercise4.js
@@ -1,5 +1,7 @@
 // Declare your function first
-
+function add(number1, number2) {
+  return number1 + number2;
+}
 // Call the function and assign to a variable `sum`
-
+const sum = add(13, 124);
 console.log(sum);
