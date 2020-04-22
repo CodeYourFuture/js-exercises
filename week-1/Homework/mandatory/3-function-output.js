435f00a83fedce8a806f72e160440016708190bf
Bek0o0 <abubkerelsafi@gmail.com>
2020-04-22 16:06:49 +0100

    Done the week1 HW js

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,31 +1,45 @@
-// Add comments to explain what this function does. You're meant to use Google!
+// Add comments to explain what this function does. You're meant to use Google!!!..
+
+// returns a Number value with positive sign, greater than or equal to 0 but less than 1 , chosen randomly or pseudo-randomly with approximately uniform distribution over that range and multiply it to 10
+
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//The concat() function concatenates the string arguments to the calling string and returns a new string.Here we concated w1 to W2 and return a new value.
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-    // Write the body of this function to concatenate three words together
-    // Look at the test case below to understand what to expect in return 
+  // Write the body of this function to concatenate three words together
+  // Look at the test case below to understand what to expect in return
+  return firstWord + " " + secondWord + " " + thirdWord;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-        status = "PASSED"
-    } else {
-        status = "FAILED"
-    }
-  
-    console.log(`${test_name}: ${status}`)
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
-  
-test("concatenate function - case 1 works", concatenate('code', 'your', 'future') === "code your future")
-test("concatenate function - case 2 works", concatenate('I', 'like', 'pizza') === "I like pizza")
-test("concatenate function - case 3 works", concatenate('I', 'am', 13) === "I am 13")
+
+test(
+  "concatenate function - case 1 works",
+  concatenate("code", "your", "future") === "code your future"
+);
+test(
+  "concatenate function - case 2 works",
+  concatenate("I", "like", "pizza") === "I like pizza"
+);
+test(
+  "concatenate function - case 3 works",
+  concatenate("I", "am", 13) === "I am 13"
+);
--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -1,7 +1,7 @@
 /**
 
 Let's peer into the future using a Magic 8 Ball!
-https://en.wikipedia.org/wiki/Magic_8-Ball 
+..https://en.wikipedia.org/wiki/Magic_8-Ball 
 
 There are a few steps to being able view the future though:
 * Ask a question
@@ -45,22 +45,118 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
-
-// The answer should come from shaking the ball
 let answer;
+let ans = Math.floor(Math.random() * 17);
+function shakeBall() {
+  console.log("The ball has shaken!");
+
+  if (ans === 0) {
+    return (answer = " It is certain");
+  }
+  if (ans === 1) {
+    return (answer = "Without a doubt");
+  }
+  if (ans === 2) {
+    return (answer = "Yes - definitely");
+  }
+  if (ans === 3) {
+    return (answer = "You may rely on it");
+  }
+  //very positive
+  if (ans === 4) {
+    return (answer = " As I see it, yes");
+  }
+  if (ans === 5) {
+    return (answer = "Most likely");
+  }
+  if (ans === 6) {
+    return (answer = "Yes");
+  }
+  if (ans === 7) {
+    return (answer = "Signs point to yes");
+  }
+
+  if (ans === 8) {
+    return (answer = "Outlook good");
+  }
+
+  //Negative
+  if (ans === 9) {
+    return (answer = " Reply hazy, try again");
+  }
+  if (ans === 10) {
+    return (answer = "Ask again later");
+  }
+  if (ans === 11) {
+    return (answer = "Better not tell you nows");
+  }
+  if (ans === 12) {
+    return (answer = "Cannot predict now");
+  }
+
+  if (ans === 13) {
+    return (answer = "Concentrate and ask again");
+  }
+
+  //Very negative
+  if (ans === 14) {
+    return (answer = "Don't count on it");
+  }
+  if (ans === 15) {
+    return (answer = "My reply is no");
+  }
+  if (ans === 16) {
+    return (answer = "My sources say no");
+  }
+  if (ans === 17) {
+    return (answer = "Outlook not so good");
+  }
+
+  if (ans === 18) {
+    return (answer = "Very doubtful");
+  }
+}
+
+// The answer should come from shaking the ball..
 
 // When checking the answer, we should tell someone if the answer is
 // - very positive
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+
+function checkAnswer() {
+  //Very positive
+  if (ans === 0 || ans === 1 || ans === 2 || ans === 3) {
+    return (answer = "very positive");
+  }
+
+  //Positive
+  if (ans === 4 || ans === 5 || ans === 6 || ans === 7 || ans === 8) {
+    return (answer = "positive");
+  }
+
+  //Negative
+  if (ans === 9 || ans === 10 || ans === 11 || ans === 12 || ans === 13) {
+    return (answer = "negative");
+  }
+  // very Negative
+  if (ans === 14 || ans === 15 || ans === 16 || ans === 17 || ans === 18) {
+    return (answer = "very negative");
+  }
+}
+
+console.log("   ");
+console.log(shakeBall());
+
+console.log(checkAnswer());
+console.log("   ");
+console.log("   ");
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
 let logged;
-console.log = function() {
+console.log = function () {
   log(...arguments);
   logged = arguments[0];
 };
