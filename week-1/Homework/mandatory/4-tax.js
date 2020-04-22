f66fb95ad299f360c33d385a03092001c160aaf4
Leida_Gandy <leida.gandy@gmail.com>
2020-04-22 14:31:07 +0100

    complete excercise 4-tax.js

--- week-1/Homework/mandatory/4-tax.js
+++ week-1/Homework/mandatory/4-tax.js
@@ -2,46 +2,57 @@
   SALES TAX
   =========
   A business requires a program that calculates how much sales tax to charge
-  Sales tax is 20% of the price of the product
-*/
+  Sales tax is 20% of the price of the product 
+
+  Remember that the prices must include the sales tax (hint: you already wrote a function for this!)*/
+
+const TAX_RATE = 0.2;
+
+function calculateSalesTax(productPrice) {
+  productPrice = productPrice + productPrice * TAX_RATE;
+  return productPrice;
+}
+console.log(calculateSalesTax);
 
 /*
-  CURRENCY FORMATTING
+  CURRENCY FORMATTING 
   ===================
   The business has informed you that prices must have 2 decimal places
   They must also start with the currency symbol
-  Write a function that transforms numbers into the format £0.00
+  Write a function that transforms numbers into the format £0.00 */
 
-  Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
-*/
-
-function calculateSalesTax() {
-
-  return Math.random() * 0.2;
+function formatCurrency(productPrice) {
+  var productPriceTwo = productPrice + productPrice * 0.2;
+  return "£" + productPriceTwo.toFixed(2);
 }
-
-function formatCurrency() {
-
-  
-}
+console.log(formatCurrency);
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
   let status;
   if (expr) {
-      status = "PASSED"
+    status = "PASSED";
   } else {
-      status = "FAILED"
+    status = "FAILED";
   }
 
-  console.log(`${test_name}: ${status}`)
+  console.log(`${test_name}: ${status}`);
 }
 
-test("calculateSalesTax function - case 1 works", calculateSalesTax(15) === 18)
-test("calculateSalesTax function - case 2 works", calculateSalesTax(17.5) === 21)
-test("calculateSalesTax function - case 3 works", calculateSalesTax(34) === 40.8)
+test("calculateSalesTax function - case 1 works", calculateSalesTax(15) === 18);
+test(
+  "calculateSalesTax function - case 2 works",
+  calculateSalesTax(17.5) === 21
+);
+test(
+  "calculateSalesTax function - case 3 works",
+  calculateSalesTax(34) === 40.8
+);
 
-test("formatCurrency function - case 1 works", formatCurrency(15) === "£18.00")
-test("formatCurrency function - case 2 works", formatCurrency(17.5) === "£21.00")
-test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.80")
+test("formatCurrency function - case 1 works", formatCurrency(15) === "£18.00");
+test(
+  "formatCurrency function - case 2 works",
+  formatCurrency(17.5) === "£21.00"
+);
+test("formatCurrency function - case 3 works", formatCurrency(34) === "£40.80");
