5e537e7f93dbbd30dcc3ad66fe308a0e43f6fc0d
JeanPaul18 <61525313+JeanPaul18@users.noreply.github.com>
2020-04-23 22:23:06 +0100

    function-output exercise homework

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,31 +1,55 @@
 // Add comments to explain what this function does. You're meant to use Google!
 function getNumber() {
-    return Math.random() * 10;
+  return Math.random() * 10; // returns a random integer from 0 to 9
 }
 
+//Example : gives a random number from 0 to 9, with both numbers included.
+// function getNumber(wer) {
+//     return Math.random(wer) * 10;
+//   }
+
+//   console.log(getNumber(9));
+
+//-----------------------------------------------------------------------------
+
 // Add comments to explain what this function does. You're meant to use Google!
+// --The concat() method is used to join two or more strings.
+// --This method does not change the existing strings, but returns a new string containing the text of the joined strings.
 function s(w1, w2) {
-    return w1.concat(w2);
+  return w1.concat(" ", w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
-    // Write the body of this function to concatenate three words together
-    // Look at the test case below to understand what to expect in return 
+  // Write the body of this function to concatenate three words together.
+  // Look at the test case below to understand what to expect in return.
+
+  return firstWord.concat(" ", secondWord, " ", thirdWord);
 }
+
+// console.log(concatenate("code ", "your", " future"));
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-        status = "PASSED"
-    } else {
-        status = "FAILED"
-    }
-  
-    console.log(`${test_name}: ${status}`)
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
-  
-test("concatenate function - case 1 works", concatenate('code', 'your', 'future') === "code your future")
-test("concatenate function - case 2 works", concatenate('I', 'like', 'pizza') === "I like pizza")
-test("concatenate function - case 3 works", concatenate('I', 'am', 13) === "I am 13")
+
+test(
+  "concatenate function - case 1 works",
+  concatenate("code", "your", "future") === "code your future"
+);
+test(
+  "concatenate function - case 2 works",
+  concatenate("I", "like", "pizza") === "I like pizza"
+);
+test(
+  "concatenate function - case 3 works",
+  concatenate("I", "am", 13) === "I am 13"
+);
