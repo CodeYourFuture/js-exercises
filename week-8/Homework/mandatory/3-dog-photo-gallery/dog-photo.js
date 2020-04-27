9a3620685df47817353ab326897482d63dbf5a3c
ellie <ellie.tms1371@gmail.com>
2020-04-27 22:37:10 +0100

    changes

--- week-8/Homework/mandatory/3-dog-photo-gallery/dog-photo.js
+++ week-8/Homework/mandatory/3-dog-photo-gallery/dog-photo.js
@@ -10,11 +10,14 @@ function getFunctionFetch(){
            let  divContainer = document.querySelector(".card-body")
            const  myLi = document.createElement("li")
            const myImage = document.createElement("img")
-            myImage.className += "card-img-top w-70"
+            myImage.className += "card-img-top w-70 m-auto p-auto"
             myImage.src = data.message
             myLi.appendChild(myImage)
             myImageContainer.appendChild(myLi)
             divContainer.appendChild(myImageContainer);
             
+        })
+        .catch((err) => {
+            console.log(err);
         })
 }
