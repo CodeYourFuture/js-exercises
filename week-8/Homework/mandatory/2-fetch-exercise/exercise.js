40d49a52db248a81b96dbf08bba31bbb856a5d18
ellie <ellie.tms1371@gmail.com>
2020-04-27 22:09:21 +0100

    hw 2 and 3

--- week-8/Homework/mandatory/2-fetch-exercise/exercise.js
+++ week-8/Homework/mandatory/2-fetch-exercise/exercise.js
@@ -17,10 +17,11 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch('*** Write the API address here ***')
+fetch('https://codeyourfuture.herokuapp.com/api/greetings')
     .then(function(response) {
         return response.text();
     })
     .then(function(greeting) {
-        // Write the code to display the greeting text here
+        let greetingText = document.querySelector("#greeting-text");
+        greetingText.textContent = greeting;
     });
---
+++ week-8/Homework/mandatory/3-dog-photo-gallery/dog-photo.js
@@ -0,0 +1,20 @@
+let myButton = document.querySelector("#button");
+myButton.addEventListener("click",getFunctionFetch);
+function getFunctionFetch(){
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response =>
+           response.json()
+            )
+        .then(data => {
+           const myImageContainer  = document.querySelector("#imageContainer")
+           let  divContainer = document.querySelector(".card-body")
+           const  myLi = document.createElement("li")
+           const myImage = document.createElement("img")
+            myImage.className += "card-img-top w-70"
+            myImage.src = data.message
+            myLi.appendChild(myImage)
+            myImageContainer.appendChild(myLi)
+            divContainer.appendChild(myImageContainer);
+            
+        })
+}
