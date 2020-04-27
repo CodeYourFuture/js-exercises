2677af6904437695138186faf6ec00e42daafdb3
Afshin <a.karamifar@gmail.com>
2020-04-28 00:36:23 +0100

    I finished Fetch Exercise

--- week-8/Homework/mandatory/2-fetch-exercise/exercise.js
+++ week-8/Homework/mandatory/2-fetch-exercise/exercise.js
@@ -17,10 +17,14 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch('*** Write the API address here ***')
+// I used timer to show greeting 0.5 every second
+let a = setInterval(() => {
+    fetch('https://codeyourfuture.herokuapp.com/api/greetings')
     .then(function(response) {
         return response.text();
     })
     .then(function(greeting) {
-        // Write the code to display the greeting text here
-    });
+        let greetingText = document.querySelector('#greeting-text');
+        greetingText.textContent = greeting;
+    });    
+}, 500);
