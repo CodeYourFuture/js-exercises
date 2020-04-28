85a6fa1207f283726a5abee36db4e1c55461b436
OBakir90 <53667329+OBakir90@users.noreply.github.com>
2020-04-28 23:26:34 +0100

    changed NaN

--- week-2/Homework/mandatory/1-fix-functions.js
+++ week-2/Homework/mandatory/1-fix-functions.js
@@ -1,117 +1,87 @@
-// Fix Functions
-
-// Aim: to understand the change code inside functions 
-//
 // The below functions are syntactically correct but not outputting the right results.
 // Look at the tests and see how you can fix them.
 
-
-
-// 1) mood function does this return `"I am not happy"`
-//    Only make edits inside the function
-
 function mood() {
-  let isHappy = true;
+    let isHappy = false;
 
-  if (isHappy) {
-    return "I am happy";
-  } else {
-    return "I am not happy";
-  }
+    if (isHappy) {
+        return "I am happy";
+    } else {
+        return "I am not happy";
+    }
 }
-
-// 2) For any numerical value greater or equal to 10
-//    Hint: use constant `num` and only change isBigEnough.
-//    Variable isBigEnough needs to evaluate to a boolean
 
 function greaterThan10() {
-  const num = 10;
-  const isBigEnough;
+    let num = 10;
+    let isBigEnough = num >= 10;
 
-  if (isBigEnough) {
-    return "num is greater than or equal to 10";
-  } else {
-    return "num is not big enough";
-  }
+    if (isBigEnough) {
+        return "num is greater than or equal to 10";
+    } else {
+        return "num is not big enough";
+    }
 }
-
-// 3) For any numerical value greater or equal to 10
-//    Hint: use the Array method sort()
-//    Remember to Google how to use sort method
 
 function sortArray() {
-  const letters = ["a", "n", "c", "e", "z", "f"];
-  let sortedLetters;
-
-  return sortedLetters;
+    let letters = ["a", "n", "c", "e", "z", "f"];
+    let sortedLetters = letters.sort();
+    return sortedLetters;
 }
-
-// 4) first5 function should return the first 5 elements of array
-//    Hint: use the Array method splice()
-//    Remember to Google how to use splice()
 
 function first5() {
-  let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  let sliced;
-
-  return sliced;
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let sliced = numbers.slice(0, 5)
+    return sliced;
 }
 
-// 5) get3rdIndex function needs to take an array `arr` and give 
-//    back third element value.
-//    For example with array `[1, 2, 3, 4, 5]` it needs to return `4`
-//
-//    Hint: remember that arrays are zero-index based
-
 function get3rdIndex(arr) {
-  let index = 3;
-  let element;
-
-  return element;
+    let index = 3;
+    let element = arr[index];
+    return element;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+    let status;
+    if (expr) {
+        status = "PASSED";
+    } else {
+        status = "FAILED";
+    }
 
-  console.log(`${test_name}: ${status}`);
+    console.log(`${test_name}: ${status}`);
 }
 
 function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
 
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
 
-  return true;
+    return true;
 }
 
 test("mood function works", mood() === "I am not happy");
 test(
-  "greaterThanTen function works",
-  greaterThan10() === "num is greater than or equal to 10"
+    "greaterThanTen function works",
+    greaterThan10() === "num is greater than or equal to 10"
 );
 test(
-  "sortArray function works",
-  arraysEqual(sortArray(), ["a", "c", "e", "f", "n", "z"])
+    "sortArray function works",
+    arraysEqual(sortArray(), ["a", "c", "e", "f", "n", "z"])
 );
 test("first5 function works", arraysEqual(first5(), [1, 2, 3, 4, 5]));
 
 test(
-  "get3rdIndex function works - case 1",
-  get3rdIndex(["fruit", "banana", "apple", "strawberry", "raspberry"]) ===
-  "strawberry"
+    "get3rdIndex function works - case 1",
+    get3rdIndex(["fruit", "banana", "apple", "strawberry", "raspberry"]) ===
+    "strawberry"
 );
 test(
-  "get3rdIndex function works - case 2",
-  get3rdIndex([11, 37, 62, 18, 19, 3, 30]) === 18
+    "get3rdIndex function works - case 2",
+    get3rdIndex([11, 37, 62, 18, 19, 3, 30]) === 18
 );
--- week-2/Homework/mandatory/4-sorting-algorithm.js
+++ week-2/Homework/mandatory/4-sorting-algorithm.js
@@ -1,78 +1,97 @@
-// Sorting Algorithms
-//
-// Aim: to understand the work of loops and nested loops and array methods
-//
-// You need to create the function sortAges that takes one array as parameter input
-
 /*
 At the start of the course, you worked in teams to sort your team members, labelled by
 numbers, in ascending or descending order.
-
 Today, you will be applying the sorting algorithm you used in that exercise in code! 
-
 Create a function called sortAges which:
 - takes an array of mixed data types as input
-- removes any non-number data types without using the built-in javascript filter method 
+- removes any non-number data types without using the built-in javascript filter method
 - returns an array of sorted ages in ascending order 
-  - HARD MODE - without using the built-in javascript sort method 😎
-
+ - HARD MODE - without using the built-in javascript sort method ??
 You don't have to worry about making this algorithm work fast! The idea is to get you to
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
-//1)Create the function inside, Think about nested loops
+
 function sortAges(arr) {
-  //create function here
+    let newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "number") {
+            newArr.push(arr[i])
+        }
+    }
+    console.log(newArr)
+        // for (i = 0; i < arr.length; i++) {
+        //     console.log("array length : " + arr.length)
+        //     if (isNaN(arr[i])) {
+        //         console.log("items: " + arr[i]);
+        //         arr.splice(i, 1);
+        //         console.log("array: " + arr)
+        //     }
+        // }
+        // console.log(arr)
+
+    for (i = 0; i < newArr.length; i++) {
+        for (j = 0; j < newArr.length; j++) {
+            if (newArr[j] > newArr[j + 1]) {
+                let term = newArr[j];
+                newArr[j] = newArr[j + 1];
+                newArr[j + 1] = term
+            }
+        }
+    }
+    return newArr
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
+/* ======= TESTS - DO NOT MODIFY ===== */
+
 const agesCase1 = [
-  "🎹",
-  100,
-  "💩",
-  55,
-  "🥵",
-  "🙈",
-  45,
-  "🍕",
-  "Sanyia",
-  66,
-  "James",
-  23,
-  "🎖",
-  "Ismeal",
+    "🎹",
+    100,
+    "💩",
+    55,
+    "🥵",
+    "🙈",
+    45,
+    "🍕",
+    "Sanyia",
+    66,
+    "James",
+    23,
+    "🎖",
+    "Ismeal",
 ];
 const agesCase2 = ["28", 100, 60, 55, "75", "🍕", "Elamin"];
 
 function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
 
-  for (let i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
 
-  return true;
+    return true;
 }
 
 function test(test_name, expr) {
-  let status;
-  if (expr) {
-    status = "PASSED";
-  } else {
-    status = "FAILED";
-  }
+    let status;
+    if (expr) {
+        status = "PASSED";
+    } else {
+        status = "FAILED";
+    }
 
-  console.log(`${test_name}: ${status}`);
+    console.log(`${test_name}: ${status}`);
 }
 
 test(
-  "sortAges function works - case 1",
-  arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
+    "sortAges function works - case 1",
+    arraysEqual(sortAges(agesCase1), [23, 45, 55, 66, 100])
 );
 
 test(
-  "sortAges function works - case 2",
-  arraysEqual(sortAges(agesCase2), [55, 60, 100])
+    "sortAges function works - case 2",
+    arraysEqual(sortAges(agesCase2), [55, 60, 100])
 );
