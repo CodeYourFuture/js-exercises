cc107d1ab591f9ec46d20e6c0adc32168992b8b2
Halitos <oskan96@gmail.com>
2020-04-29 00:00:56 +0100

    week2 exe3 done

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -13,12 +13,21 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
+        We try to log "b" on console but "b" is not defined and it will throw an error.
   2. Remove the line that throws the error.
+        Done
   3. What is printed to the console?
+        My prediction was 2, 6, 2, 9, 4, 13 6.  Correct : 2 , 6, 4, 9, 6, 13, 8. 
+        My calculation for the f2 function was wrong since i did not think it would add x becuase there were only two parameters.
+        If I added x (2) for each time f2 called I would find the correct answer.
   4. How many times is "f1" called?
+        2 times (when i equals to 1 and 3)
   5. How many times is "f2" called?
+        3 times (when i equals to 0 ,2 and 4)
   6. What value does the "a" parameter take in the first "f1" call?
+        1
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+        8
 */
 
 let x = 2;
@@ -34,7 +43,6 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
