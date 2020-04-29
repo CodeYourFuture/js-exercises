67a8e5095037aac400c4d1956494548502158b27
hacersk <hacersk41@gmail.com>
2020-04-29 20:57:38 +0100

    mandorty is over

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,16 +1,24 @@
 // Add comments to explain what this function does. You're meant to use Google!
+
+//Mart.random() returns a random number between 0 (inclusive),  and 1 (exclusive).
+// getNumber() returns a random integer from 0 to 9
 function getNumber() {
     return Math.random() * 10;
 }
 
+
 // Add comments to explain what this function does. You're meant to use Google!
+
+//The concat() method is used to join two or more strings.
 function s(w1, w2) {
+    
     return w1.concat(w2);
 }
 
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
+    return firstWord.concat(" " +secondWord).concat(" "+thirdWord);
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
