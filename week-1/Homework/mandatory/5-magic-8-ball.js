088f8b029f542a476ba64cf97e74fb8010a9fba7
mara-ber <mara.berezin@gmail.com>
2020-04-30 20:01:34 +0100

    js 5-magic-8-ball

--- week-1/Homework/mandatory/5-magic-8-ball.js
+++ week-1/Homework/mandatory/5-magic-8-ball.js
@@ -45,7 +45,20 @@ Very doubtful.
 
 // This should log "The ball has shaken!"
 // and return the answer.
-function shakeBall() {}
+function shakeBall(question) {
+  console.log("The ball has shaken!");
+  const array1 = ["It is certain.", "It is decidedly so.",
+  "Without a doubt.", "Yes - definitely.", "You may rely on it.",
+  "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", 
+  "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", 
+  "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", 
+  "Don't count on it.", "My reply is no.", "My sources say no.", 
+  "Outlook not so good.", "Very doubtful."];
+  let answer = array1[Math.floor(Math.random() * array1.length)];
+  return answer;
+}
+  
+
 
 // The answer should come from shaking the ball
 let answer;
@@ -55,7 +68,25 @@ let answer;
 // - positive
 // - negative
 // - very negative
-function checkAnswer() {}
+function checkAnswer(answer) {
+  const arr1 = ["It is certain.", "It is decidedly so.",
+  "Without a doubt.", "Yes - definitely.", "You may rely on it."];
+  const arr2 = ["As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", 
+  "Signs point to yes."];
+  const arr3 = ["Reply hazy, try again.", "Ask again later.", 
+  "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again."];
+  const arr4 = ["Don't count on it.", "My reply is no.", "My sources say no.", 
+  "Outlook not so good.", "Very doubtful."];
+  if (arr1.includes(answer)) {
+    return "very positive";
+  } else if (arr2.includes(answer)) {
+    return "positive";
+  } else if (arr3.includes(answer)) {
+    return "negative";
+  } else if (arr4.includes(answer)) {
+    return "very negative";
+  }
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 const log = console.log;
