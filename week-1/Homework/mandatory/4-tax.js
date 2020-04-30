5315f50700ba16941718bdc5882d17450adb77e7
mara-ber <mara.berezin@gmail.com>
2020-04-30 20:00:18 +0100

    js 4-tax

--- week-1/Homework/mandatory/4-tax.js
+++ week-1/Homework/mandatory/4-tax.js
@@ -5,7 +5,9 @@
   Sales tax is 20% of the price of the product
 */
 
-function calculateSalesTax() {}
+function calculateSalesTax(amount) {
+  return amount * 1.2;
+}
 
 /*
   CURRENCY FORMATTING
@@ -17,7 +19,11 @@ function calculateSalesTax() {}
   Remember that the prices must include the sales tax (hint: you already wrote a function for this!)
 */
 
-function formatCurrency() {}
+function formatCurrency(amount) {
+  const price = calculateSalesTax(amount);
+  return "£" + price.toFixed(2);
+}
+
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
