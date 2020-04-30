a9e6e53fc8f5a512c23784782701171b5ff1ed9f
mara-ber <mara.berezin@gmail.com>
2020-04-30 19:59:43 +0100

    js 3-function-output

--- week-1/Homework/mandatory/3-function-output.js
+++ week-1/Homework/mandatory/3-function-output.js
@@ -1,9 +1,11 @@
 // Add comments to explain what this function does. You're meant to use Google!
+//Takes a random number between 0 (included) and 1 (excluded) and multiply it on 10
 function getNumber() {
     return Math.random() * 10;
 }
 
 // Add comments to explain what this function does. You're meant to use Google!
+//concatenates the string arguments to the calling string and returns a new string.
 function s(w1, w2) {
     return w1.concat(w2);
 }
@@ -11,6 +13,7 @@ function s(w1, w2) {
 function concatenate(firstWord, secondWord, thirdWord) {
     // Write the body of this function to concatenate three words together
     // Look at the test case below to understand what to expect in return 
+    return `${firstWord} ${secondWord} ${thirdWord}`
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
