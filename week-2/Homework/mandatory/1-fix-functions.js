e3e85ae1f4e5c05ab61bda98ae94ab12baba8a5e
Leida_Gandy <leida.gandy@gmail.com>
2020-04-30 12:28:59 +0100

    Completed 1-fix-functions.js

--- week-2/Homework/mandatory/1-fix-functions.js
+++ week-2/Homework/mandatory/1-fix-functions.js
@@ -1,24 +1,23 @@
 // Fix Functions
 
-// Aim: to understand the change code inside functions 
+// Aim: to understand the change code inside functions
 //
 // The below functions are syntactically correct but not outputting the right results.
 // Look at the tests and see how you can fix them.
-
-
 
 // 1) mood function does this return `"I am not happy"`
 //    Only make edits inside the function
 
 function mood() {
-  let isHappy = true;
+  let isHappy = false;
 
-  if (isHappy) {
+  if (isHappy === true) {
     return "I am happy";
   } else {
     return "I am not happy";
   }
 }
+console.log(mood());
 
 // 2) For any numerical value greater or equal to 10
 //    Hint: use constant `num` and only change isBigEnough.
@@ -26,14 +25,15 @@ function mood() {
 
 function greaterThan10() {
   const num = 10;
-  const isBigEnough;
+  const isBigEnough = "boolean";
 
-  if (isBigEnough) {
+  if (true) {
     return "num is greater than or equal to 10";
   } else {
     return "num is not big enough";
   }
 }
+console.log(greaterThan10(15));
 
 // 3) For any numerical value greater or equal to 10
 //    Hint: use the Array method sort()
@@ -41,34 +41,39 @@ function greaterThan10() {
 
 function sortArray() {
   const letters = ["a", "n", "c", "e", "z", "f"];
-  let sortedLetters;
 
+  let sortedLetters = letters.sort();
   return sortedLetters;
 }
 
-// 4) first5 function shold return the first 5 elements of array
+console.log(sortArray());
+
+// 4) first5 function should return the first 5 elements of array
 //    Hint: use the Array method splice()
 //    Remember to Google how to use splice()
 
 function first5() {
   let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-  let sliced;
+  let sliced = numbers.slice(0, 5);
 
   return sliced;
 }
+console.log(first5());
 
-// 5) get3rdIndex function needs to take an array `arr` give 
+// 5) get3rdIndex function needs to take an array `arr` give
 //    back third element value.
 //    For example with array `[1, 2, 3, 4, 5]` it needs to return `4`
-//
+
 //    Hint: remember that arrays are zero-index based
 
-function get3rdIndex(arr) {
-  let index = 3;
-  let element;
+function get3rdIndex() {
+  let myArr = ["fruit", "banana", "apple", "strawberry", "raspberry"];
 
+  let element = myArr[3];
   return element;
 }
+
+console.log(get3rdIndex());
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -109,7 +114,7 @@ test("first5 function works", arraysEqual(first5(), [1, 2, 3, 4, 5]));
 test(
   "get3rdIndex function works - case 1",
   get3rdIndex(["fruit", "banana", "apple", "strawberry", "raspberry"]) ===
-  "strawberry"
+    "strawberry"
 );
 test(
   "get3rdIndex function works - case 2",
