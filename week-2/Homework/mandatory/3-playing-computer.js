f3b248476d10d040fe7a7135aff42c8bb6e3c6a6
NawalC <31913919+NawalC@users.noreply.github.com>
2020-04-30 01:00:54 +0100

    completed mandatory exercises

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -12,12 +12,19 @@
 
   Answer the following questions:
 
-  1. This program throws an error. Why? (If you can't find it, try executing it).
-  2. Remove the line that throws the error.
-  3. What is printed to the console?
-  4. How many times is "f1" called?
-  5. How many times is "f2" called?
-  6. What value does the "a" parameter take in the first "f1" call?
+  1. This program throws an error. Why? (If you can't find it, try executing it). 'b' was not defined
+  2. Remove the line that throws the error. removed console.log(b)
+  3. What is printed to the console? 
+      2
+      6
+      4
+      9
+      6
+     13
+      8
+  4. How many times is "f1" called? 5
+  5. How many times is "f2" called? 5
+  6. What value does the "a" parameter take in the first "f1" call? 1
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
 */
 
@@ -32,15 +39,14 @@ const f2 = function (a, b) {
   return a + b + x;
 };
 
-console.log(x);
-console.log(a);
-console.log(b);
+// console.log(x);
+// console.log(a);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
   if (i % 2 === 0) {
     const d = f2(i, x);
-    console.log(d);
+    // console.log(d);
   } else {
     const e = f1(i, a);
     console.log(e);
