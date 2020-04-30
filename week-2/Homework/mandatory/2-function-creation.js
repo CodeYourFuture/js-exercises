f864faaf5d96af8239a7c7bba2b8ef699f17c03b
Leida_Gandy <leida.gandy@gmail.com>
2020-04-30 12:47:44 +0100

    Completed exercise 2-function-creation.js

--- week-2/Homework/mandatory/2-function-creation.js
+++ week-2/Homework/mandatory/2-function-creation.js
@@ -14,7 +14,7 @@ function tidyUpString(strArr) {
 console.log(tidyUpString(" Code /youR fUtuRe/ "));
 
 /*
-2- Complete the function to check if the variable `num` satisfies the following requirements:
+2- Complete the function to check if the variable `num` satisfies the following requirements::
 - is a number
 - is even
 - is less than or equal to 100
@@ -37,7 +37,7 @@ console.log(validate(108));
 
 /* 
 3- Write a function that removes an element from an array
-The function must:
+The function must::
 - NOT change the original array
 - return a new array with the item removed
 - remove the item at the specified index
@@ -51,7 +51,7 @@ function remove(arr, index) {
 console.log(remove([10, 293, 292, 176, 29], 3));
 
 /*
-4- Write a function that:
+4- Write a function that::
 - takes an array of numbers as input
 - returns an array of strings formatted as percentages (e.g. 10 => "10%")
 - the numbers must be rounded to 2 decimal places
