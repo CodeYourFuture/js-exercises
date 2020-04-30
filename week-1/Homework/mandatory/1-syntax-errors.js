931e97d0d97b1bfca1d172fb207537063051a41a
Rares Matei <matei.rar@gmail.com>
2020-04-30 22:46:50 +0100

    solved week 1 exercise 1

--- week-1/Homework/mandatory/1-syntax-errors.js
+++ week-1/Homework/mandatory/1-syntax-errors.js
@@ -1,17 +1,18 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a b c) {
+function addNumbers(a, b, c) {
     return a + b + c;
 }
 
-function introduceMe(name, age)
-return "Hello, my name is " + name "and I am " age + "years old";
+function introduceMe(name, age) {
+    return "Hello, my name is " + name + " and I am " + age + " years old";
+}
 
 function getRemainder(a, b) {
-    remainder = a %% b;
+    remainder = a % b;
 
     // Use string interpolation here
-    return "The remainder is %{remainder}"
+    return `The remainder is ${remainder}`;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
