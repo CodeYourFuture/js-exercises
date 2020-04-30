94ed3a5319f80e74a6df293ce48411f1c3454db8
Sabia <sabiamushtaq2000@gmail.com>
2020-04-30 16:19:55 +0100

    ex 3 done?
    task desc unclear, not sure if ex 3 done correctly

---
+++ week-8/Homework/mandatory/3-dog-photo-gallery/dogsgallery.js
@@ -0,0 +1,22 @@
+document.getElementById("imgBtn").addEventListener("click", function (){
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => {
+        console.log(response);
+        return response.json();
+})
+
+.then(data => {
+    console.log(data);
+    let element = data.message
+    console.log(element)
+
+    let ul = document.getElementById("list")
+    let imgList = document.createElement("li")
+    let dogImg = document.createElement("img")
+    dogImg.src = element
+    imgList.appendChild(dogImg)
+    ul.appendChild(imgList)
+})
+})
+
+
