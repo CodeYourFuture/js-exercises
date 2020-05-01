95009374e0652e7660141794f7f1c8edd89ad098
JeanPaul18 <61525313+JeanPaul18@users.noreply.github.com>
2020-05-01 15:25:59 +0100

    added playing-computer

--- week-2/Homework/mandatory/3-playing-computer.js
+++ week-2/Homework/mandatory/3-playing-computer.js
@@ -13,12 +13,19 @@
   Answer the following questions:
 
   1. This program throws an error. Why? (If you can't find it, try executing it).
+  //It throws an error because b is not defined.
   2. Remove the line that throws the error.
   3. What is printed to the console?
+  //In the console is printed 2 and 6.
   4. How many times is "f1" called?
+  //In the f1 is called twice odd number.
   5. How many times is "f2" called?
+  //The f2 is called three times even numbers
   6. What value does the "a" parameter take in the first "f1" call?
+//In the first call it takes the place of 7 .
   7. What is the value of the "a" outer variable when "f1" is called for the first time?
+// it takes the place of 9
+
 */
 
 let x = 2;
@@ -34,7 +41,7 @@ const f2 = function (a, b) {
 
 console.log(x);
 console.log(a);
-console.log(b);
+// console.log(b);
 
 for (let i = 0; i < 5; ++i) {
   a = a + 1;
