1bad4f46db56377fef64731742033951c56988d0
Farhana <farhanafatema@hotmail.com>
2020-05-01 12:54:15 +0100

    struggling the first function, completed the rest

--- week-2/Homework/mandatory/2-function-creation.js
+++ week-2/Homework/mandatory/2-function-creation.js
@@ -5,7 +5,14 @@ Write a function that:
 - removes any forward slashes (/) in the strings
 - makes the string all lowercase
 */
-function tidyUpString(strArr) {}
+function tidyUpString(strArr) {
+    function tidy(name) {
+    return name.trim().toLowerCase();
+    }
+    let tidied= strArr.map(tidy);
+    return tidied;
+  }
+
 
 /*
 Complete the function to check if the variable `num` satisfies the following requirements:
@@ -15,7 +22,15 @@ Complete the function to check if the variable `num` satisfies the following req
 Tip: use logical operators
 */
 
-function validate(num) {}
+function validate(num) {
+  if (typeof num === 'number' && num % 2 === 0 && num<=100){
+    return true;
+ }
+
+else {
+    return false;
+}
+}
 
 /* 
 Write a function that removes an element from an array
@@ -26,7 +41,9 @@ The function must:
 */
 
 function remove(arr, index) {
-  return; // complete this statement
+  let removedElement;
+    removedElement = arr.splice(index,1);
+    return arr; // complete this statement
 }
 
 /*
@@ -37,7 +54,14 @@ Write a function that:
 - numbers greater 100 must be replaced with 100
 */
 
-function formatPercentage(arr) {}
+function formatPercentage(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = Number(arr[i].toFixed(2));
+      if (arr[i] >= 100) arr[i] = 100;
+      arr[i] = arr[i] + "%";
+    }
+    return arr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
