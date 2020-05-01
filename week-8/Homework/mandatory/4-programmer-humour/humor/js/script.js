2bc465f6898a7e9a78a1a27d5b3044c7a3a26a04
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-01 20:59:20 +0100

    Humor workes!

--- week-8/Homework/mandatory/4-programmer-humour/humor/js/script.js
+++ week-8/Homework/mandatory/4-programmer-humour/humor/js/script.js
@@ -1,17 +1,17 @@
-fetch(`https://xkcd.com/info.0.json`)
+ fetch(`https://xkcd.now.sh/?comic=latest`)
 .then(function(result){
     return result.json();
 })
 .then(function(pic){
     console.log(pic);
+
     let h1 = document.querySelector("h1");
     let img = document. createElement("img");
     h1.appendChild(img);
-    img.href = pic.img;
-})
-.catch(error){
-    console.error(error);
-    // expected output: ReferenceError: nonExistentFunction is not defined
-    // Note - error messages will vary depending on browser
-  }
-  
+    img.src = pic.img;
+});
+// catch(error){
+//     console.error(error);
+//     // expected output: ReferenceError: nonExistentFunction is not defined
+//     // Note - error messages will vary depending on browser
+//   }
