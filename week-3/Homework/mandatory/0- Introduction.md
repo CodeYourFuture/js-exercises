6fcaeb5d1a0be3d4a44b0679bf63cd0b155da14c
Ashaghel <ahmadshageldev@gmail.com>
2020-05-01 23:40:32 +0100

    editing exercises for week3 mcr3

--- week-3/Homework/mandatory/0- Introduction.md
+++ week-3/Homework/mandatory/0- Introduction.md
@@ -2,6 +2,10 @@
 
 You should complete all of these FreeCodeCamp exercises - https://www.freecodecamp.org/learn
 
+> -Go to JavaScript Algorithms and Data Structures Certification (300 hours)
+> -Then oepn Basic Data Structures
+> Do the following exercises
+
 - Introduction to the Basic Data Structure Challenges
 - Use an Array to Store a Collection of Data
 - Access an Array's Contents Using Bracket Notation
@@ -15,14 +19,6 @@ You should complete all of these FreeCodeCamp exercises - https://www.freecodeca
 - Check For The Presence of an Element With indexOf()
 - Iterate Through All an Array's Items Using For Loops
 - Create complex multi-dimensional arrays
-- Add Key-Value Pairs to JavaScript Objects
-- Modify an Object Nested Within an Object
-- Access Property Names with Bracket Notation
-- Use the delete Keyword to Remove Object Properties
-- Check if an Object has a Property
-- Iterate Through the Keys of an Object with a for...in Statement
-- Generate an Array of All Object Keys with Object.keys()
-- Modify an Array Stored in an Object
 
 and you should attempt all of these
 
--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -1,4 +1,30 @@
 /*
+Before we go the big story;  we will introduce more string methods. Some of the methods you're using in Array have similar ones with strings
+Methods like : IndexOf, Include, Search, Slice , Spilt and more. You can always google how a method of a string work
+Here is links for some of them 
+https://www.w3schools.com/js/js_string_methods.asp
+https://javascript.info/string#quotes
+
+
+Now let's do this small exercise
+Using string mapulation methods update the checkCodeIsThere() function 
+- The function will have a string as a paramter
+-The function should check if the string has the word "code" exists in the string
+- If it does exist, return the index of it, if not return not found 
+Hint: search string methods like Includes, IndexOF can be helpful 
+*/
+
+function checkCodeIsThere(stringText) {
+  let magicWord = "code";
+  //edit code below
+  if (stringText) {
+    return stringText;
+  } else {
+    return "Not found";
+  }
+}
+
+/*
   I am new to London and would like to know what transport I can take to different famous locations.
   An array with London locations have been provided.
 
@@ -6,19 +32,24 @@
 
   NOTE: only the names should be returned, not the means of transport.
 */
+//Hint some string methods needed as well as array methods
+//search slice and includes for string
+// and filter and map for array
 
-function journeyPlanner() {
-
-}
+function journeyPlanner() {}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
+
+const string1 = "I Love coding and perfect code makes me happy";
+const string2 = "I don't like to do coding";
+const string3 = "Can you scan the barcode for me";
 
 const londonLocations = [
   ["Angel", "tube", "bus"],
   ["London Bridge", "tube", "river boat"],
   ["Tower Bridge", "tube", "bus"],
-  ["Greenwich", "bus", "river boat"]
-]
+  ["Greenwich", "bus", "river boat"],
+];
 
 function arraysEqual(a, b) {
   if (a === b) return true;
@@ -33,33 +64,51 @@ function arraysEqual(a, b) {
 }
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
-test("journeyPlanner function works - case 1",
-  arraysEqual(
-    journeyPlanner(londonLocations, "river boat"),
-    ["London Bridge", "Greenwich"]
-  )
-)
+test(
+  "checkCodeIsThere function works - case 1",
+  checkCodeIsThere(string1) === 26
+);
 
-test("journeyPlanner function works - case 2",
-  arraysEqual(
-    journeyPlanner(londonLocations, "bus"),
-    ["Angel", "Tower Bridge", "Greenwich"]
-  )
-)
+test(
+  "checkCodeIsThere function works - case 2",
+  checkCodeIsThere(string2) === "Not found"
+);
+test(
+  "checkCodeIsThere function works - case 3",
+  checkCodeIsThere(string3) === 20
+);
+test(
+  "journeyPlanner function works - case 1",
+  arraysEqual(journeyPlanner(londonLocations, "river boat"), [
+    "London Bridge",
+    "Greenwich",
+  ])
+);
 
-test("journeyPlanner function works - case 3",
-  arraysEqual(
-    journeyPlanner(londonLocations, "tube"),
-    ["Angel", "London Bridge", "Tower Bridge"]
-  )
-)
+test(
+  "journeyPlanner function works - case 2",
+  arraysEqual(journeyPlanner(londonLocations, "bus"), [
+    "Angel",
+    "Tower Bridge",
+    "Greenwich",
+  ])
+);
+
+test(
+  "journeyPlanner function works - case 3",
+  arraysEqual(journeyPlanner(londonLocations, "tube"), [
+    "Angel",
+    "London Bridge",
+    "Tower Bridge",
+  ])
+);
