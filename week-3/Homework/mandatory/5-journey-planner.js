4feab6b7f79b6d19743a62ab0c179cebf48d3208
Zoltan Gal <zoltan.gal@sainsburys.co.uk>
2020-05-02 15:57:07 +0200

    Add more details and restructure journey planner exercise

--- week-3/Homework/mandatory/5-journey-planner.js
+++ week-3/Homework/mandatory/5-journey-planner.js
@@ -1,17 +1,23 @@
 /*
-Before we go the big story;  we will introduce more string methods. Some of the methods you're using in Array have similar ones with strings
-Methods like : IndexOf, Include, Search, Slice , Spilt and more. You can always google how a method of a string work
-Here is links for some of them 
-https://www.w3schools.com/js/js_string_methods.asp
-https://javascript.info/string#quotes
+ Before we go the big story; we will introduce more string methods.
+ Some of the methods you're using in Array have similar ones with strings.
+
+ Methods like : IndexOf, Include, Search, Slice , Spilt and more.
+ 
+ You can always google how a method of a string works!
+ Here are links to some of those:
+  - https://www.w3schools.com/js/js_string_methods.asp
+  - https://javascript.info/string#quotes
 
 
-Now let's do this small exercise
-Using string methods update the checkCodeIsThere() function 
-- The function will have a string as a paramter
--The function should check if the string has the word "code" exists in the string
-- If it does exist, return the index of it, if not return not found 
-Hint: search string methods like Includes,and IndexOf
+ Now let's do this small exercise
+ 
+ Using string methods update the checkCodeIsThere() function
+  - The function will have a string as a paramter
+  - The function should check if the string has the word "code" exists in the string
+  - If it does exist, return the index of it, if not return "Not found"
+
+ Hint: search for string methods like Includes and IndexOf.
 */
 
 function checkCodeIsThere(stringText) {
@@ -26,17 +32,94 @@ function checkCodeIsThere(stringText) {
 
 /*
   I am new to London and would like to know what transport I can take to different famous locations.
-  An array with London locations have been provided.
+  The input provided contains a list of locations in London. Each of locations is followed by a list
+  of transport modes that can be used to get there.
 
-  Return an array of where I can go if I only want to use a specific mode of transport.
+  Let's an example how your input will look. To take to Tower Bridge, you can use tube or river boat.
 
-  NOTE: only the names should be returned, not the means of transport.
+  This information will represented as 
+    ["Tower Bridge", "tube", "river boat"]
+
+  Where
+    the 1st element says the name of the location,
+    and rest of them says the transport modes.
+
+  You will then get a list of these information, e.g:
+  [
+    ["Tower Bridge", "tube", "river boat"],
+    ["Abbey road", "double decker"],
+    ["London Eye", "tube", "river boat", "bus"]
+  ]
+
+  Finish up the body of journeyPlanner function that should tell where I can go if I only want to use a specific mode of transport.
+
+  Before jumping straight to the journeyPlanner, we will break down the whole task into smaller steps that make our job easier.
+
+  (This technic is also referred as problem decomposition. It helps you to reduce scope of the problem 
+   by only focusing on a small chunk of the whole problem at a time.)
 */
-//Hint some string methods needed as well as array methods
-//search slice and includes for string
-// and filter and map for array
 
-function journeyPlanner() {}
+/*
+  Implement the function getTransportModes that
+   - Should accepts an array containing the location and available transport modes
+     e.g: ["Tower Bridge", "tube", "river boat"]
+   - Returns an array including the available transport modes to the given location
+     e.g: ["tube", "river boat"]
+  
+  Hint: Use the corresponding array method to split the array.
+*/
+function getTransportModes() { }
+
+/*
+  Implement the function isAccessibleByTransportMode that
+   - Should accept two parameters:
+     1) First parameter is an array of transport modes
+        e.g: ["tube", "river boat"]
+     2) Second parameter is a string containing a transport mode
+        e.g: "river boat"
+     
+   - Returns 
+     * True if the location in the first parameter is accessible by the transport mode given in second parameter
+     * Otherwise, returns false
+
+  Hint: Use the corresponding array method to decide if an element is member of an array.
+*/
+function isAccessibleByTransportMode() { }
+
+/*
+  Implement the function getLocationName that
+   - Should accepts a location and available transports in an array
+     e.g:["Tower Bridge", "tube", "river boat"]
+   - Returns the name of the location
+     e.g: "Tower Bridge"
+*/
+function getLocationName() { }
+
+/*
+ We arrived at the final method. it won't take long if you use the previously implemented functions wisely.
+
+ Finish up the implemention of the function journeyPlanner that
+  - Accepts two parameters:
+    1) An array with a list of locations
+       e.g:
+       [
+         ["Angel", "tube", "bus"],
+         ["London Bridge", "tube", "river boat"],
+       ]
+    2) A string containing a transport mode
+
+  - Returns an array of where I can go if I only want to use a specific mode of transport.
+    NOTE: only the names should be returned, not the name of transports.
+
+  Hint:
+   - Use the function you implemented above.
+   - Use array method to remove locations that is not accessible by the given transportMode.
+   - Use array method to manipulate its elements.
+
+*/
+function journeyPlanner(locations, transportMode) {
+  // Implement the function body
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
@@ -87,6 +170,38 @@ test(
   "checkCodeIsThere function works - case 3",
   checkCodeIsThere(string3) === 20
 );
+
+test(
+  "getTransportModes function works",
+  arraysEqual(getTransportModes(["Angel", "tube", "bus"]), ["tube", "bus"])
+);
+
+test(
+  "isAccessibleByTransportMode function works - positive case 1",
+  arraysEqual(isAccessibleByTransportMode(["tube", "bus"], "tube"), true)
+);
+
+test(
+  "isAccessibleByTransportMode function works - negative case 1",
+  arraysEqual(isAccessibleByTransportMode(["tube", "bus"], "river boat"), false)
+);
+
+test(
+  "isAccessibleByTransportMode function works - negative case 2",
+  arraysEqual(isAccessibleByTransportMode(["tube", "bus", "river boat"], "boat"), false)
+);
+
+test(
+  "getLocationName function works - case 1",
+  arraysEqual(getLocationName(["London Bridge", "tube", "river boat"]), "London Bridge")
+);
+
+test(
+  "getLocationName function works - case 2",
+  arraysEqual(getLocationName(["Angel", "tube", "bus"]), "Angel")
+);
+
+
 test(
   "journeyPlanner function works - case 1",
   arraysEqual(journeyPlanner(londonLocations, "river boat"), [
