e45a52e2b8a86fdd880f128da87f200500baf0a4
Louise Chow <louise.chow@gmail.com>
2020-05-02 13:37:29 +0100

    Add ATM exercise (Classes)

---
+++ week-9/Homework/mandatory/2-exercises/3-atm.js
@@ -0,0 +1,20 @@
+/*
+
+ Complete the code to create a simple ATM machine.
+
+ You should start with a balance of 100 pounds.
+
+ */
+
+class ATM {
+    // Add your code here
+
+}
+
+let atm = new ATM(); // Create the ATM
+
+atm.make_deposit(200);
+atm.check_balance();
+atm.make_withdrawl(100);
+
+atm.make_withdrawl(500);
