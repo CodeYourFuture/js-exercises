dd4a1b27a23a530d8349189506f1615435186007
Aronja <anovicky@thoughtworks.com>
2020-05-02 08:41:50 +0100

    Add Loops exercises

---
+++ week-2/Homework/H-loops/exercise-1.js
@@ -0,0 +1,21 @@
+/*
+  While Loops
+  ---------------------------------
+Exercise 1:
+
+  Write a while loop that prints "eating 1 slice of pizza" 6 times to the console.
+
+  Use the variables slices and hungry to create a condition.
+  If hungry is greater than slices, then we will eat a slice of pizza. 
+  If hungry is the same as slices then the loop terminates.
+
+  Don't forget to increase (slices ++) inside the while loop.
+
+  - HINT: check the cake example in the week-2 presentation 
+
+  */
+
+  let slices = 0;
+  const hungry = 6;
+  
+  // add your code here, start with: while(...) {...}
