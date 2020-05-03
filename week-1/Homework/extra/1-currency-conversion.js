23790a8794655dffa8742ad97118a72f6bf7f468
bash93 <bashirabdalmonem@gmail.com>
2020-05-03 09:57:15 +0100

    Update 1-currency-conversion.js

--- week-1/Homework/extra/1-currency-conversion.js
+++ week-1/Homework/extra/1-currency-conversion.js
@@ -5,7 +5,10 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(amountInPound) {
+  const ConvertedPrice = 1.4 * amountInPound;
+  return ConvertedPrice;
+}
 
 /*
   CURRENCY FORMATTING
@@ -16,20 +19,29 @@ function convertToUSD() {}
   Find a way to add 1% to all currency conversions (think about the DRY principle)
 */
 
-function convertToBRL() {}
+function convertToBRL(amountInPound) {
+  const convertedPrice = 5.7 * amountInPound;
+  //convertedPrice=addFee(convertedPrice)
+  return convertedPrice;
+}
+function addFee(amount) {
+  const fees = 0.01;
+  const amountWithFee = amount * (1 + fees);
+  return amountWithFee;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 function test(test_name, expr) {
   let status;
   if (expr) {
-      status = "PASSED"
+    status = "PASSED";
   } else {
-      status = "FAILED"
+    status = "FAILED";
   }
 
-  console.log(`${test_name}: ${status}`)
+  console.log(`${test_name}: ${status}`);
 }
 
-test('convertToUSD function works', convertToUSD(32) === 44.8)
-test('convertToBRL function works', convertToBRL(30) === 172.71)
+test("convertToUSD function works", convertToUSD(32) === 44.8);
+test("convertToBRL function works", convertToBRL(30) === 172.71);
