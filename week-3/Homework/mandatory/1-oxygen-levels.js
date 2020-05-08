8c5cfe7b09a33af522380d3f906c114eda55ffb3
bash93 <bashirabdalmonem@gmail.com>
2020-05-08 16:46:31 +0100

    Update 1-oxygen-levels.js

--- week-3/Homework/mandatory/1-oxygen-levels.js
+++ week-3/Homework/mandatory/1-oxygen-levels.js
@@ -14,8 +14,7 @@ example before trying the exercise.
 
 /* .replace() allows us to add something where we removed something*/
 let greeting = "Good Morning";
-greeting.replace('Morning', 'Evening'); // outputs Good Evening
-
+greeting.replace("Morning", "Evening"); // outputs Good Evening
 
 /* .substring() allows us to remove things from strings */
 let dessert = "ice cream and pancakes";
@@ -27,32 +26,36 @@ console.log(newdessert); // returns ice cream
 /* + + + + + + + */
 /* Now try the exercise */
 
-function safeLevels() {
-
+function safeLevels(arr) {
+  const numOnly = arr.map(function (o2) {
+    return o2.substring(0, 4);
+  });
+  const safeLevel = numOnly.find((n) => n < "23.5" && n > "19.5");
+  return safeLevel + "%";
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
-const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"]
-const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"]
+const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"];
+const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"];
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels1) === "19.9%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels1) === "19.9%"
 );
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels2) === "20.2%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels2) === "20.2%"
 );
