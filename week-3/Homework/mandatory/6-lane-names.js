b4e224a0f97787a72f5f82e3118c02148e495bff
efahmed <engineerfa.24816@gmail.com>
2020-05-08 05:19:16 +0100

    it is done

--- week-3/Homework/mandatory/6-lane-names.js
+++ week-3/Homework/mandatory/6-lane-names.js
@@ -5,43 +5,48 @@
   
 */
 //hint: string and array methods that could be helpful (indexOf, filter)
-function getLanes() {
-
+function getLanes(namesOfStreet) {
+  const checkLanes = namesOfStreet.filter(function (totalName) {
+    let findLanes = totalName.includes("Lane");
+    return findLanes;
+  });
+  return checkLanes;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
 const streetNames = [
-    "Abchurch Lane",
-    "Adam's Court",
-    "Addle Hill",
-    "Addle Lane",
-    "Alban Highwalk"
-]
+  "Abchurch Lane",
+  "Adam's Court",
+  "Addle Hill",
+  "Addle Lane",
+  "Alban Highwalk",
+];
 
 function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-    
-    for (let i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-   
-    return true;
-}
-  
-function test(test_name, expr) {
-    let status;
-    if (expr) {
-        status = "PASSED";
-    } else {
-        status = "FAILED";
-    }
-    
-    console.log(`${test_name}: ${status}`);
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
 }
 
-test("getLanes function works",
-    arraysEqual(getLanes(streetNames), ["Abchurch Lane", "Addle Lane"])
-)
+function test(test_name, expr) {
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
+}
+
+test(
+  "getLanes function works",
+  arraysEqual(getLanes(streetNames), ["Abchurch Lane", "Addle Lane"])
+);
