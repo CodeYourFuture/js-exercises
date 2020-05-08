c608c3c30782d6a3d5bc36910b32b6055893c128
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-05-08 15:02:34 +0100

    atm

--- week-9/Homework/mandatory/2-exercises/3-atm.js
+++ week-9/Homework/mandatory/2-exercises/3-atm.js
@@ -12,9 +12,26 @@
  */
 
 class ATM {
-    // Add your code here
+  constructor() {
+    this.balance = 100;
 
+    this.make_deposit = (amounttoDeposit) => {
+      this.balance += amounttoDeposit;
+      console.log(`Thank you ${amounttoDeposit} was added to your account`);
+    };
+    this.check_balance = () => {
+      console.log(`Your balance is ${this.balance} russian dollars`);
+    };
+    this.make_withdrawl = (toTake) => {
+      if (this.balance >= toTake) {
+        console.log(`Poor human take you ${toTake} russian dollars`);
+      } else {
+        console.log("Insufficient balance");
+      }
+    };
+  }
 }
+// Add your code here
 
 let atm = new ATM(); // Create the ATM
 
