6d0892d2166a34570d79910a5771227c910a9d30
mahmutsel <mahmutsel29@gmail.com>
2020-05-11 02:52:30 +0100

    convertion

--- week-9/Homework/mandatory/2-exercises/2-convertion.js
+++ week-9/Homework/mandatory/2-exercises/2-convertion.js
@@ -1,30 +1,37 @@
 /*
-    Convert the Function here into a JavaScript Class called 'Person'
-
-    ```
+   // Convert the Function here into a JavaScript Class called 'Person'
+    
         function createNewPerson(name) {
             const obj = {};
             obj.name = name;
             obj.greeting = function() {
-                alert('Hi! I\'m ' + obj.name + '.');
+                console.log('Hi! I\'m ' + obj.name + '.');
             };
             return obj;
         }
 
         const simon = createNewPerson('simon');
-        simon.name;
-        simon.greeting()
-    ```
-
-    When you run this code using `node 2-convertion.js` you should be able to
+        console.log(simon.name);
+       simon.greeting()
     
 
-*/
+    //When you run this code using `node 2-convertion.js` you should be able to
+    */
+
 
 // Write your code here
+class Person {
+    constructor(name){
+        this.name=name;
+   }   
 
+    greeting(){
+            console.log('Hi! I\'m ' + this.name + '.')
+    }
+    
+}
 // Do not edit this section
-const simon = Person("simon");
+const simon = new Person("simon");
 console.log(simon.name);
 simon.greeting();
 // Do not edit this section
