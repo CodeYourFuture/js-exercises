c4f12f2eb80c763e63146fdc988684da5dfed315
Chris Owen <chris@thecodethatchriswrote.com>
2020-05-12 18:04:34 +0100

    Add Old Homework To Scotland Class 4 Branch (#852)

---
+++ week-2/Homework/L-extra/level-3/2-validation.js
@@ -0,0 +1,25 @@
+// Complete the function to check if the variable `num` satisfies the following requirements:
+// - is a number
+// - is an integer (not a float)
+// - is not equal any of the numbers in the array `excludedNums`
+// Tip: write other small functions for each requirement
+
+var excludedNums = [6, 14, 91, 111];
+
+function validate(num) {}
+
+console.log(validate(6));
+console.log(validate(10.5));
+console.log(validate(101));
+console.log(validate(-91));
+console.log(validate("16"));
+
+/*
+  EXPECTED RESULT
+  ---------------
+  false
+  false
+  true
+  true
+  false
+*/
