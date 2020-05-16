a56d7a57e37f44fdc3dfa3ac029b5087493fc34c
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-16 20:13:35 +0100

    almost done weatherapp needs debug

--- week-9/Homework/mandatory/1-practice/weatherapp/app.js
+++ week-9/Homework/mandatory/1-practice/weatherapp/app.js
@@ -4,6 +4,8 @@ window.addEventListener(`load`, ()=>{
     let temperatureDescription = document.querySelector(`.temperature-description`);
     let temperatureDegree = document.querySelector(`.temperature-degree`);
     let locationTimezone = document.querySelector(`.location-timezone`);
+    let temperatureSection = document.querySelector(`.temperature`);
+    const temperatureSpan = document.querySelector(`span`);
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             long = position.coords.longitude;
@@ -16,14 +18,32 @@ window.addEventListener(`load`, ()=>{
                return response.json()
             })
             .then(data=>{
-                console.log(data);
-                const {temperature, summary, timezone} = data.currently;
+                const {temperature, summary, timezone, icon} = data.currently;
 //Set DOM Elements from API
 temperatureDegree.textContent = temperature;
 temperatureDescription.textContent = summary;
 locationTimezone.textContent = data.timezone;
+let celcius = (temperature - 32 )*(5/9);
+
+setIcons(icon, document.querySelector(".icon"));
+temperatureSection.addEventListener("click",()=>{
+    if(temperatureSpan.textContent === "F"){
+        temperatureSpan.textContent = "C";
+        temperatureDegree.textContent = celsius;
+    }
+    else{
+        temperatureSpan.textContent = "F";
+    };
+})
 
             });
     });
+}
+    function setIcons( icon, iconID){
+        const skycons = new Skycons ({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon])
+   
 }
 });
