2c03bac7a46ac8fb602dea323be9acc3fce81ad3
azadaafo <azadaafo@gmail.com>
2020-05-18 06:42:25 +0100

    completed InClass/A-objects-intro/exercise-part-1.js

--- week-4/InClass/A-objects-intro/exercise-part-1.js
+++ week-4/InClass/A-objects-intro/exercise-part-1.js
@@ -6,4 +6,47 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+let person = {
+    hight: 185,
+    weight: 80,
+    hairColor: "black",
+    smoker: false
+}
 
+let location = {
+    cityName: "Manchester",
+    population: 600000,
+    highBuildings: "yes",
+    diverseCity: true
+}
+
+let CYF = {
+    purpose: "educational",
+    helpfull: true,
+    supportLevel: "very high",
+    encouraging: true,
+    numOfMentors: 10,
+    numOfStudents: 25
+}
+
+let homework = {
+    hardPart: "functions and loops",
+    useful: "not for me until now",
+    harm: "I lost my confidence",
+    whyThat: "online style and missed some impo parts",
+    longValues: true,
+    reason: "wanted to deliver my feedback",
+    percentOfHelpNeeded: 100,
+    didYouUnderstandObjects: "except functions and loops part"
+}
+
+let feelings = {
+    LikeTheCourse: true,
+    hoursDevotedPerDay: 5,
+    goodResources: "not enough",
+    oldStyleOrNew: "old",
+    interestedInTheCourse: "of course",
+    didIrunMyObjects: true
+}
+
+console.log(feelings, homework, CYF);
