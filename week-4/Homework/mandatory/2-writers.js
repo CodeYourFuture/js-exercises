d42ced6e3628d4c762933cd9835c578bb1502ce4
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2020-05-19 20:24:50 +0100

    word mistake fixed

--- week-4/Homework/mandatory/2-writers.js
+++ week-4/Homework/mandatory/2-writers.js
@@ -5,14 +5,10 @@ Did you know you can also have an Array of Objects? You might think "This is mad
 it is quite a frequent combination. Just think about what benefits we can get from this construct.
 
 An object lets you store multiple values in a single variable, then you can store complex objects in an array.
-Let's assume you have a list of data about people names and their birthday and you would like to print each name
-with corresponding birthday together. Storing these pieces of information in different arrays and then pairing them up
-makes the iteration unnecessarily complicated, code will be less intuitive, needs extra cognitive effort to
-reason about and last but not least it can be error-prone (for example, you pick up the wrong birthday to a name).
 
-In this exercise you will practice how to access to Objects stored in an Array and their properties. You already know 
-different ways of looping through Arrays, it won't be different in this case. The only extra step is that you have to 
-use values inside Objects.
+Let's assume you have a list of data about people names and their birthday and you would like to print each name with corresponding birthday together. Storing these pieces of information in different arrays and then pairing them up makes the iteration unnecessarily complicated, code will be less intuitive, needs extra cognitive effort to reason about and last but not least it can be error-prone (for example, you pick up the wrong birthday to a name).
+
+In this exercise you will practice how to access to Objects stored in an Array and their properties. You already know different ways of looping through Arrays, it won't be different in this case. The only extra step is that you have to use values inside Objects.
 */
 
 // We've created an array of objects for you here:
@@ -58,7 +54,7 @@ Exercise 1:
 
 /*
 Exercise 2:
-  Only `console.log()` out the writers who are in their 40s (meaning between 40 and 49)
+  Only `console.log()` out the writers who were in their 40s (meaning between 40 and 49)
   and not alive anymore. Use the below sentence format:
   "Writer {firstName} {lastName} died at {age} years old."
 */
