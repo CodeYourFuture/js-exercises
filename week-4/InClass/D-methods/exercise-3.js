43f6449c32afb9e8d8a436c69969b7131de207f1
azadaafo <azadaafo@gmail.com>
2020-05-21 00:41:07 +0100

    corrected InClass/D-methods/exercise-3.js

--- week-4/InClass/D-methods/exercise-3.js
+++ week-4/InClass/D-methods/exercise-3.js
@@ -7,9 +7,9 @@ Once you fix them, run this file, it should output the correct values!
 let person = {
     name: "Alice",
     age: 25,
-    currentAddress: "Edinburgh",
+    currentAddress: "Glasgow",
     changeAddress: function (newAddress) {
-        currentAddress = newAddress;
+        return this.currentAddress = newAddress;
     },
     celebrateBirthday: function () {
         this.age = this.age + 1;
