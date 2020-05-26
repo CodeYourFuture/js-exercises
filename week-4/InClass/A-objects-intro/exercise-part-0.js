88721409caaae4b05f34f5b3f692a248ff41a3ff
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-05-26 22:19:13 +0100

    parrt0 is done

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,9 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+const laptop = {
+  type: "Mac",
+  screenSize: 13,
+  isTouchScreen: false,
+};
