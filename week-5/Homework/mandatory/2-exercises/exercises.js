27ce36210d474468315d05917e13b07487146d48
Zoltan Gal <zoltan.gal@sainsburys.co.uk>
2020-05-29 22:36:01 +0200

    Rewrite week-5/2-exercises

--- week-5/Homework/mandatory/2-exercises/exercises.js
+++ week-5/Homework/mandatory/2-exercises/exercises.js
@@ -1,63 +1,77 @@
-/**
- *
- * For each of the names in the array passed into this function
- *
- * - Add a <h1> tag to the website containing the name of the person
- * - Add a <h2> tag to the website containing the job of the person
- *
- * All of your HTML should go inside the Div tag with the id "content".
- *
- * <div id="content">
- *      <h1>{Name Here}</h1>
- *      <h2>{Job Here}</h2>
- *      .....
- * </div>
+/*
+ In this exercise you will deal with Objects stored in array and DOM manipulation with JavaScript.
+ From now, exercise descriptions don't provide detailed examples about input and output of functions.
+ You should be already able to read and understand the code in order to extract the necessary information
+ you need to implement functions' body.
  */
-function exerciseOne(arrayOfPeople) {
-  let content = document.querySelector("#content");
-}
+
+/*
+ Optional question:
+
+ Notice how this JavaScript file linked to the index.html file.
+ What do you think when this JavaScript file is being executed?
+
+ When you finished this exercise try to move the script tag around the Div tag with the id "content",
+ refresh the page and observe what happens. (This should answer the question above.)
+ 
+ What do you think why this changes shouldn't work?
+ */
+// LEAVE YOUR ANSWER HERE (THIS IS OPTIONAL)
+
 
 /**
+ * This function recieves a lists of people. Each object should contain the name and the occupation of a person.
+ * Look for usage of the function in the code and see what variable is passed into it as an argument.
+ * 
+ * Create and insert the below HTML elements to the DOM for each of Objects in the Array as follows:
+ *  1. Add a <h1> tag to the website containing the name of the person
+ *  2. Add a <h2> tag to the website containing the job of the person
  *
- * Create a list of shopping items. You should use an unordered list.
- *
- * All of your HTML should go inside the Div tag with the id "content".
- *
+ * All of your HTML elements should go inside the Div tag with the id "content".
+ * 
+ * An example "content" div should look like:
+ * <div id="content">
+ *      <h1>Mario</h1>
+ *      <h2>Plumber</h2>
+ *      <h1>Luigi</h1>
+ *      <h2>Plumber</h2>
+ * </div>
  */
-function exerciseTwo(shopping) {
+function insertPeopleData(arrayOfPeople) {
+  let content = document.querySelector("#content");
   //Write your code in here
 }
 
 /**
-    I'd like to display my three favorite books inside a nice webpage!
+ *
+ * Create a list of shopping items using an unordered HTML list.
+ * The input of this function is a simple Array of Strings, look for the concrete example in the code.
+ * 
+ * All of your HTML elements should go inside the Div tag with the id "content".
+ *
+ * Hint for type of lists in HTML: https://www.w3schools.com/html/html_lists.asp
+ */
+function insertShoppingList(shoppingList) {
+  //Write your code in here
+}
 
-    const books = [
-        {
-            title: "The Design of Everyday Things",
-            author: "Don Norman",
-            alreadyRead: false
-        },
-        {
-            title: "The Most Human Human",
-            author: "Brian Christian",
-            alreadyRead: true
-        },
-        {
-            title: "The Pragmatic Programmer",
-            author: "Andrew Hunt",
-            alreadyRead: true
-        }
-    ];
-
-    Iterate through the array of books.
-    - For each book, create a <p> element with the book title and author and append it to the page.
-    - Use a <ul> and <li> to display the books.
-    - Add an <img> to each book that links to a URL of the book cover.
-    - Change the style of the book depending on whether you have read it (green) or not (red).
-
-    The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
-**/
-function exerciseThree(books) {
+/**
+ * I'd like to display my three favorite books inside a nice webpage!
+ * 
+ * Iterate through the array of books passed into this function and insert each book to page as follows:
+ *   - Create a <ul> list and display each book details in <li> element.
+ *   - For each book, create a <p> element with the book title and author and append it to the page.
+ *   - Add an <img> after <p> element to each book that links to a URL of the book cover.
+ *     You should find an appropriate image to each book.
+ *   - Change the style of the book depending on whether you have read it (green) or not (red).*
+ * 
+ * Find in the code what properties a book object has in the array.
+ *  
+ * All of your HTML elements should go inside the Div tag with the id "content".
+ * 
+ * The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/*
+ */
+function insertBooks(books) {
   //Write your code in here
 }
 
@@ -77,11 +91,11 @@ let people = [
   { name: "Boris", job: "Prime Minister" }
 ];
 
-exerciseOne(people);
+insertPeopleData(people);
 
 let shopping = ["Milk", "Break", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
-exerciseTwo(shopping);
+insertShoppingList(shopping);
 
 const books = [
   {
@@ -101,4 +115,4 @@ const books = [
   }
 ];
 
-exerciseThree(books);
+insertBooks(books);
