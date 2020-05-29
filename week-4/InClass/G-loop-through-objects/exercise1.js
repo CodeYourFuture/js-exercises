d6bed7ce5293d5dd1d15a5fcbbde4d68e8d616c9
Rares Matei <matei.rar@gmail.com>
2020-05-29 23:11:37 +0100

    update

--- week-4/InClass/G-loop-through-objects/exercise1.js
+++ week-4/InClass/G-loop-through-objects/exercise1.js
@@ -1,6 +1,5 @@
 // You are given an object of student names and grades
 // Loop through all the students' grades and console log the name and grade of the ones with grade more than 18
-// Try to use both methods shown above to achieve this
 
 const studentGrades = {
   tom: 20,
