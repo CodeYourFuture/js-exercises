2b0206a2987be295afc61dcbeec271f2d0f00f52
Rares Matei <matei.rar@gmail.com>
2020-05-30 11:39:40 +0100

    add itworked file

--- week-4/InClass/G-loop-through-objects/exercise1.js
+++ week-4/InClass/G-loop-through-objects/exercise1.js
@@ -1,12 +1,17 @@
-// You are given an object of student names and grades
-// Loop through all the students' grades and console log the name and grade of the ones with grade more than 18
-
-const studentGrades = {
-  tom: 20,
-  george: 17,
-  abdul: 19,
+let kitten1 = {
+  name: "Fluffy",
+  weeksOld: 2,
 };
 
-// Prints
-// TOM - 20
-// ABDUL - 19
+let kitten2 = {
+  name: "Megatron",
+  weeksOld: 1,
+};
+
+let kittens = [kitten1, kitten2];
+
+function getName(kitten) {
+  return kitten.name.length === 5;
+}
+
+kittens.map(getName); // should return ["Fluffy", "Megatron", "Billy"]
