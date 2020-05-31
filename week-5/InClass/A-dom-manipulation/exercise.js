9e9313d154fb6d7023e7725efa2bb46b5bc62c5f
supertata <laetitiachira@gmail.com>
2020-05-31 08:43:45 +0100

    first commit

--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -14,15 +14,39 @@ Write JavaScript below that logs:
     4. all the "p" elements of contained inside  the .primary-content element node
     --> should log a list of nodes with a length of 3
 
-*/
+    
 
 
 /*
+
+
+
+
+
+
+
 Task 2
 ======
 
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
+
+// Task 1
+
+let getAllP = document.querySelectorAll("p");
+console.log(getAllP)
+let firstDiv = document.getElementsByTagName("div")[0]
+console.log(firstDiv)
+let getById = document.getElementById("jumbotron-text")
+console.log(getById)
+let allthePs = document.getElementsByClassName("primary-content")[0].getElementsByTagName("p")
+console.log(allthePs)
+
+
+let alertButton = document.getElementById("alertBtn")
+alertButton.addEventListener('click', function () {
+    alert('Thanks for visiting Bikes for Refugees!')
+})
 
 
 /*
@@ -31,7 +55,9 @@ Task 3
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
+let backgroundColorButton = document.getElementById("bgrChangeBtn");
+let changeBackgroundColor = () => document.body.style.backgroundColor = "red";
+backgroundColorButton.addEventListener('click', changeBackgroundColor);
 
 /*
 Task 4
@@ -48,3 +74,10 @@ Task 5
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+// 1st argument: the type of event I'm listening to, 2nd argument: a callback function.
+// 3 steps
+// 1) Grab an element
+// 2) Add an event listener
+// 3) Add an action (function)
+// Run the HTML File Using Live Server - Right click on HTML file, Open with Live server.
