5d2a70078df6cc9b0cf2001dc211f45a11c5f1c4
tintax01 <tinta5000@yahoo.fr>
2020-06-05 13:12:07 +0100

    first commite of my week5 homework

--- week-5/Homework/mandatory/2-exercises/exercises.js
+++ week-5/Homework/mandatory/2-exercises/exercises.js
@@ -18,17 +18,16 @@
  */
 // LEAVE YOUR ANSWER HERE (THIS IS OPTIONAL)
 
-
 /**
  * This function recieves a lists of people. Each object should contain the name and the occupation of a person.
  * Look for usage of the function in the code and see what variable is passed into it as an argument.
- * 
+ *
  * Create and insert the below HTML elements to the DOM for each of Objects in the Array as follows:
  *  1. Add a <h1> tag to the website containing the name of the person
  *  2. Add a <h2> tag to the website containing the job of the person
  *
  * All of your HTML elements should go inside the Div tag with the id "content".
- * 
+ *
  * An example "content" div should look like:
  * <div id="content">
  *      <h1>Mario</h1>
@@ -37,42 +36,57 @@
  *      <h2>Plumber</h2>
  * </div>
  */
-function insertPeopleData(arrayOfPeople) {
-  let content = document.querySelector("#content");
-  //Write your code in here
+function insertPeopleData(arrayOfpeople) {
+	let content = document.querySelector("#content");
+	document.getElementsByTagName("h1,h2").innerHTML =
+		"The innerHTML is h1 and h2 tags containing the name and job: " +
+		content[0][1].innerHTML;
+
+	//Write your code in here
 }
 
 /**
  *
  * Create a list of shopping items using an unordered HTML list.
  * The input of this function is a simple Array of Strings, look for the concrete example in the code.
- * 
+ *
+ <h2>An Unordered HTML List</h2>
+
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>  
+
  * All of your HTML elements should go inside the Div tag with the id "content".
  *
  * Hint for type of lists in HTML: https://www.w3schools.com/html/html_lists.asp
  */
 function insertShoppingList(shoppingList) {
-  //Write your code in here
+	const Shopping = document.createElement("ul");
+	document.body.appendChild(Shopping);
 }
-
 /**
  * I'd like to display my three favorite books inside a nice webpage!
- * 
+ *
  * Iterate through the array of books passed into this function and insert each book to page as follows:
  *   - Create a <ul> list and display each book details in <li> element.
  *   - For each book, create a <p> element with the book title and author and append it to the page.
  *   - Add an <img> after <p> element to each book that links to a URL of the book cover.
  *     You should find an appropriate image to each book.
  *   - Change the style of the book depending on whether you have read it (green) or not (red).*
- * 
+ *
  * Find in the code what properties a book object has in the array.
- *  
+ *
  * All of your HTML elements should go inside the Div tag with the id "content".
- * 
+ *
  * The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com
  */
 function insertBooks(books) {
-  //Write your code in here
+	const bookOwners = document.createElement("ul");
+	for (let i = 0; i < bookOwners.length; i++) {
+		bookOwners[i].style.Color = "blue";
+	} //Write your code in here
 }
 
 //
@@ -86,9 +100,9 @@ function insertBooks(books) {
 //
 
 let people = [
-  { name: "Chris", job: "Teacher" },
-  { name: "Joanna", job: "Student" },
-  { name: "Boris", job: "Prime Minister" }
+	{ name: "Chris", job: "Teacher" },
+	{ name: "Joanna", job: "Student" },
+	{ name: "Boris", job: "Prime Minister" },
 ];
 
 insertPeopleData(people);
@@ -98,21 +112,21 @@ let shopping = ["Milk", "Break", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 insertShoppingList(shopping);
 
 const books = [
-  {
-    title: "The Design of Everyday Things",
-    author: "Don Norman",
-    alreadyRead: false
-  },
-  {
-    title: "The Most Human Human",
-    author: "Brian Christian",
-    alreadyRead: true
-  },
-  {
-    title: "The Pragmatic Programmer",
-    author: "Andrew Hunt",
-    alreadyRead: true
-  }
+	{
+		title: "The Design of Everyday Things",
+		author: "Don Norman",
+		alreadyRead: false,
+	},
+	{
+		title: "The Most Human Human",
+		author: "Brian Christian",
+		alreadyRead: true,
+	},
+	{
+		title: "The Pragmatic Programmer",
+		author: "Andrew Hunt",
+		alreadyRead: true,
+	},
 ];
 
 insertBooks(books);
--- week-5/InClass/A-dom-manipulation/exercise.js
+++ week-5/InClass/A-dom-manipulation/exercise.js
@@ -15,7 +15,14 @@ Write JavaScript below that logs:
     --> should log a list of nodes with a length of 3
 
 */
-
+let getAllP = document.querySelectorAll("p");
+console.log(getAllP);
+let firstDiv = document.getElementsByTagName("div")[0];
+console.log(firstDiv);
+let getById = document.getElementById("jumbotron-text");
+console.log(getById);
+let allthePs = document.getElementsByClassName("primary-content")[0].getElementsByTagName("p");
+console.log(allthePs);
 
 /*
 Task 2
@@ -24,14 +31,12 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
-
 /*
 Task 3
 =======
 
 Write JavaScript below that changes the background colour of the page when the 'Change colour' button is clicked.
 */
-
 
 /*
 Task 4
@@ -40,11 +45,174 @@ Task 4
 When a user clicks the 'Add some text' button, a new paragraph should be added below the buttons that says "Read more below."
 */
 
-
-
 /*
 Task 5
 ======
 
 When the 'Larger links!' button is clicked, the text of all links on the page should increase.
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Creating DOM Elements</title>
+//   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+// </head>
+// <body>
+//   <div class="wizards">
+//   </div>
+//   <script src="creating.js"></script>
+// </body>
+// </html>
+
+/* once you got the files, comment the line below and check your browser's developer tools console to see if you see this text
+if you don't, go to index.html and change the way you import your javascript file to this <script src="./creating.js"></script>
+*/
+// console.log('it works!');
+​
+// You are given the following array of objects
+const wizards = [
+  {
+    name: 'Harry Potter',
+    house: 'Gryfindor'
+  },
+  {
+    name: 'Cedric Diggory',
+    house: 'Hufflepuff'
+  },
+  {
+    name: 'Tonks',
+    house: 'Hufflepuff'
+  },
+  {
+    name: 'Ronald Weasley',
+    house: 'Gryfindor'
+  },
+  {
+    name: 'Hermione Granger',
+    house: 'Gryfindor'
+  }
+];
+// Create an unordered list
+​
+// Create a list item for each wizzard and his/her house and
+// append it to the list
+// if you can, give EVERY SECOND wizard a background colour of #785AA8
+// as an example, each item's text should read '{name of wizard} belongs to the {name of house} house'
+​
+// Create a button with a class of button-primary and a value/text of "Add wizards"
+​
+// inject the button into the dom AFTER the wizards div
+​
+// when the button is clicked the list of wizards is injected into the DOM, inside the div with a class of wizards.
+​
+// insert a heading above the wizards div to read 'List of Harry Potter wizards'
+​
+// create a button with a text of Delete wizards and a marginLeft of 1em
+​
+// insert button in the dom after the Add Wizards button you created above
+​
+// when clicking the remove wizards button, the list of wizards whould be removed from the 
+
+/* once you got the files, comment the line below and check your browser's developer tools console to see if you see this text
+if you don't, go to index.html and change the way you import your javascript file to this <script src="./creating.js"></script>
+*/
+// console.log('it works!');
+​
+// You are given the following array of objects
+const wizards = [
+  {
+    name: 'Harry Potter',
+    house: 'Gryfindor'
+  },
+  {
+    name: 'Cedric Diggory',
+    house: 'Hufflepuff'
+  },
+  {
+    name: 'Tonks',
+    house: 'Hufflepuff'
+  },
+  {
+    name: 'Ronald Weasley',
+    house: 'Gryfindor'
+  },
+  {
+    name: 'Hermione Granger',
+    house: 'Gryfindor'
+  }
+];
+
+
+
+// Create an unordered list
+const ul = document.createElement('ul');
+// Create a list item for each wizzard and his/her house and
+// append it to the list
+wizards.forEach(function (wizard, index) {
+  let listItem = document.createElement('li');
+  // as an example, each item's text should read '{name of wizard} belongs to the {name of house} house'
+  listItem.textContent = `${wizard.name} belongs to the ${wizard.house} house.`
+  // if you can, give EVERY SECOND wizard a background colour of #785AA8
+  if (index % 2) listItem.style.backgroundColor = '#785AA8';
+  ul.appendChild(listItem);
+})
+​
+const wizardsDiv = document.querySelector('.wizards');
+​
+// Create a button with a class of button-primary and a value/text of "Add wizards"
+const button = document.createElement('button');
+button.classList.add('button-primary');
+button.textContent = 'Add wizards';
+// inject the button into the dom AFTER the wizards div
+wizardsDiv.insertAdjacentElement('afterend', button);
+// when the button is clicked the list of wizards is injected into the DOM, inside the div with a class of wizards.
+button.addEventListener('click', () => wizardsDiv.appendChild(ul));
+​
+// insert a heading above the wizards div to read 'List of Harry Potter wizards'
+const heading = document.createElement('h1');
+heading.textContent = 'List of Harry Potter Wizards';
+wizardsDiv.insertAdjacentElement('beforebegin', heading);
+​
+// create a button with a text of Delete wizards and a marginLeft of 1em
+const removeWizardsButton = document.createElement('button');
+removeWizardsButton.textContent = 'Delete wizards';
+removeWizardsButton.style.marginLeft = '1em';
+​
+// insert button in the dom after the Add Wizards button you created above
+button.insertAdjacentElement('afterend', removeWizardsButton);
+​
+// when clicking the remove wizards button, the list of wizards whould be removed from the DOM
+removeWizardsButton.addEventListener('click', () => ul.remove());
+
+
+
+
+
