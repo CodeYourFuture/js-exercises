97b0eb80f7304b6d1a7d9658e913cd496cb44e56
Halit <60817499+halitos@users.noreply.github.com>
2020-06-09 23:31:36 +0100

    worked on slideshow

--- week-6/Homework/mandatory/3-slideshow/slideshow.js
+++ week-6/Homework/mandatory/3-slideshow/slideshow.js
@@ -1,1 +1,36 @@
-// Write your code here
+const mainContainer = document.createElement("div");
+document.body.appendChild(mainContainer);
+
+const imgContainer = document.createElement("img");
+
+const slideImages = ["img-01.jpg", "img-02.jpg", "img-03.jpg", "img-04.jpg"];
+
+let n = 0;
+
+function moveForward() {
+  imgContainer.setAttribute("src", `${slideImages[n]}`);
+  n++;
+}
+
+function moveBack() {
+  imgContainer.setAttribute("src", `${slideImages[n]}`);
+  n--;
+}
+
+// const slideImages = ["img1", "img2", "img3", "img4"];
+
+mainContainer.appendChild(imgContainer);
+
+// back button
+
+const backBtn = document.createElement("button");
+backBtn.innerText = "Back";
+document.body.appendChild(backBtn);
+backBtn.addEventListener("click", moveBack);
+
+// forward button
+
+const nextBtn = document.createElement("button");
+nextBtn.innerText = "Forward";
+document.body.appendChild(nextBtn);
+nextBtn.addEventListener("click", moveForward);
