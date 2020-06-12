72e3e21fea6850bfc1b1903c5a45e6a5aed0ddca
Rares Matei <matei.rar@gmail.com>
2020-06-12 22:49:39 +0100

    simplify dom exercise

--- week-6/InClass/2-Callbacks/exercise-2/exercise.js
+++ week-6/InClass/2-Callbacks/exercise-2/exercise.js
@@ -2,69 +2,56 @@
 ================
 Exercise 2
 ----------
-You are given the following list of movies
-
-Task 1
-Create a function called "showMovies" that
-- iterates through the "movies" array and
-- for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
-- it sets the innerText of the #movies-number element to the total number of the movies in the array "movies"
-
-Task 2
-Amend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that
-Create a new function called "addMovie"
-- it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide 
-- it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
-Call addMovies to add the new movie to the list and then showMovies to see the movies added on the screen.
-How many movies can you see on your page?
-
-Task 3
-Can you make sure the new movie you just added is showing on the screen? 
-TIP: use callbacks
-
-Task 4 - **Extra**
-Create a form anywhere on your page. The form should have
-- 4 input text fields, one for each property of your movie object
-- a "save" button.
-When the button is clicked
-- The field values should be used to create a new movie object literal
-- The new movie is then added to the list of movies and gets displayed on your page
-TIP: Use the functions you created on tasks 1-3
-
-Prefer to work on a codepen? https://codepen.io/makanti/pen/MWwMgmW?editors
 ================
 */
 const movies = [
   {
     title: "Color Out of Space",
-    director: "Richard Stanley",
-    type: "sci-fi",
     haveWatched: true,
   },
   {
     title: "A Twelve-Year Night",
-    director: "Álvaro Brechner",
-    type: "horror",
     haveWatched: false,
   },
   {
     title: "The Whistlers",
-    director: "Corneliu Porumboiu",
-    type: "comedy",
     haveWatched: true,
   },
   {
     title: "The Invisible Man",
-    director: "Leigh Whannell",
-    type: "horror",
     haveWatched: false,
   },
 ];
 
-// create showMovies function
+function createDomMovie(movie) {
+  const movieInfo = document.createElement("p");
+  movieInfo.innerText = `Title: ${movie.title}, director ${movie.director}`;
+  const allMovies = document.querySelector("#all-movies");
+  allMovies.appendChild(movieInfo);
+}
 
+function reloadMovieList() {
+  const allMovies = document.querySelector("#all-movies");
+  allMovies.innerHTML = "";
+  movies.forEach(createDomMovie);
+}
 
-// create a new movie object for your favorite movie
+reloadMovieList();
 
+function addMovie() {
+  const loadingText = document.querySelector("");
+  const movieTitleInput = document.querySelector("#new-movie-input");
+  const movieTitle = movieTitleInput.value;
 
-// create addMovies function
+  // Your task - write the code in this function:
+  // 1. The moment this function is called:
+  //      - clear the input so it's empty
+  //      - show the loading text
+  // 2. after 2 seconds:
+  //  - create a new movie object and add it to the "movies" array
+  //  - hide the "loading text"
+  //  - make sure the new movie shows up in the movie list
+}
+
+const addMovieButton = document.querySelector("#add-movie-btn");
+addMovieButton.addEventListener("click", addMovie);
