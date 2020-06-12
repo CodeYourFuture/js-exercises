c0c8b8b49fa548485c16bd72cd118ed3d6ecc2be
Rares Matei <matei.rar@gmail.com>
2020-06-12 23:01:13 +0100

    add more exercise details

--- week-6/InClass/2-Callbacks/exercise-2/exercise.js
+++ week-6/InClass/2-Callbacks/exercise-2/exercise.js
@@ -25,7 +25,7 @@ const movies = [
 
 function createDomMovie(movie) {
   const movieInfo = document.createElement("p");
-  movieInfo.innerText = `Title: ${movie.title}, director ${movie.director}`;
+  movieInfo.innerText = `Title: ${movie.title}. Seen: ${movie.haveWatched}`;
   const allMovies = document.querySelector("#all-movies");
   allMovies.appendChild(movieInfo);
 }
@@ -39,16 +39,16 @@ function reloadMovieList() {
 reloadMovieList();
 
 function addMovie() {
-  const loadingText = document.querySelector("");
+  const loadingText = document.querySelector("#loading-text");
   const movieTitleInput = document.querySelector("#new-movie-input");
   const movieTitle = movieTitleInput.value;
 
   // Your task - write the code in this function:
   // 1. The moment this function is called:
   //      - clear the input so it's empty
-  //      - show the loading text
-  // 2. after 2 seconds:
-  //  - create a new movie object and add it to the "movies" array
+  //      - show the loading text (Hint: look inside index.html for some CSS classes you can use to show and hide this element)
+  // 2. after 4 seconds:
+  //  - create a new movie object and add it to the "movies" array (make sure its "haveWatched" value is false)
   //  - hide the "loading text"
   //  - make sure the new movie shows up in the movie list
 }
