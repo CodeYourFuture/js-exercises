3e32b890e9bb5e40bc9164b85d3592f12cf79c42
Rares Matei <matei.rar@gmail.com>
2020-06-12 21:26:25 +0100

    simplify exercise 1

--- week-6/InClass/1-Async/exercise1.js
+++ week-6/InClass/1-Async/exercise1.js
@@ -7,8 +7,5 @@ const greekGods = [
   "Zeus",
 ];
 
-// before running the code in your browser, discuss the expected order of each loop
-
-// synchronous - loop through the array of greek gods and print the index numbers and values to the console, e.g. '1. Ares'
-
-// asynchronous - same as before but the index and the value of the god at position 2 in array should be printed after 2 seconds. Use: setTimeout()
+// 1. Console.log() the name of the first and second god in the list
+// 2. Console.log() the name of the first god after 3 seconds, and the name of the second god after 1 second
