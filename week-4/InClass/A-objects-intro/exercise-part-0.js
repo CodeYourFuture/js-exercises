90530918d4db7498da9c46cbf53b2f11b46e0256
erol-g <foodengineer.com@gmail.com>
2020-06-20 05:39:35 +0100

    ok

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,24 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+
+let laptop = {
+    brand: "Lenova",
+    screenSize: 50,
+    isTouchScreen: true,
+    //deviceSpecifications: { memory: 4, videoGraph: "IntelHD" },
+    };
+    
+  
+
+laptop.feathers = ["black", "usb"]
+
+laptop.brand= "Leno"
+
+//delete laptop.deviceSpecifications;
+
+console.log(laptop);
+
+           
+
---
+++ week-9/ace.js
@@ -0,0 +1,19 @@
+let house = {
+    address: "1 Kinning Park",
+    previousOwners: ["Claire M.", "John A."],
+    currentOwner: {
+      firstName: "Margaret",
+      lastName: "Conway",
+    },
+    
+     myFunction: function() {
+        return fullName;
+        
+    };
+  };
+//   var house2 = Object.values(house.currentOwner);
+  
+//   console.log(house2.toString());
+  
+ //getCurrentOwnerFullName(house);
+ 
