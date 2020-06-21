a53a50b13c7469aa70a65fed67c3081ffa053ce4
supertata <laetitiachira@gmail.com>
2020-06-21 14:54:22 +0100

    fixed first 2 bugs

--- week-7/Homework/mandatory/0-thinking-like-a-programmer/task.md
+++ week-7/Homework/mandatory/0-thinking-like-a-programmer/task.md
@@ -26,12 +26,12 @@ After you've watched these videos I'd like you to answer these questions
 
 ## 1. What do you think the most important quality for a programmer is?
 
-<!-- Write your answer here -->
+<!-- Problem solving -->
 
 ## 2. When trying to solve a challenge, what should you do first?
 
-<!-- Write your answer here -->
+<!-- Understand what the problem is -->
 
 ## 3. What should you do if you get stuck?
 
-<!-- Write your answer here -->
+<!-- Split the problem into smaller problems/review code line by line -->
--- week-7/Homework/mandatory/1-debugging-practice/script.js
+++ week-7/Homework/mandatory/1-debugging-practice/script.js
@@ -1,10 +1,11 @@
+
 let myLibrary = [];
 
 window.addEventListener("load", function (e) {
   populateStorage();
   render();
 });
-
+//rajouter 2 nouveau livres quand la page load
 function populateStorage() {
   if (myLibrary.length == 0) {
     let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
@@ -54,7 +55,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n--) {
     table.deleteRow(n);
   }
   //insert updated row and cells
@@ -89,7 +90,7 @@ function render() {
     });
 
     //add delete button to every row and render again
-    let delButton = document.createElement("button");
+    let delBut = document.createElement("button");
     delBut.id = i + 5;
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
