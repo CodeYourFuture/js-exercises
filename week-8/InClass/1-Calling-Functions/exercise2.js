aac016762078ed8f27f82c410c72bc832d4e53df
r-darby <richiodarby@gmail.com>
2020-06-30 07:00:31 +0100

    Create Function InClass exercises

---
+++ week-8/InClass/1-Calling-Functions/exercise2.js
@@ -0,0 +1,25 @@
+/*
+ONLY CHANGE LINE 23 to answer this question
+
+This code has a bug - if we run it we see the result "NaN"
+This means "Not a Number", we get this when we try to multiply (or divide, add, subtract etc)
+a variable which is not of type = number
+
+Why are we getting this result?
+
+Please fix this code so we get the result 10
+
+ONLY CHANGE LINE 23
+*/
+
+function doubleNumber(num){
+    num = num * 2;
+    return num;
+}
+
+let a = 5;
+
+// ONLY CHANGE CODE ON LINE 23
+let b = doubleNumber();
+
+console.log(b);
---
+++ week-8/InClass/1-Calling-Functions/exercise3.js
@@ -0,0 +1,25 @@
+/*
+ONLY CHANGE LINE 23 to answer this question
+
+This code has a bug - if we run it we see the result "John undefined"
+
+Why are we getting this result?
+
+Please fix this code so we get the result "John Legere"
+
+ONLY CHANGE LINE 23
+*/
+
+function fullName(firstName, surname){
+    let fullName = firstName + " " + surname;
+    return fullName;
+}
+
+let customerFirstName = "John";
+
+let customerSurname = "Legere";
+
+// ONLY CHANGE CODE ON LINE 23
+let customerFullName = fullName(customerFirstName);
+
+console.log(customerFullName);
