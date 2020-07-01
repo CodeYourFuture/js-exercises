744ef4024ce938d6d78e8db0f03b6084f0041922
supertata <laetitiachira@gmail.com>
2020-07-01 19:23:47 +0100

    completed exercise 4

--- week-8/Homework/mandatory/4-programmer-humour/script.js
+++ week-8/Homework/mandatory/4-programmer-humour/script.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+let ul = document.getElementById("ul");
+let li = document.createElement("li");
+let img = document.createElement("img");
+ul.appendChild(li);
+li.append(img);
+
+function fetchJoke () {
+
+    fetch("https://xkcd.now.sh/?comic=latest")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(programmerJoke) {
+        getImage(programmerJoke)
+        // console.log(programmerJoke);
+    })   
+    .catch((error) => console.log(error));
+    }
+
+    function getImage (image) {
+        img.src = image.img;
+    }
+
+  onload = fetchJoke()
+
