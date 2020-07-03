379c59e1ac5916e07d264d30d0232295d21514ba
Rares Matei <matei.rar@gmail.com>
2020-07-03 23:39:44 +0100

    add fetch and API exercises

---
+++ week-8/InClass/4-APIs/exercise.js
@@ -0,0 +1,9 @@
+/*
+    1. Fetch the results from this API: https://cat-fact.herokuapp.com/facts
+       And console.log the first cat fact
+
+    2. Fetch the results from this API: https://restcountries.eu/rest/v2/name/Great%20Britain?fullText=true
+       And console.log the population of the UK
+
+       Remember to open "index.html" using Live Preview, and test that you get the correct results in the console!
+ */
