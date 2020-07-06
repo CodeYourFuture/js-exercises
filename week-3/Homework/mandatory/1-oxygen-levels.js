2501105db3cedc23e5cc57edc0d68a671e99720f
azadaafo <azadaafo@gmail.com>
2020-07-07 00:23:00 +0100

    failed oxygen level exercise

--- week-3/Homework/mandatory/1-oxygen-levels.js
+++ week-3/Homework/mandatory/1-oxygen-levels.js
@@ -1,42 +1,29 @@
 /*
 Many years into the future, a team of Space Voyagers find their ship is low on Oxygen and need to dock
 somewhere safe while they call home for help.
-  
 Their computer detects a list of nearby planets that have Oxygen in their atmosphere.
-
 To be safe, they need to land on the first unamed planet that has Oxygen levels between 19.5% and 23.5%.
-
 Write a function that finds the oxygen level of the first safe planet - Oxygen between 19.5% and 23.5%
-
 Some string methods that might help you here are .replace() and .substring(). Let's look at a quick
 example before trying the exercise.
 */
-
 /* .replace() allows us to add something where we removed something*/
 let greeting = "Good Morning";
 greeting.replace('Morning', 'Evening'); // outputs Good Evening
-
-
 /* .substring() allows us to remove things from strings */
 let dessert = "ice cream and pancakes";
-
 let newdessert = dessert.substring(0, 9);
-
 console.log(newdessert); // returns ice cream
 
 /* + + + + + + + */
 /* Now try the exercise */
-function safeLevels(arrStr) {
-  //first solution
-  let levelNumber = arrStr.map((level) => {
-    let removingPercentage = level.substring(0, 4);
-    let stringToNumber = parseFloat(removingPercentage);
-    return stringToNumber;
-  });
-  let findOxyArray = levelNumber.find(
-    (findOxy) => findOxy > 19.5 && findOxy < 23.5
-  );
-  return findOxyArray + "%";
+
+function safeLevels() {
+  if (oxygenLevels1 >= "19.5%" && oxygenLevels1 <= "23.5%") {
+    return `oxygen level is safe`
+  } else {
+    `oxygen level is not safe`
+  }
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
@@ -45,22 +32,22 @@ const oxygenLevels1 = ["24.2%", "11.3%", "19.9%", "23.1%", "29.3%", "20.2%"]
 const oxygenLevels2 = ["30.8%", "23.5%", "18.8%", "19.5%", "20.2%", "31.6%"]
 
 function test(test_name, expr) {
-    let status;
-    if (expr) {
-      status = "PASSED";
-    } else {
-      status = "FAILED";
-    }
-  
-    console.log(`${test_name}: ${status}`);
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
 }
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels1) === "19.9%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels1) === "19.9%"
 );
 
 test(
-    "safeLevels function works - case 2",
-    safeLevels(oxygenLevels2) === "20.2%"
+  "safeLevels function works - case 2",
+  safeLevels(oxygenLevels2) === "20.2%"
 );
