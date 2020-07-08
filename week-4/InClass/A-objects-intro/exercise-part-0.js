cff3ce96c1129f3fe41e6f06f91c804265e1f97f
azadaafo <azadaafo@gmail.com>
2020-07-08 03:34:03 +0100

    half of in class exercises done for week 4 Objects

--- week-4/InClass/A-objects-intro/exercise-part-0.js
+++ week-4/InClass/A-objects-intro/exercise-part-0.js
@@ -1,20 +1,4 @@
 /*
-
 Describe your own laptop as a JavaScript object
-
 Try to think of as many properties as you can!
-
 */
-
-let myLaptop = {
-    brand: "Dell",
-    screenSize: 15,
-    ram: 16,
-    diskType: "SSD",
-    screenTouch: false,
-    batteryLife: "6 hr",
-    multiLanguage: true,
-    bulitInCamera: true,
-    externalSpeakers: false,
-    antiVirusInstalled: "Yes"
-}
