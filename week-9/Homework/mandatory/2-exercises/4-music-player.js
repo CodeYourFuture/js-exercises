ec6ba3f9650afc971fbfd56834cba2ef147df346
Halit <60817499+halitos@users.noreply.github.com>
2020-07-10 22:22:26 +0100

    sorted after feedback

--- week-9/Homework/mandatory/2-exercises/4-music-player.js
+++ week-9/Homework/mandatory/2-exercises/4-music-player.js
@@ -17,10 +17,11 @@ Optional 2: Can you implement the shuffle functionality for your music player?
 This means the order the songs are played in will be random, but each song will only play once.
 
  */
-let i;
+
 class MusicPlayer {
   constructor() {
     this.songList = [];
+    this.index = 0;
   }
   add(song, singer) {
     this.album = {};
@@ -32,32 +33,45 @@ class MusicPlayer {
     if (this.songList.length == 0) {
       console.log("Song List is empty. Please add song first");
     } else {
-      i = 0;
+      this.index = 0;
       console.log(
-        `Currently playing:${this.songList[i].song} by ${this.songList[i].singer}`
+        `Currently playing:${this.songList[this.index].song} by ${
+          this.songList[this.index].singer
+        }`
       );
     }
   }
 
   skip() {
-    i++;
-    if (i < this.songList.length) {
+    this.index++;
+    if (this.index < this.songList.length) {
       console.log(
-        `Skipped...Currently playing:${this.songList[i].song} by ${this.songList[i].singer}`
+        `Skipped...Currently playing:${this.songList[this.index].song} by ${
+          this.songList[this.index].singer
+        }`
       );
     } else {
       console.log(
         `Reached the end of the Song List. Returning to first song: ${this.songList[0].song} by ${this.songList[0].singer}`
       );
-      i = 0;
+      this.index = 0;
     }
   }
 
   previous() {
-    i--;
-    console.log(
-      `Back...Currently playing:${this.songList[i].song} by ${this.songList[i].singer}`
-    );
+    this.index--;
+    if (this.index >= 0) {
+      console.log(
+        `Back...Currently playing:${this.songList[this.index].song} by ${
+          this.songList[this.index].singer
+        }`
+      );
+    } else {
+      console.log(
+        `Reached the beginning of the Song List. Playing the first song: ${this.songList[0].song} by ${this.songList[0].singer}`
+      );
+      this.index = 0;
+    }
   }
 
   shuffle() {
@@ -65,7 +79,7 @@ class MusicPlayer {
     console.log(
       `Randomly playing:${this.songList[j].song} by ${this.songList[j].singer}`
     );
-    i = j;
+    this.index = j;
   }
 }
 
@@ -75,18 +89,10 @@ let myMusicPlayer = new MusicPlayer(); // Create an empty playlist
 myMusicPlayer.add("Bohemian Rhapsody", "Queen");
 myMusicPlayer.add("Yesterday", "The Beatles");
 myMusicPlayer.add("Vogue", "Madonna");
-
 myMusicPlayer.play(); // Output: "Currently playing: Bohemian Rhapsody by Queen"
-
 myMusicPlayer.skip(); // Output: "Currently playing: Yesterday by The Beatles"
-
 myMusicPlayer.previous(); // Output: "Currently playing: Bohemian Rhapsody by Queen"
-
+myMusicPlayer.previous(); // Output: "Currently playing: Bohemian Rhapsody by Queen"
 myMusicPlayer.skip(); // Output: "Currently playing: Yesterday by The Beatles"
-
-myMusicPlayer.skip(); // Output: "Currently playing: Vogue by Madonna"
-myMusicPlayer.skip(); // Output: "Currently playing: Vogue by Madonna"
-myMusicPlayer.skip(); // Output: "Currently playing: Vogue by Madonna"
-
 myMusicPlayer.shuffle();
 myMusicPlayer.skip();
