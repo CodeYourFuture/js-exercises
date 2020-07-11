f88f6170c8936e4daca06d663567012a4580c0d6
Rares Matei <matei.rar@gmail.com>
2020-07-11 10:03:14 +0100

    update exercise 4

--- week-9/InClass/1-Block-Scope/exercise2.js
+++ week-9/InClass/1-Block-Scope/exercise2.js
@@ -2,8 +2,8 @@
  This code throws an error. By keeping only the single console.log(), can you move it to a different line so that it logs:
   1. "Astrid"
   2. Now move it to a different line so that it logs "Reyam"
-  3. Now move it to a different line so that it logs "Richard"
-  4. And finally, "Awet"
+  3. And finally, "Awet"
+  4. BONUS: change the "car" variable, and move the console.log() to a different line, so it logs "Richard"
 */
 
 console.log(driverName);
