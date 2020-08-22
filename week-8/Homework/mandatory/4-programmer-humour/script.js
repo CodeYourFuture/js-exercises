e41e1acf32463b76a1d60ee707ef36020e2f4a3e
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-08-22 20:08:10 +0100

    finished functionalities

---
+++ week-8/Homework/mandatory/4-programmer-humour/script.js
@@ -0,0 +1,16 @@
+//
+const fetchData = () => {
+  fetch("https://xkcd.now.sh/?comic=latest")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.img);
+      let img = document.createElement("img");
+      img.src = data.img;
+      document.getElementById("container").appendChild(img);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+window.onload = fetchData();
